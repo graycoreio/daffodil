b1e55755ba42898fabd001986bd7b34e8740c500
Damien Retzinger <damienwebdev@gmail.com>
2018-04-17 15:18:25 -0400

    adding travis ci and updating karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+     // you can define custom flags
+     customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
