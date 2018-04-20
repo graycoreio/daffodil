984b4a8292fdba1b4ee799b3829a06adc6b7ea67
Luke Derrick <lderrickable@gmail.com>
2018-04-20 11:44:35 -0400

    remove a function call from ngModule

--- src/app/core/daffodil.module.ts
+++ src/app/core/daffodil.module.ts
@@ -4,6 +4,9 @@ import { CoreProductModule } from '@daffodil/product/product.module';
 import { DaffodilConfig } from '@daffodil/config/model';
 import { DaffodilConfigService } from '@daffodil/config/daffodil-config.service';
 
+let configValue;
+let instantiateDaffodilConfigService = new DaffodilConfigService(configValue);
+
 @NgModule({
   imports: [
     CoreProductModule
@@ -15,9 +18,10 @@ import { DaffodilConfigService } from '@daffodil/config/daffodil-config.service'
 export class DaffodilModule {
 
   static forRoot(config: DaffodilConfig): ModuleWithProviders {
+    configValue = config;
     return {
       ngModule: DaffodilModule,
-      providers: [{provide: DaffodilConfigService, useValue: new DaffodilConfigService(config)}]
+      providers: [{provide: DaffodilConfigService, useValue: instantiateDaffodilConfigService}]
     };
   }
 }
