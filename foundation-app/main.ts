265e30295c852f5a583df8ff493ec0082b96f1ac
Damien Retzinger <damienwebdev@gmail.com>
2018-04-20 19:12:37 -0400

    reorganizing codebase

--- foundation-app/main.ts
+++ foundation-app/main.ts
@@ -1,9 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { FoundationModule } from '../app/foundation/app.module';
+import { FoundationModule } from './app/app.module';
 
-import { environment } from '../environments/environment';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
