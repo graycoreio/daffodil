582ed4af56dc5b4615ee11df7dd3218a88eb1d96
Luke Derrick <lderrickable@gmail.com>
2018-04-30 17:10:24 -0400

    add cart service with getAll call

---
+++ libs/core/src/cart/services/cart.service.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { CartService } from './cart.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
+import { CartTestingModule } from '../testing/cart-testing.module';
+import { DaffodilConfigService } from '../../config/daffodil-config.service';
+import { DaffodilConfigFactory } from '../../config/testing/daffodil-config.factory';
+
+describe('Core | Product | ProductService', () => {
+  let productService;
+  let http: HttpClient;
+  let daffodilConfigService: DaffodilConfigService;
+  let daffodilConfigFactory: DaffodilConfigFactory;
+
+  beforeEach(() => {
+    daffodilConfigFactory = new DaffodilConfigFactory();
+    daffodilConfigService = new DaffodilConfigService(daffodilConfigFactory.create());
+
+    TestBed.configureTestingModule({
+      imports: [
+        CartTestingModule
+      ],
+      providers: [
+        CartService,
+        {provide: DaffodilConfigService, useValue: daffodilConfigService}
+      ]
+    });
+    http = TestBed.get(HttpClient);
+    productService = TestBed.get(CartService);
+  });
+
+  it('should be created', () => {
+    expect(productService).toBeTruthy();
+  });
+
+  describe('getAll', () => {
+      
+    it('should send a get request', () => {
+      spyOn(http, 'get');
+      productService.getAll();
+      
+      expect(http.get).toHaveBeenCalled();
+    });
+  });
+});
---
+++ libs/core/src/cart/services/cart.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { Cart } from '../model/cart';
+import { DaffodilConfigService } from '../../config/daffodil-config.service';
+
+@Injectable()
+export class CartService {
+
+  url = this.daffodilConfigService.config.BASE_URL + 'api/cart';
+
+  constructor(
+    private http: HttpClient,
+    private daffodilConfigService: DaffodilConfigService
+  ) {}
+
+  getAll(): Observable<Cart[]> {
+    return this.http.get<Cart[]>(this.url);
+  }
+}
