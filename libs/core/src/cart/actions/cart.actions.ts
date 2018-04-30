954ac49ed2b1e01e527ee726a380ad67b3ae887b
Luke Derrick <lderrickable@gmail.com>
2018-04-30 17:40:26 -0400

    add cart load, success, failure actions

---
+++ libs/core/src/cart/actions/cart.actions.ts
@@ -0,0 +1,32 @@
+import { Action } from '@ngrx/store';
+
+import { Cart } from '../model/cart';
+
+export enum CartActionTypes {
+    CartLoadAction = "[Cart] Load Action",
+    CartLoadSuccessAction = "[Cart] Load Success Action",
+    CartLoadFailureAction = "[Cart] Load Failure Action"
+}
+
+export class CartLoad implements Action {
+  readonly type = CartActionTypes.CartLoadAction;
+
+  constructor() {}
+}
+
+export class CartLoadSuccess implements Action {
+    readonly type = CartActionTypes.CartLoadSuccessAction;
+
+    constructor(public payload: Cart[]) {}
+}
+
+export class CartLoadFailure implements Action {
+  readonly type = CartActionTypes.CartLoadFailureAction;
+
+  constructor(public payload: string) {}
+}
+
+export type CartActions = 
+    | CartLoad 
+    | CartLoadSuccess
+    | CartLoadFailure;
