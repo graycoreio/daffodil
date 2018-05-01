67585dae88c68c02bfb2bc282dd211aa9b673b4a
Luke Derrick <lderrickable@gmail.com>
2018-05-01 11:19:59 -0400

    add models for cart, cart-item, cart-address, cart-payment, and cart-shipping-address

--- libs/core/src/cart/model/cart.ts
+++ libs/core/src/cart/model/cart.ts
@@ -1,3 +1,19 @@
+import { CartItem } from './cart-item';
+import { CartAddress } from './cart-address';
+import { CartPayment } from './cart-payment';
+
 export interface Cart {
-  id: string;
+  id: number;
+  created_at: Date;
+  updated_at: Date;
+  store_to_base_rate: number;
+  grand_total: number;
+  checkout_method: string;
+  customer_id: number;
+  coupon_code: string;
+  subtotal: number;
+  subtotal_with_discount: number;
+  items: CartItem[];
+  addresses: CartAddress[];
+  payment: CartPayment;
 }
