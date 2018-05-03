c711ea5078ba8a8dd4a069452ef713fe683da00f
Luke Derrick <lderrickable@gmail.com>
2018-05-03 15:47:49 -0400

    add import to fix a failing test in app component

--- apps/foundation-app/src/app/app.component.spec.ts
+++ apps/foundation-app/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { Component } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 @Component({selector: 'foundation-product-list-view', template: ''})
 class ProductListContainerMock {}
@@ -8,6 +9,9 @@ class ProductListContainerMock {}
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent,
         ProductListContainerMock
