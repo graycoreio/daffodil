176a081ff47ae3fc6507be0f819e56bb596cc4e5
Luke Derrick <lderrickable@gmail.com>
2018-05-03 07:59:09 -0400

    add core cart module and state module; add to daffodil/core exports

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,7 +1,12 @@
 export { DaffodilModule } from './daffodil.module';
 export { MockModule } from './mock/mock.module';
 export { CoreProductModule } from './product/product.module';
+export { CoreCartModule } from './cart/cart.module';
 
 export { Product } from './product/model/product';
 export { ProductFactory } from './product/testing/factories/product.factory';
 export { ProductListContainer } from './product/containers/product-list/product-list.component';
+
+export { Cart } from './cart/model/cart';
+export { CartFactory } from './cart/testing/factories/cart.factory';
+export { CartContainer } from './cart/containers/cart/cart.component';
