bb27c617dabf78e8c3098862b6edcb489f2363da
Luke Derrick <lderrickable@gmail.com>
2018-05-03 15:10:56 -0400

    add AddToCart action

--- libs/core/src/cart/actions/cart.actions.ts
+++ libs/core/src/cart/actions/cart.actions.ts
@@ -1,11 +1,15 @@
 import { Action } from '@ngrx/store';
 
 import { Cart } from '../model/cart';
+import { Product } from '../..';
 
 export enum CartActionTypes {
     CartLoadAction = "[Cart] Load Action",
     CartLoadSuccessAction = "[Cart] Load Success Action",
     CartLoadFailureAction = "[Cart] Load Failure Action",
+    AddToCartAction = "[Cart] Add To Cart Action",
+    AddToCartSuccessAction = "[Cart] Add to Cart Success Action",
+    AddToCartFailureAction = "[Cart] Add to Cart Failure Action",    
     CartResetAction = "[Cart] Reset Action"
 }
 
@@ -27,6 +31,24 @@ export class CartLoadFailure implements Action {
   constructor(public payload: string) {}
 }
 
+export class AddToCart implements Action {
+    readonly type = CartActionTypes.AddToCartAction;
+
+    constructor(public payload: Product) {}
+}
+
+export class AddToCartSuccess implements Action {
+    readonly type = CartActionTypes.AddToCartSuccessAction;
+
+    constructor() {}
+}
+
+export class AddToCartFailure implements Action {
+    readonly type = CartActionTypes.AddToCartFailureAction;
+
+    constructor(public payload: string) {}
+}
+
 export class CartReset implements Action {
     readonly type = CartActionTypes.CartResetAction;
 
@@ -37,4 +59,7 @@ export type CartActions =
     | CartLoad 
     | CartLoadSuccess
     | CartLoadFailure
+    | AddToCart
+    | AddToCartSuccess
+    | AddToCartFailure
     | CartReset;
