1070101d91ba2f7850ae21f9b8c8a93510df106d
Luke Derrick <lderrickable@gmail.com>
2018-05-02 21:33:36 -0400

    change service call for cart to return a Cart instead of Array(Cart)

--- libs/core/src/mock/services/mock.service.ts
+++ libs/core/src/mock/services/mock.service.ts
@@ -5,18 +5,25 @@ import { ProductTestingService } from '../../product/testing/services/product.te
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { ProductFactory } from '../../product/testing/factories/product.factory';
 import { Product } from '../../product/model/product';
+import { CartTestingService } from '../../cart/testing/services/cart.testing.service';
+import { Cart } from '../../cart/model/cart';
 
 @Injectable()
 export class MockService implements InMemoryDbService{
-  constructor(private productTestingService: ProductTestingService) {}
+  constructor(
+    private productTestingService: ProductTestingService,
+    private cartTestingService: CartTestingService
+  ) {}
 
   createDb() : MockDatabase {
     return {
-      ...this.productTestingService.createDb()
+      ...this.productTestingService.createDb(),
+      ...this.cartTestingService.createDb()
     };
   }
 }
 
 export interface MockDatabase {
-  products: Product[]
+  products: Product[],
+  cart: Cart
 }
