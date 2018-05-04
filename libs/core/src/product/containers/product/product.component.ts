cf99831f782d81e2a8bc62cb09e68093364d7419
Luke Derrick <lderrickable@gmail.com>
2018-05-04 15:33:01 -0400

    add product container component. Remove unneeded tests from product-list container

---
+++ libs/core/src/product/containers/product/product.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+
+import { Store, select } from '@ngrx/store';
+
+import { Product } from '../../model/product';
+import * as fromProduct from '../../reducers';
+import { ProductLoad } from '../../actions/product.actions';
+
+@Component({
+  selector: '[product-container]',
+  template: '<ng-content></ng-content>',
+  exportAs: 'ProductContainer'
+})
+export class ProductContainer implements OnInit {
+
+  @Input() selectedProductId: string;
+
+  loading$: Observable<boolean>;
+  product$: Observable<Product>;
+
+  constructor(
+    private store: Store<fromProduct.State>
+  ) { }
+
+  ngOnInit() {
+    this.store.dispatch(new ProductLoad(this.selectedProductId));
+
+    this.loading$ = this.store.pipe(
+      select(fromProduct.selectSelectedProductLoadingState)
+    );
+
+    this.product$ = this.store.pipe(
+      select(fromProduct.selectSelectedProduct)
+    );
+  }
+}
