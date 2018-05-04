dbe24db317b8eb64490857519482890a87b47759
Luke Derrick <lderrickable@gmail.com>
2018-05-04 14:27:06 -0400

    add product reducer to select one product

--- libs/core/src/product/reducers/index.spec.ts
+++ libs/core/src/product/reducers/index.spec.ts
@@ -8,6 +8,7 @@ import { selectProductListState } from "../reducers";
 import { ProductListLoadSuccess } from "../actions/product-list.actions";
 import { ProductFactory } from "../testing/factories/product.factory";
 import { Product } from "../model/product";
+import { ProductLoad } from "../actions/product.actions";
 
 describe('selectProductListState', () => {
 
@@ -27,64 +28,111 @@ describe('selectProductListState', () => {
     mockProduct = productFactory.create();
     store = TestBed.get(Store);
     store.dispatch(new ProductListLoadSuccess(new Array(mockProduct)));
+    store.dispatch(new ProductLoad(mockProduct.id));
   }));
 
-  describe('selectProductIds', () => {
+  describe('ProductEntitiesState', () => {
     
-    it('selects product ids', () => {
-      store.pipe(select(fromProduct.selectProductIds)).subscribe((ids) => {
-        expect(ids).toEqual(new Array(mockProduct.id));
+    describe('selectProductIds', () => {
+    
+      it('selects product ids', () => {
+        store.pipe(select(fromProduct.selectProductIds)).subscribe((ids) => {
+          expect(ids).toEqual(new Array(mockProduct.id));
+        });
+      });
+    });
+  
+    describe('selectProductEntities', () => {
+      
+      it('selects product entities as a dictionary object', () => {
+        store.pipe(select(fromProduct.selectProductEntities)).subscribe((products) => {
+          expect(products[mockProduct.id]).toEqual(mockProduct);
+        });
+      });
+    });
+  
+    describe('selectAllProducts', () => {
+      
+      it('selects all products as an array', () => {
+        store.pipe(select(fromProduct.selectAllProducts)).subscribe((products) => {
+          expect(products[0]).toEqual(mockProduct);
+        });
+      });
+    });
+  
+    describe('selectTotalProducts', () => {
+      
+      it('selects the total number of products', () => {
+        store.pipe(select(fromProduct.selectTotalProducts)).subscribe((numberOfProducts) => {
+          expect(numberOfProducts).toEqual(1);
+        });
+      });
+    });  
+  });
+  
+  describe('ProductListState', () => {
+    
+    describe('selectProductListState', () => {
+    
+      it('selects product list state', () => {
+        let expectedListState = {
+          products: [],
+          loading: false,
+          errors: []
+        }
+  
+        store.pipe(select(fromProduct.selectProductListState)).subscribe((productList) => {
+          expect(productList).toEqual(expectedListState);
+        });
+      });
+    });
+  
+    describe('selectProductListLoadingState', () => {
+      
+      it('selects product list loading state', () => {
+        store.pipe(select(fromProduct.selectProductListLoadingState)).subscribe((productListLoading) => {
+          expect(productListLoading).toEqual(false);
+        });
+      });
+    });
+  });
+  
+  describe('SelectedProductState', () => {
+    
+    describe('selectSelectedProductState', () => {
+
+      let expectedProductState;
+
+      beforeEach(() => {
+        expectedProductState = {
+          selectedProductId: mockProduct.id,
+          loading: true,
+          errors: []
+        }
+      });
+      
+      it('returns the selected product state', () => {
+        store.pipe(select(fromProduct.selectSelectedProductState)).subscribe((selectedProductState) => {
+          expect(selectedProductState).toEqual(expectedProductState);
+        });
+      });
+    });
+
+    describe('selectSelectedProductId', () => {
+      
+      it('returns the selected product id', () => {
+        store.pipe(select(fromProduct.selectSelectedProductId)).subscribe((selectedProductId) => {
+          expect(selectedProductId).toEqual(mockProduct.id);
+        });
       });
     });
   });
 
-  describe('selectProductEntities', () => {
+  describe('selectSelectedProduct', () => {
     
-    it('selects product entities as a dictionary object', () => {
-      store.pipe(select(fromProduct.selectProductEntities)).subscribe((products) => {
-        expect(products[mockProduct.id]).toEqual(mockProduct);
-      });
-    });
-  });
-
-  describe('selectAllProducts', () => {
-    
-    it('selects all products as an array', () => {
-      store.pipe(select(fromProduct.selectAllProducts)).subscribe((products) => {
-        expect(products[0]).toEqual(mockProduct);
-      });
-    });
-  });
-
-  describe('selectTotalProducts', () => {
-    
-    it('selects the total number of products', () => {
-      store.pipe(select(fromProduct.selectTotalProducts)).subscribe((numberOfProducts) => {
-        expect(numberOfProducts).toEqual(1);
-      });
-    });
-  });
-
-  describe('selectProductListState', () => {
-    
-    it('selects product list state', () => {
-      let expectedListState = {
-        products: [],
-        loading: false,
-        errors: []
-      }
-
-      store.pipe(select(fromProduct.selectProductListState)).subscribe((productList) => {
-        expect(productList).toEqual(expectedListState);
-      });
-    });
-  });
-
-  describe('selectProductListLoadingState', () => {
-    
-    it('selects product list loading state', () => {
-      store.pipe(select(fromProduct.selectProductListLoadingState)).subscribe((productListLoading) => {
-        expect(productListLoading).toEqual(false);
+    it('selects the selected product', () => {
+      store.pipe(select(fromProduct.selectSelectedProduct)).subscribe((selectedProduct) => {
+        expect(selectedProduct).toEqual(mockProduct);
       });
     });
   });
---
+++ libs/core/src/product/reducers/product.reducer.spec.ts
@@ -0,0 +1,103 @@
+import { createSelector } from "@ngrx/store";
+
+import { Product } from "../model/product";
+import { ProductFactory } from "../testing/factories/product.factory";
+import { initialState, reducer, getProductLoading, getSelectedProductId } from "../reducers/product.reducer";
+import { ProductLoad, ProductLoadSuccess, ProductLoadFailure } from "../actions/product.actions";
+
+
+describe('Product | Product List Reducer', () => {
+
+  let productFactory;
+  let product: Product;
+  let productId: string;
+
+  beforeEach(() => {
+    productFactory = new ProductFactory();
+
+    product = productFactory.create();
+    productId = product.id;
+  });
+
+  describe('when an unknown action is triggered', () => {
+
+    it('should return the current state', () => {
+      const action = {} as any;
+
+      const result = reducer(initialState, action);
+
+      expect(result).toBe(initialState);
+    });
+  });
+
+  describe('when ProductLoadAction is triggered', () => {
+    let result;
+
+    beforeEach(() => {
+      const productLoadAction: ProductLoad = new ProductLoad(productId);
+
+      result = reducer(initialState, productLoadAction);
+    });
+
+    it('sets loading state to true', () => {
+      expect(result.loading).toEqual(true);
+    });
+
+    it('sets selectedProductId to the product id', () => {
+      expect(result.selectedProductId).toEqual(productId);
+    });
+  });
+
+  describe('when ProductLoadSuccessAction is triggered', () => {
+
+    let result;
+
+    beforeEach(() => {
+      initialState.loading = true;
+      let productLoadSuccess = new ProductLoadSuccess(product);
+      
+      result = reducer(initialState, productLoadSuccess);
+    });
+
+    it('sets loading to false', () => {
+      expect(result.loading).toEqual(false);
+    });
+  });
+
+  describe('when ProductLoadFailureAction is triggered', () => {
+
+    let error: string;
+    let result;
+
+    beforeEach(() => {
+      initialState.loading = true;
+      error = 'error';      
+      initialState.errors = new Array('firstError');
+      let productLoadFailure = new ProductLoadFailure(error);
+
+      result = reducer(initialState, productLoadFailure);
+    });
+
+    it('sets loading to false', () => {
+      expect(result.loading).toEqual(false);
+    });
+
+    it('adds an error to state.errors', () => {
+      expect(result.errors.length).toEqual(2);
+    });
+  });
+
+  describe('getProductLoading', () => {
+    
+    it('returns loading state', () => {
+      expect(getProductLoading(initialState)).toEqual(initialState.loading);
+    });
+  });
+
+  describe('getSelectedProductId', () => {
+    
+    it('returns selectedProductId', () => {
+      expect(getSelectedProductId(initialState)).toEqual(initialState.selectedProductId);
+    });
+  });
+});
