f1b282677c29c5a3486be979b7d0c1880d74f6d1
Luke Derrick <lderrickable@gmail.com>
2018-05-07 08:01:33 -0400

    add product page component

---
+++ apps/foundation-app/src/app/product/pages/product-view/product-view.component.spec.ts
@@ -0,0 +1,69 @@
+import { Component, Input } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { By } from '@angular/platform-browser';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { ProductViewComponent } from './product-view.component';
+
+import { Product } from '@daffodil/core';
+import { ProductFactory } from '@daffodil/core';
+
+let productFactory = new ProductFactory();
+let product$ = of(productFactory.create());
+
+@Component({
+  selector: '[product-container]', 
+  template: '<ng-content></ng-content>', 
+  exportAs: 'ProductContainer'
+})
+class ProductContainerMock {
+  product$: Observable<Product> = product$;
+}
+
+@Component({
+  selector: 'product',
+  template: ''
+})
+class ProductMock { 
+  @Input() product: Product;
+}
+
+describe('ProductViewComponent', () => {
+  let component: ProductViewComponent;
+  let fixture: ComponentFixture<ProductViewComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ 
+        ProductViewComponent,
+        ProductContainerMock,
+        ProductMock
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('on product', () => {
+    
+    it('should set product to value passed by product-container directive', () => {
+      let productComponent = fixture.debugElement.query(By.css('product'));
+      
+      product$.subscribe((product) => {
+        expect(productComponent.componentInstance.product).toEqual(product);
+      });
+    });
+  });
+});
---
+++ apps/foundation-app/src/app/product/pages/product-view/product-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'foundation-product-view',
+  templateUrl: './product-view.component.html',
+  styleUrls: ['./product-view.component.scss']
+})
+export class ProductViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
