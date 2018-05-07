d53be120980393beba3f50f6df6da447fa8ba3a6
Luke Derrick <lderrickable@gmail.com>
2018-05-07 08:01:33 -0400

    add product page component

--- apps/foundation-app/src/app/product/product.module.ts
+++ apps/foundation-app/src/app/product/product.module.ts
@@ -5,6 +5,7 @@ import { CoreProductModule } from '@daffodil/core';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 import { ProductListViewComponent } from './pages/product-list-view/product-list-view.component';
+import { ProductViewComponent } from './pages/product-view/product-view.component';
 import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
@@ -16,11 +17,13 @@ import { ProductComponent } from './components/product/product.component';
   declarations: [
     ProductListComponent,
     ProductListViewComponent,
+    ProductViewComponent,
     ProductComponent
   ],
   exports: [
     ProductListComponent,
     ProductListViewComponent,
+    ProductViewComponent,
     ProductComponent
   ]
 })
