5ebdea1127272350c3dc4b625690233a4a35e1fc
Luke Derrick <lderrickable@gmail.com>
2018-05-08 14:40:07 -0400

    add more comprehensive teseting to cart component

--- apps/foundation-app/src/app/cart/components/cart/cart.component.spec.ts
+++ apps/foundation-app/src/app/cart/components/cart/cart.component.spec.ts
@@ -1,27 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-import { CartFactory } from '@daffodil/core';
+import { CartFactory, Cart } from '@daffodil/core';
 
 import { CartComponent } from './cart.component';
+import { Component } from '@angular/core';
+
+let cartFactory = new CartFactory();
+let mockCart = cartFactory.create();
+
+@Component({template: '<cart [cart]="cartValue"></cart>'})
+class TestCartWrapper {
+  cartValue: Cart;
+}
 
 describe('CartComponent', () => {
-  let component: CartComponent;
-  let fixture: ComponentFixture<CartComponent>;
-  let cartFactory = new CartFactory();
+  let component: TestCartWrapper;
+  let fixture: ComponentFixture<TestCartWrapper>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CartComponent ]
+      declarations: [
+        CartComponent,
+        TestCartWrapper
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CartComponent);
+    fixture = TestBed.createComponent(TestCartWrapper);
     component = fixture.componentInstance;
 
-    component.cart = cartFactory.create();
+    component.cartValue = mockCart;
+
     fixture.detectChanges();
   });
 
@@ -31,5 +43,11 @@ describe('CartComponent', () => {
 
   it('renders a cart-container', () => {
     expect(fixture.debugElement.query(By.css('.cart-container'))).toBeDefined();
+  });
+
+  it('can be passed a Cart object', () => {
+    let cartComponent = fixture.debugElement.query(By.css('cart'));
+
+    expect(cartComponent.componentInstance.cart).toEqual(mockCart);
   });
 });
