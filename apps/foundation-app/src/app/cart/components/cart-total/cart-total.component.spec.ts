75655f708424206a1440ca61c1072efb213161a3
Luke Derrick <lderrickable@gmail.com>
2018-05-13 07:59:03 -0400

    add cart total component

---
+++ apps/foundation-app/src/app/cart/components/cart-total/cart-total.component.spec.ts
@@ -0,0 +1,45 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { CartFactory, Cart } from '@daffodil/core';
+
+import { CartTotalComponent } from './cart-total.component';
+import { Component } from '@angular/core';
+
+let cartFactory = new CartFactory();
+let mockCart = cartFactory.create();
+
+@Component({template: '<cart-total [cart]="cartValue"></cart-total>'})
+class TestCartTotalWrapper {
+  cartValue: Cart;
+}
+
+describe('CartTotalComponent', () => {
+  let component: TestCartTotalWrapper;
+  let fixture: ComponentFixture<TestCartTotalWrapper>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CartTotalComponent,
+        TestCartTotalWrapper
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestCartTotalWrapper);
+    component = fixture.componentInstance;
+
+    component.cartValue = mockCart;
+
+    fixture.detectChanges();
+  });
+
+  it('can be passed a Cart object', () => {
+    let cartTotalComponent = fixture.debugElement.query(By.css('cart-total'));
+
+    expect(cartTotalComponent.componentInstance.cart).toEqual(mockCart);
+  });
+});
