9468659818d63e9d6e4764175e70ec07c20bd1c2
Luke Derrick <lderrickable@gmail.com>
2018-05-23 11:08:43 -0400

    add ngrx/routing to foundation-app

--- apps/foundation-app/src/app/app-routing.module.ts
+++ apps/foundation-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ProductGridViewComponent } from './product/pages/product-grid-view/product-grid-view.component';
 import { CartViewComponent } from './cart/pages/cart-view/cart-view.component';
+import { ProductViewComponent } from './product/pages/product-view/product-view.component';
 
 export const appRoutes: Routes = [
   {
@@ -15,6 +16,10 @@ export const appRoutes: Routes = [
   {
     path: 'cart',
     component: CartViewComponent
+  },
+  {
+    path: 'product:id',
+    component: ProductViewComponent
   }
 ]
 
