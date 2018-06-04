456968adac9ccbf7840064984c2d6a4388ef5545
Luke Derrick <lderrickable@gmail.com>
2018-06-04 17:36:28 -0400

    enable add-to-cart button to dispatch addToCart action

--- apps/foundation-app/src/app/product/components/add-to-cart/add-to-cart.component.ts
+++ apps/foundation-app/src/app/product/components/add-to-cart/add-to-cart.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'add-to-cart',
   templateUrl: './add-to-cart.component.html',
   styleUrls: ['./add-to-cart.component.scss']
 })
-export class AddToCartComponent implements OnInit {
+export class AddToCartComponent {
+
+  @Output() addToCart: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit() {
+  emitAddToCart() {
+    this.addToCart.emit();
   }
-
 }
