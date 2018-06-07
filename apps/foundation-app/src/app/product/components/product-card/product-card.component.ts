32c9d265d0e18356b8db729595cf3ab66e8dde99
Luke Derrick <lderrickable@gmail.com>
2018-06-07 15:27:46 -0400

    enable product-cards to navigate to its associated product

--- apps/foundation-app/src/app/product/components/product-card/product-card.component.ts
+++ apps/foundation-app/src/app/product/components/product-card/product-card.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '@daffodil/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent {
 
   @Input() product: Product;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  navigateToProduct() {
+    this.router.navigateByUrl('product/' + this.product.id);
   }
-
 }
