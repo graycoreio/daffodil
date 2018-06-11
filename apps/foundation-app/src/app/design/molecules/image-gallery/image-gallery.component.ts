5c88b282d0bb55a7fa54b28b1eb8874c6e2b7d27
Damien Retzinger <damienwebdev@gmail.com>
2018-06-11 11:20:06 -0400

    refactoring image gallery to use angular rendering

--- apps/foundation-app/src/app/design/molecules/image-gallery/image-gallery.component.ts
+++ apps/foundation-app/src/app/design/molecules/image-gallery/image-gallery.component.ts
@@ -1,33 +1,28 @@
 import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Image } from '../../interfaces/image';
 
 @Component({
   selector: 'image-gallery',
   templateUrl: './image-gallery.component.html',
   styleUrls: ['./image-gallery.component.scss']
 })
-export class ImageGalleryComponent implements AfterViewInit {
+export class ImageGalleryComponent implements  OnInit {
 
-  @Input() imgUrls: string[];
+  @Input() images: Image[];
+  @Input() selectedImage: Image;
 
-  selectedImg: string;
-
-  ngAfterViewInit() {
-    setTimeout(this.updateImageGalleryHeight);
+  ngOnInit() {
+    //Consider throwing an exception if you don't give it any images?
+    if(this.images.length != 0){
+      this.selectedImage = this.selectedImage 
+        ? this.selectedImage
+        : this.images[0];
+    }
   }
 
   constructor() { }
 
-  setSelectedImg(selectedImgUrl) {
-    this.selectedImg = selectedImgUrl;
-  }
-
-  onResize() {
-    this.updateImageGalleryHeight();
-  }
-
-  updateImageGalleryHeight() {
-    let imageGalleryWidth = (<HTMLElement>document.getElementsByClassName('image-gallery')[0]).offsetWidth;
-
-    (<HTMLElement>document.getElementsByClassName('image-gallery__active-image')[0]).style.height = (imageGalleryWidth).toString() + 'px';
+  changeImage(image: Image) {
+    this.selectedImage = image;
   }
 }
