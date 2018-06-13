321b0a0f5cf6e12eb0a3876c295837a183c2db10
Luke Derrick <lderrickable@gmail.com>
2018-06-13 10:41:21 -0400

    change accordion padding; enable accordion to be active by default

--- apps/foundation-app/src/app/design/molecules/accordion/accordion.component.spec.ts
+++ apps/foundation-app/src/app/design/molecules/accordion/accordion.component.spec.ts
@@ -4,17 +4,17 @@ import { AccordionComponent } from './accordion.component';
 import { Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
-@Component({template: '<accordion [title]="titleValue" [id]="idValue"></accordion>'})
+@Component({template: '<accordion [title]="titleValue" [initiallyActive]="initiallyActiveValue"></accordion>'})
 class TestAccordionWrapper {
   titleValue: string;
-  idValue: string;
+  initiallyActiveValue: boolean;
 }
 
 describe('AccordionComponent', () => {
   let component: TestAccordionWrapper;
   let fixture: ComponentFixture<TestAccordionWrapper>;
   let stubTitle: string;
-  let stubId: string;
+  let stubInitiallyActive: boolean;
   let accordionComponent;
   let accordionButton;
 
@@ -30,13 +30,13 @@ describe('AccordionComponent', () => {
 
   beforeEach(() => {
     stubTitle = 'title';
-    stubId = 'id';
+    stubInitiallyActive = true;
 
     fixture = TestBed.createComponent(TestAccordionWrapper);
     component = fixture.componentInstance;
 
     component.titleValue = stubTitle;
-    component.idValue = stubId;
+    component.initiallyActiveValue = stubInitiallyActive;
 
     fixture.detectChanges();
     accordionComponent = fixture.debugElement.query(By.css('accordion'));    
@@ -51,8 +51,28 @@ describe('AccordionComponent', () => {
     expect(accordionComponent.componentInstance.title).toEqual(stubTitle);
   });
 
-  it('should set active to false', () => {
-    expect(accordionComponent.componentInstance.active).toBeFalsy();
+  it('should be able to accept an initiallyActive input', () => {
+    expect(accordionComponent.componentInstance.initiallyActive).toEqual(stubInitiallyActive);
+  });
+
+  describe('ngOnInit', () => {
+    
+    describe('when initiallyActive is true', () => {
+      
+      it('should set active to true', () => {
+        expect(accordionComponent.componentInstance.active).toBeTruthy();
+      });
+    });
+
+    describe('when initiallyActive is not set', () => {
+      
+      it('should set active to false', () => {
+        accordionComponent.componentInstance.initiallyActive = null;
+        accordionComponent.componentInstance.ngOnInit();
+        
+        expect(accordionComponent.componentInstance.active).toBeFalsy();
+      });
+    });
   });
 
   describe('when accordion button is clicked', () => {
@@ -74,6 +94,7 @@ describe('AccordionComponent', () => {
   describe('toggleActive', () => {
     
     it('should toggle active', () => {
+      accordionComponent.componentInstance.active = false;
       accordionComponent.componentInstance.toggleActive();
 
       expect(accordionComponent.componentInstance.active).toBeTruthy();
@@ -103,6 +124,12 @@ describe('AccordionComponent', () => {
   });
 
   describe('when active is false', () => {
+
+    beforeEach(() => {
+      accordionComponent.componentInstance.active = false;
+
+      fixture.detectChanges();
+    });
 
     it('accordion__panel should not have the accordion__panel--active', () => {
       let accordionPanel = fixture.debugElement.query(By.css('.accordion__panel'));
