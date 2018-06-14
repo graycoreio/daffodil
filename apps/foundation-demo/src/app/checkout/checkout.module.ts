85028398a6929fc1a50fbb70bc0a872068bb3423
Luke Derrick <lderrickable@gmail.com>
2018-06-14 07:41:56 -0400

    add base shipping component

--- apps/foundation-demo/src/app/checkout/checkout.module.ts
+++ apps/foundation-demo/src/app/checkout/checkout.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutViewComponent } from './pages/checkout-view/checkout-view.component';
+import { ShippingComponent } from './components/shipping/shipping.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    CheckoutViewComponent
+    CheckoutViewComponent,
+    ShippingComponent
   ],
   exports: [
-    CheckoutViewComponent
+    CheckoutViewComponent,
+    ShippingComponent
   ]
 })
 export class CheckoutModule { }
