754150b88ef47dd96378ae3065fee5d8563cfcb8
Damien Retzinger <damienwebdev@gmail.com>
2018-06-14 10:41:22 -0400

    adding back styles to cart-view

--- apps/foundation-demo/src/app/cart/pages/cart-view/cart-view.component.ts
+++ apps/foundation-demo/src/app/cart/pages/cart-view/cart-view.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'foundation-cart-view',
   templateUrl: './cart-view.component.html',
+  styleUrls: ['./cart-view.component.scss']
 })
 export class CartViewComponent implements OnInit {
 
