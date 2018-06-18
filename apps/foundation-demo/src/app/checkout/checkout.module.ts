3d77d63c84fdf75c12a7ed24836945a0b17a1812
Luke Derrick <lderrickable@gmail.com>
2018-06-18 08:17:11 -0400

    redo shipping component and add validation

--- apps/foundation-demo/src/app/checkout/checkout.module.ts
+++ apps/foundation-demo/src/app/checkout/checkout.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutViewComponent } from './pages/checkout-view/checkout-view.component';
 import { ShippingComponent } from './components/shipping/shipping.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     CheckoutViewComponent,
