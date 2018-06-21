02b60adc330425c6e9a3bb511105bcdff5464b22
Luke Derrick <lderrickable@gmail.com>
2018-06-21 07:46:31 -0400

    refactor(foundation-demo): rename shipping to shipping-form
    rename shipping to shipping-form so the checkout view can be toggled between the form and the completed shipping address

--- apps/foundation-demo/src/app/checkout/checkout.module.ts
+++ apps/foundation-demo/src/app/checkout/checkout.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutViewComponent } from './pages/checkout-view/checkout-view.component';
-import { ShippingComponent } from './components/shipping/shipping.component';
+import { ShippingFormComponent } from './components/shipping/shipping.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CoreCheckoutModule } from '@daffodil/core';
 
@@ -14,11 +14,11 @@ import { CoreCheckoutModule } from '@daffodil/core';
   ],
   declarations: [
     CheckoutViewComponent,
-    ShippingComponent
+    ShippingFormComponent
   ],
   exports: [
     CheckoutViewComponent,
-    ShippingComponent
+    ShippingFormComponent
   ]
 })
 export class CheckoutModule { }
