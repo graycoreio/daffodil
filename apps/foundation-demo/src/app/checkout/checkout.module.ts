7d0247c9965564dc829b4fe43e1ac81d4024fb29
Luke Derrick <lderrickable@gmail.com>
2018-07-16 15:31:59 -0400

    refactor(foundation-demo): remove unneeded import
    remove unneeded import

--- apps/foundation-demo/src/app/checkout/checkout.module.ts
+++ apps/foundation-demo/src/app/checkout/checkout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutViewComponent } from './pages/checkout-view/checkout-view.component';
 import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CoreCheckoutModule } from '@daffodil/core';
 import { ShippingSummaryComponent } from './components/shipping-summary/shipping-summary.component';
 import { ShippingOptionComponent } from './components/shipping-option/shipping-option.component';
@@ -12,7 +12,6 @@ import { DesignModule } from '../design/design.module';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     ReactiveFormsModule,
     CoreCheckoutModule,
     DesignModule
