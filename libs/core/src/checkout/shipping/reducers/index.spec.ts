2de692a736201c43fdb5046e03558583977c6395
Luke Derrick <lderrickable@gmail.com>
2018-07-17 16:50:57 -0400

    refactor(core): revert previous commit
    remove unneeded selectIsShippingOptionSelected selector

--- libs/core/src/checkout/shipping/reducers/index.spec.ts
+++ libs/core/src/checkout/shipping/reducers/index.spec.ts
@@ -70,13 +70,4 @@ describe('selectShippingState', () => {
       });
     });
   });
-
-  describe('selectIsShippingOptionSelected', () => {
-    
-    it('selects isShippingOptionSelected state', () => {
-      store.pipe(select(fromShipping.selectIsShippingOptionSelected)).subscribe((isShippingOptionSelected) => {
-        expect(isShippingOptionSelected).toBeTruthy();
-      });
-    });
-  });
 });
