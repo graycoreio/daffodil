44f369eac5fe887a97591ae0b0077b7647eac2c7
Damien Retzinger <damienwebdev@gmail.com>
2018-07-30 10:35:29 -0400

    build(foundation-demo): Adds @ngrx/store-devtools in dev mode for easier redux debugging

--- apps/foundation-demo/src/app/app.module.ts
+++ apps/foundation-demo/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { environment } from '../environments/environment';
 
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { NxModule } from '@nrwl/nx';
 
 import { AppComponent } from './app.component';
@@ -30,6 +32,11 @@ import { CheckoutModule } from './checkout/checkout.module';
 
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
+
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
     
     AppRoutingModule,
 
