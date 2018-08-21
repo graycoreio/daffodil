e4361d583ead292418e08c66ee9f04b178c630f4
Luke Derrick <lderrickable@gmail.com>
2018-08-21 16:59:42 -0400

    add sidebar-item component
    add sidebar-item component to header

--- apps/foundation-demo/src/app/misc/misc.module.ts
+++ apps/foundation-demo/src/app/misc/misc.module.ts
@@ -5,6 +5,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { HelpBoxComponent } from './help-box/help-box.component';
 import { HeaderComponent } from './header/components/header/header.component';
 import { FoundationMiscStateModule } from './misc-state.module';
+import { SidebarItemComponent } from './header/components/sidebar-item/sidebar-item.component';
 
 @NgModule({
   imports: [
@@ -14,12 +15,14 @@ import { FoundationMiscStateModule } from './misc-state.module';
   declarations: [
     NotFoundComponent,
     HelpBoxComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarItemComponent
   ],
   exports: [
     NotFoundComponent,
     HelpBoxComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarItemComponent
   ]
 })
 export class MiscModule { }
