85084a5bbc294a4dbc0ecc23bedbfeadfe1d9627
Luke Derrick <lderrickable@gmail.com>
2018-08-22 10:33:20 -0400

    feat(foundation-demo): make sidebar close when user clicks outside of sidebar
    make sidebar close when user clicks outside of sidebar

--- apps/foundation-demo/src/app/misc/header/components/header/header.component.ts
+++ apps/foundation-demo/src/app/misc/header/components/header/header.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
-import * as fromFoundationHeader from '../../reducers';
 import { ToggleShowSidebar } from '../../actions/sidebar.actions';
+import * as fromFoundationHeader from '../../reducers';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  showSidebar$: Observable<boolean>;
 
   constructor(
     private store: Store<fromFoundationHeader.State>
   ) { }
 
+  ngOnInit() {
+    this.showSidebar$ = this.store.pipe(
+      select(fromFoundationHeader.selectShowSidebar)
+    );
+  }
+
   toggleShowSidebar() {
-    this.store.dispatch(new ToggleShowSidebar());
+    let that = this;
+    // setTimeout included to give the sidebar component time to determine the location of the click first.
+    setTimeout(() => {
+      that.store.dispatch(new ToggleShowSidebar());
+    });
   }
 }
