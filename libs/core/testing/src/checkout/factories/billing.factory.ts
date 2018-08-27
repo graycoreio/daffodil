cf59a3aebe05298bd7014414f6657c51152b346c
Damien Retzinger <damienwebdev@gmail.com>
2018-08-27 14:51:09 -0400

    feat(core): allow core factories to be injected from root injector

--- libs/core/testing/src/checkout/factories/billing.factory.ts
+++ libs/core/testing/src/checkout/factories/billing.factory.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { PaymentInfo } from '@daffodil/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class BillingFactory {
   
   create() : MockPaymentInfo {
