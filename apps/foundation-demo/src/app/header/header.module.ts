c174cc7459f637f7ed75d8e76279b5729336c8a2
Luke Derrick <lderrickable@gmail.com>
2018-08-28 15:31:22 -0400

    refactor(foundation-demo): move header and sidebar out of misc directory
    move header and sidebar out of misc directory

--- apps/foundation-demo/src/app/header/header.module.ts
+++ apps/foundation-demo/src/app/header/header.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DaffHeaderModule } from '../../design/molecules/daff-header/daff-header.module';
+import { DaffHeaderModule } from '../design/molecules/daff-header/daff-header.module';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarModule } from '../sidebar/sidebar.module';
 import { RouterModule } from '@angular/router';
--- apps/foundation-demo/src/app/sidebar/sidebar.module.ts
+++ apps/foundation-demo/src/app/sidebar/sidebar.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SidebarItemComponent } from './components/sidebar-item/sidebar-item.component';
-import { DaffSidebarModule } from '../../design/molecules/daff-sidebar/daff-sidebar.module';
+import { DaffSidebarModule } from '../design/molecules/daff-sidebar/daff-sidebar.module';
 import { FoundationSidebarStateModule } from './sidebar.state.module';
 import { SidebarContainer } from './containers/sidebar.component';
 import { SidebarViewComponent } from './components/sidebar-view/sidebar-view.component';
