31345413f41bd4517ae9b5ee16cde5a9745294df
Luke Derrick <lderrickable@gmail.com>
2018-09-03 07:12:53 -0400

    feat(foundation-demo): add validation to shipping-options
    add validation to shipping-options

--- apps/foundation-demo/src/app/checkout/components/shipping/shipping-options/components/shipping-options/shipping-options.component.ts
+++ apps/foundation-demo/src/app/checkout/components/shipping/shipping-options/components/shipping-options/shipping-options.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { ShippingOption } from '@daffodil/core';
 
 import { ShippingOptionsService } from '../services/shipping-options.service';
+import { ErrorStateMatcher } from '../../../../../../design/molecules/error-state-matcher/error-state-matcher.component';
 
 @Component({
   selector: 'shipping-options',
@@ -14,10 +15,20 @@ export class ShippingOptionsComponent {
   @Input() formGroup: FormGroup;
   @Input() submitted: boolean;
   shippingOptions: ShippingOption[];
+  errorState: boolean;
+  errorStateMatcher: ErrorStateMatcher;
 
   constructor(
     private shippingOptionsService: ShippingOptionsService
   ) {
     this.shippingOptions = shippingOptionsService.getShippingOptions();
+  }
+
+  ngOnInit() {
+    this.errorStateMatcher = new ErrorStateMatcher();
+  }
+
+  ngDoCheck() {
+    this.errorState = this.errorStateMatcher.isErrorState(<FormControl> this.formGroup.controls['id'], this.submitted);
   }
 }
