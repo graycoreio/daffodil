e339d6f7a71d57b955193d6a11f0a9f876293fa5
Luke Derrick <lderrickable@gmail.com>
2018-09-03 07:12:53 -0400

    feat(foundation-demo): add validation to shipping-options
    add validation to shipping-options

--- apps/foundation-demo/src/app/checkout/components/shipping/shipping-options/components/shipping-options/shipping-options.component.spec.ts
+++ apps/foundation-demo/src/app/checkout/components/shipping/shipping-options/components/shipping-options/shipping-options.component.spec.ts
@@ -3,16 +3,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShippingOptionsComponent } from './shipping-options.component';
 import { Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { FormGroup, FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, FormsModule, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { ShippingOptionsService } from '../services/shipping-options.service';
 import { ShippingOptionsFactory } from '../factories/shipping-options.factory';
 
 let formBuilder: FormBuilder = new FormBuilder();
 
 let stubFormGroupValue = formBuilder.group({
-  id: ''
+  id: ['', Validators.required]
 });
-let stubSubmitted = true;
+let stubSubmitted = false;
 
 @Component({
   template: '<shipping-options ' + 
@@ -71,5 +71,74 @@ describe('ShippingOptionsComponent', () => {
 
   it('should set shippingOptions', () => {
     expect(shippingOptionsComponent.shippingOptions).toEqual(shippingOptionsService.getShippingOptions());
+  });
+
+  describe('when formControl has not been touched or submitted', () => {
+    
+    it('should set errorState to false', () => {
+      shippingOptionsComponent.formGroup.controls['id'].markAsUntouched();
+   
+      fixture.detectChanges();
+
+      expect(shippingOptionsComponent.errorState).toBeFalsy();
+    });
+  });
+
+  describe('when formControl has been touched', () => {
+
+    beforeEach(() => {
+      shippingOptionsComponent.formGroup.controls['id'].markAsTouched();
+    });
+
+    describe('and does not have an error', () => {
+      
+      it('should set errorState to false', () => {
+        shippingOptionsComponent.formGroup.controls['id'].setValue('valid');
+        shippingOptionsComponent.ngDoCheck();
+
+        expect(shippingOptionsComponent.errorState).toBeFalsy();
+      });
+    });
+    
+    describe('and has an error', () => {
+      
+      it('should set errorState to true', () => {
+        shippingOptionsComponent.formGroup.controls['id'].setValue(null);
+    
+        fixture.detectChanges();
+
+        expect(shippingOptionsComponent.errorState).toBeTruthy();
+      });
+    });
+  });
+
+  describe('when errorState is true', () => {
+    
+    beforeEach(() => {
+      shippingOptionsComponent.formGroup.controls['id'].setValue(null);
+      shippingOptionsComponent.formGroup.controls['id'].markAsTouched();
+      fixture.detectChanges();
+    });
+    
+    it('should set hidden to false on shipping-options__invalid', () => {
+      let hostNativeElement = fixture.debugElement.query(By.css('.shipping-options__invalid')).nativeElement;
+
+      expect(hostNativeElement.hidden).toBeFalsy();
+    });
+  });
+
+  describe('when errorState is false', () => {
+    
+    beforeEach(() => {
+      shippingOptionsComponent.formGroup.controls['id'].setValue('valid');
+
+      fixture.detectChanges();
+    });
+
+    it('should set hidden to true on shipping-options__invalid', () => {
+      let hostNativeElement = fixture.debugElement.query(By.css('.shipping-options__invalid')).nativeElement;
+
+      expect(hostNativeElement.hidden).toBeTruthy();
+    });
   });
 });
