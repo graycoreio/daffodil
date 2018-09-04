cd606f32de906ecf9c3731685523f536ba10cfc6
Luke Derrick <lderrickable@gmail.com>
2018-09-04 15:46:43 -0400

    feat(state): add best-sellers call to product testing service
    add best-sellers call to product testing service

---
+++ libs/state/src/product/testing/services/product.testing.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+import { ProductFactory, Product } from '@daffodil/core';
+
+import { InMemoryDbService, RequestInfoUtilities, ParsedRequestUrl, STATUS } from 'angular-in-memory-web-api';
+
+
+@Injectable()
+export class ProductTestingService implements InMemoryDbService {
+
+  products: Product[];
+
+  constructor(private productFactory: ProductFactory) { 
+    this.products = this.productFactory.createStyleTestingList();
+  }
+
+  parseRequestUrl(url: string, utils: RequestInfoUtilities): ParsedRequestUrl {
+    return utils.parseRequestUrl(url);
+  }
+
+  createDb(): any {
+    return {
+      products: this.products
+    };
+  }
+
+  get(reqInfo: any) {
+    return reqInfo.utils.createResponse$(() => {
+
+      if(reqInfo.id === "best-sellers") {
+        return {
+          body: this.products.splice(1,4),
+          status: STATUS.OK
+        };
+      }
+      
+      return {
+        body: this.products,
+        status: STATUS.OK
+      }
+    })
+  }
+}
