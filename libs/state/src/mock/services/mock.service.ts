732df51b20f5343f0fdea1d9b33bf0237d4d3198
Luke Derrick <lderrickable@gmail.com>
2018-09-07 10:27:19 -0400

    feat(state): add custom productTestingService.get filter to mock service
    add custom productTestingService.get filter to mock service

---
+++ libs/state/src/mock/services/mock.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+
+import { ProductTestingService } from '../../product/testing/services/product.testing.service';
+
+import { InMemoryDbService, RequestInfoUtilities, ParsedRequestUrl } from 'angular-in-memory-web-api';
+import { Product } from '@daffodil/core';
+import { CartTestingService } from '../../cart/testing/services/cart.testing.service';
+import { Cart } from '@daffodil/core';
+
+@Injectable()
+export class MockService implements InMemoryDbService{
+  constructor(
+    private productTestingService: ProductTestingService,
+    private cartTestingService: CartTestingService
+  ) {}
+
+  parseRequestUrl(url: string, utils: RequestInfoUtilities): ParsedRequestUrl {
+    return utils.parseRequestUrl(url);
+  }
+
+  post(reqInfo: any) {
+    const collectionName = reqInfo.collectionName;
+    if (collectionName === 'cart') {
+      return this.cartTestingService.post(reqInfo);
+    }
+
+    return undefined;
+  }
+
+  get(reqInfo: any) {
+    const collectionName = reqInfo.collectionName;
+    if (collectionName === 'products') {
+      return this.productTestingService.get(reqInfo);
+    }
+  }
+
+  createDb() : MockDatabase {
+    return {
+      ...this.productTestingService.createDb(),
+      ...this.cartTestingService.createDb()
+    };
+  }
+}
+
+export interface MockDatabase {
+  products: Product[],
+  cart: Cart
+}
