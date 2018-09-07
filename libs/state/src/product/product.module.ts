ae352b1d5b47eab21131641add80eeb03f173954
Luke Derrick <lderrickable@gmail.com>
2018-09-07 09:58:37 -0400

    feat(state): add best sellers container to StateProductModule
    add best sellers container to StateProductModule

--- libs/state/src/product/product.module.ts
+++ libs/state/src/product/product.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProductGridContainer } from './containers/product-grid/product-grid.component';
 import { StateProductStateModule } from './product-state.module';
 import { ProductContainer } from './containers/product/product.component';
+import { BestSellersContainer } from './containers/best-sellers/best-sellers.component';
 
 @NgModule({
   imports: [
@@ -16,11 +17,13 @@ import { ProductContainer } from './containers/product/product.component';
   ],
   declarations: [
     ProductGridContainer,
-    ProductContainer
+    ProductContainer,
+    BestSellersContainer
   ],
   exports: [
     ProductGridContainer,
-    ProductContainer
+    ProductContainer,
+    BestSellersContainer
   ]
 })
 export class StateProductModule { }
