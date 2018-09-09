aa4b5f9eddc532c14b925d24558fde5e2396e90b
Luke Derrick <lderrickable@gmail.com>
2018-09-09 13:18:36 -0400

    fix(foundation-demo): fix broken import file name
    fix broken import file name

--- apps/foundation-demo/src/app/misc/misc.module.ts
+++ apps/foundation-demo/src/app/misc/misc.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HelpBoxComponent } from './help-box/help-box.component';
 import { FooterComponent } from './footer/footer.component';
-import { DaffFooterModule } from '../design/molecules/daff-footer/daff-footer.module';
+import { DaffFooterModule } from '../design/molecules/footer/footer.module';
 
 @NgModule({
   imports: [
