2f2621a02a4f5d79900e7c81d0c5ca7359a942cd
Damien Retzinger <damienwebdev@gmail.com>
2018-09-09 21:30:28 -0400

    refactor(foundation-demo): add test coverage for daff-sidebar-content component

--- apps/foundation-demo/src/app/design/molecules/sidebar/sidebar-content/sidebar-content.component.spec.ts
+++ apps/foundation-demo/src/app/design/molecules/sidebar/sidebar-content/sidebar-content.component.spec.ts
@@ -1,20 +1,28 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DaffSidebarContentComponent } from './sidebar-content.component';
 
-describe('DaffSidebarContentComponent', () => {
+@Component({template: '<daff-sidebar-content class="sidebar-content-wrapper">Title</daff-sidebar-content>>'})
+class TestSidebarContentWrapper {}
+
+fdescribe('DaffSidebarContentComponent', () => {
   let component: DaffSidebarContentComponent;
   let fixture: ComponentFixture<DaffSidebarContentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DaffSidebarContentComponent ]
+      declarations: [ 
+        DaffSidebarContentComponent,
+        TestSidebarContentWrapper
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DaffSidebarContentComponent);
+    fixture = TestBed.createComponent(TestSidebarContentWrapper);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -22,4 +30,10 @@ describe('DaffSidebarContentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add a class of `daff-sidebar-content` to its host', () => {
+    let sidebarContentWrapper = fixture.debugElement.query(By.css('.sidebar-content-wrapper'));
+
+    expect(sidebarContentWrapper.nativeElement.classList.contains('daff-sidebar-content')).toBeTruthy();
+  })
 });
