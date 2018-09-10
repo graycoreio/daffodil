3871cb25ce2512a032a20e4bde5dcc0c54a26738
Damien Retzinger <damienwebdev@gmail.com>
2018-09-10 17:47:09 -0400

    style(core): cleanup unncessary imports

--- libs/driver/testing/src/cart/services/cart.service.spec.ts
+++ libs/driver/testing/src/cart/services/cart.service.spec.ts
@@ -1,9 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Cart } from '@daffodil/core';
-import { DaffCoreTestingModule, CartFactory } from '@daffodil/core/testing';
-
-
 describe('Driver | Shopify | Cart | CartService', () => {
   
 });
