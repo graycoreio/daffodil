f79e4f8f65ea2f11270e5da80a8a8bcc87e2b7a9
Damien Retzinger <damienwebdev@gmail.com>
2018-09-10 12:05:52 -0400

    refactor(foundation-demo): update animation to remove unused states

--- apps/foundation-demo/src/app/design/molecules/sidebar/animation/sidebar-animation-state.ts
+++ apps/foundation-demo/src/app/design/molecules/sidebar/animation/sidebar-animation-state.ts
@@ -2,9 +2,6 @@ export const getAnimationState = (open : boolean, enabled: boolean = true) => {
     if(open && enabled){
         return "open";
     }
-    else if(open && !enabled) {
-        return "open-instant";
-    }
     else {
         return "void";
     }
