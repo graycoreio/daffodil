4d4d9d743f7334eb90bb012a3c8f0213ef6ff2b3
Damien Retzinger <damienwebdev@gmail.com>
2018-09-10 12:10:22 -0400

    refactor(foundation-demo): modify sidebar animation function return void if the animation is not enabled

--- apps/foundation-demo/src/app/design/molecules/sidebar/animation/sidebar-animation-state.spec.ts
+++ apps/foundation-demo/src/app/design/molecules/sidebar/animation/sidebar-animation-state.spec.ts
@@ -4,15 +4,15 @@ import { DaffSidebarAnimationStates } from "./sidebar-animation";
 describe('SidebarAnimationState Calculation', () => {
 
     it('should return `open` if it is open', () => {
-        expect(getAnimationState(true)).toBe(DaffSidebarAnimationStates.OPEN);
+        expect(getAnimationState(true)).toEqual(DaffSidebarAnimationStates.OPEN);
     })
 
     it('should return `void` if it is disabled`', () => {
-        expect(getAnimationState(false, false)).toBe(DaffSidebarAnimationStates.VOID);
-        expect(getAnimationState(true, false)).toBe(DaffSidebarAnimationStates.VOID);
+        expect(getAnimationState(false, false)).toEqual(DaffSidebarAnimationStates.VOID);
+        expect(getAnimationState(true, false)).toEqual(DaffSidebarAnimationStates.VOID);
     });
 
     it('should return `void` if it is not open', () => {
-        expect(getAnimationState(false, true)).toBe(DaffSidebarAnimationStates.VOID);
+        expect(getAnimationState(false, true)).toEqual(DaffSidebarAnimationStates.VOID);
     })
 })
