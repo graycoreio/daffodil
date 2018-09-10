9e5148bf92c988011bf4ba7c14889d92b99ed332
Damien Retzinger <damienwebdev@gmail.com>
2018-09-10 12:12:36 -0400

    feat(foundation-demo): update header to use new components and apis

--- apps/foundation-demo/src/app/core/header/components/header/header.component.spec.ts
+++ apps/foundation-demo/src/app/core/header/components/header/header.component.spec.ts
@@ -1,30 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
-import { Component, Output, EventEmitter } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-@Component({selector: '[sidebar-container]', template: '<ng-content></ng-content>', exportAs: 'SidebarContainer'})
-class MockSidebarContainer {
-  toggleSidebarVisibility: Function = () => {};
-}
-
-@Component({selector: 'header', template: ''})
-class MockHeaderComponent {
-  @Output() toggleSidebarVisibility: EventEmitter<any> = new EventEmitter();
-}
+import { HeaderComponent } from './header.component';
+import { DaffNavbarModule } from '../../../../design/molecules/navbar/navbar.module';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-  let sidebarContainer: MockSidebarContainer;
-  let header: MockHeaderComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        DaffNavbarModule
+      ],
       declarations: [ 
-        MockSidebarContainer,
-        MockHeaderComponent,
         HeaderComponent
       ]
     })
@@ -35,23 +25,9 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
-    header = fixture.debugElement.query(By.css('header')).componentInstance;
-    sidebarContainer = fixture.debugElement.query(By.css('[sidebar-container]')).componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  describe('when <header> emits toggleSidebarVisibility', () => {
-    
-    it('should call sidebarContainer.toggleSidebarVisibility', () => {
-      spyOn(sidebarContainer, 'toggleSidebarVisibility');
-
-      header.toggleSidebarVisibility.emit();
-
-      expect(sidebarContainer.toggleSidebarVisibility).toHaveBeenCalled();
-    });
   });
 });
--- apps/foundation-demo/src/app/core/header/containers/header/header.component.ts
+++ apps/foundation-demo/src/app/core/header/containers/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 import { Store } from '@ngrx/store';
 
