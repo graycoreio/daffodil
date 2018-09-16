2ec3833853d2f79d805c56dd1323c2d9534005bd
Luke Derrick <lderrickable@gmail.com>
2018-09-16 13:59:40 -0400

    feat(foundation-demo): add not-found module
    add not-found module

--- apps/foundation-demo/src/app/misc/misc.module.ts
+++ apps/foundation-demo/src/app/misc/misc.module.ts
@@ -1,27 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NotFoundComponent } from './not-found/not-found.component';
 import { HelpBoxComponent } from './help-box/help-box.component';
 import { FooterComponent } from './footer/footer.component';
 import { DaffListModule } from '../design/molecules/list/list.module';
 import { DaffButtonSetModule } from '../design/molecules/button-set/button-set.module';
-import { ProductModule } from '../product/product.module';
 
 @NgModule({
   imports: [
     CommonModule,
     DaffListModule,
-    DaffButtonSetModule,
-    ProductModule
+    DaffButtonSetModule
   ],
   declarations: [
-    NotFoundComponent,
     HelpBoxComponent,
     FooterComponent
   ],
   exports: [
-    NotFoundComponent,
     HelpBoxComponent,
     FooterComponent
   ]
