542d5caad80da2cdd3fbccbeb2e4c6c1d230bd25
Luke Derrick <lderrickable@gmail.com>
2018-10-02 12:52:02 -0400

    fix(foundation-demo): fix daff-input component
    fix daff-input component

--- apps/foundation-demo/src/app/design/atoms/form/input/input.component.ts
+++ apps/foundation-demo/src/app/design/atoms/form/input/input.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: '[daff-input]',
   host: {'class': 'daff-input'},
   template: '<ng-content></ng-content>',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DaffInputComponent {
 }
