b82bb366e7d91bb23398ea56894c54ce1ef64e97
Luke Derrick <lderrickable@gmail.com>
2018-10-05 17:17:51 -0400

    feat(foundation-demo): make modal able to have multiple positions
    make modal able to have multiple positions

--- apps/foundation-demo/src/app/design/molecules/modal/modal/modal.component.ts
+++ apps/foundation-demo/src/app/design/molecules/modal/modal/modal.component.ts
@@ -15,9 +15,13 @@ export class DaffModalComponent implements OnInit {
 
   ngOnInit() {
     this._animationState = getAnimationState(this.show);
+    this.updatePositionClasses();
   }
 
+  _modalContentClasses: {[key: string]: boolean} = {};
   _animationState: string;
+  _verticalPosition: string = 'center';
+  _horizontalPosition: string = 'center';
 
   /**
    * Property for the "show" state of the modal
@@ -27,6 +31,28 @@ export class DaffModalComponent implements OnInit {
   set show(value: boolean) { 
     this._show = value;
     this._animationState = getAnimationState(value);
+  }
+
+  @Input()
+  get verticalPosition(): string { return this._verticalPosition; }
+  set verticalPosition(value: string) {
+    if (value !== 'center' && value !== 'top' && value !== 'bottom') {
+      throw Error(`verticalPosition value must be either 'center', 'top', or 'bottom'.
+      Example: <daff-modal verticalPosition="top"></daff-modal>`);
+    }
+    this._verticalPosition = value;
+    this.updatePositionClasses();
+  }
+
+  @Input()
+  get horizontalPosition(): string { return this._horizontalPosition; }
+  set horizontalPosition(value: string) {
+    if (value !== 'center' && value !== 'left' && value !== 'right') {
+      throw Error(`horizontalPosition value must be either 'center', 'left', or 'right'.
+      Example: <daff-modal horizontalPosition="right"></daff-modal>`);
+    }
+    this._horizontalPosition = value;
+    this.updatePositionClasses();
   }
 
   /**
@@ -49,4 +75,14 @@ export class DaffModalComponent implements OnInit {
    * "visible" to the human eye
    */
   @Input() backdropIsVisible: boolean = true;
+
+  private updatePositionClasses() {
+    const modalContentClasses = this._modalContentClasses;
+    modalContentClasses['daff-modal__content--left'] = this._horizontalPosition === 'left';
+    modalContentClasses['daff-modal__content--right'] = this._horizontalPosition === 'right';
+    modalContentClasses['daff-modal__content--center'] = this._horizontalPosition === 'center';
+    modalContentClasses['daff-modal__content--top'] = this._verticalPosition === 'top';
+    modalContentClasses['daff-modal__content--bottom'] = this._verticalPosition === 'bottom';
+    modalContentClasses['daff-modal__content--middle'] = this._verticalPosition === 'center';
+  }
 }
