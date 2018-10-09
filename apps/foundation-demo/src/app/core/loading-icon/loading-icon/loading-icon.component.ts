74912463697cc3234cf901ee82ef5f174f885912
Luke Derrick <lderrickable@gmail.com>
2018-10-09 11:12:47 -0400

    feat(foundation-demo): change ChangeDetectionStrategy to OnPush
    change ChangeDetectionStrategy to OnPush

--- apps/foundation-demo/src/app/core/loading-icon/loading-icon/loading-icon.component.ts
+++ apps/foundation-demo/src/app/core/loading-icon/loading-icon/loading-icon.component.ts
@@ -1,8 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'loading-icon',
   templateUrl: './loading-icon.component.html',
-  styleUrls: ['./loading-icon.component.scss']
+  styleUrls: ['./loading-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoadingIconComponent { }
