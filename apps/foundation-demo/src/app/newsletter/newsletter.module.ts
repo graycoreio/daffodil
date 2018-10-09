5e2c3a828012199171e13644b814313ab1c8046b
xelaint <xelaint@gmail.com>
2018-10-09 18:52:49 -0400

    feat(foundation-demo): update mobile style

--- apps/foundation-demo/src/app/newsletter/newsletter.module.ts
+++ apps/foundation-demo/src/app/newsletter/newsletter.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DaffNewsletterComponent } from './newsletter.component';
 import { DaffInputModule } from '../design/atoms/form/input/input.module';
+import { DaffButtonModule } from '../design/atoms/button/button.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    DaffInputModule
+    DaffInputModule,
+    DaffButtonModule
   ],
   declarations: [
     DaffNewsletterComponent
