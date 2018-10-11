6d52358b6fcc6f0795a62db31ec6b92674e8d57c
xelaint <xelaint@gmail.com>
2018-10-11 11:47:15 -0400

    feat(foundation-demo): add daff-container

--- apps/foundation-demo/src/app/newsletter/newsletter.module.ts
+++ apps/foundation-demo/src/app/newsletter/newsletter.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DaffNewsletterComponent } from './newsletter.component';
 import { DaffInputModule } from '../design/atoms/form/input/input.module';
 import { DaffButtonModule } from '../design/atoms/button/button.module';
+import { DaffContainerModule } from '../design/atoms/container/container.module';
 
 @NgModule({
   imports: [
     CommonModule,
     DaffInputModule,
-    DaffButtonModule
+    DaffButtonModule,
+    DaffContainerModule
   ],
   declarations: [
     DaffNewsletterComponent
