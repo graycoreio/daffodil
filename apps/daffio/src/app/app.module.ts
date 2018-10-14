e2aa9a1010474d2e23443bdee143dff48aafc7f7
Margaret Landefeld <mlandefeld@gmail.com>
2018-10-14 15:28:33 -0400

    fix(daffio): removed nrwl reference in app.module.ts

--- apps/daffio/src/app/app.module.ts
+++ apps/daffio/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NxModule } from '@nrwl/nx';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
