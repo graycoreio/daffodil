a8ab54a62f72a97ef231b89e9fa8e06919f75b26
xelaint <xelaint@gmail.com>
2018-10-15 15:20:19 -0500

    style(foundation-demo): update product-grid styles

--- apps/foundation-demo/src/app/product/components/product-grid/product-grid.component.ts
+++ apps/foundation-demo/src/app/product/components/product-grid/product-grid.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 import { Product } from '@daffodil/core';
 
 @Component({
   selector: 'product-grid',
+  host: {'class': 'product-grid'},
   templateUrl: './product-grid.component.html',
-  styleUrls: ['./product-grid.component.scss']
+  styleUrls: ['./product-grid.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProductGridComponent {
   @Input() products: Product[];
