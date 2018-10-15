53772650794b70b3d5026039c63441a751bb9a4e
xelaint <xelaint@gmail.com>
2018-10-15 12:01:01 -0500

    style(foundation-demo): update footer and newsletter styles

--- apps/foundation-demo/src/app/core/footer/footer.module.ts
+++ apps/foundation-demo/src/app/core/footer/footer.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { DaffListModule } from '../../design/molecules/list/list.module';
 import { DaffButtonSetModule } from '../../design/molecules/button-set/button-set.module';
 import { FooterComponent } from './footer.component';
+import { DaffButtonModule } from '../../design/atoms/button/button.module';
+import { DaffContainerModule } from '../../design/atoms/container/container.module';
 
 @NgModule({
   imports: [
     CommonModule,
     DaffListModule,
-    DaffButtonSetModule
+    DaffButtonModule,
+    DaffButtonSetModule,
+    DaffContainerModule
   ],
   declarations: [
     FooterComponent
