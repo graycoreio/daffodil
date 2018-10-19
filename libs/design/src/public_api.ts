584851ac03d7ba0d84ff55f363c1bc892b213a93
Damien Retzinger <damienwebdev@gmail.com>
2018-10-19 11:55:04 -0400

    feat(design): migrate modal into @daffodil/design

--- libs/design/src/public_api.ts
+++ libs/design/src/public_api.ts
@@ -15,6 +15,7 @@ export * from './molecules/image-gallery/public_api';
 export * from './molecules/image-list/public_api';
 export * from './molecules/input-validator/public_api';
 export * from './molecules/list/public_api';
+export * from './molecules/modal/public_api';
 export * from './molecules/navbar/public_api';
 export * from './molecules/qty-dropdown/public_api';
 export * from './molecules/select-validator/public_api';
