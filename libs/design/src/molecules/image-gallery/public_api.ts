8e7585f0fb57b48c4dff91c3ea97a0eb4a1b3afb
Damien Retzinger <damienwebdev@gmail.com>
2018-10-19 09:58:26 -0400

    refactor(design): cleanup public_apis for design modules

---
+++ libs/design/src/molecules/image-gallery/public_api.ts
@@ -0,0 +1,2 @@
+export { ImageGalleryComponent } from './image-gallery.component';
+export { DaffImageGalleryModule } from './image-gallery.module';
---
+++ libs/design/src/molecules/select-validator/public_api.ts
@@ -0,0 +1,2 @@
+export { DaffSelectValidatorModule } from './select-validator.module';
+export { SelectValidatorComponent } from './select-validator.component';
