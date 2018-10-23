d7f8fb7b4285f4920fc6c3657381b6da5b6eee38
xelaint <xelaint@gmail.com>
2018-10-23 11:17:30 -0400

    refactor(foundation-demo): move cart header into cart wrapper

--- apps/foundation-demo/src/app/cart/components/cart/cart.component.ts
+++ apps/foundation-demo/src/app/cart/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Cart } from '@daffodil/core';
 
 @Component({
@@ -6,20 +6,7 @@ import { Cart } from '@daffodil/core';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
 
   @Input() cart: Cart;
-  @Input() title: string;
-
-  hasOneItem: boolean = false;
-
-  constructor() { }
-
-  ngOnInit() {
-    this.hasOneItem = this.cart.items.length === 1;
-  }
-
-  get itemText() {
-    return this.hasOneItem ? 'Item' : 'Items';
-  }
 }
