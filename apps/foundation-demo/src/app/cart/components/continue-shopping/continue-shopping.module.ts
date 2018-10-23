682e74f40d59d25633ef51cdf431fed96d2cc1d9
xelaint <xelaint@gmail.com>
2018-10-23 11:16:09 -0400

    fix(foundation-demo): add router module to continue shopping and proceed to checkout modules

--- apps/foundation-demo/src/app/cart/components/continue-shopping/continue-shopping.module.ts
+++ apps/foundation-demo/src/app/cart/components/continue-shopping/continue-shopping.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContinueShoppingDirective } from './continue-shopping.directive';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     ContinueShoppingDirective
