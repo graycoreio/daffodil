e18b92016d9cb9ff8850edb2175c06d365ed337f
xelaint <xelaint@gmail.com>
2018-10-31 14:53:21 -0400

    feat(daffio): add header and mobile sidebar to app page

--- apps/daffio/src/app/app.component.ts
+++ apps/daffio/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'daffodil-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'daffio';
--- apps/daffio/src/app/app.module.ts
+++ apps/daffio/src/app/app.module.ts
@@ -2,11 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DaffioHeaderModule } from './core/header/header.module';
+import { StoreModule } from '@ngrx/store';
+import { DaffioSidebarModule } from './core/sidebar/sidebar.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DaffioHeaderModule,
+    StoreModule.forRoot({}),
+    DaffioSidebarModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {}
