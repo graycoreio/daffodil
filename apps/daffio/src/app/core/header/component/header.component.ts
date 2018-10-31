18f7a6e653ce8561f78223c24998100cbb599b76
xelaint <xelaint@gmail.com>
2018-10-31 14:54:41 -0400

    feat(daffio): add header core container and component

---
+++ apps/daffio/src/app/core/header/component/header.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'daffio-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DaffioHeaderComponent { }
