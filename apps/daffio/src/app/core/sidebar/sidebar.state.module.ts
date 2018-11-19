41ff31b79b38a0ae1b72516df3094cdfd2cd0b8f
xelaint <xelaint@gmail.com>
2018-11-19 16:43:04 -0500

    test(daffio): remove SidebarEffects from imports

--- apps/daffio/src/app/core/sidebar/sidebar.state.module.ts
+++ apps/daffio/src/app/core/sidebar/sidebar.state.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 
 import { reducers } from './reducers/index';
-import { EffectsModule } from '@ngrx/effects';
-import { SidebarEffects } from './effects/sidebar.effects';
 
 @NgModule({
   imports: [
-    StoreModule.forFeature('daffioSidebar', reducers),
-    EffectsModule.forFeature([SidebarEffects]),
+    StoreModule.forFeature('daffioSidebar', reducers)
   ]
 })
 export class DaffioSidebarStateModule { }
