c12164a598058e491ce40774f9ae1805c80b6194
xelaint <xelaint@gmail.com>
2018-11-20 18:44:17 -0500

    refactor(foundation-demo): remove foundation prefix from all selectors

--- apps/foundation-demo/src/app/core/header/components/header/header.component.ts
+++ apps/foundation-demo/src/app/core/header/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'foundation-header',
+  selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
