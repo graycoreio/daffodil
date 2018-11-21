f6924b6b2d4d50575d1d511726bab3e9840eae21
xelaint <xelaint@gmail.com>
2018-11-21 10:33:34 -0500

    feat(daffio): remove 404 routing

--- apps/daffio/src/app/app-routing.module.ts
+++ apps/daffio/src/app/app-routing.module.ts
@@ -7,10 +7,6 @@ export const appRoutes: Routes = [
     path: '', component: TemplateComponent, children: [
       { path: '', pathMatch: 'full', loadChildren: './content/homepage/homepage.module#DaffioHomepageModule'}
     ]
-  },
-  {
-    path: '**',
-    redirectTo: '/404'
   }
 ]
 
