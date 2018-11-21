53bc96fd69ac7802763bd67c76c607ef1aa024a0
xelaint <xelaint@gmail.com>
2018-11-21 14:08:33 -0500

    test(daffio): assert that router-outlet is included in the template and app template

--- apps/daffio/src/app/core/template/template.component.spec.ts
+++ apps/daffio/src/app/core/template/template.component.spec.ts
@@ -1,13 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TemplateComponent } from './template.component';
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 
 describe('TemplateComponent', () => {
   let component: TemplateComponent;
   let fixture: ComponentFixture<TemplateComponent>;
+  let sidebarViewport:DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,17 +29,19 @@ describe('TemplateComponent', () => {
     fixture = TestBed.createComponent(TemplateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    sidebarViewport = fixture.debugElement.query(By.css('sidebar-viewport-container'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render a `daffio-header-container` inside a `sidebar-viewport-container`', () => {
-    const fixture = TestBed.createComponent(TemplateComponent);
-
-    let sidebarViewport = fixture.debugElement.query(By.css('sidebar-viewport-container'));
-    
+  it('should render a <daffio-header-container> inside a <sidebar-viewport-container>', () => {
     expect(sidebarViewport.query(By.css('daffio-header-container'))).not.toBeNull()
-  })
+  });
+
+  it('should render a <router-outlet> inside a <sidebar-viewport-container>', () => {
+    expect(sidebarViewport.query(By.css('router-outlet'))).not.toBeNull()
+  });
 });
