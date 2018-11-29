c6d64b7f5b6c873ccca0bcfe5d927bd225b9863c
Damien Retzinger <damienwebdev@gmail.com>
2018-11-29 11:45:58 -0500

    style(foundation-demo): remove old comments

--- apps/foundation-demo/src/polyfills.ts
+++ apps/foundation-demo/src/polyfills.ts
@@ -40,10 +40,6 @@
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
 
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-
-
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
