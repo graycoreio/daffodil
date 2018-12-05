566b93f6d8d760801f5b1816aa81a2616b9222a0
Luke Derrick <lderrickable@gmail.com>
2018-12-05 09:57:07 -0500

    refactor(state): remove product files and any references to them from @daffodil/state
    remove product files and any references to them from @daffodil/state

--- libs/state/src/daffodil-state.module.ts
+++ libs/state/src/daffodil-state.module.ts
@@ -1,17 +1,14 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import { StateProductModule } from './product/product.module';
 import { StateCartModule } from './cart/cart.module';
 import { StateCheckoutModule } from './checkout/checkout.module';
 
 @NgModule({
   imports: [
-    StateProductModule,
     StateCartModule,
     StateCheckoutModule
   ],
   exports: [
-    StateProductModule,
     StateCartModule,
     StateCheckoutModule
   ]
