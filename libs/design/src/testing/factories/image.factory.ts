9674acb0d5896f7612e4c1ebf2b9ed33b2fb7c5a
Luke <luke@lukes-imac.lan>
2018-12-10 10:23:47 -0500

    feat(design): implement the new ModelFactory in images factory
    implement the new ModelFactory in images factory

--- libs/design/src/testing/factories/image.factory.ts
+++ libs/design/src/testing/factories/image.factory.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as faker from 'faker';
+import * as faker from 'faker/locale/en_US';
 import { Image } from '../../interfaces/image';
 
 @Injectable()
