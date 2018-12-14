d575a417669d5541df335a38f2926a2b1ba0f4dd
xelaint <xelaint@gmail.com>
2018-12-14 13:25:46 -0500

    refactor(design): move title and subtitle into subfolders

--- libs/design/src/molecules/hero/public_api.ts
+++ libs/design/src/molecules/hero/public_api.ts
@@ -1,4 +1,4 @@
 export { DaffHeroModule } from './hero.module';
 export { DaffHeroComponent } from './hero.component';
-export { DaffHeroTitleComponent } from './hero-title.component';
-export { DaffHeroSubtitleComponent } from './hero-subtitle.component';
+export { DaffHeroTitleComponent } from './hero-title/hero-title.component';
+export { DaffHeroSubtitleComponent } from './hero-subtitle/hero-subtitle.component';
