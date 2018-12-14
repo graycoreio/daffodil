5d5ceeee13096bbb5363fa58b60a5ce3e16aac85
xelaint <xelaint@gmail.com>
2018-12-14 17:49:47 -0500

    refactor(design): add enums and types to daff-hero inputs

--- libs/design/src/molecules/hero/hero.component.spec.ts
+++ libs/design/src/molecules/hero/hero.component.spec.ts
@@ -1,30 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { DaffHeroComponent } from './hero.component';
-import { By } from '@angular/platform-browser';
 import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { DaffHeroComponent, DaffHeroLayout, DaffHeroLayoutEnum, DaffHeroSize, DaffHeroSizeEnum } from './hero.component';
 import { DaffPalette } from '../../core/colorable/colorable';
 
-@Component ({
-  template: `
-    <daff-hero [layout]="layout" [size]="size" [color]="color"></daff-callout>
-  `
+@Component({
+  template: `<daff-hero [layout]="layout" [size]="size" [color]="color"></daff-hero>`
 })
-class Wrapper {
+class WrapperComponent {
+  layout: DaffHeroLayout;
+  size: DaffHeroSize;
   color: DaffPalette;
-  layout: string;
-  size: string;
 }
 
-fdescribe('DaffHeroComponent', () => {
-  let wrapper: Wrapper;
+describe('DaffHeroComponent', () => {
+  let wrapper: WrapperComponent;
   let component: DaffHeroComponent;
-  let fixture: ComponentFixture<Wrapper>;
   let de: DebugElement;
+  let fixture: ComponentFixture<WrapperComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        Wrapper,
+      declarations: [ 
+        WrapperComponent,
         DaffHeroComponent
       ]
     })
@@ -32,21 +31,20 @@ fdescribe('DaffHeroComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(Wrapper);
-    wrapper = fixture.componentInstance;
-    component = fixture.debugElement.query(By.css('daff-hero')).componentInstance;
+    fixture = TestBed.createComponent(WrapperComponent);
+    wrapper = fixture.debugElement.componentInstance;
     de = fixture.debugElement.query(By.css('daff-hero'));
-
+    component = de.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
+  }); 
 
   describe('using a layout variant of a callout', () => {
     it('should set a layout class on the callout', () => {
-      wrapper.layout = "centered";
+      wrapper.layout = DaffHeroLayoutEnum.Centered;
       fixture.detectChanges();
 
       expect(de.nativeElement.classList.contains('daff-hero--centered')).toEqual(true);
@@ -59,7 +57,7 @@ fdescribe('DaffHeroComponent', () => {
 
   describe('using a size variant of a hero', () => {
     it('should set a size class on the hero', () => {
-      wrapper.size = "small";
+      wrapper.size = DaffHeroSizeEnum.Small;;
       fixture.detectChanges();
 
       expect(de.nativeElement.classList.contains('daff-hero--small')).toEqual(true);
@@ -70,7 +68,7 @@ fdescribe('DaffHeroComponent', () => {
     });
   });
 
-  describe('using a colored variant of a hero',() => {
+  describe('using a color variant of a hero',() => {
     it('should set a color class on the hero', () => {
       wrapper.color = "primary";
       fixture.detectChanges();
--- libs/design/src/molecules/hero/public_api.ts
+++ libs/design/src/molecules/hero/public_api.ts
@@ -1,4 +1,4 @@
 export { DaffHeroModule } from './hero.module';
-export { DaffHeroComponent } from './hero.component';
+export { DaffHeroComponent, DaffHeroLayout, DaffHeroLayoutEnum, DaffHeroSize, DaffHeroSizeEnum } from './hero.component';
 export { DaffHeroTitleComponent } from './hero-title/hero-title.component';
 export { DaffHeroSubtitleComponent } from './hero-subtitle/hero-subtitle.component';
