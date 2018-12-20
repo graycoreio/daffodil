d24373500f0282551862caf61670ece0056c8490
xelaint <xelaint@gmail.com>
2018-12-20 11:09:14 -0500

    feat(daffio): update daffio header styles

--- apps/daffio/src/app/core/header/component/header.component.ts
+++ apps/daffio/src/app/core/header/component/header.component.ts
@@ -1,9 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'daffio-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./header.component.scss']
 })
+
 export class DaffioHeaderComponent { }
