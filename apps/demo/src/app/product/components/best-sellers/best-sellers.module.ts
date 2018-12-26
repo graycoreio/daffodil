c02eb8ced5578e245c85982ffda9b111bbffd6c4
Luke Derrick <lderrickable@gmail.com>
2018-12-26 09:00:28 -0500

    refactor(foundation-demo): remove demo-loading-icon
    remove demo-loading-icon

--- apps/demo/src/app/product/components/best-sellers/best-sellers.module.ts
+++ apps/demo/src/app/product/components/best-sellers/best-sellers.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoadingIconModule } from '../../../core/loading-icon/loading-icon.module';
+import { DaffLoadingIconModule } from '@daffodil/design';
 import { ProductGridModule } from '../product-grid/product-grid.module';
 import { BestSellersComponent } from './best-sellers.component';
 import { StateProductModule } from '@daffodil/state';
@@ -9,7 +9,7 @@ import { StateProductModule } from '@daffodil/state';
 @NgModule({
   imports: [
     CommonModule,
-    LoadingIconModule,
+    DaffLoadingIconModule,
     ProductGridModule,
     StateProductModule
   ],
--- apps/demo/src/app/product/pages/product-view/product-view.module.ts
+++ apps/demo/src/app/product/pages/product-view/product-view.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { StateProductModule } from '@daffodil/state';
 
-import { LoadingIconModule } from '../../../core/loading-icon/loading-icon.module';
+import { DaffLoadingIconModule } from '@daffodil/design';
 import { ProductComponentModule } from '../../components/product/product.module';
 import { AddToCartModule } from '../../components/add-to-cart/add-to-cart.module';
 import { AddToCartNotificationModule } from '../../../modals/add-to-cart-notification/add-to-cart-notification.module';
@@ -12,7 +12,7 @@ import { ProductViewComponent } from './product-view.component';
 @NgModule({
   imports: [
     CommonModule,
-    LoadingIconModule,
+    DaffLoadingIconModule,
     ProductComponentModule,
     AddToCartModule,
     AddToCartNotificationModule,
