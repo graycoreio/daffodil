1b1ec78012c42c11cbffeb3e8d58d94d2187a68e
xelaint <xelaint@gmail.com>
2018-12-27 15:22:55 -0500

    feat(daffio): update link path

--- apps/daffio/src/app/core/footer/footer.component.spec.ts
+++ apps/daffio/src/app/core/footer/footer.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 
 import { DaffioFooterComponent } from './footer.component';
@@ -20,6 +21,7 @@ describe('DaffioFooterComponent', () => {
         DaffioFooterComponent
       ],
       imports: [
+        RouterTestingModule,
         DaffContainerModule,
         DaffioLogoModule,
         DaffListModule,
