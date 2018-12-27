363e854d0493d320c384875cdc2305b4d00f0326
xelaint <xelaint@gmail.com>
2018-12-27 15:28:34 -0500

    feat(daffio): add animate on scroll library

--- apps/daffio/src/main.ts
+++ apps/daffio/src/main.ts
@@ -5,11 +5,16 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import * as WebFont from 'webfontloader';
+import * as AOS from 'aos';
 
 WebFont.load({
   typekit: {
     id: 'hle3nby'
   }
+});
+
+AOS.init({
+  once: 'true',
 });
 
 if (environment.production) {
