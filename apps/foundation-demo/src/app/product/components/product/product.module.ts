0f24d87363ff95d3451a273266f5930ea4339848
xelaint <xelaint@gmail.com>
2019-01-04 11:48:34 -0500

    style(demo): update product page styles and sample text

--- apps/foundation-demo/src/app/product/components/product/product.module.ts
+++ apps/foundation-demo/src/app/product/components/product/product.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DaffQtyDropdownModule, DaffAccordionModule, DaffContainerModule } from '@daffodil/design';
-
 import { LoadingIconModule } from '../../../core/loading-icon/loading-icon.module';
 import { ImageGalleryModule } from '../../../core/image-gallery/image-gallery.module';
 import { ProductComponent } from './product.component';
+
+import {
+  DaffQtyDropdownModule,
+  DaffAccordionModule,
+  DaffContainerModule,
+  DaffLinkModule
+} from '@daffodil/design';
 
 @NgModule({
   imports: [
@@ -14,7 +19,8 @@ import { ProductComponent } from './product.component';
     LoadingIconModule,
     DaffQtyDropdownModule,
     DaffAccordionModule,
-    DaffContainerModule
+    DaffContainerModule,
+    DaffLinkModule
   ],
   declarations: [
     ProductComponent
