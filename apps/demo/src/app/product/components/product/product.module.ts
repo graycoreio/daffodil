05d934ac17435754a5b1fdc3fd967f17cfb47b31
Damien Retzinger <damienwebdev@gmail.com>
2019-01-08 13:58:14 -0500

    fix(demo): remove duplicate identifier from rebase

--- apps/demo/src/app/product/components/product/product.module.ts
+++ apps/demo/src/app/product/components/product/product.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DaffQtyDropdownModule, DaffAccordionModule, DaffContainerModule, DaffLoadingIconModule } from '@daffodil/design';
 
 import { ImageGalleryModule } from '../../../core/image-gallery/image-gallery.module';
 import { ProductComponent } from './product.component';
 
-import {
-  DaffQtyDropdownModule,
-  DaffAccordionModule,
-  DaffContainerModule,
-  DaffLinkModule
-} from '@daffodil/design';
+import { DaffQtyDropdownModule, 
+  DaffLinkModule,
+  DaffAccordionModule, 
+  DaffContainerModule, 
+  DaffLoadingIconModule } from '@daffodil/design';
 
 @NgModule({
   imports: [
