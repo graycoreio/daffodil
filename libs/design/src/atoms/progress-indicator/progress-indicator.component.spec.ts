567a3dbf6664d69b04521cd7ef8012e6499e0b54
Damien Retzinger <damienwebdev@gmail.com>
2019-01-25 13:06:37 -0500

    feat(design): add tests for progress indicator and make progress indicator match spec

--- libs/design/src/atoms/progress-indicator/progress-indicator.component.spec.ts
+++ libs/design/src/atoms/progress-indicator/progress-indicator.component.spec.ts
@@ -1,22 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed, flush, fakeAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DaffPalette } from '../../core/colorable/colorable';
 
 import { DaffProgressIndicatorComponent } from './progress-indicator.component';
 
+@Component({
+  template: `
+  <daff-progress-indicator [color]="color" [percentage]="percentage" (finished)="onComplete()">
+  </daff-progress-indicator>
+  `
+})
+class WrapperComponent {
+  color: DaffPalette;
+  percentage: number;
+  onComplete(): void {};
+}
+
 describe('DaffProgressIndicatorComponent', () => {
+  let wrapper: WrapperComponent;
   let component: DaffProgressIndicatorComponent;
-  let fixture: ComponentFixture<DaffProgressIndicatorComponent>;
+  let de: DebugElement;
+  let fixture: ComponentFixture<WrapperComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DaffProgressIndicatorComponent ]
+      imports: [
+        NoopAnimationsModule
+      ],
+      declarations: [ 
+        WrapperComponent,
+        DaffProgressIndicatorComponent 
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DaffProgressIndicatorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(WrapperComponent);
+    wrapper = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('daff-progress-indicator'));
+    component = de.componentInstance;
   });
 
   it('should create', () => {
@@ -24,8 +50,33 @@ describe('DaffProgressIndicatorComponent', () => {
   });
 
   it('should be able to take `percentage` as an input', () =>{
-
+    wrapper.percentage = 20;
+    fixture.detectChanges();
+    expect(component.percentage).toEqual(20);
   });
 
-  it('should ')
+  /**
+   * This needs to be in a fakeAsync as there are animationFrames being processed
+   */
+  it('should emit `finished` when the progress bar is filled and the animation is complete', fakeAsync(() => {
+    wrapper.percentage = 100;
+    spyOn(wrapper, 'onComplete');
+   
+    //console.log(wrapper.onComplete);
+    fixture.detectChanges();
+    flush();
+
+    expect(wrapper.onComplete).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should be unfilled by default', () => {
+    expect(component.percentage).toEqual(0);
+  });
+
+  it('should be colorable', () => {
+    wrapper.color = "primary";
+    fixture.detectChanges();
+    expect(component.color).toEqual("primary");
+    expect(de.nativeElement).toHaveClass("daff-primary");
+  });
 });
