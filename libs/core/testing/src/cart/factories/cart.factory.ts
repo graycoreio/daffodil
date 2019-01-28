6b05f60e3da312abd823c95f11d036af157fcf09
Luke <lderrickable@gmail.com>
2019-01-28 17:55:58 -0500

    fix(core): make all randomly generated prices under 1000
    make all randomly generated prices under 1000 to make formatted prices easier to test

--- libs/core/testing/src/cart/factories/cart.factory.ts
+++ libs/core/testing/src/cart/factories/cart.factory.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import * as faker from 'faker/locale/en_US';
 
-import { Cart, CartItem, CartAddress} from '@daffodil/core';
+import { Cart } from '@daffodil/core';
 import { ModelFactory } from '../../factories/factory';
 
 export class MockCart implements Cart {
   id = faker.random.number(1000);
   created_at = new Date();
   updated_at = new Date();
-  store_to_base_rate = faker.random.number(1000);
-  grand_total = faker.random.number(1000);
+  store_to_base_rate = faker.random.number(999);
+  grand_total = faker.random.number(999);
   checkout_method = 'card';
   customer_id = faker.random.number(1000);
   coupon_code = faker.random.number(100000).toString();
-  subtotal = faker.random.number(1000);
-  subtotal_with_discount = faker.random.number(1000);
+  subtotal = faker.random.number(999);
+  subtotal_with_discount = faker.random.number(999);
   items = [];
   addresses = [];
   payment = null;
