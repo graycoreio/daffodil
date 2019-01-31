bc22adbcca7bdb79b61732ca9f93196a82c0e8ba
Damien Retzinger <damienwebdev@gmail.com>
2019-01-31 09:46:47 -0500

    testing(design): add new design land app to e2e test design components

---
+++ apps/design-land/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+export const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/loading-icon',
+    pathMatch: 'full'
+  },
+  {path: 'loading-icon', loadChildren: './loading-icon/loading-icon.module#LoadingIconModule'}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class DesignLandAppRoutingModule {}
