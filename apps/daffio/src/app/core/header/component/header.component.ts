191174e6d59cce834a36e861c591b4f738d1f500
xelaint <xelaint@gmail.com>
2019-01-30 22:11:31 -0500

    feat(daffio): update header host styles

--- apps/daffio/src/app/core/header/component/header.component.ts
+++ apps/daffio/src/app/core/header/component/header.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'daffio-header',
+  host: {'class': 'daffio-header'},
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   encapsulation: ViewEncapsulation.None
