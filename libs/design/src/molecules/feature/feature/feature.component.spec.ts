504a6a51875fa0ea71ee2292dbd8277addc888ba
xelaint <xelaint@gmail.com>
2019-02-04 15:49:47 -0600

    feat(design): add modes to feature component

--- libs/design/src/molecules/feature/feature/feature.component.spec.ts
+++ libs/design/src/molecules/feature/feature/feature.component.spec.ts
@@ -1,14 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-import { DaffFeatureComponent } from './feature.component';
+import { DaffFeatureComponent, DaffFeatureMode, DaffFeatureModeEnum } from './feature.component';
 
-describe('DaffFeatureComponent', () => {
+@Component({
+  template: `<daff-feature [mode]="mode"></daff-feature>`
+})
+
+class WrapperComponent {
+  mode: DaffFeatureMode;
+}
+
+fdescribe('DaffFeatureComponent', () => {
+  let wrapper: WrapperComponent;
   let component: DaffFeatureComponent;
-  let fixture: ComponentFixture<DaffFeatureComponent>;
+  let de: DebugElement;
+  let fixture: ComponentFixture<WrapperComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
+      declarations: [ 
+        WrapperComponent,
         DaffFeatureComponent
       ]
     })
@@ -16,9 +29,10 @@ describe('DaffFeatureComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DaffFeatureComponent);
-    component = fixture.componentInstance;
-
+    fixture = TestBed.createComponent(WrapperComponent);
+    wrapper = fixture.debugElement.componentInstance;
+    de = fixture.debugElement.query(By.css('daff-feature'));
+    component = de.componentInstance;
     fixture.detectChanges();
   });
 
@@ -27,6 +41,42 @@ describe('DaffFeatureComponent', () => {
   });
 
   it('should add a class of `daff-feature` to its host', () => {
-    expect(fixture.nativeElement.classList.contains('daff-feature')).toBeTruthy();
+    expect(de.nativeElement.classList.contains('daff-feature')).toBeTruthy();
+  });
+
+  describe('setting the mode of the feature', () => {
+    it('should set default mode to normal', () => {
+      wrapper.mode = DaffFeatureModeEnum.Normal;
+      fixture.detectChanges();
+
+      expect(de.nativeElement.classList.contains('daff-feature--normal')).toEqual(true);
+    });
+
+    describe('when mode is compact', () => {
+      it('should set "daff-feature--compact" on host element', () => {
+        wrapper.mode = DaffFeatureModeEnum.Compact;
+        fixture.detectChanges();
+
+        expect(de.nativeElement.classList.contains('daff-feature--compact')).toEqual(true);
+      });
+    });
+
+    describe('when mode is normal', () => {
+      it('should set "daff-feature--normal" on host element', () => {
+        wrapper.mode = DaffFeatureModeEnum.Normal;
+        fixture.detectChanges();
+  
+        expect(de.nativeElement.classList.contains('daff-feature--normal')).toEqual(true);
+      });
+    });
+
+    describe('when mode is undefined', () => {
+      it('should set "daff-feature--normal" as default', () => {
+        wrapper.mode = DaffFeatureModeEnum.Normal;
+        fixture.detectChanges();
+
+        expect(de.nativeElement.classList.contains('daff-feature--normal')).toEqual(true);
+      });
+    });
   });
 });
