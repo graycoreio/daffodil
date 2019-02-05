38d6353e8e06a05c23f92e8fe904093546bbbc73
Damien Retzinger <damienwebdev@gmail.com>
2019-02-05 14:24:15 -0500

    feat(design): update design land with feature implementation

---
+++ apps/design-land/src/app/feature/feature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'design-land-feature',
+  templateUrl: './feature.component.html',
+  styleUrls: ['./feature.component.scss']
+})
+export class FeatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
