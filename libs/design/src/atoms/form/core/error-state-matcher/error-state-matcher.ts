1288d94fe28879f5e01f60ecb8cd253d55e3cc93
Luke Derrick <lderrickable@gmail.com>
2019-02-06 15:11:11 -0500

    refactor(design): move form components into a common folder
    move form components into a common folder

--- libs/design/src/atoms/form/core/error-state-matcher/error-state-matcher.ts
+++ libs/design/src/atoms/form/core/error-state-matcher/error-state-matcher.ts
@@ -1,6 +1,6 @@
 import { FormControl } from "@angular/forms";
 
-export class ErrorStateMatcher {
+export class DaffErrorStateMatcher {
   
   isErrorState(control: FormControl, formSubmitted: boolean): boolean {
     return control.errors && (control.touched || formSubmitted);
