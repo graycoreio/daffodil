fd72ea964e39026f9d624a32ae6f1fe04e4bea06
xelaint <xelaint@gmail.com>
2019-02-07 11:25:51 -0500

    test(design): add tests to title and subtitle

--- libs/design/src/molecules/hero/hero-subtitle/hero-subtitle.directive.spec.ts
+++ libs/design/src/molecules/hero/hero-subtitle/hero-subtitle.directive.spec.ts
@@ -1,8 +1,44 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { DaffHeroSubtitleDirective } from './hero-subtitle.directive';
 
+@Component({
+  template: `
+    <h1 daffHeroSubtitle>Lorem Ipsum</h1>
+  `
+})
+class WrapperComponent {}
+
 describe('DaffHeroSubtitleDirective', () => {
-  it('should create an instance', () => {
-    const component = new DaffHeroSubtitleDirective();
-    expect(component).toBeTruthy();
+  let wrapper: WrapperComponent;
+  let heroSubtitle: DebugElement;
+  let fixture: ComponentFixture<WrapperComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DaffHeroSubtitleDirective,
+        WrapperComponent
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WrapperComponent);
+    heroSubtitle = fixture.debugElement.query(By.css('[daffHeroSubtitle]'));
+    wrapper = heroSubtitle.componentInstance;
+    fixture.detectChanges();
   });
-});
+
+  it('should create', () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  describe('[daffHeroSubtitle]',() => {
+    it('should add a class of `daff-hero__subtitle` to its host element', () => {
+      expect(heroSubtitle.nativeElement.classList.contains('daff-hero__subtitle')).toEqual(true);
+    });
+  });
+}); 
