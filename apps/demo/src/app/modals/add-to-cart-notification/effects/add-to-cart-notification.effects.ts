7668ae2045a99dee2f9efd539b233b1e792f81ef
Luke <lderrickable@gmail.com>
2019-02-13 14:53:37 -0500

    feat(demo): remove delayed close of add-to-cart-notification; fix bug where loading gif would show on mobile
    remove delayed close of add-to-cart-notification; fix bug where loading gif would show on mobile

--- apps/demo/src/app/modals/add-to-cart-notification/effects/add-to-cart-notification.effects.ts
+++ apps/demo/src/app/modals/add-to-cart-notification/effects/add-to-cart-notification.effects.ts
@@ -1,31 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { map, delay } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import { CartActionTypes } from '@daffodil/state';
 
-import { CloseAddToCartNotification, OpenAddToCartNotification } from '../actions/add-to-cart-notification.actions';
+import { OpenAddToCartNotification } from '../actions/add-to-cart-notification.actions';
 
 @Injectable()
 export class AddToCartNotificationEffects {
 
   constructor(private actions$: Actions) {}
-
-  @Effect()
-  productAddedToCart$ = 
-    this.actions$.pipe(
-      ofType(CartActionTypes.AddToCartSuccessAction),
-        delay(3000),
-        map(() => {
-          return new CloseAddToCartNotification();
-        })
-    )
-
+    
   @Effect()
   addToCart$ = this.actions$.pipe(
     ofType(CartActionTypes.AddToCartAction),
-    map(() => {
-      return new OpenAddToCartNotification();
-    })
+    map(() => new OpenAddToCartNotification())
   )
 }
