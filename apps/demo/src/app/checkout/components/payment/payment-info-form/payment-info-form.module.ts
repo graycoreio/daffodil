d519209a7d6f13ca78565ae7e64e88d09a9119b5
Luke <lderrickable@gmail.com>
2019-02-15 17:48:25 -0500

    refactor(demo): update daff-input components with form-field

--- apps/demo/src/app/checkout/components/payment/payment-info-form/payment-info-form.module.ts
+++ apps/demo/src/app/checkout/components/payment/payment-info-form/payment-info-form.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { PaymentInfoFormComponent } from './components/payment-info-form/payment-info-form.component';
-import { DaffInputModule, DaffSelectModule } from '@daffodil/design';
+import { DaffInputModule, DaffSelectModule, DaffFormFieldModule } from '@daffodil/design';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     DaffInputModule,
-    DaffSelectModule
+    DaffSelectModule,
+    DaffFormFieldModule
   ],
   declarations: [
     PaymentInfoFormComponent
