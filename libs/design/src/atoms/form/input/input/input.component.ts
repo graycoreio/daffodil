e77a8b047b8457bc05408f19f406f9389f009309
Luke <lderrickable@gmail.com>
2019-02-15 17:43:56 -0500

    feat(design): add daff-form-field and daff-error-message for form control components

--- libs/design/src/atoms/form/input/input/input.component.ts
+++ libs/design/src/atoms/form/input/input/input.component.ts
@@ -1,8 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, Input, HostBinding, DoCheck } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   // tslint:disable-next-line: component-selector
-  selector: 'input[daff-input] ',
+  selector: 'input[daff-input]',
   template: '<ng-content></ng-content>',
   styleUrls: ['./input.component.scss'],
   host: {
@@ -10,4 +11,18 @@ import { Component, ViewEncapsulation } from '@angular/core';
   },
   encapsulation: ViewEncapsulation.None,
 })
-export class DaffInputComponent {}
+export class DaffInputComponent implements DoCheck {
+
+  //todo: will be removed once we upgrade to Angular 8, where markAllAsTouched will be a method on FormControl.
+  @Input() formSubmitted: boolean;
+  @Input() formControl: FormControl;
+  @HostBinding('class.daff-input__error') isError: boolean;
+  @HostBinding('class.daff-input__valid') isValid: boolean;
+
+  ngDoCheck() {
+    if(this.formControl) {
+      this.isError = this.formControl.errors && (this.formControl.touched || this.formSubmitted);
+      this.isValid = !this.formControl.errors && this.formControl.touched;
+    }
+  }
+}
