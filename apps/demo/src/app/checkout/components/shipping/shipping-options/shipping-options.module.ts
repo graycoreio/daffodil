a95a84157d0eca9e6617863c854d3e569eb2cb89
xelaint <xelaint@gmail.com>
2019-02-15 12:56:54 -0500

    feat(demo): update radio buttons

--- apps/demo/src/app/checkout/components/shipping/shipping-options/shipping-options.module.ts
+++ apps/demo/src/app/checkout/components/shipping/shipping-options/shipping-options.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShippingOptionsComponent } from './components/shipping-options/shipping-options.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DaffRadioModule } from '@daffodil/design';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DaffRadioModule
   ],
   declarations: [
     ShippingOptionsComponent
