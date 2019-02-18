6280061ae95d87b5fb06e2282c8a9ed8c97748be
Luke <lderrickable@gmail.com>
2019-02-18 18:43:03 -0500

    refactor(product): add core/testing product factories to product/testing module

---
+++ libs/product/testing/src/helpers/product-helper.ts
@@ -0,0 +1,14 @@
+import { Product } from "@daffodil/product";
+
+/**
+ * A helper function to verify that a model is a Product.
+ * @param product
+ */
+export function isProduct(product: Product): boolean {
+  return !!product.id 
+    && !!product.price 
+    && !!product.name 
+    && !!product.brand 
+    && !!product.description 
+    && !!product.images;
+}
---
+++ libs/product/testing/src/index.ts
@@ -0,0 +1,4 @@
+export { DaffProductFactory } from "./factories/product.factory";
+export { DaffProductImageFactory } from "./factories/product-image.factory";
+import * as productHelper from './helpers/product-helper';
+export { productHelper };
