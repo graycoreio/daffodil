c1425109551918f6c40954d3bf4bf26112b72ca9
Luke <lderrickable@gmail.com>
2019-02-20 11:21:37 -0500

    feat(core): add order models and factories

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -7,6 +7,12 @@ export { CartAddress } from './cart/cart-address';
 export { CartPayment } from './cart/cart-payment';
 export { CartShippingRate } from './cart/cart-shipping-rate';
 
+export { Order } from './order/order';
+export { OrderItem } from './order/order-item';
+export { OrderAddress } from './order/order-address';
+export { OrderPayment } from './order/order-payment';
+export { OrderShippingRate } from './order/order-shipping-rate';
+
 export { PaymentInfo } from './payment/payment-info';
 
 export { ShippingRate } from './shipping/shipping-rate';
