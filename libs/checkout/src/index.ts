1ac95774ea4896eb35d1e1ba0b82e37c87c57e9e
Luke <lderrickable@gmail.com>
2019-02-20 17:13:40 -0500

    refactor(checkout): move core checkout models into checkout module

---
+++ libs/checkout/src/index.ts
@@ -0,0 +1,10 @@
+export { Order } from './models/order/order';
+export { OrderItem } from './models/order/order-item';
+export { OrderAddress } from './models/order/order-address';
+export { OrderPayment } from './models/order/order-payment';
+export { OrderShippingRate } from './models/order/order-shipping-rate';
+
+export { PaymentInfo } from './models/payment/payment-info';
+
+export { ShippingRate } from './models/shipping/shipping-rate';
+export { ShippingOption } from './models/shipping/shipping-option';
