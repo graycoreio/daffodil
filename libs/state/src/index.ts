391e24895451ecb147ac39ee3670fb89ecc8dffc
Luke <lderrickable@gmail.com>
2019-02-21 13:10:23 -0500

    delete(state): remove product components from state

--- libs/state/src/index.ts
+++ libs/state/src/index.ts
@@ -1,11 +1,7 @@
 export { DaffodilStateModule } from './daffodil-state.module';
 
-export { StateProductModule } from './product/product.module';
 export { StateCartModule } from './cart/cart.module';
 export { StateCheckoutModule } from './checkout/checkout.module';
-
-export { ProductGridContainer } from './product/containers/product-grid/product-grid.component';
-export { BestSellersContainer } from './product/containers/best-sellers/best-sellers.component';
 
 export { CartContainer } from './cart/containers/cart/cart.component';
 export * from './cart/actions/cart.actions';
@@ -13,9 +9,6 @@ import * as fromCart from './cart/reducers/index';
 export { fromCart };
 import * as fromCartReducer from './cart/reducers/cart.reducer';
 export { fromCartReducer };
-
-import * as fromProduct from './product/reducers/index';
-export { fromProduct };
 
 export { ShippingContainer } from './checkout/shipping/containers/shipping.component';
 export { PaymentContainer } from './checkout/payment/containers/payment.component';
