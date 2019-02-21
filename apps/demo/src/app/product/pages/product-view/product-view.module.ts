c17739ead751b7b119eace89c6f9752af85e2aac
Luke <lderrickable@gmail.com>
2019-02-21 13:13:44 -0500

    refactor(demo): use product module instead of state module for product components

--- apps/demo/src/app/product/pages/product-view/product-view.module.ts
+++ apps/demo/src/app/product/pages/product-view/product-view.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { StateProductModule, StateCartModule } from '@daffodil/state';
-
+import { StateCartModule } from '@daffodil/state';
+import { StateProductModule } from '@daffodil/product';
 import { DaffLoadingIconModule } from '@daffodil/design';
+
 import { ProductComponentModule } from '../../components/product/product.module';
 import { AddToCartModule } from '../../components/add-to-cart/add-to-cart.module';
 import { AddToCartNotificationModule } from '../../../modals/add-to-cart-notification/add-to-cart-notification.module';
