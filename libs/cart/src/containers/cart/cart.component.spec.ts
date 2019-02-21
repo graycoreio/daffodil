fe0779ee60f85386fcaca32e19ecce98d2050d36
Luke <lderrickable@gmail.com>
2019-02-21 11:40:44 -0500

    refactor(cart): move state cart components to cart module

---
+++ libs/cart/src/containers/cart/cart.component.spec.ts
@@ -0,0 +1,80 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Store, StoreModule, combineReducers } from '@ngrx/store';
+
+import { CartContainer } from './cart.component';
+import { CartLoad, AddToCart } from '../../actions/cart.actions';
+import * as fromCart from '../../reducers/index';
+import { Cart } from '../../models/cart';
+import { DaffCartFactory } from '../../../testing/src/factories/cart.factory';
+
+describe('CartContainer', () => {
+  let component: CartContainer;
+  let fixture: ComponentFixture<CartContainer>;
+  let store;
+  let initialLoading: boolean;
+  let initialCart: Cart;
+  let cartFactory: DaffCartFactory;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({
+          carts: combineReducers(fromCart.reducers),
+        })
+      ],
+      declarations: [ CartContainer ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    cartFactory = TestBed.get(DaffCartFactory);
+    fixture = TestBed.createComponent(CartContainer);
+    component = fixture.componentInstance;
+    store = TestBed.get(Store);
+
+    initialLoading = false;
+    initialCart = cartFactory.create();
+
+    spyOn(fromCart, 'selectCartLoadingState').and.returnValue(initialLoading);
+    spyOn(fromCart, 'selectCartValueState').and.returnValue(initialCart);
+    spyOn(store, 'dispatch');
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('ngInit', () => {
+    
+    it('dispatches a CartLoad action', () => {
+      expect(store.dispatch).toHaveBeenCalledWith(new CartLoad());
+    });
+
+    it('initializes loading$', () => {
+      component.loading$.subscribe((loading) => {
+        expect(loading).toEqual(initialLoading);
+      });
+    });
+
+    it('initializes cart$', () => {
+      component.cart$.subscribe((cart) => {
+        expect(cart).toEqual(initialCart);
+      });
+    });
+  });
+
+  describe('addToCart', () => {
+    
+    it('should call store.dispatch', () => {
+      const qty = 3;
+      const payload = {productId: '', qty: qty};
+      component.addToCart(payload);
+
+      expect(store.dispatch).toHaveBeenCalledWith(new AddToCart(payload));
+    });
+  });
+});
---
+++ libs/cart/src/reducers/index.ts
@@ -0,0 +1,36 @@
+import { ActionReducerMap, createSelector, createFeatureSelector, MemoizedSelector } from '@ngrx/store';
+
+import * as fromCart from './cart.reducer';
+import { Cart } from '../models/cart';
+
+export interface CartState {
+  cart: fromCart.State;
+}
+
+export interface State {
+  cart: CartState
+}
+
+export const reducers : ActionReducerMap<CartState> = {
+  cart: fromCart.reducer
+}
+
+/**
+ * Cart State
+ */
+export const selectCartState = createFeatureSelector<CartState>('cart');
+
+export const cartStateSelector = createSelector(
+  selectCartState,
+  (state: CartState) => state.cart
+)
+
+export const selectCartValueState : MemoizedSelector<object, Cart> = createSelector(
+  cartStateSelector,
+  fromCart.getCart
+)
+
+export const selectCartLoadingState : MemoizedSelector<object, boolean> = createSelector(
+  cartStateSelector,
+  fromCart.getCartLoading
+)
