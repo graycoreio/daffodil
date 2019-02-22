777aae32999d989ccf6c77ae6538bf778508ccc1
Luke <lderrickable@gmail.com>
2019-02-22 13:03:55 -0500

    delete(state): remove cart components from state

--- libs/state/src/index.ts
+++ libs/state/src/index.ts
@@ -1,14 +1,6 @@
 export { DaffodilStateModule } from './daffodil-state.module';
 
-export { StateCartModule } from './cart/cart.module';
 export { StateCheckoutModule } from './checkout/checkout.module';
-
-export { CartContainer } from './cart/containers/cart/cart.component';
-export * from './cart/actions/cart.actions';
-import * as fromCart from './cart/reducers/index';
-export { fromCart };
-import * as fromCartReducer from './cart/reducers/cart.reducer';
-export { fromCartReducer };
 
 export { ShippingContainer } from './checkout/shipping/containers/shipping.component';
 export { PaymentContainer } from './checkout/payment/containers/payment.component';
