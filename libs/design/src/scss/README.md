73fa89f0dcbf7ee803ee3bf1ef76ba7f8635e720
Damien Retzinger <damienwebdev@gmail.com>
2019-02-28 15:07:19 -0500

    feat(design,demo,daffio): create end-user theming implementation

--- libs/design/src/scss/README.md
+++ libs/design/src/scss/README.md
@@ -1,2 +1,7 @@
-`daff-util.scss`
-Utility library that contains all the different mixins and functions.
+# `@daffodil/design` SCSS Utilities
+
+`@daffodil/design` provides several utility files for rapid application development. 
+
+* `daff-util.scss` - A utility library containing things like breakpoints and sass language additions
+* `daff-typography.scss` - A typographic library
+* `daff-theme.scss` - A utility library that allows you to theme `@daffodil/design` components
---
+++ libs/design/src/test_scss.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const sassTrue = require('sass-true')
+const glob = require('glob');
+
+describe('Sass', () => {
+
+  // Find all of the Sass files that end in `*.spec.scss` in any directory in this project.
+  // I use path.resolve because True requires absolute paths to compile test files.
+  const sassTestFiles = glob.sync(path.resolve(__dirname, './**/*.spec.scss'));
+
+  // Run True on every file found with the describe and it methods provided
+  sassTestFiles.forEach(file =>
+    sassTrue.runSass({ file }, describe, it)
+  )
+})
