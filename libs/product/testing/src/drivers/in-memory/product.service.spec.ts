e26b5910b6473ca05352919a3b1ed8dbd568f9f2
Luke <lderrickable@gmail.com>
2019-03-03 18:09:49 -0500

    refactor(product): copy driver product services to product module

---
+++ libs/product/testing/src/drivers/in-memory/product.service.spec.ts
@@ -0,0 +1,80 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { DaffInMemoryProductService } from './product.service';
+import { DaffProductFactory } from '../../factories/product.factory';
+
+describe('Driver | InMemory | Product | ProductService', () => {
+  let productService;
+  let httpMock: HttpTestingController;
+  let productFactory: DaffProductFactory;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        DaffInMemoryProductService
+      ]
+    });
+
+    httpMock = TestBed.get(HttpTestingController);
+    productService = TestBed.get(DaffInMemoryProductService);
+    productFactory = TestBed.get(DaffProductFactory);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(productService).toBeTruthy();
+  });
+
+  describe('getAll | getting a list of products', () => {
+    it('should send a get request', () => {
+      const mockProducts = productFactory.createMany();
+
+      productService.getAll().subscribe(products => {
+        expect(products).toEqual(mockProducts);
+      });
+
+      const req = httpMock.expectOne(`${productService.url}`);
+      expect(req.request.method).toBe("GET");
+
+      req.flush(mockProducts);
+    });
+  });
+
+  describe('getBestSellers', () => {
+    it('should send a get request and return an array of products', () => {
+      const mockProducts = productFactory.createMany();
+
+      productService.getBestSellers().subscribe(products => {
+        expect(products).toEqual(mockProducts);
+      });
+
+      const req = httpMock.expectOne(`${productService.url}best-sellers`);
+      expect(req.request.method).toBe("GET");
+
+      req.flush(mockProducts);
+    });
+  });
+
+  describe('get | getting a single product', () => {
+
+    it('should send a get request', () => {
+      const mockProduct = productFactory.create();
+
+      productService.get(mockProduct.id).subscribe(product => {
+        expect(product).toEqual(mockProduct);
+      });
+
+      const req = httpMock.expectOne(`${productService.url}${mockProduct.id}`);
+      expect(req.request.method).toBe("GET");
+
+      req.flush(mockProduct);
+    });
+  });
+});
---
+++ libs/product/testing/src/inmemory-backend/product.service.spec.ts
@@ -0,0 +1,65 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DaffInMemoryBackendProductService } from './product.service';
+import { productHelper } from '..';
+
+describe('Driver | InMemory | Product | DaffInMemoryBackendProductService', () => {
+  let productTestingService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DaffInMemoryBackendProductService]
+    });
+
+    productTestingService = TestBed.get(DaffInMemoryBackendProductService);
+  });
+
+  it('should be created', () => {
+    expect(productTestingService).toBeTruthy();
+  });
+
+  describe('createDb', () => {
+    let result;
+
+    beforeEach(() => {
+      result = productTestingService.createDb();
+    });
+
+    it('should return a object with an array of Products', () => {
+      expect(Array.isArray(result.products)).toEqual(true);
+      expect(result.products.length).toBeGreaterThan(2);
+      expect(productHelper.isProduct(result.products[0])).toBeTruthy();
+    });
+
+    it('should return Products that have 5 images', () => {
+      expect(result.products[0].images.length).toEqual(5);
+    });
+  });
+
+  describe('get', () => {
+    
+    describe('when reqInfo.id is "best-sellers"', () => {
+      
+      let reqInfoStub;
+      let result;
+
+      beforeEach(() => {
+        reqInfoStub = {
+          id: 'best-sellers',
+          utils: {
+            createResponse$: (func) => {
+              return func();
+            }
+          }
+        }
+
+        result = productTestingService.get(reqInfoStub);
+      });
+
+      it('should return an Array of 4 products', () => {
+        expect(result.body).toEqual(jasmine.any(Array));
+        expect(result.body.length).toEqual(4);
+      });
+    });
+  });
+});
