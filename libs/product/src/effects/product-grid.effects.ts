e85df96ac708d3917b61b79cecdd5dabcfb657b0
Luke <lderrickable@gmail.com>
2019-03-18 12:24:23 -0400

    refactor(product): remove dependency on driver

--- libs/product/src/effects/product-grid.effects.ts
+++ libs/product/src/effects/product-grid.effects.ts
@@ -3,26 +3,26 @@ import { switchMap, map, catchError } from 'rxjs/operators';
 import { of ,  Observable } from 'rxjs';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 
-import { DaffDriver, DaffDriverInterface } from '@daffodil/driver';
-
 import { 
   ProductGridActionTypes, 
   ProductGridLoad, 
   ProductGridLoadSuccess, 
   ProductGridLoadFailure } from '../actions/product-grid.actions';
+import { DaffProductServiceInterface } from '../drivers/interfaces/product-service.interface';
+import { DaffProductDriver } from '../drivers/injection-tokens/product-driver.token';
 
 @Injectable()
 export class ProductGridEffects {
 
   constructor(
     private actions$: Actions,
-    @Inject(DaffDriver) private driver: DaffDriverInterface){}
+    @Inject(DaffProductDriver) private driver: DaffProductServiceInterface){}
 
   @Effect()
   loadAll$ : Observable<any> = this.actions$.pipe(
     ofType(ProductGridActionTypes.ProductGridLoadAction),
     switchMap((action: ProductGridLoad) =>
-      this.driver.productService.getAll()
+      this.driver.getAll()
         .pipe(
           map((resp) => {
             return new ProductGridLoadSuccess(resp);
