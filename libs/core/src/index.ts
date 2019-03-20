cd8357d287deac05f82c9cde2e2e31730fbba44e
Luke <lderrickable@gmail.com>
2019-03-20 17:26:24 -0400

    feat(core): remove unneeded files

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,23 +1,3 @@
-export { Product } from './product/product';
-export { ProductImage } from './product/product-image';
-
-export { Cart } from './cart/cart';
-export { CartItem } from './cart/cart-item';
-export { CartAddress } from './cart/cart-address';
-export { CartPayment } from './cart/cart-payment';
-export { CartShippingRate } from './cart/cart-shipping-rate';
-
-export { Order } from './order/order';
-export { OrderItem } from './order/order-item';
-export { OrderAddress } from './order/order-address';
-export { OrderPayment } from './order/order-payment';
-export { OrderShippingRate } from './order/order-shipping-rate';
-
-export { PaymentInfo } from './payment/payment-info';
-
-export { ShippingRate } from './shipping/shipping-rate';
-export { ShippingOption } from './shipping/shipping-option';
-
 export { DaffodilAddress } from './address/address';
 
 export { range } from './utils/range';
