246c205cedc5721b5da6f0f56e6a4c52c975c947
Damien Retzinger <damienwebdev@gmail.com>
2019-03-20 13:57:18 -0400

    feat(daffio): add pr previews with now to daffio

--- apps/daffio/server/server.ts
+++ apps/daffio/server/server.ts
@@ -1,30 +1,23 @@
 import 'zone.js/dist/zone-node';
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
 // Express Engine
 
-import {ngExpressEngine} from '@nguniversal/express-engine';
+import { ngExpressEngine } from '@nguniversal/express-engine';
 // Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 
 
 import * as express from 'express';
-import {join} from 'path';
-import { readFileSync } from 'fs';
-
+import { join } from 'path';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
 // Express server
-const app = express();
+export const app = express();
 
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), './../../dist/apps/daffio');
-const BROWSER_FOLDER = join(DIST_FOLDER, 'browser');
-
-// Our index.html we'll use as our template
-const template = readFileSync(join(BROWSER_FOLDER, 'index.html')).toString();
+const BROWSER_FOLDER = join(__dirname, 'browser');
 
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./../../../dist/apps/daffio/server/main');
 
@@ -45,13 +38,4 @@ app.get('*.*', express.static(BROWSER_FOLDER));
 // All regular routes use the Universal engine
 app.get('*', (req, res) => {
   res.render(join(BROWSER_FOLDER, 'index.html'), { req });
-});
-
-// Start up the Node server
-app.listen(PORT, () => {
-  console.log(`Node server listening on http://localhost:${PORT}`);
-});
-
-process.on('SIGINT', function() {
-  process.exit();
 });
