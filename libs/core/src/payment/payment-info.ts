91e63d2f6eb95e6ff0ff8edd87ca3b7cdbe42e3d
Luke <lderrickable@gmail.com>
2019-03-20 17:26:24 -0400

    feat(core): remove unneeded files

--- libs/core/src/payment/payment-info.ts
+++
@@ -1,7 +0,0 @@
-export interface PaymentInfo {
-    name: string;
-    cardnumber: number;
-    month: number;
-    year: number;
-    securitycode: number;
-}
--- libs/core/src/shipping/shipping-rate.ts
+++
@@ -1,6 +0,0 @@
-export interface ShippingRate {
-    rate_id: number;
-    price: number;
-    carrier: string;
-    [key: string]: any;
-}
