5b4ce07573e1191fc340aaba57ce63f01d13f1b4
Luke <lderrickable@gmail.com>
2019-03-28 23:02:54 -0400

    fix(checkout): fix linting

--- libs/checkout/gulpfile.ts
+++ libs/checkout/gulpfile.ts
@@ -2,6 +2,6 @@ import { Dgeni } from "dgeni";
 import { checkoutDocs } from './docs/config/index';
 
 exports.dgeni = function() {
-  var dgeni = new Dgeni([checkoutDocs]);
+  const dgeni = new Dgeni([checkoutDocs]);
   return dgeni.generate();
 }
