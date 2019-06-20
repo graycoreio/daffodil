ed204dedce00fce2804d55f8bea01856ea860a60
Damien Retzinger <damienwebdev@gmail.com>
2019-06-20 11:20:39 -0400

    feat(cart): add new cart facades (#347)
    * feat(cart): add new cart facades

---
+++ libs/cart/src/facades/cart/cart.facade.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+import { DaffCartModule, fromCart, DaffCart } from '@daffodil/cart';
+import { Action, Store, select } from '@ngrx/store';
+import { DaffStoreFacade } from 'libs/core/src';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: DaffCartModule
+})
+export class DaffCartFacade implements DaffStoreFacade<Action> {
+
+  loading$: Observable<boolean>;
+  cart$: Observable<DaffCart>;
+  
+  constructor(private store: Store<fromCart.State>) {
+    this.loading$ = this.store.pipe(select(fromCart.selectCartLoadingState));
+    this.cart$ = this.store.pipe(select(fromCart.selectCartValueState));
+  }
+
+  dispatch(action: Action) {
+    this.store.dispatch(action);
+  }
+}
