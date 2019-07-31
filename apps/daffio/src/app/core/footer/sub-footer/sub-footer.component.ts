0b167f1b025c0e05cffce99d04cbdb2e47ab86eb
xelaint <xelaint@gmail.com>
2019-07-31 17:56:28 -0400

    feat(design, daffio): update default callout color and deprecate accent

--- apps/daffio/src/app/core/footer/sub-footer/sub-footer.component.ts
+++ apps/daffio/src/app/core/footer/sub-footer/sub-footer.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'daffio-sub-footer',
-  host: {'class': 'sub-footer'},
   templateUrl: './sub-footer.component.html',
   styleUrls: ['./sub-footer.component.scss'],
   encapsulation: ViewEncapsulation.None
