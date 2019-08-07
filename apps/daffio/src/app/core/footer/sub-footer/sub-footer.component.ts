55bf24aab8b56224411e4442dd57ea4d62c16768
xelaint <xelaint@gmail.com>
2019-08-07 19:35:55 -0400

    refactor(daffio): re-add host class in subfooter

--- apps/daffio/src/app/core/footer/sub-footer/sub-footer.component.ts
+++ apps/daffio/src/app/core/footer/sub-footer/sub-footer.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'daffio-sub-footer',
+  host: {'class': 'sub-footer'},
   templateUrl: './sub-footer.component.html',
   styleUrls: ['./sub-footer.component.scss'],
   encapsulation: ViewEncapsulation.None
