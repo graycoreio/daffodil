4d337df014f20b5da25ce60f58088a804433a7d7
Nolan Arnold <nolan.arnold@gmail.com>
2019-08-19 12:15:45 -0500

    feat(design-land): added DaffImageGallery to design land page

--- apps/design-land/src/app/app.module.ts
+++ apps/design-land/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DesignLandAppComponent } from './app.component';
 import { DaffSidebarModule } from '@daffodil/design';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -16,7 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     DesignLandAppRoutingModule
   ],
   declarations: [
-    DesignLandAppComponent
+    DesignLandAppComponent,
   ],
   providers: [],
   bootstrap: [DesignLandAppComponent]
---
+++ apps/design-land/src/app/image-gallery/image-gallery.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImageGalleryComponent } from './image-gallery.component';
+
+describe('ImageGalleryComponent', () => {
+  let component: ImageGalleryComponent;
+  let fixture: ComponentFixture<ImageGalleryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ImageGalleryComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImageGalleryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
