a9728aca855b20f4516d7d996169b4ad7efb71db
Luke <lderrickable@gmail.com>
2019-08-20 22:52:24 -0400

    feat(navigation): add factory, drivers, and mock backend for the navigation module

---
+++ libs/navigation/testing/src/drivers/testing/navigation.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { DaffNavigationTree, DaffNavigationServiceInterface } from '@daffodil/navigation';
+
+import { DaffNavigationTreeFactory } from '../../factories/navigation-tree.factory';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DaffTestingNavigationService implements DaffNavigationServiceInterface {
+ 
+  constructor(
+    private navigationTreeFactory: DaffNavigationTreeFactory) {}
+
+  get(navigationTreeId: string): Observable<DaffNavigationTree> {
+    return of(this.navigationTreeFactory.create());
+  }
+}
--- libs/navigation/testing/src/index.ts
+++ libs/navigation/testing/src/index.ts
@@ -1,1 +1,6 @@
-export { DaffNavigationTestingModule } from './testing.module';
+export { DaffNavigationTreeFactory } from "./factories/navigation-tree.factory";
+export { DaffInMemoryBackendNavigationService } from './inmemory-backend/navigation.service';
+export { DaffTestingNavigationService } from './drivers/testing/navigation.service';
+export { DaffInMemoryNavigationService } from './drivers/in-memory/navigation.service';
+export { DaffNavigationInMemoryDriverModule } from './drivers/in-memory/navigation-driver.module';
+export { DaffNavigationTestingDriverModule } from './drivers/testing/navigation-driver.module';
