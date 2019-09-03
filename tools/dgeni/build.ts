454f793d1ea702ffba4b4de1ce258a3a5a262355
Damien Retzinger <damienwebdev@gmail.com>
2019-09-03 14:24:16 -0400

    fix(docs): update docs cleaning command to not delete root folder

--- tools/dgeni/build.ts
+++ tools/dgeni/build.ts
@@ -4,6 +4,6 @@ import { guideDocPackage } from './src/transforms/daffodil-guides-package';
 
 import * as rimraf  from 'rimraf';
 
-rimraf("../../dist/docs", function () { 
+rimraf("../../dist/docs/**/*", function () { 
   new Dgeni([apiDocs, guideDocPackage]).generate().catch(() => process.exit(1));
 });
