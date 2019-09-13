98a7702ba3250fe36674c7e171531f7faaf4d000
Luke <lderrickable@gmail.com>
2019-09-13 11:33:14 -0400

    feat(category): change query-manager interface argument typing

--- libs/category/src/drivers/interfaces/category-query-manager.interface.ts
+++ libs/category/src/drivers/interfaces/category-query-manager.interface.ts
@@ -1,5 +1,5 @@
 import { QueryOptions } from "apollo-client";
 
 export interface DaffCategoryQueryManagerInterface {
-  getACategoryQuery(identifier: any): QueryOptions;
+  getACategoryQuery(identifier: string | number): QueryOptions;
 }
