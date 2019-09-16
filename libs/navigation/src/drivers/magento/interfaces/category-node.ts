2393ce3385ed9c9856283b5618f0f3cef014c40a
Damien Retzinger <damienwebdev@gmail.com>
2019-09-16 16:07:42 -0400

    feat(navigation): improve navigation

--- libs/navigation/src/drivers/magento/interfaces/category-node.ts
+++ libs/navigation/src/drivers/magento/interfaces/category-node.ts
@@ -3,6 +3,7 @@ import { CategoryProductNode } from "./category-product-node";
 export interface CategoryNode {
   id: string;
   name?: string;
+  include_in_menu: boolean;
   products?: CategoryProductNode;
   level?: number;
   children_count?: number;
