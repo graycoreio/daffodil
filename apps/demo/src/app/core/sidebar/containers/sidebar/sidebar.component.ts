ae8e9262bb5a4c0f6f78fe2672eabe39f2428d51
Damien Retzinger <damienwebdev@gmail.com>
2019-09-16 16:07:14 -0400

    feat(demo): implement navigation sidebar in demo

--- apps/demo/src/app/core/sidebar/containers/sidebar/sidebar.component.ts
+++ apps/demo/src/app/core/sidebar/containers/sidebar/sidebar.component.ts
@@ -1,18 +1,37 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DaffNavigationTree, DaffNavigationFacade, DaffNavigationLoad } from '@daffodil/navigation';
 
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromDemoSidebar from '../../reducers/index';
+import { CloseSidebar } from '../../actions/sidebar.actions';
 
 @Component({
   selector: 'demo-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarContainer {
+export class SidebarContainer implements OnInit {
   faTimes = faTimes;
+  tree$: Observable<DaffNavigationTree>;
+  treeLoading$: Observable<boolean>;
+  treeErrors$: Observable<string[]>;
 
-  @Output() close: EventEmitter<any> = new EventEmitter();
+  constructor(
+    private store: Store<fromDemoSidebar.State>,
+    private navigationFacade: DaffNavigationFacade
+    ) {}
+
+  ngOnInit() {
+    this.tree$ = this.navigationFacade.tree$;
+    this.treeLoading$ = this.navigationFacade.loading$;
+    this.treeErrors$ = this.navigationFacade.errors$;
+    this.navigationFacade.dispatch(new DaffNavigationLoad('2'));
+  }
 
   onClose() {
-    this.close.emit();
+    this.store.dispatch(new CloseSidebar);
   }
 }
