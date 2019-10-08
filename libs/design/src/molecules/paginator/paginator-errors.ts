14555a500fa17f3598e7545a3f0d08629bfe00d4
Luke <lderrickable@gmail.com>
2019-10-08 18:22:21 -0400

    change an error message for the paginator

--- libs/design/src/molecules/paginator/paginator-errors.ts
+++ libs/design/src/molecules/paginator/paginator-errors.ts
@@ -1,2 +1,2 @@
 export const DaffPaginatorNumberOfPagesErrorMessage = 'The numberOfPages in the daff-paginator must not be less than 1';
-export const DaffPaginatorPageOutOfRangeErrorMessage = 'The numberOfPages is less than the currentPage';
+export const DaffPaginatorPageOutOfRangeErrorMessage = 'The numberOfPages in the daff-paginator should not be less than the currentPage';
