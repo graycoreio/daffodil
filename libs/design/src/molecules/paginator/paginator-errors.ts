64281533c6a7cbb0563f24640ca60baff1cd2a6e
Luke <lderrickable@gmail.com>
2019-10-08 18:02:09 -0400

    fix(design): fix a typo

--- libs/design/src/molecules/paginator/paginator-errors.ts
+++ libs/design/src/molecules/paginator/paginator-errors.ts
@@ -1,2 +1,2 @@
-export const DaffPaginatorNumberOfPagesErrorMessage = 'The numberOfPages in the golf-paginator must not be less than 1';
+export const DaffPaginatorNumberOfPagesErrorMessage = 'The numberOfPages in the daff-paginator must not be less than 1';
 export const DaffPaginatorPageOutOfRangeErrorMessage = 'The numberOfPages is less than the currentPage';
