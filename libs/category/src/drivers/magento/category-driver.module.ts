5402374a6f55f4be505585706764d2ee8fd39c85
Luke <lderrickable@gmail.com>
2019-10-21 16:04:43 -0400

    fix(category): make magento driver provide the transformer and the query manager

--- libs/category/src/drivers/magento/category-driver.module.ts
+++ libs/category/src/drivers/magento/category-driver.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DaffCategoryDriver } from '../injection-tokens/category-driver.token';
 import { DaffMagentoCategoryService } from './category.service';
+import { DaffCategoryTransformer } from '../injection-tokens/category-transformer.token';
+import { DaffMagentoCategoryTransformerService } from './transformers/category-transformer.service';
+import { DaffCategoryQueryManager } from '../injection-tokens/category-query-manager.token';
+import { DaffMagentoCategoryGraphQlQueryManagerService } from './queries/category-query-manager.service';
 
 @NgModule({
   imports: [
@@ -17,6 +21,14 @@ export class DaffCategoryMagentoDriverModule {
         {
           provide: DaffCategoryDriver,
           useExisting: DaffMagentoCategoryService
+        },
+        {
+          provide: DaffCategoryTransformer,
+          useExisting: DaffMagentoCategoryTransformerService
+        },
+        {
+          provide: DaffCategoryQueryManager,
+          useExisting: DaffMagentoCategoryGraphQlQueryManagerService
         }
       ]
     };
