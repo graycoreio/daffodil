f0e4c8cc6249b49bab0f5ad64ee6ffd7fb978156
Nolan Arnold <nolan.arnold@gmail.com>
2019-12-10 10:44:52 -0600

    feat(contact): added driver interface layer and testing driver

--- libs/contact/src/index.ts
+++ libs/contact/src/index.ts
@@ -1,1 +1,2 @@
-export {}
+export { DaffContactServiceInterface } from './driver/interfaces/contact-service.interface'
+export { DaffContactUnion } from './models/contact-union';
---
+++ libs/contact/src/models/contact-union.ts
@@ -0,0 +1,3 @@
+export interface DaffContactUnion {
+  [x:string]: any;
+}
