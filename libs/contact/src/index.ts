70e8fdf15e3ee49cc83544c655ec8ba60864a9c0
Nolan Arnold <nolan.arnold@gmail.com>
2019-12-18 09:57:18 -0600

    feat(contact): updated index and tsconfig

--- libs/contact/src/index.ts
+++ libs/contact/src/index.ts
@@ -1,2 +1,3 @@
 export { DaffContactServiceInterface } from './driver/interfaces/contact-service.interface'
 export { DaffContactUnion } from './models/contact-union';
+export { DaffContactDriver } from './driver/injection-tokens/contact-driver.token';
