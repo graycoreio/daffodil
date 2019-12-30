bf36811058f8baff0b4fd2deb9f4f970bee30fa0
Luke <lderrickable@gmail.com>
2019-12-30 15:50:09 -0500

    feat(checkout): add Daff to shipping action names

--- libs/checkout/src/shipping/actions/shipping.actions.ts
+++ libs/checkout/src/shipping/actions/shipping.actions.ts
@@ -2,23 +2,23 @@ import { Action } from '@ngrx/store';
 
 import { DaffAddress } from '@daffodil/core';
 
-export enum ShippingActionTypes {
+export enum DaffShippingActionTypes {
   UpdateShippingAddressAction = '[Shipping] Update Shipping Address Action',
   SelectShippingOptionAction = '[Shipping] Select Shipping Option Action'
 }
 
-export class UpdateShippingAddress implements Action {
-  readonly type = ShippingActionTypes.UpdateShippingAddressAction;
+export class DaffUpdateShippingAddress implements Action {
+  readonly type = DaffShippingActionTypes.UpdateShippingAddressAction;
 
   constructor(public payload: DaffAddress) {}
 }
 
-export class SelectShippingOption implements Action {
-  readonly type = ShippingActionTypes.SelectShippingOptionAction;
+export class DaffSelectShippingOption implements Action {
+  readonly type = DaffShippingActionTypes.SelectShippingOptionAction;
 
   constructor(public payload: string) {}
 }
 
-export type ShippingActions =
-    | UpdateShippingAddress
-    | SelectShippingOption;
+export type DaffShippingActions =
+    | DaffUpdateShippingAddress
+    | DaffSelectShippingOption;
