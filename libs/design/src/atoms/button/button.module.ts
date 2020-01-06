ab917360e1065b9e6b6b298f38b62fca35ce4a79
Elain Tsai <xelaint@gmail.com>
2020-01-07 06:00:10 +0800

    feat(design): add prefix/suffix to button (#539)
    Co-authored-by: Elain Tsai <xelaint@gmail.com>
    Co-authored-by: Damien Retzinger <damienwebdev@gmail.com>

--- libs/design/src/atoms/button/button.module.ts
+++ libs/design/src/atoms/button/button.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DaffButtonComponent } from './button.component';
+import { DaffPrefixSuffixModule } from '../../core/prefix-suffix/prefix-suffix.module';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DaffPrefixSuffixModule
   ],
   declarations: [
     DaffButtonComponent
   ],
   exports: [
-    DaffButtonComponent
+    DaffButtonComponent,
+    DaffPrefixSuffixModule
   ]
 })
 export class DaffButtonModule { }
