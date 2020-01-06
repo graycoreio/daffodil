7ebcfb11bcb71f17cdc095e9e0879f1b4355fdd8
Nolan Arnold <nolan.arnold@gmail.com>
2020-01-06 10:50:42 -0600

    feat(contact): added ngrx effects to contact

--- libs/contact/testing/src/drivers/testing/contact.service.ts
+++ libs/contact/testing/src/drivers/testing/contact.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { DaffContactServiceInterface } from '@daffodil/contact';
 import { of, Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DaffTestingContactService implements DaffContactServiceInterface<string, any>{
   send(payload: string): Observable<any>{
-    return of('Success');
+    return of('Success').pipe(delay(10));
   }
 }
