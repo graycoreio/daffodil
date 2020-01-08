2ab60b92dad66dfb1d6e2f75ffe29d89a985f43b
Nolan Arnold <44958241+Nolan-Arnold@users.noreply.github.com>
2020-01-08 13:59:43 -0600

    feat(driver): removed unused files from driver for repurposing of library (#547)

--- libs/driver/src/index.ts
+++ libs/driver/src/index.ts
@@ -1,5 +1,1 @@
-//Config
-export { DaffDriverConfigService, DaffDriverConfigServiceFactory } from './config/driver-config.service';
-export { _DAFFODIL_DRIVER_CONFIG } from './config/tokens/driver-config.token';
-export { DaffDriverConfig } from './config/models/driver-config';
-export { DaffDriverConfigFactory } from './config/factories/driver-config.factory';
+export {}
