e215812bad1472ad43129aada9fd86b3c04c57eb
Peter Lauck <griest024@gmail.com>
2020-01-23 21:31:38 -0500

    fix(demo): cart-view styles not being included

--- apps/demo/src/app/cart/pages/cart-view/cart-view.component.ts
+++ apps/demo/src/app/cart/pages/cart-view/cart-view.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './cart-view.component.html'
+  templateUrl: './cart-view.component.html',
+  styleUrls: ['./cart-view.component.scss']
 })
 export class DemoCartViewComponent {}
