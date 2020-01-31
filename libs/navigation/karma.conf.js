0175d575493ed00e51109b81ec4026d81dc9954e
Peter Lauck <griest024@gmail.com>
2020-01-30 21:50:21 -0500

    build(all): enable lib code coverage reporting

--- libs/navigation/karma.conf.js
+++ libs/navigation/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
         clearContext: false // leave Jasmine Spec Runner output visible in browser
       },
       coverageIstanbulReporter: {
-        dir: require('path').join(__dirname, '../../coverage'),
+        dir: require('path').join(__dirname, '../../coverage/libs/navigation'),
         reports: ['html', 'lcovonly'],
         fixWebpackSourcePaths: true
       },
@@ -29,4 +29,3 @@ module.exports = function (config) {
       singleRun: false
     });
   };
-  
