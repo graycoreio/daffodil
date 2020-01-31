7883b76213d1b0d4e899b6dcd1de743e1d990832
xelaint <xelaint@gmail.com>
2020-01-31 23:44:29 +0800

    feat(design): remove hostbinding from input component

--- libs/design/src/atoms/form/input/input.component.ts
+++ libs/design/src/atoms/form/input/input.component.ts
@@ -3,16 +3,17 @@ import { NgControl } from '@angular/forms';
 
 import { DaffFormFieldControl } from '../form-field/form-field-control';
 
+/**
+ * DaffInputComponent provides the same functionality as a native `<input>` and contains custom styling and functionality.
+ */
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'input[daff-input]',
   template: '<ng-content></ng-content>',
   styleUrls: ['./input.component.scss'],
-  host: {
-    'class': 'daff-input'
-  },
-  encapsulation: ViewEncapsulation.None,
-  providers: [{provide: DaffFormFieldControl, useExisting: DaffInputComponent}],
+  providers: [
+    {provide: DaffFormFieldControl, useExisting: DaffInputComponent}
+  ],
 })
 export class DaffInputComponent implements DaffFormFieldControl {
 
