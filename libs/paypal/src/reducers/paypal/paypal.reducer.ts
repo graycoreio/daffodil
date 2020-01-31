3991377d393d090347593c4171a1b168af80cfab
Luke <lderrickable@gmail.com>
2020-01-31 17:09:47 -0500

    refactor(paypal): make initial state of reducer less generic

--- libs/paypal/src/reducers/paypal/paypal.reducer.ts
+++ libs/paypal/src/reducers/paypal/paypal.reducer.ts
@@ -3,16 +3,14 @@ import { DaffPaypalTokenResponse } from '../../models/paypal-token-response';
 import { DaffPaypalActions, DaffPaypalActionTypes } from '../../actions/paypal.actions';
 import { DaffPaypalTokenRequest } from '../../models/paypal-token-request';
 
-function getInitialState <T extends DaffPaypalTokenResponse>(): DaffPaypalReducerState<T> {
-	return {
-		paypalTokenResponse: null,
-		loading: false,
-		error: null
-	}
+export const initialState: DaffPaypalReducerState<any> = {
+	paypalTokenResponse: null,
+	loading: false,
+	error: null
 }
 
 export function daffPaypalReducer <T extends DaffPaypalTokenRequest, V extends DaffPaypalTokenResponse>
-	(state: DaffPaypalReducerState<V> = getInitialState(), action: DaffPaypalActions<T, V>): DaffPaypalReducerState<V> {
+	(state: DaffPaypalReducerState<V> = initialState, action: DaffPaypalActions<T, V>): DaffPaypalReducerState<V> {
   switch (action.type) {
 		case DaffPaypalActionTypes.GeneratePaypalExpressTokenAction:
 			return {...state, loading: true};
