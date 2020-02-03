b335bd68ae01457c3cea1bf95a421edd59454065
Peter Lauck <griest024@gmail.com>
2020-02-03 13:34:13 -0500

    feat(auth): add in-memory backend

--- libs/auth/testing/src/index.ts
+++ libs/auth/testing/src/index.ts
@@ -2,6 +2,7 @@ export { DaffAccountRegistrationFactory } from './factories/account-registration
 export { DaffCustomerRegistrationFactory } from './factories/customer-registration.factory';
 export { DaffAuthFactory } from './factories/auth.factory';
 
+export { DaffInMemoryBackendAuthService } from './inmemory-backend/auth.service';
 
 export { DaffAuthTestingModule } from './helpers/auth-testing.module';
 export { MockDaffAuthFacade } from './helpers/mocks/mock-auth.facade';
