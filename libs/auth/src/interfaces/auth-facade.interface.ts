dc6e10cd3ef75888cce3afa5b49098afe0962d0a
Peter Lauck <griest024@gmail.com>
2020-02-04 09:50:09 -0500

    feat(auth): use generics in facade interface and add auth$ field

--- libs/auth/src/interfaces/auth-facade.interface.ts
+++ libs/auth/src/interfaces/auth-facade.interface.ts
@@ -2,8 +2,10 @@ import { Observable } from 'rxjs';
 import { Action } from '@ngrx/store';
 
 import { DaffStoreFacade } from '@daffodil/core';
+import { DaffAuthToken } from '../models/auth-token';
 
-export interface DaffAuthFacadeInterface extends DaffStoreFacade<Action> {
+export interface DaffAuthFacadeInterface<T extends DaffAuthToken> extends DaffStoreFacade<Action> {
+  auth$: Observable<T>
   loading$: Observable<boolean>;
   token$: Observable<string>;
   errors$: Observable<string[]>;
