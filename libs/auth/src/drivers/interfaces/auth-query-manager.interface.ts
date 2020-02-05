5850ae93c3e04ed2330f2a523a7133d9b507315d
Peter Lauck <griest024@gmail.com>
2020-02-04 19:20:04 -0500

    feat(auth): add generics to DaffAuthQueryManagerInterface

--- libs/auth/src/drivers/interfaces/auth-query-manager.interface.ts
+++ libs/auth/src/drivers/interfaces/auth-query-manager.interface.ts
@@ -1,17 +1,14 @@
 import { MutationOptions } from 'apollo-client';
 import { DaffRegisterRequest } from '../../models/register-request';
 import { DaffCustomerRegistration } from '../../models/customer-registration';
+import { DaffLoginRequest } from '../../models/login-request';
 
 export interface DaffAuthQueryManagerInterface<
   RegisterRequest extends DaffRegisterRequest<RegisterRequestT>,
-  RegisterRequestT extends DaffCustomerRegistration
+  RegisterRequestT extends DaffCustomerRegistration,
+  LoginRequest extends DaffLoginRequest
 > {
-  generateATokenMutation(
-    email: string,
-    password: string
-  ): MutationOptions<any>;
+  generateATokenMutation(request: LoginRequest): MutationOptions<any>;
 
-  createACustomerMutation(
-    info: RegisterRequest
-  ): MutationOptions<any>
+  createACustomerMutation(request: RegisterRequest): MutationOptions<any>
 }
