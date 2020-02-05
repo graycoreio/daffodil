6f5a47cba28cfb229ba3a73da184b57c276240f0
Peter Lauck <griest024@gmail.com>
2020-02-04 19:26:33 -0500

    feat(auth): remove type parameter in query manager injection token

--- libs/auth/src/drivers/injection-tokens/auth-query-manager.token.ts
+++ libs/auth/src/drivers/injection-tokens/auth-query-manager.token.ts
@@ -1,8 +1,6 @@
 import { InjectionToken } from '@angular/core';
-import { DaffAuthQueryManagerInterface } from '../interfaces/auth-query-manager.interface';
 
 /**
  * Injection token that serves as a placeholder for any service that implements the DaffAuthQueryManagerInterface.
  */
-export const DaffAuthQueryManager =
-    new InjectionToken<DaffAuthQueryManagerInterface>('DaffAuthQueryManager');
+export const DaffAuthQueryManager = new InjectionToken('DaffAuthQueryManager');
