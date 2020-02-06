1a95a9af431ef7586054a24888daca6bf8f9d8d4
Luke <lderrickable@gmail.com>
2020-02-06 18:47:49 -0500

    fix test

--- libs/core/src/utils/random-subset.spec.ts
+++ libs/core/src/utils/random-subset.spec.ts
@@ -3,7 +3,7 @@ import { randomSubset } from './random-subset';
 describe('Core | Utils | random-subset', () => {
 	it('should return a random subset of the given array', () => {
 		const initialArray = [1,2,3,4,5,6,7,8,9];
-		const result = randomSubset(initialArray);
+		const result = randomSubset(initialArray, 7);
 		expect(initialArray.includes(result[0])).toBeTruthy();
 		expect(initialArray.includes(result[result.length-1])).toBeTruthy();
 	});
