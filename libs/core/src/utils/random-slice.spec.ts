edac34dce64063bce9e1b64ef901318b68bd8b22
Luke <lderrickable@gmail.com>
2020-02-07 13:13:40 -0500

    add one more test

--- libs/core/src/utils/random-slice.spec.ts
+++ libs/core/src/utils/random-slice.spec.ts
@@ -1,6 +1,13 @@
 import { randomSlice } from './random-slice';
 
 describe('Core | Utils | random-slice', () => {
+
+	it('should return an empty array if given an empty array', () => {
+		const initialArray = [];
+		const result = randomSlice(initialArray);
+		expect(result).toEqual([]);
+	});
+
 	it('should return a slice of the given array', () => {
 		const initialArray = [1,2,3,4,5,6,7,8,9];
 		const result = randomSlice(initialArray);
