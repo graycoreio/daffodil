ea17cf5a9c51e4166b09a5c156fa98ace7cc3622
Nolan Arnold <nolan.arnold@gmail.com>
2020-02-14 11:00:23 -0600

    fix(contact): exported contact backend service

--- libs/contact/testing/src/index.ts
+++ libs/contact/testing/src/index.ts
@@ -1,2 +1,3 @@
 export { DaffContactTestingDriverModule } from './drivers/testing/testing.module';
 export { DaffContactInMemoryDriverModule } from './drivers/in-memory/in-memory.module';
+export { DaffInMemoryBackendContactService } from './in-memory-backend/contact-in-memory-backend.service';
