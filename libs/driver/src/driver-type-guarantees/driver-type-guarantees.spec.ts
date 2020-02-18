04b537f23feeccfe245aec57924105d31f5d27fc
Peter Lauck <griest024@gmail.com>
2020-02-18 16:49:47 -0500

    feat(driver): add other driver scenarios to driver type guarantees

--- libs/driver/src/driver-type-guarantees/driver-type-guarantees.spec.ts
+++ libs/driver/src/driver-type-guarantees/driver-type-guarantees.spec.ts
@@ -2,77 +2,232 @@ import { async, TestBed } from '@angular/core/testing'
 import { ApolloTestingModule, ApolloTestingController } from 'apollo-angular/testing'
 
 import { PlatformDriverModule } from './drivers/platform/platform-driver.module'
-import { TransformerIn, QueryManager, Driver } from './drivers/platform'
-import { CustomTransformerIn, CustomQueryManager, CustomPlatformResponse, CustomResponse } from './driver-type-guarantees'
-import { ServiceInterface } from './drivers/interfaces'
-import { DaffRequest, PlatformRequest } from './drivers/models'
+import { TransformerIn, QueryManager, Driver, TransformerOut } from './drivers/platform'
+import { CustomTransformerIn, CustomQueryManager, CustomPlatformResponse, CustomResponse, CustomRequest, CustomPlatformRequest, CustomTransformerOut } from './driver-type-guarantees'
+import { ServiceInterface, QueryManagerInterface } from './drivers/interfaces'
+import { DaffRequest, PlatformRequest, PlatformResponse } from './drivers/models'
+import { catchError } from 'rxjs/operators'
+import { of } from 'rxjs'
 
 describe('Driver | Type Guarantees', () => {
   let service: ServiceInterface<CustomResponse>;
   let controller: ApolloTestingController;
-  let queryManager: CustomQueryManager;
+  let queryManager: QueryManagerInterface;
 
   let mockRequest: DaffRequest;
+  let mockCustomRequest: CustomRequest;
   let mockPlatformRequest: PlatformRequest;
+  let mockCustomPlatformRequest: CustomPlatformRequest;
+  let mockPlatformResponse: PlatformResponse;
   let mockCustomPlatformResponse: CustomPlatformResponse;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        ApolloTestingModule,
-        PlatformDriverModule,
-      ],
-      providers: [
-        {
-          provide: TransformerIn,
-          useExisting: CustomTransformerIn
-        },
-        {
-          provide: QueryManager,
-          useExisting: CustomQueryManager
-        }
-      ]
-    });
-
-    service = TestBed.get(Driver);
-    controller = TestBed.get(ApolloTestingController);
-    queryManager = TestBed.get(QueryManager);
-
+  beforeEach(() => {
     mockRequest = {
       steph: 'steph',
       curry: 'curry'
     };
+    mockCustomRequest = {
+      steph: 'steph',
+      curry: 'curry',
+      points: 50
+    };
     mockPlatformRequest = {
       donovan: mockRequest.steph,
       mitchell: mockRequest.curry
+    };
+    mockCustomPlatformRequest = {
+      donovan: mockCustomRequest.steph,
+      mitchell: mockCustomRequest.curry,
+      points: mockCustomRequest.points
+    };
+    mockPlatformResponse  = {
+      damian: 'damian',
+      lillard: 'lillard'
     };
     mockCustomPlatformResponse  = {
       damian: 'damian',
       lillard: 'lillard',
       points: 60
     };
-  }));
+  });
 
-  afterEach(() => {
-    controller.verify();
-  })
-
-  describe('get | getting a custom response', () => {
-    it('should return the custom response', done => {
-      service.get(mockRequest).subscribe(result => {
-        // result is strictly typed as a CustomResponse
-        expect(result).toEqual(jasmine.objectContaining({
-          luka: mockCustomPlatformResponse.damian,
-          doncic: mockCustomPlatformResponse.lillard,
-          points: mockCustomPlatformResponse.points
-        }));
-        done();
+  describe('Custom Driver | Custom Arguments', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          ApolloTestingModule,
+          PlatformDriverModule,
+        ],
+        providers: [
+          {
+            provide: TransformerIn,
+            useExisting: CustomTransformerIn
+          },
+          {
+            provide: TransformerOut,
+            useExisting: CustomTransformerOut
+          },
+          {
+            provide: QueryManager,
+            useExisting: CustomQueryManager
+          }
+        ]
       });
 
-      const op = controller.expectOne(queryManager.getPlatformResponse(mockPlatformRequest).query);
+      service = TestBed.get(Driver);
+      controller = TestBed.get(ApolloTestingController);
+      queryManager = TestBed.get(QueryManager);
+    }));
 
-      op.flush({
-        data: mockCustomPlatformResponse
+    afterEach(() => {
+      controller.verify();
+    });
+
+    describe('get | getting a custom response', () => {
+      it('should return the custom response', done => {
+        service.get(mockCustomRequest).subscribe(result => {
+          // result is strictly typed as a CustomResponse
+          expect(result).toEqual(jasmine.objectContaining({
+            luka: mockCustomPlatformResponse.damian,
+            doncic: mockCustomPlatformResponse.lillard,
+            points: mockCustomPlatformResponse.points
+          }));
+          done();
+        });
+
+        const op = controller.expectOne(queryManager.getPlatformResponse(mockCustomPlatformRequest).query);
+
+        op.flush({
+          data: mockCustomPlatformResponse
+        });
+      });
+    });
+  });
+
+  describe('Custom Driver | Vanilla Arguments', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          ApolloTestingModule,
+          PlatformDriverModule,
+        ],
+        providers: [
+          {
+            provide: TransformerIn,
+            useExisting: CustomTransformerIn
+          },
+          {
+            provide: TransformerOut,
+            useExisting: CustomTransformerOut
+          },
+          {
+            provide: QueryManager,
+            useExisting: CustomQueryManager
+          }
+        ]
+      });
+
+      service = TestBed.get(Driver);
+      controller = TestBed.get(ApolloTestingController);
+      queryManager = TestBed.get(QueryManager);
+    }));
+
+    afterEach(() => {
+      controller.verify();
+    });
+
+    describe('get | getting a custom response', () => {
+      it('should throw a TypeError', done => {
+        service.get(mockRequest).pipe(
+          catchError(err => {
+            expect(err).toBeDefined();
+            done();
+            return of();
+          })
+        ).subscribe(result => {
+          fail();
+          done();
+        })
+
+        const op = controller.expectOne(queryManager.getPlatformResponse(mockPlatformRequest).query);
+
+        op.flush({
+          data: mockPlatformResponse
+        });
+      });
+    });
+  });
+
+  describe('Vanilla Driver | Custom Arguments', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          ApolloTestingModule,
+          PlatformDriverModule,
+        ],
+      });
+
+      service = TestBed.get(Driver);
+      controller = TestBed.get(ApolloTestingController);
+      queryManager = TestBed.get(QueryManager);
+    }));
+
+    afterEach(() => {
+      controller.verify();
+    });
+
+    describe('get | getting a vanilla response', () => {
+      it('should return the vanilla response', done => {
+        service.get(mockCustomRequest).subscribe(result => {
+          expect(result).toEqual(jasmine.objectContaining({
+            luka: mockCustomPlatformResponse.damian,
+            doncic: mockCustomPlatformResponse.lillard,
+          }));
+          done();
+        });
+
+        const op = controller.expectOne(queryManager.getPlatformResponse(mockCustomPlatformRequest).query);
+
+        op.flush({
+          data: mockCustomPlatformResponse
+        });
+      });
+    });
+  });
+
+  describe('Vanilla Driver | Vanilla Arguments', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          ApolloTestingModule,
+          PlatformDriverModule,
+        ],
+      });
+
+      service = TestBed.get(Driver);
+      controller = TestBed.get(ApolloTestingController);
+      queryManager = TestBed.get(QueryManager);
+    }));
+
+    afterEach(() => {
+      controller.verify();
+    });
+
+    describe('get | getting a vanilla response', () => {
+      it('should return the vanilla response', done => {
+        service.get(mockRequest).subscribe(result => {
+          expect(result).toEqual(jasmine.objectContaining({
+            luka: mockPlatformResponse.damian,
+            doncic: mockPlatformResponse.lillard,
+          }));
+          done();
+        });
+
+        const op = controller.expectOne(queryManager.getPlatformResponse(mockPlatformRequest).query);
+
+        op.flush({
+          data: mockPlatformResponse
+        });
       });
     });
   });
--- libs/driver/src/driver-type-guarantees/driver-type-guarantees.ts
+++ libs/driver/src/driver-type-guarantees/driver-type-guarantees.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@angular/core';
-
-import { DaffResponse, PlatformResponse, PlatformRequest } from './drivers/models';
-import { TransformerInInterface, QueryManagerInterface } from './drivers/interfaces';
 import { QueryOptions } from 'apollo-client';
 import gql from 'graphql-tag';
+
+import { DaffResponse, PlatformResponse, PlatformRequest, DaffRequest } from './drivers/models';
+import { TransformerInInterface, QueryManagerInterface, TransformerOutInterface } from './drivers/interfaces';
+
+// Customization by overriding lower level services and extending models
 
 export interface CustomResponse extends DaffResponse {
   points: number
 }
 
+export interface CustomRequest extends DaffRequest {
+  points: number
+}
+
+export interface CustomPlatformRequest extends PlatformRequest {
+  points: number;
+}
+
 export interface CustomPlatformResponse extends PlatformResponse {
   points: number
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomTransformerOut implements TransformerOutInterface {
+  transform(arg: CustomRequest): CustomPlatformRequest {
+    return {
+      donovan: arg.steph,
+      mitchell: arg.curry,
+      points: arg.points
+    }
+  }
 }
 
 @Injectable({
@@ -30,10 +53,10 @@ export class CustomTransformerIn implements TransformerInInterface<CustomRespons
   providedIn: 'root'
 })
 export class CustomQueryManager implements QueryManagerInterface {
-  getPlatformResponse(request: PlatformRequest): QueryOptions {
+  getPlatformResponse(request: CustomPlatformRequest): QueryOptions {
     return {
       query: gql`
-        query GetPlatformResponse($first: String!, $last: String!) {
+        query GetPlatformResponse($first: String!, $last: String!, $points: Number!) {
           damian,
           lillard,
           points
@@ -42,6 +65,7 @@ export class CustomQueryManager implements QueryManagerInterface {
       variables: {
         first: request.donovan,
         last: request.mitchell,
+        points: request.points
       }
     }
   }
