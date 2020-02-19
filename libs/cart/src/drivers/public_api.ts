53a8450cf96427ab42da3c14dd323f6209a175f6
Damien Retzinger <damienwebdev@gmail.com>
2020-02-19 13:41:19 -0500

    feat(cart): remove transformer interfaces

--- libs/cart/src/drivers/public_api.ts
+++ libs/cart/src/drivers/public_api.ts
@@ -7,9 +7,3 @@ export { DaffCartPaymentServiceInterface, DaffCartPaymentDriver} from './interfa
 export { DaffCartShippingAddressServiceInterface, DaffCartShippingAddressDriver } from './interfaces/cart-shipping-address-service.interface';
 export { DaffCartShippingInformationServiceInterface, DaffCartShippingInformationDriver } from './interfaces/cart-shipping-information-service.interface';
 export { DaffCartShippingMethodsServiceInterface, DaffCartShippingMethodsDriver } from './interfaces/cart-shipping-methods-service.interface';
-
-export { DaffCartAddressTransformerInterface, DaffCartAddressTransformer } from './interfaces/transformers/cart-address-transformer.interface';
-export { DaffCartItemTransformer, DaffCartItemTransformerInterface } from './interfaces/transformers/cart-item-transformer.interface';
-export { DaffCartPaymentTransformer, DaffCartPaymentTransformerInterface } from './interfaces/transformers/cart-payment-transformer.interface';
-export { DaffCartTotalTransformer, DaffCartTotalsTransformerInterface } from './interfaces/transformers/cart-totals-transformer.interface';
-export { DaffCartTransformer, DaffCartTransformerInterface } from './interfaces/transformers/cart-transformer.interface';
