9f2723f6de6552bed26a715c763f0776717052c9
Peter Lauck <griest024@gmail.com>
2020-02-19 14:27:29 -0500

    style(auth): remove unused imports

--- libs/auth/testing/src/drivers/in-memory/auth/auth.service.spec.ts
+++ libs/auth/testing/src/drivers/in-memory/auth/auth.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
-import { catchError } from 'rxjs/operators';
 
 import {
   DaffAuthToken,
--- libs/auth/testing/src/drivers/testing/auth/auth.service.spec.ts
+++ libs/auth/testing/src/drivers/testing/auth/auth.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-import { catchError } from 'rxjs/operators';
 
 import {
   DaffAuthToken,
