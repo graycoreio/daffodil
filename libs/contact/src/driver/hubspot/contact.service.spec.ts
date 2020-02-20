f9c83c13484824a02994bd5b1052fb8f665f0a80
Damien Retzinger <damienwebdev@gmail.com>
2020-02-20 09:47:24 -0500

    linting

--- libs/contact/src/driver/hubspot/contact.service.spec.ts
+++ libs/contact/src/driver/hubspot/contact.service.spec.ts
@@ -7,39 +7,36 @@ import { DaffHubspotFormsService } from '@daffodil/driver/hubspot';
 import { DaffContactHubspotService } from './contact.service';
 import { DaffContactDriver } from '../public_api';
 
-
-
 describe('DaffContactHubspotService', () => {
-  let contactService;
+	let contactService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        {provide: DaffContactDriver, useClass: DaffContactHubspotService},
-        {
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [
+				{ provide: DaffContactDriver, useClass: DaffContactHubspotService },
+				{
 					provide: DaffHubspotFormsService,
-          useValue: {
-            submit(): Observable<any> {
-              return hot('--a', { a: {test: "123"}});
-            }
-          }
-        }
-      ]
-    });
+					useValue: {
+						submit(): Observable<any> {
+							return hot('--a', { a: { test: '123' } });
+						},
+					},
+				},
+			],
+		});
 
-    contactService = TestBed.get(DaffContactDriver);
-  });
-  
+		contactService = TestBed.get(DaffContactDriver);
+	});
 
-  it('should be created', () => {
-    expect(contactService).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(contactService).toBeTruthy();
+	});
 
-  describe('when sending', () => {
+	describe('when sending', () => {
 		it('should return an observable of HubspotResponse', () => {
-      const payload = { email: 'email@email.edu' };
-      const expected = cold('--b', { b: { test: '123'} });
-      expect(contactService.send(payload)).toBeObservable(expected);
-    });
+			const payload = { email: 'email@email.edu' };
+			const expected = cold('--b', { b: { test: '123' } });
+			expect(contactService.send(payload)).toBeObservable(expected);
+		});
 	});
 });
