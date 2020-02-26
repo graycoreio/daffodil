16fd66cf0d33d2ad5379b957b267b4b5e93559a5
Peter Lauck <griest024@gmail.com>
2020-02-26 17:31:00 -0500

    feat(cart): export factories from testing package

--- libs/cart/testing/src/index.ts
+++ libs/cart/testing/src/index.ts
@@ -1,8 +1,5 @@
-export { DaffCartAddressFactory } from './factories/cart-address.factory';
-export { DaffCartItemFactory } from './factories/cart-item.factory';
-export { DaffCartPaymentFactory } from './factories/cart-payment.factory';
-export { DaffCartShippingRateFactory } from './factories/cart-shipping-rate.factory';
-export { DaffCartFactory } from './factories/cart.factory';
+export * from './factories';
+
 export { DaffTestingCartService } from './drivers/testing/cart.service';
 export { DaffInMemoryCartService } from './drivers/in-memory/cart.service';
 export { DaffInMemoryBackendCartService } from './in-memory-backend/cart.service';
