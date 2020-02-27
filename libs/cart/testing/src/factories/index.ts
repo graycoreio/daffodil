1454e00c4429cf209ea2fc7ee560ee50186271cb
Peter Lauck <griest024@gmail.com>
2020-02-27 07:57:54 -0500

    feat(cart): add magento payment method testing factory

---
+++ libs/cart/testing/src/factories/index.ts
@@ -0,0 +1,7 @@
+export { DaffCartFactory } from './cart.factory';
+export { DaffCartItemFactory } from './cart-item.factory';
+export { DaffCartAddressFactory } from './cart-address.factory';
+export { DaffCartPaymentFactory } from './cart-payment.factory';
+export { DaffCartShippingRateFactory } from './cart-shipping-rate.factory';
+
+export * from './magento';
