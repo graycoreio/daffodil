7f3f535165cd78ed73840e60bcc3fe669183e8aa
Peter Lauck <griest024@gmail.com>
2020-02-29 13:08:44 -0500

    feat(cart): add fragments

--- libs/cart/src/drivers/magento/queries/fragments/index.ts
+++ libs/cart/src/drivers/magento/queries/fragments/index.ts
@@ -1,3 +1,7 @@
 export { moneyFragment } from './money';
+export { cartCouponFragment } from './cart-coupon';
+export { cartAddressFragment } from './cart-address';
+export { cartItemFragment } from './cart-item';
 export { cartPaymentMethodFragment } from './cart-payment-method';
 export { cartShippingMethodFragment } from './cart-shipping-method';
+export { cartFragment } from './cart';
