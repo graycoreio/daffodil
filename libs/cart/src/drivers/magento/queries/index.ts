551f6268b3769d685caba941d35dd7fa99e0b8f6
Peter Lauck <griest024@gmail.com>
2020-02-29 13:09:10 -0500

    feat(cart): add shipping address queries

--- libs/cart/src/drivers/magento/queries/index.ts
+++ libs/cart/src/drivers/magento/queries/index.ts
@@ -2,3 +2,5 @@ export * from './fragments';
 
 export { listPaymentMethods } from './list-payment-methods';
 export { listShippingMethods } from './list-shipping-methods';
+export { getShippingAddress } from './get-shipping-address';
+export { updateShippingAddress } from './update-shipping-address';
