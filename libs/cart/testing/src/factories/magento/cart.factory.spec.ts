83c3fea515da244daa4464535f3a6e7de8b64710
Peter Lauck <griest024@gmail.com>
2020-03-02 12:24:24 -0500

    feat(cart): add MagentoCart testing factory

---
+++ libs/cart/testing/src/factories/magento/cart.factory.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MagentoCartFactory } from './cart.factory';
+import { MagentoCart } from '@daffodil/cart';
+
+describe('Cart | Testing | Factories | CartFactory', () => {
+  let factory: MagentoCartFactory;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MagentoCartFactory]
+    });
+
+    factory = TestBed.get(MagentoCartFactory);
+  });
+
+  it('should be created', () => {
+    expect(factory).toBeTruthy();
+  });
+
+  describe('create', () => {
+    let result: MagentoCart;
+
+    beforeEach(() => {
+      result = factory.create();
+    });
+
+    it('should return a Cart with all required fields defined', () => {
+      expect(result.id).toBeDefined();
+      expect(result.applied_coupons).toBeDefined();
+      expect(result.items).toBeDefined();
+      expect(result.billing_address).toBeDefined();
+      expect(result.shipping_addresses).toBeDefined();
+      expect(result.available_payment_methods).toBeDefined();
+      expect(result.selected_payment_method).toBeDefined();
+      expect(result.prices.subtotal_excluding_tax).toBeDefined();
+      expect(result.prices.grand_total).toBeDefined();
+      expect(result.prices.subtotal_including_tax).toBeDefined();
+      expect(result.prices.subtotal_with_discount_excluding_tax).toBeDefined();
+      expect(result.email).toBeDefined();
+    });
+  });
+});
--- libs/cart/testing/src/factories/magento/index.ts
+++ libs/cart/testing/src/factories/magento/index.ts
@@ -2,6 +2,7 @@ export { MagentoCartAddressFactory } from './cart-address.factory';
 export { MagentoCartCouponFactory } from './cart-coupon.factory';
 export { MagentoCartPaymentMethodFactory } from './cart-payment-method.factory';
 export { MagentoCartShippingMethodFactory } from './cart-shipping-method.factory';
+export { MagentoCartFactory } from './cart.factory';
 export { MagentoCartItemFactory } from './cart-item.factory';
 export { MagentoMoneyFactory } from './money.factory';
 export { MagentoShippingAddressFactory } from './shipping-address.factory';
