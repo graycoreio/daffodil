391c6c64d9b260e53ca43c349f47fb9379d12eb0
Peter Lauck <griest024@gmail.com>
2020-03-02 12:21:30 -0500

    feat(cart): add MagentoCartCoupon testing factory

--- libs/cart/testing/src/factories/magento/index.ts
+++ libs/cart/testing/src/factories/magento/index.ts
@@ -1,3 +1,4 @@
+export { MagentoCartCouponFactory } from './cart-coupon.factory';
 export { MagentoCartPaymentMethodFactory } from './cart-payment-method.factory';
 export { MagentoCartShippingMethodFactory } from './cart-shipping-method.factory';
 export { MagentoMoneyFactory } from './money.factory';
