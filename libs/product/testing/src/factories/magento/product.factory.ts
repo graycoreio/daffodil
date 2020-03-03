495f7e95de100400cdb343fd612fc15640926c75
griest024 <griest024@gmail.com>
2020-03-03 10:54:50 -0500

    feat(product): add MagentoProduct testing factory (#665)
    * feat(product): export ProductNode as MagentoProduct
    
    * feat(product): add MagentoProduct testing factory

---
+++ libs/product/testing/src/factories/magento/product.factory.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import * as faker from 'faker/locale/en_US';
+
+import {
+  DaffModelFactory,
+} from '@daffodil/core/testing';
+import { MagentoProduct } from '@daffodil/product';
+
+export class MockMagentoProduct implements MagentoProduct {
+  id = faker.random.number(1000);
+  image = {
+    label: faker.random.words(3),
+    url: faker.image.imageUrl()
+  };
+  name = faker.random.word();
+  description = faker.random.words(5);
+  sku = faker.random.alphaNumeric(16);
+  url_key = faker.random.alphaNumeric(16);
+  price = {
+    regularPrice: faker.random.number(1000)
+  };
+  short_description = faker.random.words(3);
+  media_gallery_entries = null;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MagentoProductFactory extends DaffModelFactory<MagentoProduct> {
+
+  constructor(){
+    super(MockMagentoProduct);
+  }
+}
