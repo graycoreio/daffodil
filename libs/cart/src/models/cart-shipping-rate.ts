2e76791ccb741a5ce700265517d11d3360f861b7
Peter Lauck <griest024@gmail.com>
2020-03-05 16:04:25 -0500

    feat(cart): add method_code field to DaffCartShippingRate

--- libs/cart/src/models/cart-shipping-rate.ts
+++ libs/cart/src/models/cart-shipping-rate.ts
@@ -2,6 +2,7 @@ export interface DaffCartShippingRate {
   id: string | number;
   carrier: string;
   carrier_title: string;
+  method_code: string;
   method_title: string;
   method_description: string;
   price: number;
