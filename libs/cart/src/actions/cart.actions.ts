9b3bb24d9306300760d995022154ff4ec080c766
Peter Lauck <griest024@gmail.com>
2020-03-06 14:43:44 -0500

    feat(cart): add cart actions

--- libs/cart/src/actions/cart.actions.ts
+++ libs/cart/src/actions/cart.actions.ts
@@ -3,27 +3,28 @@ import { Action } from '@ngrx/store';
 import { DaffCart } from '../models/cart';
 
 export enum DaffCartActionTypes {
-    CartLoadAction = '[DaffCart] Load Action',
-    CartLoadSuccessAction = '[DaffCart] Load Success Action',
-    CartLoadFailureAction = '[DaffCart] Load Failure Action',
-    AddToCartAction = '[DaffCart] Add To Cart Action',
-    AddToCartSuccessAction = '[DaffCart] Add to Cart Success Action',
-    AddToCartFailureAction = '[DaffCart] Add to Cart Failure Action',    
-    CartResetAction = '[DaffCart] Reset Action',
-    CartResetSuccessAction = '[DaffCart] Reset Success Action',
-    CartResetFailureAction = '[DaffCart] Reset Failure Action'
+  CartLoadAction = '[DaffCart] Cart Load Action',
+  CartLoadSuccessAction = '[DaffCart] Cart Load Success Action',
+  CartLoadFailureAction = '[DaffCart] Cart Load Failure Action',
+  CartCreateAction = '[DaffCart] Cart Create Action',
+  CartCreateSuccessAction = '[DaffCart] Cart Create Success Action',
+  CartCreateFailureAction = '[DaffCart] Cart Create Failure Action',
+  AddToCartAction = '[DaffCart] Cart Add To Cart Action',
+  AddToCartSuccessAction = '[DaffCart] Cart Add to Cart Success Action',
+  AddToCartFailureAction = '[DaffCart] Cart Add to Cart Failure Action',
+  CartResetAction = '[DaffCart] Cart Reset Action',
+  CartResetSuccessAction = '[DaffCart] Cart Reset Success Action',
+  CartResetFailureAction = '[DaffCart] Cart Reset Failure Action'
 }
 
 export class DaffCartLoad implements Action {
   readonly type = DaffCartActionTypes.CartLoadAction;
-
-  constructor() {}
 }
 
-export class DaffCartLoadSuccess implements Action {
-    readonly type = DaffCartActionTypes.CartLoadSuccessAction;
+export class DaffCartLoadSuccess<T extends DaffCart> implements Action {
+  readonly type = DaffCartActionTypes.CartLoadSuccessAction;
 
-    constructor(public payload: DaffCart) {}
+  constructor(public payload: T) {}
 }
 
 export class DaffCartLoadFailure implements Action {
@@ -32,46 +33,66 @@ export class DaffCartLoadFailure implements Action {
   constructor(public payload: string) {}
 }
 
-export class DaffAddToCart implements Action {
-    readonly type = DaffCartActionTypes.AddToCartAction;
+export class DaffCartCreate implements Action {
+  readonly type = DaffCartActionTypes.CartCreateAction;
+}
 
-    constructor(public payload: {productId: string, qty: number}) {}
+export class DaffCartCreateSuccess<T extends DaffCart> implements Action {
+  readonly type = DaffCartActionTypes.CartCreateSuccessAction;
+
+  constructor(public payload: {id: T['id']}) {}
+}
+
+export class DaffCartCreateFailure implements Action {
+  readonly type = DaffCartActionTypes.CartCreateFailureAction;
+
+  constructor(public payload: string) {}
+}
+
+export class DaffAddToCart implements Action {
+  readonly type = DaffCartActionTypes.AddToCartAction;
+
+  constructor(public payload: {productId: string, qty: number}) {}
 }
 
 export class DaffAddToCartSuccess implements Action {
-    readonly type = DaffCartActionTypes.AddToCartSuccessAction;
+  readonly type = DaffCartActionTypes.AddToCartSuccessAction;
 
-    constructor(public payload: DaffCart) {}
+  constructor(public payload: DaffCart) {}
 }
 
 export class DaffAddToCartFailure implements Action {
-    readonly type = DaffCartActionTypes.AddToCartFailureAction;
+  readonly type = DaffCartActionTypes.AddToCartFailureAction;
 
-    constructor(public payload: string) {}
+  constructor(public payload: string) {}
 }
 
 export class DaffCartReset implements Action {
-    readonly type = DaffCartActionTypes.CartResetAction;
+  readonly type = DaffCartActionTypes.CartResetAction;
 }
 
-export class DaffCartResetSuccess implements Action {
-    readonly type = DaffCartActionTypes.CartResetSuccessAction;
-    constructor(public payload: Partial<DaffCart>){}
+export class DaffCartResetSuccess<T extends DaffCart> implements Action {
+  readonly type = DaffCartActionTypes.CartResetSuccessAction;
+
+  constructor(public payload: Partial<T>) {}
 }
 
 export class DaffCartResetFailure implements Action {
-    readonly type = DaffCartActionTypes.CartResetFailureAction;
+  readonly type = DaffCartActionTypes.CartResetFailureAction;
 
-    constructor(public payload: string){}
+  constructor(public payload: string) {}
 }
 
-export type DaffCartActions = 
-    | DaffCartLoad 
-    | DaffCartLoadSuccess
-    | DaffCartLoadFailure
-    | DaffAddToCart
-    | DaffAddToCartSuccess
-    | DaffAddToCartFailure
-    | DaffCartReset
-    | DaffCartResetSuccess
-    | DaffCartResetFailure;
+export type DaffCartActions<T extends DaffCart> =
+  | DaffCartLoad
+  | DaffCartLoadSuccess<T>
+  | DaffCartLoadFailure
+  | DaffCartCreate
+  | DaffCartCreateSuccess<T>
+  | DaffCartCreateFailure
+  | DaffAddToCart
+  | DaffAddToCartSuccess
+  | DaffAddToCartFailure
+  | DaffCartReset
+  | DaffCartResetSuccess<T>
+  | DaffCartResetFailure;
