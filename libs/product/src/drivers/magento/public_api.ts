450c9237d04910388732810286d2017092d50f14
Luke <lderrickable@gmail.com>
2020-03-11 16:52:34 -0400

    fix build

--- libs/product/src/drivers/magento/public_api.ts
+++ libs/product/src/drivers/magento/public_api.ts
@@ -2,3 +2,4 @@ export * from './models/product-node';
 export * from './models/bundled-product';
 export { DaffMagentoProductTransformerService } from './transforms/product-transformer.service';
 export { DaffSortField } from './models/sort-field';
+export { magentoProductFragment } from './queries/fragments/product';
