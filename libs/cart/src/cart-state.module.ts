4c8ac17167b29626f7e5879fd1e6e972bea81421
Peter Lauck <griest024@gmail.com>
2020-03-13 15:48:31 -0400

    feat(cart): add effects to store module

--- libs/cart/src/cart-state.module.ts
+++ libs/cart/src/cart-state.module.ts
@@ -5,12 +5,26 @@ import { EffectsModule } from '@ngrx/effects';
 
 import { daffCartReducers } from './reducers';
 import { DaffCartEffects } from './effects/cart.effects';
+import { DaffCartItemEffects } from './effects/cart-item.effects';
+import { DaffCartBillingAddressEffects } from './effects/cart-billing-address.effects';
+import { DaffCartShippingAddressEffects } from './effects/cart-shipping-address.effects';
+import { DaffCartShippingInformationEffects } from './effects/cart-shipping-information.effects';
+import { DaffCartShippingMethodsEffects } from './effects/cart-shipping-methods.effects';
+import { DaffCartPaymentEffects } from './effects/cart-payment.effects';
+import { DaffCartPaymentMethodsEffects } from './effects/cart-payment-methods.effects';
 
 @NgModule({
   imports: [
     StoreModule.forFeature('cart', daffCartReducers),
     EffectsModule.forFeature([
-      DaffCartEffects
+      DaffCartEffects,
+      DaffCartItemEffects,
+      DaffCartBillingAddressEffects,
+      DaffCartShippingAddressEffects,
+      DaffCartShippingInformationEffects,
+      DaffCartShippingMethodsEffects,
+      DaffCartPaymentEffects,
+      DaffCartPaymentMethodsEffects
     ]),
   ]
 })
