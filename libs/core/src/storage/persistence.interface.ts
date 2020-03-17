aba8c4efee91125518a8c4b88fe8598f0a0c84a4
Peter Lauck <griest024@gmail.com>
2020-03-17 18:22:46 -0400

    feat(core, cart): inject persistence service with token

--- libs/core/src/storage/persistence.interface.ts
+++ libs/core/src/storage/persistence.interface.ts
@@ -1,6 +1,15 @@
+import { InjectionToken, inject, PLATFORM_ID } from '@angular/core';
+
+import { DaffLocalStorageService } from './localstorage/localstorage.service';
+
 export interface DaffPersistenceService {
   setItem(key : string, value: any): void;
   getItem(key: string): any;
   clear(): void;
   removeItem(key: string): void;
 }
+
+export const DaffPersistenceServiceToken = new InjectionToken<DaffPersistenceService>('DaffPersistenceService', {
+  providedIn: 'root',
+  factory: () => new DaffLocalStorageService(inject<string>(PLATFORM_ID)),
+});
--- libs/core/src/storage/public_api.ts
+++ libs/core/src/storage/public_api.ts
@@ -1,4 +1,4 @@
 export { DaffStorageServiceError } from './error/error';
 export { DaffErrorStorageService } from './error/error.service';
 export { DaffLocalStorageService } from './localstorage/localstorage.service';
-export { DaffPersistenceService } from './persistence.interface';
+export { DaffPersistenceService, DaffPersistenceServiceToken } from './persistence.interface';
