c359375fad34853231c8019aadfaad7f9198ff93
Peter Lauck <griest024@gmail.com>
2020-03-23 13:03:48 -0400

    feat(geography): add transformers

---
+++ libs/geography/src/drivers/magento/transforms/responses/subdivision.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { DaffSubdivision } from '../../../../models/subdivision';
+import { MagentoRegion } from '../../models/responses/region';
+
+/**
+ * Transforms magento carts into an object usable by daffodil.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class DaffMagentoSubdivisionTransformer {
+  /**
+   * Transforms the MagentoRegion from the magento region query into a DaffSubdivision.
+   * @param region the region from a magento region query.
+   */
+  transform(region: MagentoRegion): DaffSubdivision {
+    return region ? {
+      ...{magento_region: region},
+
+      id: String(region.id),
+      name: region.name,
+      iso_3166_2: region.code
+    } : null
+  }
+}
