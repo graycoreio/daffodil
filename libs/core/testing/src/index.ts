dc7e3ff6b5ba5cafe236bbd90f4544745471e0e2
Luke <lderrickable@gmail.com>
2020-03-25 13:30:13 -0400

    feat(core, cart): add interface for in memory angular web api services

--- libs/core/testing/src/index.ts
+++ libs/core/testing/src/index.ts
@@ -3,6 +3,7 @@ export { DaffAddressFactory } from './address/factories/address.factory';
 
 //@angular/core
 export { DaffMockCurrencyPipe } from './angular/mocks/pipes/currency';
+export { DaffInMemoryDataServiceInterface } from './angular/interfaces/in-memory-data-service.interface';
 
 //Core
 export { DaffCoreTestingModule } from './testing.module';
