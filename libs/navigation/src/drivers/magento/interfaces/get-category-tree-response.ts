a68dc770c66f6f6a06c677545bcae26dca451387
Luke <lderrickable@gmail.com>
2020-03-30 13:27:12 -0400

    refactor(navigation): update magento navigation call to use categoryList query

--- libs/navigation/src/drivers/magento/interfaces/get-category-tree-response.ts
+++ libs/navigation/src/drivers/magento/interfaces/get-category-tree-response.ts
@@ -1,5 +1,5 @@
 import { CategoryNode } from './category-node';
 
 export interface GetCategoryTreeResponse {
-  category?: CategoryNode
+  categoryList?: CategoryNode[]
 }
--- libs/navigation/src/drivers/magento/queries/get-category-tree.ts
+++ libs/navigation/src/drivers/magento/queries/get-category-tree.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export const GetCategoryTree = gql`
-  query GetCategoryTree($id: Int!){
-    category(id: $id) {
+  query GetCategoryTree($filters: CategoryFilterInput!){
+    categoryList(filters: $filters) {
       id
       name
       include_in_menu
