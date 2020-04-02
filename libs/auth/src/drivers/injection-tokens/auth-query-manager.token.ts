4597752ea92140cc5f85242ddac7237ac4551f22
Peter Lauck <griest024@gmail.com>
2020-04-02 16:12:35 -0400

    feat(auth): remove unneeded interfaces and injection tokens

--- libs/auth/src/drivers/injection-tokens/auth-query-manager.token.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-/**
- * Injection token that serves as a placeholder for any service that implements the DaffAuthQueryManagerInterface.
- */
-export const DaffAuthQueryManager = new InjectionToken('DaffAuthQueryManager');
--- libs/auth/src/drivers/injection-tokens/auth-transformer.token.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-/**
- * Injection token that serves as a placeholder for any service that implements the DaffAuthTransformerInterface.
- */
-export const DaffAuthTransformer =
-    new InjectionToken('DaffAuthTransformer');
