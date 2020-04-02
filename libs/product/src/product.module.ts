754a1c98e0a081b9352595ea2a8fd83b5329bc05
Luke <lderrickable@gmail.com>
2020-04-02 14:12:12 -0400

    feat(product): remove templating containers

--- libs/product/src/product.module.ts
+++ libs/product/src/product.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DaffProductGridContainer } from './containers/product-grid/product-grid.component';
 import { DaffProductStateModule } from './product-state.module';
-import { DaffProductContainer } from './containers/product/product.component';
-import { DaffBestSellersContainer } from './containers/best-sellers/best-sellers.component';
 
 @NgModule({
   imports: [
@@ -14,16 +11,6 @@ import { DaffBestSellersContainer } from './containers/best-sellers/best-sellers
      * Ngrx/store
      */
     DaffProductStateModule,
-  ],
-  declarations: [
-    DaffProductGridContainer,
-    DaffProductContainer,
-    DaffBestSellersContainer
-  ],
-  exports: [
-    DaffProductGridContainer,
-    DaffProductContainer,
-    DaffBestSellersContainer
   ]
 })
 export class DaffProductModule { }
