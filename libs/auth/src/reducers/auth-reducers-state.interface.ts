c7a43639bfa3b427319c80b993f1d4325a8d233d
Peter Lauck <griest024@gmail.com>
2020-04-03 15:46:36 -0400

    refactor(auth): change reducers state to feature state

--- libs/auth/src/reducers/auth-reducers-state.interface.ts
+++ libs/auth/src/reducers/auth-reducers-state.interface.ts
@@ -3,7 +3,7 @@ import { DaffAuthReducerState } from './auth/public_api';
 import { DaffAuthLoginReducerState } from './login/public_api';
 import { DaffAuthRegisterReducerState } from './register/public_api';
 
-export interface DaffAuthReducersState<T extends DaffAuthToken> {
+export interface DaffAuthFeatureState<T extends DaffAuthToken> {
   auth: DaffAuthReducerState,
   login: DaffAuthLoginReducerState<T>,
   register: DaffAuthRegisterReducerState
--- libs/auth/src/reducers/auth-reducers.ts
+++ libs/auth/src/reducers/auth-reducers.ts
@@ -1,11 +1,11 @@
 import { ActionReducerMap } from '@ngrx/store';
 
-import { DaffAuthReducersState } from './auth-reducers-state.interface';
+import { DaffAuthFeatureState } from './auth-reducers-state.interface';
 import { daffAuthReducer } from './auth/public_api';
 import { daffAuthLoginReducer } from './login/public_api';
 import { daffAuthRegisterReducer } from './register/public_api';
 
-export const daffAuthReducers : ActionReducerMap<DaffAuthReducersState<any>> = {
+export const daffAuthReducers : ActionReducerMap<DaffAuthFeatureState<any>> = {
   auth: daffAuthReducer,
   login: daffAuthLoginReducer,
   register: daffAuthRegisterReducer
