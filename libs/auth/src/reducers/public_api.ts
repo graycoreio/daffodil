142c3a5226be3f5e63acb4e4efa5afd9482cbb99
Peter Lauck <griest024@gmail.com>
2020-04-03 11:30:27 -0400

    feat(auth): export feature key

--- libs/auth/src/reducers/public_api.ts
+++ libs/auth/src/reducers/public_api.ts
@@ -1,3 +1,4 @@
 export { DaffAuthReducerState } from './auth-reducer-state.interface';
 export { daffAuthReducer } from './auth.reducer';
 export { daffAuthInitialState } from './auth-initial-state'
+export { DAFF_AUTH_STORE_FEATURE_KEY } from './auth-store-feature-key';
