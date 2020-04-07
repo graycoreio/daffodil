64304c930277f69ac4b2efea4259765fb9ec90db
Peter Lauck <griest024@gmail.com>
2020-04-07 15:20:57 -0400

    action reducer map implicit type

--- libs/auth/src/reducers/auth-reducers.ts
+++ libs/auth/src/reducers/auth-reducers.ts
@@ -5,7 +5,7 @@ import { daffAuthReducer } from './auth/public_api';
 import { daffAuthLoginReducer } from './login/public_api';
 import { daffAuthRegisterReducer } from './register/public_api';
 
-export const daffAuthReducers: ActionReducerMap<DaffAuthFeatureState<any>> = {
+export const daffAuthReducers = {
   auth: daffAuthReducer,
   login: daffAuthLoginReducer,
   register: daffAuthRegisterReducer
