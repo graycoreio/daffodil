bc1538a1d4c66f4eb91ae55807019ee96a643d09
Peter Lauck <griest024@gmail.com>
2020-04-07 15:38:35 -0400

    remove unused imports

--- libs/auth/src/reducers/auth-reducers.ts
+++ libs/auth/src/reducers/auth-reducers.ts
@@ -1,6 +1,3 @@
-import { ActionReducerMap } from '@ngrx/store';
-
-import { DaffAuthFeatureState } from './auth-feature-state.interface';
 import { daffAuthReducer } from './auth/public_api';
 import { daffAuthLoginReducer } from './login/public_api';
 import { daffAuthRegisterReducer } from './register/public_api';
