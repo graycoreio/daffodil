68800f27a6e757e6c6fe22b98a51b107c945bc3a
Nolan Arnold <nolan.arnold@gmail.com>
2020-04-07 12:17:36 -0500

    feat(daffio): added a functional guide list to sidebar

--- apps/daffio/src/app/docs/shared/components/guides-viewer/guides-viewer.component.spec.ts
+++ apps/daffio/src/app/docs/shared/components/guides-viewer/guides-viewer.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DaffioGuidesViewerComponent } from './guides-viewer.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DaffLinkSetModule } from '@daffodil/design';
 
 describe('GuidesViewerComponent', () => {
   let component: DaffioGuidesViewerComponent;
@@ -8,9 +10,13 @@ describe('GuidesViewerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DaffioGuidesViewerComponent ]
+      declarations: [DaffioGuidesViewerComponent],
+      imports: [
+        RouterTestingModule,
+        DaffLinkSetModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- apps/daffio/src/app/docs/testing/factories/doc.factory.ts
+++ apps/daffio/src/app/docs/testing/factories/doc.factory.ts
@@ -9,90 +9,90 @@ export class MockDoc implements DaffioDoc {
   contents = faker.lorem.paragraph()
 };
 export const mockGuides = {
-  "id": "",
-  "title": "",
-  "children": [
+  'id': '',
+  'title': '',
+  'children': [
     {
-      "id": "cart",
-      "title": "@daffodil/cart",
-      "children": [
+      'id': 'cart',
+      'title': '@daffodil/cart',
+      'children': [
         {
-          "id": "",
-          "title": "Overview",
-          "children": [],
-          "path": "guides/cart"
+          'id': '',
+          'title': 'Overview',
+          'children': [],
+          'path': 'guides/cart'
         },
         {
-          "id": "install",
-          "title": "Installation",
-          "children": [],
-          "path": "guides/cart/install"
+          'id': 'install',
+          'title': 'Installation',
+          'children': [],
+          'path': 'guides/cart/install'
         },
         {
-          "id": "testing",
-          "title": "Testing",
-          "children": [],
-          "path": "guides/cart/testing"
+          'id': 'testing',
+          'title': 'Testing',
+          'children': [],
+          'path': 'guides/cart/testing'
         }
       ]
     },
     {
-      "id": "category",
-      "title": "@daffodil/category",
-      "children": [],
-      "path": "guides/category"
+      'id': 'category',
+      'title': '@daffodil/category',
+      'children': [],
+      'path': 'guides/category'
     },
     {
-      "id": "checkout",
-      "title": "@daffodil/checkout",
-      "children": [],
-      "path": "guides/checkout"
+      'id': 'checkout',
+      'title': '@daffodil/checkout',
+      'children': [],
+      'path': 'guides/checkout'
     },
     {
-      "id": "core",
-      "title": "@daffodil/core",
-      "children": [],
-      "path": "guides/core"
+      'id': 'core',
+      'title': '@daffodil/core',
+      'children': [],
+      'path': 'guides/core'
     },
     {
-      "id": "design",
-      "title": "@daffodil/design",
-      "children": [
+      'id': 'design',
+      'title': '@daffodil/design',
+      'children': [
         {
-          "id": "",
-          "title": "Overview",
-          "children": [],
-          "path": "guides/design"
+          'id': '',
+          'title': 'Overview',
+          'children': [],
+          'path': 'guides/design'
         },
         {
-          "id": "theming",
-          "title": "Theming",
-          "children": [
+          'id': 'theming',
+          'title': 'Theming',
+          'children': [
             {
-              "id": "installation",
-              "title": "Daffodil Theming Installation Guide",
-              "children": [],
-              "path": "guides/design/theming/installation"
+              'id': 'installation',
+              'title': 'Daffodil Theming Installation Guide',
+              'children': [],
+              'path': 'guides/design/theming/installation'
             }
           ]
         }
       ]
     },
     {
-      "id": "product",
-      "title": "@daffodil/product",
-      "children": [
+      'id': 'product',
+      'title': '@daffodil/product',
+      'children': [
         {
-          "id": "",
-          "title": "Overview",
-          "children": [],
-          "path": "guides/product"
+          'id': '',
+          'title': 'Overview',
+          'children': [],
+          'path': 'guides/product'
         },
         {
-          "id": "in-memory-driver",
-          "title": "Using the InMemory Driver",
-          "children": [],
-          "path": "guides/product/in-memory-driver"
+          'id': 'in-memory-driver',
+          'title': 'Using the InMemory Driver',
+          'children': [],
+          'path': 'guides/product/in-memory-driver'
         }
       ]
     }
