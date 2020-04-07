aebd0acdd4696c9530732571591b91ad2755f717
Peter Lauck <griest024@gmail.com>
2020-04-07 15:08:32 -0400

    concrete action reducer map

--- libs/geography/src/reducers/geography-reducers.ts
+++ libs/geography/src/reducers/geography-reducers.ts
@@ -3,9 +3,8 @@ import { ActionReducerMap } from '@ngrx/store';
 import { daffGeographyReducer } from './geography/geography.reducer';
 import { DaffGeographyFeatureState } from './geography-reducers-state.interface';
 import { daffCountryEntitiesReducer } from './country-entities/country-entities.reducer';
-import { DaffCountry } from '../models/country';
 
-export const daffGeographyReducers = <T extends DaffCountry>(): ActionReducerMap<DaffGeographyFeatureState<T>> => ({
+export const daffGeographyReducers: ActionReducerMap<DaffGeographyFeatureState<any>> = ({
   geography: daffGeographyReducer,
   countries: daffCountryEntitiesReducer
 })
