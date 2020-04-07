33612641915b15c6d3b7330746c4974ad44961ed
Peter Lauck <griest024@gmail.com>
2020-04-07 11:45:44 -0400

    feat(auth): make reducers generic

--- libs/auth/src/reducers/auth-reducers.ts
+++ libs/auth/src/reducers/auth-reducers.ts
@@ -1,12 +1,15 @@
 import { ActionReducerMap } from '@ngrx/store';
 
-import { DaffAuthFeatureState } from './auth-reducers-state.interface';
+import { DaffAuthFeatureState } from './auth-feature-state.interface';
 import { daffAuthReducer } from './auth/public_api';
 import { daffAuthLoginReducer } from './login/public_api';
 import { daffAuthRegisterReducer } from './register/public_api';
+import { DaffAuthToken } from '../models/auth-token';
 
-export const daffAuthReducers : ActionReducerMap<DaffAuthFeatureState<any>> = {
-  auth: daffAuthReducer,
-  login: daffAuthLoginReducer,
-  register: daffAuthRegisterReducer
+export function daffAuthReducers <T extends DaffAuthToken>(): ActionReducerMap<DaffAuthFeatureState<T>> {
+  return {
+    auth: daffAuthReducer,
+    login: daffAuthLoginReducer,
+    register: daffAuthRegisterReducer
+  };
 }
--- libs/auth/src/reducers/public_api.ts
+++ libs/auth/src/reducers/public_api.ts
@@ -4,4 +4,4 @@ export * from './register/public_api';
 
 export { DAFF_AUTH_STORE_FEATURE_KEY } from './auth-store-feature-key';
 export { daffAuthReducers } from './auth-reducers';
-export { DaffAuthFeatureState } from './auth-reducers-state.interface';
+export { DaffAuthFeatureState } from './auth-feature-state.interface';
