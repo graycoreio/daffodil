d12221bcf10056b105c6cbaec138ea0b6e94c93c
Luke <lderrickable@gmail.com>
2020-04-15 10:38:48 -0400

    add stronger typing to an interface function

--- libs/product/src/facades/product-grid/product-grid-facade.interface.ts
+++ libs/product/src/facades/product-grid/product-grid-facade.interface.ts
@@ -1,9 +1,10 @@
 import { Observable } from 'rxjs';
+import { Action } from '@ngrx/store';
 
 import { DaffProduct } from '../../models/product';
 
 export interface DaffProductGridFacadeInterface<T extends DaffProduct = DaffProduct> {
 	loading$: Observable<boolean>;
 	products$: Observable<T[]>;
-	dispatch: Function;
+	dispatch(action: Action): void;
 }
