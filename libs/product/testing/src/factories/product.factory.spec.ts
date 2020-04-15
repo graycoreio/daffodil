0299d5102099ab0adb55b256bef6257a6669a3c5
Luke Derrick <lderrickable@gmail.com>
2020-04-15 19:24:07 -0400

    feat(product): remove deprecated __typename value from DaffProduct (#817)

--- libs/product/testing/src/factories/product.factory.spec.ts
+++ libs/product/testing/src/factories/product.factory.spec.ts
@@ -29,8 +29,8 @@ describe('Product | Testing | Factories | DaffProductFactory', () => {
     
     it('should return a Product with all required fields defined', () => {
       expect(result.type).toBeDefined();
-      expect(result.__typename).toBeDefined();
-      expect(result.id).toBeDefined();
+			expect(result.id).toBeDefined();
+      expect(result.url).toBeDefined();
       expect(result.price).toBeDefined();
       expect(result.name).toBeDefined();
       expect(result.brand).toBeDefined(); 
