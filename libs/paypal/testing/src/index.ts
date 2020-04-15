81c8b6737280a091a5cc3c7905b4257d6c3523ab
Luke <lderrickable@gmail.com>
2020-04-14 20:39:35 -0400

    feat(paypal): add paypal testing module to test facade usage

--- libs/paypal/testing/src/index.ts
+++ libs/paypal/testing/src/index.ts
@@ -4,3 +4,5 @@ export { DaffTestingPaypalService } from './drivers/testing/paypal.service';
 export { DaffInMemoryPaypalService } from './drivers/in-memory/paypal.service';
 export { DaffPaypalInMemoryDriverModule } from './drivers/in-memory/paypal-driver.module';
 export { DaffPaypalTestingDriverModule } from './drivers/testing/paypal-driver.module';
+export { DaffPaypalTestingModule } from './helpers/paypal-testing.module';
+export { MockDaffPaypalFacade } from './helpers/mock-paypal-facade';
