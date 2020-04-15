4149e25ebe36207d653dc53b396779faa28e0c0c
Peter Lauck <griest024@gmail.com>
2020-04-15 15:59:26 -0400

    change typing

--- libs/cart/src/drivers/interfaces/cart-order-service.interface.ts
+++ libs/cart/src/drivers/interfaces/cart-order-service.interface.ts
@@ -7,11 +7,14 @@ import { DaffCartPaymentMethod } from '../../models/cart-payment';
 /**
  * The interface responsible for managing a customer's cart.
  */
-export interface DaffCartOrderServiceInterface<T extends DaffCart = DaffCart> {
+export interface DaffCartOrderServiceInterface<
+  T extends DaffCart = DaffCart,
+  V extends DaffCartPaymentMethod = DaffCartPaymentMethod
+> {
 	/**
 	 * Place an order.
 	 */
-  placeOrder(id: T['id'], payment?: DaffCartPaymentMethod): Observable<T>;
+  placeOrder(id: T['id'], payment?: V): Observable<{id: string | number}>;
 }
 
 export const DaffCartOrderDriver = new InjectionToken<DaffCartOrderServiceInterface>(
