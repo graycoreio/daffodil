b50bfe6c8f448fdf3c165cc40a83e14abee6ad9e
Luke <lderrickable@gmail.com>
2020-04-15 20:06:42 -0400

    fix category package from rebase

--- libs/category/src/drivers/magento/models/outputs/category-products-node.ts
+++
@@ -1,8 +0,0 @@
-import { MagentoProduct } from '@daffodil/product';
-import { CategoryPageInfoNode } from './category-page-info-node';
-
-export interface CategoryProductsNode {
-  total_count: number;
-  page_info: CategoryPageInfoNode;
-  items: MagentoProduct[];
-}
