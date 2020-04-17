dc550a756af55e14a88c82b09c1c4e5e1db429a4
Luke <lderrickable@gmail.com>
2020-04-17 13:02:20 -0400

    change the cart coupon interface

--- libs/cart/src/drivers/interfaces/cart-coupon-service.interface.ts
+++ libs/cart/src/drivers/interfaces/cart-coupon-service.interface.ts
@@ -5,13 +5,23 @@ import { DaffCartCoupon } from '../../models/cart-coupon';
 import { DaffCart } from '../../models/cart';
 
 /**
- * The interface responsible for apply a coupon to a cart.
+ * The interface responsible for applying a coupon to a cart.
  */
 export interface DaffCartCouponServiceInterface<T extends DaffCart = DaffCart, V extends DaffCartCoupon = DaffCartCoupon> {
 	/**
 	 * Apply a coupon to the cart and return a partial of the cart.
 	 */
-  applyCoupon(cartId: T['id'], coupon: V): Observable<Partial<T>>;
+	apply(cartId: T['id'], coupon: V): Observable<Partial<T>>;
+	
+	/**
+	 * Remove a coupon from the cart and return a partial of the cart.
+	 */
+	remove(cartId: T['id'], coupon: V): Observable<Partial<T>>;
+
+	/**
+	 * Remove all coupons from the cart and return a partial of the cart.
+	 */
+	removeAll(cartId: T['id']): Observable<Partial<T>>;
 }
 
 export const DaffCartCouponDriver = new InjectionToken<DaffCartCouponServiceInterface>(
