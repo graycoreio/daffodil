de9cd2815bae18e35728c77b0efe8d40081ed81c
griest024 <griest024@gmail.com>
2020-04-19 17:55:35 -0400

    feat(demo): implement IntrospectionFragmentMatcher (#778)

--- apps/demo/src/app/drivers/magento/magento.module.ts
+++ apps/demo/src/app/drivers/magento/magento.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { IntrospectionFragmentMatcher, InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 
-import { ApolloBoostModule, ApolloBoost } from 'apollo-angular-boost';
 import { DaffProductMagentoDriverModule } from '@daffodil/product';
 import { DaffCheckoutInMemoryDriverModule } from '@daffodil/checkout/testing';
 import { DaffNavigationMagentoDriverModule } from '@daffodil/navigation';
@@ -9,10 +13,19 @@ import { DaffCartMagentoDriverModule } from '@daffodil/cart';
 
 import { environment } from '../../../environments/environment';
 import { MagentoEnvironmentDriverConfiguration } from '../../../environments/environment.interface';
+import introspectionQueryResultData from './fragmentTypes.json';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData
+});
+
+const cache = new InMemoryCache({ fragmentMatcher });
 
 @NgModule({
   imports: [
-    ApolloBoostModule,
+    //Magento
+    ApolloModule,
+    HttpLinkModule,
     DaffProductMagentoDriverModule.forRoot(),
     DaffCartMagentoDriverModule.forRoot(),
     DaffCheckoutInMemoryDriverModule.forRoot(),
@@ -20,11 +33,26 @@ import { MagentoEnvironmentDriverConfiguration } from '../../../environments/env
     DaffNewsletterInMemoryDriverModule.forRoot()
   ]
 })
-export class DemoMagentoDriverModule { 
+export class DemoMagentoDriverModule {
   // Magento
-  constructor(boost: ApolloBoost) {
-    boost.create({
-      uri: (<MagentoEnvironmentDriverConfiguration>environment.driver).domain + '/graphql',
-    })
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: ApolloLink.from([
+        onError(({graphQLErrors, networkError}) => {
+          if (graphQLErrors)
+            graphQLErrors.map(({message, locations, path}) =>
+              console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+              ),
+            );
+          if (networkError) console.log(`[Network error]: ${networkError}`);
+        }),
+        httpLink.create({
+          uri: (<MagentoEnvironmentDriverConfiguration>environment.driver).domain + '/graphql',
+          withCredentials: false,
+        }),
+      ]),
+      cache,
+    });
   }
 }
