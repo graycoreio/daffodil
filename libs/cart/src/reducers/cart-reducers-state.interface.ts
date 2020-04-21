7a0b6bb46b3f000ade1a7778e2dda0e618e5d08d
Peter Lauck <griest024@gmail.com>
2020-04-21 14:34:07 -0400

    add reducers state generic

--- libs/cart/src/reducers/cart-reducers-state.interface.ts
+++ libs/cart/src/reducers/cart-reducers-state.interface.ts
@@ -1,8 +1,12 @@
 import { DaffCartReducerState } from './cart-state.interface';
 import { DaffCart } from '../models/cart';
 import { DaffCartOrderReducerState } from './cart-order-state.interface';
+import { DaffCartOrderResult } from '../models/cart-order-result';
 
-export interface DaffCartReducersState<T extends DaffCart = DaffCart> {
+export interface DaffCartReducersState<
+  T extends DaffCart = DaffCart,
+  V extends DaffCartOrderResult = DaffCartOrderResult
+> {
   cart: DaffCartReducerState<T>,
-  order: DaffCartOrderReducerState
+  order: DaffCartOrderReducerState<V>
 }
