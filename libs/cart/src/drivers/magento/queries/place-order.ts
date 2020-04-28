1bb6fa910f04d09ea9a9d3b3fd6fc5aa75d70ea8
Peter Lauck <griest024@gmail.com>
2020-04-28 14:31:04 -0400

    fix(cart): malformed Magento place order query

--- libs/cart/src/drivers/magento/queries/place-order.ts
+++ libs/cart/src/drivers/magento/queries/place-order.ts
@@ -2,15 +2,13 @@ import gql from 'graphql-tag';
 
 export const placeOrder = gql`
   mutation PlaceOrder($cartId: String!) {
-    mutation {
-      placeOrder(
-        input: {
-          cart_id: $cartId
-        }
-      ) {
-        order {
-          order_number
-        }
+    placeOrder(
+      input: {
+        cart_id: $cartId
+      }
+    ) {
+      order {
+        order_number
       }
     }
   }
