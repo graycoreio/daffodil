263bc33b4a7870732490a0d065719b39076e998b
Peter Lauck <griest024@gmail.com>
2020-05-04 18:49:03 -0400

    feat(order): add testing factories

---
+++ libs/order/testing/src/factories/order.factory.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as faker from 'faker/locale/en_US';
+
+import { DaffOrder } from '@daffodil/order';
+import { DaffModelFactory } from '@daffodil/core/testing';
+
+export class MockOrder implements DaffOrder {
+  id = faker.random.number(1000);
+  created_at = new Date();
+  updated_at = new Date();
+  store_to_base_rate = faker.random.number(1000);
+  grand_total = faker.random.number(1000);
+  checkout_method = 'card';
+  customer_id = faker.random.number(1000);
+  coupon_code = faker.random.number(100000).toString();
+  subtotal = faker.random.number(1000);
+  subtotal_with_discount = faker.random.number(1000);
+  items = [];
+  addresses = [];
+  payment = null;
+};
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DaffOrderFactory extends DaffModelFactory<DaffOrder>{
+  constructor(){
+    super(MockOrder);
+  }
+}
