b26e3e9922c91d7d010f82627b36c708cd227861
Peter Lauck <griest024@gmail.com>
2020-05-04 13:41:28 -0400

    feat(order): add testing factories (#863)

---
+++ libs/order/testing/src/factories/order-payment.factory.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DaffOrderPayment } from '@daffodil/order';
+import { DaffOrderPaymentFactory } from './order-payment.factory';
+
+describe('Checkout | Testing | Order | Factories | OrderPaymentFactory', () => {
+
+  let orderPaymentFactory: DaffOrderPaymentFactory;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DaffOrderPaymentFactory]
+    });
+
+    orderPaymentFactory = TestBed.get(DaffOrderPaymentFactory);
+  });
+
+  it('should be created', () => {
+    expect(orderPaymentFactory).toBeTruthy();
+  });
+
+  describe('create', () => {
+
+    let result: DaffOrderPayment;
+
+    beforeEach(() => {
+      result = orderPaymentFactory.create();
+    });
+
+    xit('should return a DaffOrderPayment with all required fields defined', () => {
+
+    });
+  });
+
+  describe('createMany', () => {
+    let result: DaffOrderPayment[];
+
+    it('should create as many order payments as desired', () => {
+      result = orderPaymentFactory.createMany(2);
+      expect(result.length).toEqual(2);
+
+      result = orderPaymentFactory.createMany(3);
+      expect(result.length).toEqual(3);
+    })
+  })
+});
---
+++ libs/order/testing/src/factories/order-shipping-rate.factory.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from '@angular/core/testing';
+
+import {  DaffOrderShippingRate } from '@daffodil/order';
+import { DaffOrderShippingRateFactory } from './order-shipping-rate.factory';
+
+describe('Checkout | Testing | Order | Factories | OrderShippingRateFactory', () => {
+
+  let orderShippingRateFactory: DaffOrderShippingRateFactory;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DaffOrderShippingRateFactory]
+    });
+
+    orderShippingRateFactory = TestBed.get(DaffOrderShippingRateFactory);
+  });
+
+  it('should be created', () => {
+    expect(orderShippingRateFactory).toBeTruthy();
+  });
+
+  describe('create', () => {
+
+    let result: DaffOrderShippingRate;
+
+    beforeEach(() => {
+      result = orderShippingRateFactory.create();
+    });
+
+    xit('should return a DaffOrderShippingRate with all required fields defined', () => {
+
+    });
+  });
+
+  describe('createMany', () => {
+    let result: DaffOrderShippingRate[];
+
+    it('should create as many order shipping rates as desired', () => {
+      result = orderShippingRateFactory.createMany(2);
+      expect(result.length).toEqual(2);
+
+      result = orderShippingRateFactory.createMany(3);
+      expect(result.length).toEqual(3);
+    })
+  })
+});
