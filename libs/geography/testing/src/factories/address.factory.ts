1e48abde35b1cdc81b7945e10c10c348f7a05288
griest024 <griest024@gmail.com>
2020-05-06 10:57:35 -0400

    feat(geography): add personal address factory (#873)

--- libs/geography/testing/src/factories/address.factory.ts
+++ libs/geography/testing/src/factories/address.factory.ts
@@ -6,14 +6,10 @@ import { DaffAddress } from '@daffodil/geography';
 import * as faker from 'faker/locale/en_US';
 
 export class MockDaffAddress implements DaffAddress {
-  firstname = faker.name.firstName();
-  lastname = faker.name.lastName();
   street = faker.address.streetName();
   city = faker.address.city();
   region = faker.address.stateAbbr();
-  email = faker.internet.email();
   postcode = faker.address.zipCode();
-  telephone = faker.phone.phoneNumber();
 }
 
 @Injectable({
