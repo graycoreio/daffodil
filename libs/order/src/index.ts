d534dab486b5bb1775a825b80fee9dd6202e71ee
Peter Lauck <griest024@gmail.com>
2020-05-08 09:47:06 -0400

    feat(order): add basic driver interface

--- libs/order/src/index.ts
+++ libs/order/src/index.ts
@@ -1,3 +1,5 @@
+export { DaffOrderDriver, DaffOrderServiceInterface } from './drivers/interfaces/order-service.interface';
+
 export { DaffOrderModule } from './order.module';
 
 export * from './models/public_api';
