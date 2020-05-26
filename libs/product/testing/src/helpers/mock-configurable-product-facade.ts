86a624f7be7b0b3c08e96734df99577e49623cd2
Luke <lderrickable@gmail.com>
2020-05-26 09:54:09 -0400

    testing(product): add more detail to the configurable product spec data

--- libs/product/testing/src/helpers/mock-configurable-product-facade.ts
+++ libs/product/testing/src/helpers/mock-configurable-product-facade.ts
@@ -4,14 +4,17 @@ import { Dictionary } from '@ngrx/entity';
 import { DaffConfigurableProductFacadeInterface } from '@daffodil/product';
 
 export class MockDaffConfigurableProductFacade implements DaffConfigurableProductFacadeInterface {
+	getAllAttributes(id: string): BehaviorSubject<Dictionary<string[]>> {
+		return new BehaviorSubject({});
+	};
 	getAppliedAttributes(id: string): BehaviorSubject<Dictionary<string>> {
 		return new BehaviorSubject({});
 	};
 	getPrice(id: string): BehaviorSubject<string> {
 		return new BehaviorSubject(null);
 	};
-	getUndeterminedAttributes(id: string): BehaviorSubject<Dictionary<string[]>> {
+	getSelectableAttributes(id: string): BehaviorSubject<Dictionary<string[]>> {
 		return new BehaviorSubject({});
 	};
-	dispatch() {};
+	dispatch(action) {};
 }
