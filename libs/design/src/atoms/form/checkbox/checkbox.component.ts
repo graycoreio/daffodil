95af4b98c6ae5b5042e53b9c42b7c58272aa9508
Nolan Arnold <nolan.arnold@gmail.com>
2020-05-26 11:20:59 -0500

    fix(design): fixed linting error

--- libs/design/src/atoms/form/checkbox/checkbox.component.ts
+++ libs/design/src/atoms/form/checkbox/checkbox.component.ts
@@ -59,10 +59,12 @@ export class DaffCheckboxComponent {
   /**
    * The aria-label of the checkbox. If not set by user then it defaults to the name of the checkbox.
    */
+  //tslint:disable-next-line:no-input-rename
   @Input('aria-label') label = name;
   /**
    * The aria-labeledby of the checkbox.
    */
+  //tslint:disable-next-line:no-input-rename
   @Input('aria-labelledby') labeledBy: string;
 
   /**
