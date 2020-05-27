bbd215b505738c6cbd7a46e414aac6cb610c14da
Nolan Arnold <nolan.arnold@gmail.com>
2020-05-27 12:53:18 -0500

    feat(design-land): generated article examples and added them to article page

--- apps/design-land/src/app/article/article.module.ts
+++ apps/design-land/src/app/article/article.module.ts
@@ -1,20 +1,49 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, ComponentFactoryResolver } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { createCustomElement } from '@angular/elements';
+
 
 import { DesignLandArticleComponent } from './article.component';
 import { DesignLandArticleRoutingModule } from './article-routing.module';
 
 import { DaffArticleModule } from '@daffodil/design';
+import { ARTICLE_EXAMPLES } from './examples/public_api';
+import { DesignLandExampleViewerModule } from '../core/code-preview/container/example-viewer.module';
 
 @NgModule({
   declarations: [
-    DesignLandArticleComponent
+    DesignLandArticleComponent,
+    ...ARTICLE_EXAMPLES
   ],
   imports: [
     CommonModule,
     DesignLandArticleRoutingModule,
+    CommonModule,
+    DaffArticleModule,
+    DesignLandExampleViewerModule,
 
-    DaffArticleModule
+  ],
+  entryComponents: [
+    ...ARTICLE_EXAMPLES
   ]
 })
-export class DesignLandArticleModule { }
+export class DesignLandArticleModule {
+  constructor(
+    injector: Injector,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {
+    ARTICLE_EXAMPLES.map((classConstructor) => {
+      return {
+        element: createCustomElement(classConstructor, { injector }),
+        class: classConstructor
+      }
+    })
+      .map((customElement) => {
+        // Register the custom element with the browser.
+        customElements.define(
+          this.componentFactoryResolver.resolveComponentFactory(customElement.class).selector + '-example',
+          customElement.element
+        );
+      });
+  }
+}
