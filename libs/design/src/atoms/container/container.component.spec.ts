5f8a8b13f20175ee60b9771f8b0b816e379c4a23
Elain Tsai <xelaint@gmail.com>
2020-06-01 12:36:30 -0400

    feat(design): update container to use size interface (#897)

--- libs/design/src/atoms/container/container.component.spec.ts
+++ libs/design/src/atoms/container/container.component.spec.ts
@@ -1,14 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { DaffContainerComponent, DaffContainerSize } from './container.component';
 import { Component, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+
+import { DaffContainerComponent } from './container.component';
+import { DaffSizeAllType } from '../../core/sizeable/sizeable';
 
 @Component({
   template: `<daff-container [size]="size"></daff-container>`
 })
 class WrapperComponent {
-  size: DaffContainerSize;
+  size: DaffSizeAllType;
 }
 
 describe('DaffContainerComponent', () => {
@@ -49,70 +50,15 @@ describe('DaffContainerComponent', () => {
 
   describe('setting the size', () => {
     it('should not set a default size', () => {
-      expect(component.size).toBeFalsy();
-
-      expect(de.classes).toEqual(jasmine.objectContaining({
-        'daff-container--xs': false,
-        'daff-container--sm': false,
-        'daff-container--md': false,
-        'daff-container--lg': false,
-        'daff-container--xl': false
-      }));
+      de = fixture.debugElement.query(By.css('daff-container'));
+      expect(de.nativeElement.classList.toString()).toEqual('daff-container');
     });
 
-    describe('when size="xs"', () => {
-      it('should add a class of "daff-container--xs" to the host element', () => {
-        wrapper.size = 'xs';
-        fixture.detectChanges();
-
-        expect(de.classes).toEqual(jasmine.objectContaining({
-          'daff-container--xs': true
-        }));
-      });
-    });
-
-    describe('when size="sm"', () => {
-      it('should add a class of "daff-container--sm" to the host element', () => {
-        wrapper.size = 'sm';
-        fixture.detectChanges();
-
-        expect(de.classes).toEqual(jasmine.objectContaining({
-          'daff-container--sm': true
-        }));
-      });
-    });
-
-    describe('when size="md"', () => {
-      it('should add a class of "daff-container--md" to the host element', () => {
-        wrapper.size = 'md';
-        fixture.detectChanges();
-
-        expect(de.classes).toEqual(jasmine.objectContaining({
-          'daff-container--md': true
-        }));
-      });
-    });
-
-    describe('when size="lg"', () => {
-      it('should add a class of "daff-container--lg" to the host element', () => {
-        wrapper.size = 'lg';
-        fixture.detectChanges();
-
-        expect(de.classes).toEqual(jasmine.objectContaining({
-          'daff-container--lg': true
-        }));
-      });
-    });
-
-    describe('when size="xl"', () => {
-      it('should add a class of "daff-container--xl" to the host element', () => {
-        wrapper.size = 'xl';
-        fixture.detectChanges();
-
-        expect(de.classes).toEqual(jasmine.objectContaining({
-          'daff-container--xl': true
-        }));
-      });
+    it('should add the size class on the host element for the defined size', () => {
+      wrapper.size = 'xs';
+      fixture.detectChanges();
+      
+      expect(de.nativeElement.classList.contains('daff-xs')).toEqual(true);
     });
   });
 });
