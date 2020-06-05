3c2663d004b28d093f3020a991b4836493cdeffd
Luke <lderrickable@gmail.com>
2020-06-05 17:05:23 -0400

    add a doc explaining the empty cart guard

--- libs/cart/src/guards/empty-cart/empty-cart-guard.ts
+++ libs/cart/src/guards/empty-cart/empty-cart-guard.ts
@@ -5,6 +5,9 @@ import { map } from 'rxjs/operators';
 
 import { DaffCartFacade } from '../../facades/cart/cart.facade';
 
+/**
+ * A routing guard that guards against an empty cart, which is often useful for a checkout page.
+ */
 @Injectable()
 export class DaffEmptyCartGuard implements CanActivate {
   constructor(private facade: DaffCartFacade) {}
