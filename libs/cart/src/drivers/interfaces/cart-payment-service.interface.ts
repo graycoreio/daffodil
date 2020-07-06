ff10ded00061986f669cc18a13f607526278ff53
Peter Lauck <griest024@gmail.com>
2020-07-06 15:24:56 -0400

    feat(cart): add updateWithBilling method to payment service interface

--- libs/cart/src/drivers/interfaces/cart-payment-service.interface.ts
+++ libs/cart/src/drivers/interfaces/cart-payment-service.interface.ts
@@ -3,20 +3,30 @@ import { Observable } from 'rxjs';
 
 import { DaffCartPaymentMethod } from '../../models/cart-payment';
 import { DaffCart } from '../../models/cart';
+import { DaffCartAddress } from '../../models/cart-address';
 
 /**
  * The interface responsible for managing the selected payment method of a cart.
  */
-export interface DaffCartPaymentServiceInterface<T extends DaffCartPaymentMethod = DaffCartPaymentMethod, V extends DaffCart = DaffCart> {
+export interface DaffCartPaymentServiceInterface<
+  T extends DaffCartPaymentMethod = DaffCartPaymentMethod,
+  V extends DaffCart = DaffCart,
+  R extends DaffCartAddress = DaffCartAddress
+> {
 	/**
 	 * Get the currently applied payment method of a cart.
 	 */
 	get(cartId: V['id']): Observable<T>;
 
 	/**
-	 * Update the payment method applied to a cart. 
+	 * Update the payment method applied to a cart.
 	 */
-	update(cartId: V['id'], payment: Partial<T>): Observable<Partial<V>>;
+  update(cartId: V['id'], payment: Partial<T>): Observable<Partial<V>>;
+
+  /**
+	 * Update the billing address and payment method applied to a cart.
+	 */
+	updateWithBilling(cartId: V['id'], payment: Partial<T>, address: Partial<R>): Observable<Partial<V>>;
 
 	/**
 	 * Remove the payment method applied to a cart.
