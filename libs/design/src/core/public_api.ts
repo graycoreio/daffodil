d2bc097ac42eb93688edb7bac38d18680551e939
xelaint <xelaint@gmail.com>
2020-07-08 14:25:31 -0400

    feat(design): add statusable as a core interface

--- libs/design/src/core/public_api.ts
+++ libs/design/src/core/public_api.ts
@@ -3,3 +3,4 @@ export * from './prefix-suffix/public_api';
 export * from './colorable/colorable';
 export * from './breakpoints/breakpoints';
 export * from './constructor/constructor';
+export * from './statusable/statusable';
