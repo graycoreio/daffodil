17f7563b7decf879602902fc930b6c7b71e795b9
griest024 <griest024@gmail.com>
2020-07-15 09:48:59 -0400

    feat(cart): create new cart after successful order (#993)

--- libs/cart/src/effects/cart-order.effects.spec.ts
+++ libs/cart/src/effects/cart-order.effects.spec.ts
@@ -18,6 +18,7 @@ import {
 
 import { DaffCartOrderEffects } from './cart-order.effects';
 import {
+  DaffCartCreate,
   DaffCartPlaceOrder,
   DaffCartPlaceOrderSuccess,
   DaffCartPlaceOrderFailure,
@@ -125,6 +126,26 @@ describe('Cart | Effect | CartOrderEffects', () => {
       it('should return a DaffCartStorageFailure', () => {
         expect(effects.placeOrder$).toBeObservable(expected);
       });
+    });
+  });
+
+  describe('resetCart$ | resetting the cart after a successful order', () => {
+    let expected;
+    let cartOrderSuccessAction;
+
+    beforeEach(() => {
+      const cartCreateAction = new DaffCartCreate();
+      const response: DaffCartOrderResult = {
+        orderId: 'orderId',
+        cartId: mockCart.id,
+      };
+      cartOrderSuccessAction = new DaffCartPlaceOrderSuccess(response);
+      actions$ = hot('--a', { a: cartOrderSuccessAction });
+      expected = cold('--b', {b: cartCreateAction});
+    });
+
+    it('should create a new cart', () => {
+      expect(effects.resetCart$).toBeObservable(expected);
     });
   });
 });
