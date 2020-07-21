54b2d5541d97188cbc853cf055184cd5ad7023cc
Peter Lauck <griest024@gmail.com>
2020-07-21 11:22:46 -0400

    add comments

--- libs/cart/src/drivers/magento/queries/update-address-with-email.ts
+++ libs/cart/src/drivers/magento/queries/update-address-with-email.ts
@@ -2,6 +2,13 @@ import gql from 'graphql-tag';
 
 import { cartFragment } from './fragments/public_api';
 
+/**
+ * Update the shipping and billing address of the cart.
+ * At the time of writing, Magento 2 processes compound queries in the order they are defined.
+ * We rely on this fact and only use the return of the last query.
+ * This helps us keep query complexity down and save some server CPU cycles.
+ * Driver behavior is not guaranteed if Magento no longer processes compound queries in the order they are defined.
+ */
 export const updateAddressWithEmail = gql`
   mutation UpdateAddress($cartId: String!, $address: CartAddressInput!, $email: String!) {
     setBillingAddressOnCart(input: {
