9227d72da95577a2b513460bd50381b9c73d2c06
Peter Lauck <griest024@gmail.com>
2020-07-23 15:57:39 -0400

    ensure boolean return

--- libs/geography/src/comparators/address.ts
+++ libs/geography/src/comparators/address.ts
@@ -6,13 +6,13 @@ import { DaffAddress } from '../models/address';
  * @param address0 An address.
  * @param address1 An address.
  */
-export function daffCompareAddresses(address0: DaffAddress, address1: DaffAddress) {
-  return address0 && address1 &&
+export function daffCompareAddresses(address0: DaffAddress, address1: DaffAddress): boolean {
+  return !!(address0 && address1 &&
     address0.street === address1.street &&
     address0.street2 === address1.street2 &&
     address0.city === address1.city &&
     address0.region === address1.region &&
     address0.country === address1.country &&
     address0.country_id === address1.country_id &&
-    address0.postcode === address1.postcode
+    address0.postcode === address1.postcode)
 }
--- libs/geography/src/comparators/personal-address.ts
+++ libs/geography/src/comparators/personal-address.ts
@@ -7,8 +7,8 @@ import { daffCompareAddresses } from './address';
  * @param address0 A personal address.
  * @param address1 A personal address.
  */
-export function daffComparePersonalAddresses(address0: DaffPersonalAddress, address1: DaffPersonalAddress) {
-  return address0 && address1 &&
+export function daffComparePersonalAddresses(address0: DaffPersonalAddress, address1: DaffPersonalAddress): boolean {
+  return !!(address0 && address1 &&
     address0.prefix === address1.prefix &&
     address0.suffix === address1.suffix &&
     address0.firstname === address1.firstname &&
@@ -16,5 +16,5 @@ export function daffComparePersonalAddresses(address0: DaffPersonalAddress, addr
     address0.lastname === address1.lastname &&
     address0.telephone === address1.telephone &&
     address0.email === address1.email &&
-    daffCompareAddresses(address0, address1)
+    daffCompareAddresses(address0, address1))
 }
