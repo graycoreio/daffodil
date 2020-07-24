de575b6c21beae81f659566a3cd77ec5d7f76aa1
griest024 <griest024@gmail.com>
2020-07-23 23:56:14 -0400

    feat(geography): add personal address and address comparators (#1010)

---
+++ libs/geography/src/comparators/address.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DaffAddress } from '@daffodil/geography';
+import { DaffAddressFactory } from '@daffodil/geography/testing'
+
+import { daffCompareAddresses as comparator } from './address';
+
+describe('Geography | Comparators | DaffAddress', () => {
+  let addressFactory: DaffAddressFactory;
+
+  let address0: DaffAddress;
+  let address1: DaffAddress;
+
+  beforeEach(() => {
+    addressFactory = TestBed.get(DaffAddressFactory);
+
+    address0 = addressFactory.create();
+    address1 = addressFactory.create();
+  });
+
+  describe('when the addresses are null', () => {
+    it('should return false', () => {
+      const result = comparator(null, null);
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe('when the addresses are the same', () => {
+    it('should return true', () => {
+      const result = comparator(address0, address0);
+
+      expect(result).toBeTruthy();
+    });
+  });
+
+  describe('when the addresses are not the same', () => {
+    it('should return true', () => {
+      const result = comparator(address0, address1);
+
+      expect(result).toBeFalsy();
+    });
+  });
+});
--- libs/geography/src/index.ts
+++ libs/geography/src/index.ts
@@ -7,6 +7,7 @@ export * from './actions/public_api';
 export * from './selectors/public_api';
 export * from './reducers/public_api';
 export * from './errors/public_api';
+export * from './comparators/public_api';
 
 export { DaffGeographyModule } from './geography.module';
 export { DaffGeographyFacade } from './facades/geography/geography.facade';
