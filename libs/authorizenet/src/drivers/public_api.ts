77b1fc3710ce4ea34c8c8f92b91f41ca85172da9
Luke <lderrickable@gmail.com>
2020-08-03 10:02:08 -0400

    fix(authorizenet): export a needed injection token

--- libs/authorizenet/src/drivers/public_api.ts
+++ libs/authorizenet/src/drivers/public_api.ts
@@ -1,3 +1,4 @@
 export * from './interfaces/authorize-net-config.interface';
 export * from './interfaces/authorize-net-service.interface';
+export * from './interfaces/authorize-net-payment-id.token';
 export * from './magento/public_api';
