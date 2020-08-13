f1e01bb83ac1a5a3cbfb7360e9dd3a0689448609
xelaint <xelaint@gmail.com>
2020-08-13 13:53:10 -0400

    address comment

--- libs/core/src/storage/noopstorage/noopstorage.service.ts
+++ libs/core/src/storage/noopstorage/noopstorage.service.ts
@@ -1,5 +1,9 @@
-import { DaffPersistenceService } from "../persistence.interface";
+import { Injectable } from '@angular/core';
+import { DaffPersistenceService } from '../persistence.interface';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class DaffNoopStorageService implements DaffPersistenceService {
   setItem(key : string, value: any): void {};
   getItem(key: string): any {};
