ab9852f291321c395319f9f248d4fa79931f297d
Luke <lderrickable@gmail.com>
2020-08-20 21:23:41 -0400

    fix tests

--- libs/product/src/drivers/magento/transforms/bundled-product-transformers.spec.ts
+++ libs/product/src/drivers/magento/transforms/bundled-product-transformers.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { transformMagentoBundledProduct } from './bundled-product-transformers';
 import daffCompositeProductData from './spec-data/daff-composite-product.json';
 import magentoBundledProductData from './spec-data/magento-bundled-product.json';
+import { MagentoProductStockStatusEnum } from '../models/magento-product';
 
 describe('DaffMagentoBundledProductTransformers', () => {
 	const mediaUrl = 'media url';
@@ -12,9 +13,13 @@ describe('DaffMagentoBundledProductTransformers', () => {
   });
 
 	describe('transform', () => {
+		const magentoBundledProduct = {
+			...magentoBundledProductData,
+			stock_status: MagentoProductStockStatusEnum.InStock
+		}
 		
 		it('should transform a MagentoBundledProduct to a DaffCompositeProduct', () => {
-			expect(transformMagentoBundledProduct(magentoBundledProductData, mediaUrl)).toEqual(jasmine.objectContaining(daffCompositeProductData));
+			expect(transformMagentoBundledProduct(magentoBundledProduct, mediaUrl)).toEqual(jasmine.objectContaining(daffCompositeProductData));
 		});
 	});
 });
