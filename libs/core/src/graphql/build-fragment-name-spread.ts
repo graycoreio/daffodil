0416c8fc094500d10013dbe846d3098b42c81ccd
Peter Lauck <griest024@gmail.com>
2020-08-27 20:09:37 -0400

    remove function from public api

--- libs/core/src/graphql/build-fragment-name-spread.ts
+++ libs/core/src/graphql/build-fragment-name-spread.ts
@@ -1,6 +1,19 @@
-import { DocumentNode } from 'graphql'
+import { DocumentNode, FragmentDefinitionNode } from 'graphql'
 
-import { daffGetFragmentNames } from './get-fragment-names'
+const getFragmentNames = (fragment: DocumentNode) =>
+  fragment.definitions.filter(def =>
+    def.kind === 'FragmentDefinition'
+  ).map(def =>
+    (def as FragmentDefinitionNode).name.value
+  )
+
+/**
+ * Builds a list of fragment names present inside the specified GraphQL document nodes.
+ * Returns an empty array if no fragments have been defined or if null is passed.
+ * @param fragments The created fragments.
+ */
+const daffGetFragmentNames = (...fragments: DocumentNode[]): string[] =>
+  fragments.reduce((acc, fragment) => acc.concat(getFragmentNames(fragment)), [])
 
 /**
  * Builds a string of fragment names that can be interpolated into a GraphQL query.
--- libs/core/src/graphql/public_api.ts
+++ libs/core/src/graphql/public_api.ts
@@ -1,3 +1,2 @@
-export { daffGetFragmentNames } from './get-fragment-names';
 export { daffBuildFragmentNameSpread } from './build-fragment-name-spread';
 export { daffBuildFragmentDefinition } from './build-fragment-definition';
