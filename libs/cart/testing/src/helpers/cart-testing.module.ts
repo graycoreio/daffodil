192653887607eb6eebd0061547cb497cb19503f3
Peter Lauck <griest024@gmail.com>
2020-09-15 10:26:28 -0400

    fix build

--- libs/cart/testing/src/helpers/cart-testing.module.ts
+++ libs/cart/testing/src/helpers/cart-testing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 
-import { DaffCartFacade, DaffCartPaymentMethodMap } from '@daffodil/cart';
+import { DaffCartFacade, DaffCartPaymentMethodIdMap } from '@daffodil/cart';
 
 import { MockDaffCartFacade } from './mock-cart-facade';
 
 @NgModule({
   providers: [
-		{ provide: DaffCartPaymentMethodMap, useValue: {} },
+		{ provide: DaffCartPaymentMethodIdMap, useValue: {} },
 		{ provide: DaffCartFacade, useClass: MockDaffCartFacade }
 	]
 })
