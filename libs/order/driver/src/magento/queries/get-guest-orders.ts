5007cb636ef0a081c9d8c805bdb7a0ef024252c4
griest024 <griest024@gmail.com>
2020-09-18 14:47:27 -0400

    fix(order): wrong cart ID GraphQL variable type (#1067)

--- libs/order/driver/src/magento/queries/get-guest-orders.ts
+++ libs/order/driver/src/magento/queries/get-guest-orders.ts
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 import { orderFragment } from './fragments/public_api';
 
 export const getGuestOrders = gql`
-  query GetGuestOrders($cartId: String) {
+  query GetGuestOrders($cartId: String!) {
     graycoreGuestOrders(cartId: $cartId) {
       orders {
         ...order
