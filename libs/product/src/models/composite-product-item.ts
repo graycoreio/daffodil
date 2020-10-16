664c20f387d3f68a318e80922d89e786e996ea4d
Luke <lderrickable@gmail.com>
2020-10-16 14:42:08 -0400

    fix a typo

--- libs/product/src/models/composite-product-item.ts
+++ libs/product/src/models/composite-product-item.ts
@@ -12,7 +12,7 @@ export enum DaffCompositeProductItemInputEnum {
  * The composite product item describes one set of product options that the user can add to the composite product.
  * A composite product item can be required or optional. If it is required, an option _must_ be chosen in order to add the product to the cart.
  * If the item is optional, the product can be added to the cart without an option having been chosen.
- * For example, f a composite product is a toolbox bundle, a composite product item might be a screw driver, and the options contained in that
+ * For example, if a composite product is a toolbox bundle, a composite product item might be a screw driver, and the options contained in that
  * item might be a phillips head and a flathead. The customer could choose to add either a phillips head or a flathead to the
  * composite product, or neither if the item is optional.
  */
