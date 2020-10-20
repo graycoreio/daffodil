3a031cb4a61951e2e07a1502f51b5500e7930e75
Peter Lauck <griest024@gmail.com>
2020-10-20 17:07:50 -0400

    export state module

--- libs/order/state/src/index.ts
+++ libs/order/state/src/index.ts
@@ -3,3 +3,5 @@ export * from './reducers/public_api';
 export * from './selectors/public_api';
 export * from './facades/public_api';
 export * from './guards/public_api';
+
+export { DaffOrderStateModule } from './order-state.module';
