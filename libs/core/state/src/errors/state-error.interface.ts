80758f22159b80b37029c0c3720dee93bace860f
Peter Lauck <griest024@gmail.com>
2020-11-04 18:51:46 -0500

    docs

--- libs/core/state/src/errors/state-error.interface.ts
+++ libs/core/state/src/errors/state-error.interface.ts
@@ -1,3 +1,6 @@
+/**
+ * An error object that can be serialized in state.
+ */
 export interface DaffStateError {
   code: string;
   message: string;
--- libs/core/state/src/errors/transform-error-to-state-error.ts
+++ libs/core/state/src/errors/transform-error-to-state-error.ts
@@ -2,6 +2,9 @@ import { DaffError } from '@daffodil/core';
 
 import { DaffStateError } from './state-error.interface';
 
+/**
+ * Transforms an error instance to a state error object.
+ */
 export function daffTransformErrorToStateError(error: DaffError): DaffStateError {
   return error;
 }
