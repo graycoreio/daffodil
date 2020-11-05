0539578d325703612c6a35eebe5747e0667b52f7
Peter Lauck <griest024@gmail.com>
2020-11-04 21:24:44 -0500

    safe transform

--- libs/core/state/src/errors/transform-error-to-state-error.ts
+++ libs/core/state/src/errors/transform-error-to-state-error.ts
@@ -5,6 +5,6 @@ import { DaffStateError } from './state-error.interface';
 /**
  * Transforms an error instance to a state error object.
  */
-export function daffTransformErrorToStateError(error: DaffError): DaffStateError {
-  return error;
+export function daffTransformErrorToStateError({code, message}: DaffError): DaffStateError {
+  return {code, message};
 }
