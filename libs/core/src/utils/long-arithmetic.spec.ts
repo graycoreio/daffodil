8b8e5e392442f7ee54550d555ba50c815a97b8bc
Luke <lderrickable@gmail.com>
2020-11-06 12:50:31 -0500

    fix(core): null or undefined values cause an infinite loop in arithmetic functions

--- libs/core/src/utils/long-arithmetic.spec.ts
+++ libs/core/src/utils/long-arithmetic.spec.ts
@@ -8,6 +8,14 @@ describe('Core | Utils | Long Arithmetic', () => {
 			expect(1.005 + 0.99 + 0.002).not.toEqual(1.997);
 			expect(daffAdd(1.005, 0.99, 0.002)).toEqual(1.997);
 		});
+
+		it('should return NaN if given an undefined value', () => {
+			expect(daffAdd(undefined, 1)).toEqual(NaN);
+		});
+
+		it('should treat null values as zero', () => {
+			expect(daffAdd(null, 1)).toEqual(1);
+		});
 	});
 
 	describe('daffSubtract', () => {
@@ -15,6 +23,14 @@ describe('Core | Utils | Long Arithmetic', () => {
 		it('should subtract long decimals and return the expected value', () => {
 			expect(1.005 - 0.99 - 0.002).not.toEqual(0.013);
 			expect(daffSubtract(1.005, 0.99, 0.002)).toEqual(0.013);
+		});
+
+		it('should return NaN if given an undefined value', () => {
+			expect(daffSubtract(undefined, 1)).toEqual(NaN);
+		});
+
+		it('should treat null values as zero', () => {
+			expect(daffSubtract(null, 1)).toEqual(-1);
 		});
 	});
 
@@ -24,6 +40,14 @@ describe('Core | Utils | Long Arithmetic', () => {
 			expect(1.005 * 0.99 * 0.002).not.toEqual(0.0019899);
 			expect(daffMultiply(1.005, 0.99, 0.002)).toEqual(0.0019899);
 		});
+
+		it('should return NaN if given an undefined value', () => {
+			expect(daffMultiply(undefined, 1)).toEqual(NaN);
+		});
+
+		it('should treat null values as zero', () => {
+			expect(daffMultiply(null, 1)).toEqual(0);
+		});
 	});
 
 	describe('daffDivide', () => {
@@ -31,6 +55,14 @@ describe('Core | Utils | Long Arithmetic', () => {
 		it('should divide long decimals and return the expected value', () => {
 			expect(1.005 / 5 / 0.001).not.toEqual(201);
 			expect(daffDivide(1.005, 5, 0.001)).toEqual(201);
+		});
+
+		it('should return NaN if given an undefined value', () => {
+			expect(daffDivide(undefined, 1)).toEqual(NaN);
+		});
+
+		it('should treat null values as zero', () => {
+			expect(daffDivide(null, 1)).toEqual(0);
 		});
 	});
 });
