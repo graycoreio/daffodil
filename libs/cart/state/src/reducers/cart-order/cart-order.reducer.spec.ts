113b3fe30216982ad46812e2ef07c3fc333b340f
Peter Lauck <griest024@gmail.com>
2020-11-16 18:37:47 -0500

    move import

--- libs/cart/state/src/reducers/cart-order/cart-order.reducer.spec.ts
+++ libs/cart/state/src/reducers/cart-order/cart-order.reducer.spec.ts
@@ -1,8 +1,7 @@
 import { DaffLoadingState, DaffStateError } from '@daffodil/core/state';
-import { DaffCartPlaceOrder, DaffCartOrderReducerState, DaffCartPlaceOrderSuccess, DaffCartPlaceOrderFailure } from '@daffodil/cart/state';
+import { DaffCartPlaceOrder, DaffCartOrderReducerState, DaffCartPlaceOrderSuccess, DaffCartPlaceOrderFailure, daffCartOrderInitialState as initialState } from '@daffodil/cart/state';
 
 import { daffCartOrderReducer as reducer } from './cart-order.reducer';
-import { daffCartOrderInitialState as initialState } from './cart-order-initial-state';
 
 describe('Cart | Reducer | CartOrder', () => {
   describe('when an unknown action is triggered', () => {
