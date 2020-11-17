0206618a4eebc345c308cef294527b93a08a0b7b
Luke <lderrickable@gmail.com>
2020-11-16 22:49:54 -0500

    fix(product): product page resolver should dispatch a product load regardless of environment

--- libs/product/src/resolvers/public_api.ts
+++ libs/product/src/resolvers/public_api.ts
@@ -1,2 +1,1 @@
-export { DaffProductPageServerSideResolver } from './product-page-server-side/product-page-server-side.resolver';
 export { DaffProductPageResolver } from './product-page/product-page.resolver';
