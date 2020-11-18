3c346b3b3c1e6a667459a7ab3f90bf1bc73d2951
Peter Lauck <griest024@gmail.com>
2020-11-18 14:46:48 -0500

    add default config

--- libs/navigation/src/drivers/magento/navigation-driver.module.ts
+++ libs/navigation/src/drivers/magento/navigation-driver.module.ts
@@ -7,13 +7,17 @@ import { DaffNavigationTransformer } from '../injection-tokens/navigation-transf
 import { DaffMagentoNavigationTransformerService } from './transformers/navigation-transformer';
 import { MagentoNavigationDriverConfiguration, MAGENTO_NAVIGATION_TREE_QUERY_DEPTH } from '../interfaces/navigation-config.interface';
 
+export const MAGENTO_NAVIGATION_DEFAULT_CONFIGURATION: MagentoNavigationDriverConfiguration = {
+  navigationTreeQueryDepth: 3
+}
+
 @NgModule({
   imports: [
     CommonModule
   ]
 })
 export class DaffNavigationMagentoDriverModule {
-  static forRoot(config: MagentoNavigationDriverConfiguration): ModuleWithProviders<DaffNavigationMagentoDriverModule> {
+  static forRoot(config: MagentoNavigationDriverConfiguration = MAGENTO_NAVIGATION_DEFAULT_CONFIGURATION): ModuleWithProviders<DaffNavigationMagentoDriverModule> {
     return {
       ngModule: DaffNavigationMagentoDriverModule,
       providers: [
