715920d12432d12e529e1a70c1c5a8713cca9973
Luke <lderrickable@gmail.com>
2020-11-20 20:26:52 -0500

    fix(category): add category page effects to DaffCategoryStateModule

--- libs/category/src/category-state.module.ts
+++ libs/category/src/category-state.module.ts
@@ -5,11 +5,12 @@ import { EffectsModule } from '@ngrx/effects';
 import { daffCategoryReducers } from './reducers/category-reducers';
 import { DaffCategoryEffects } from './effects/category.effects';
 import { DaffDefaultCategoryPageSize } from './resolvers/public_api';
+import { DaffCategoryPageEffects } from './effects/category-page.effects';
 
 @NgModule({
   imports: [
     StoreModule.forFeature('category', daffCategoryReducers),
-    EffectsModule.forFeature([DaffCategoryEffects]),
+    EffectsModule.forFeature([DaffCategoryEffects, DaffCategoryPageEffects]),
 	],
 	providers: [
 		{ provide: DaffDefaultCategoryPageSize, useValue: 12 }
