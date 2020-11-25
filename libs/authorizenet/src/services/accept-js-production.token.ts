80179512b8e9d747a16956170b70ed332a8bd772
Damien Retzinger <damienwebdev@gmail.com>
2020-11-25 10:39:38 -0500

    fix tests

--- libs/authorizenet/src/services/accept-js-production.token.ts
+++ libs/authorizenet/src/services/accept-js-production.token.ts
@@ -2,8 +2,8 @@ import { InjectionToken } from '@angular/core';
 
 /**
  * A token that represents the version to use for Accept.JS calls.
- * A true configures the package to use the production AcceptJs endpoint, 
- * and a false value configures it to use the Sandbox AcceptJs endpoint. 
+ * A "true" value configures the package to use the production AcceptJs endpoint, 
+ * and a "false" value configures it to use the Sandbox AcceptJs endpoint. 
  * By default, we assume you're using the sandbox (false).
  */
 export const DAFF_AUTHORIZENET_ACCEPT_JS_PRODUCTION = new InjectionToken<
