ce96d0cd25a6fdab64ff25ea1bd04ea9d5d779dd
Peter Lauck <griest024@gmail.com>
2020-11-25 17:03:17 -0500

    more docs

--- libs/cart/state/src/guards/non-empty-cart/non-empty-cart-guard-redirect.token.ts
+++ libs/cart/state/src/guards/non-empty-cart/non-empty-cart-guard-redirect.token.ts
@@ -1,3 +1,6 @@
 import { InjectionToken } from '@angular/core';
 
+/**
+ * The path to which the user should be redirected if the cart has no items when {@link DaffNonEmptyCartGuard} is invoked.
+ */
 export const DaffCartNonEmptyCartGuardRedirectUrl = new InjectionToken<string>('DaffCartNonEmptyCartGuardRedirectUrl');
