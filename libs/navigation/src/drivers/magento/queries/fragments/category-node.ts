f0840e575c233c2fc4331956a4ed1409fba77feb
Luke <lderrickable@gmail.com>
2020-11-30 12:07:45 -0500

    add some todos concerning nested fragments

--- libs/navigation/src/drivers/magento/queries/fragments/category-node.ts
+++ libs/navigation/src/drivers/magento/queries/fragments/category-node.ts
@@ -18,6 +18,7 @@ const categoryNodeFragment = `
  * Generates a category tree fragment with the specified number of nested child category trees.
  * @param depth The maximum depth to which category children should be added to the fragment.
  */
+//todo: use nested fragments when this bug is fixed: https://github.com/magento/magento2/issues/31086
 export function getCategoryNodeFragment(depth: number = 3): DocumentNode {
   const fragmentBody = new Array(depth).fill(null).reduce(acc => `
     ${categoryNodeFragment}
