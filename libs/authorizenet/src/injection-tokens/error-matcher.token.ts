56baf7368a60c86649896d0f996b7e05072064e8
griest024 <griest024@gmail.com>
2020-12-02 18:44:47 -0500

    feat(authorizenet): add error matcher injection token (#1172)

---
+++ libs/authorizenet/src/injection-tokens/error-matcher.token.ts
@@ -0,0 +1,9 @@
+import { InjectionToken } from '@angular/core';
+
+import { daffTransformErrorToStateError } from '@daffodil/core/state';
+
+/**
+ * Transforms `DaffError`s into `DaffStateError`s before they are serialized into state.
+ * Can be used to further refine Daffodil errors into more specific app errors.
+ */
+export const DAFF_AUTHORIZENET_ERROR_MATCHER = new InjectionToken<typeof daffTransformErrorToStateError>('DAFF_AUTHORIZENET_ERROR_MATCHER', {factory: () => daffTransformErrorToStateError});
