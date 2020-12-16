4eeb03741d65a3d3f9609d55911f647f9f2e0871
Luke Derrick <lderrickable@gmail.com>
2020-12-16 17:37:21 -0500

    fix(category): category breadcrumbs return from magento in alphabetic… (#1188)
    * fix(category): category breadcrumbs return from magento in alphabetical order instead of by order of category level

--- libs/category/src/drivers/magento/transformers/category-transformer.service.spec.ts
+++ libs/category/src/drivers/magento/transformers/category-transformer.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
+import { DaffCategoryBreadcrumb } from '@daffodil/category';
 import { DaffCategoryFactory } from '@daffodil/category/testing';
 
 import { DaffMagentoCategoryTransformerService } from './category-transformer.service';
@@ -27,8 +28,10 @@ describe('DaffMagentoCategoryTransformerService', () => {
 
   describe('transform', () => {
 
-    it('should return a DaffCategory', () => {
-      const magentoCategory: MagentoCategory = {
+		let magentoCategory: MagentoCategory;
+
+		beforeEach(() => {
+			magentoCategory = {
         id: stubCategory.id,
 				name: stubCategory.name,
 				description: stubCategory.description,
@@ -55,8 +58,51 @@ describe('DaffMagentoCategoryTransformerService', () => {
 				},
         children_count: stubCategory.children_count
       }
+		});
 
+    it('should return a DaffCategory', () => {
       expect(service.transform(magentoCategory)).toEqual(stubCategory);
+		});
+
+    it('should return breadcrumbs in order of category_level', () => {
+      magentoCategory.breadcrumbs = [{
+				category_id: 3,
+				category_name: 'category3',
+				category_level: 3,
+				category_url_key: 'urlKey3'
+			},
+			{
+				category_id: 1,
+				category_name: 'category1',
+				category_level: 1,
+				category_url_key: 'urlKey1'
+			},
+			{
+				category_id: 2,
+				category_name: 'category2',
+				category_level: 2,
+				category_url_key: 'urlKey2'
+			}];
+			const expectedBreadcrumbs: DaffCategoryBreadcrumb[] = [{
+				categoryId: 1,
+				categoryName: 'category1',
+				categoryLevel: 1,
+				categoryUrlKey: 'urlKey1'
+			},
+			{
+				categoryId: 2,
+				categoryName: 'category2',
+				categoryLevel: 2,
+				categoryUrlKey: 'urlKey2'
+			},
+			{
+				categoryId: 3,
+				categoryName: 'category3',
+				categoryLevel: 3,
+				categoryUrlKey: 'urlKey3'
+			}];
+
+      expect(service.transform(magentoCategory).breadcrumbs).toEqual(expectedBreadcrumbs);
     });
   });
 });
--- libs/category/src/drivers/magento/transformers/category-transformer.service.ts
+++ libs/category/src/drivers/magento/transformers/category-transformer.service.ts
@@ -14,8 +14,13 @@ export class DaffMagentoCategoryTransformerService {
       id: category.id,
 			name: category.name,
 			description: category.description,
-      children_count: category.children_count,
-			breadcrumbs: category.breadcrumbs ? category.breadcrumbs.map(breadcrumb => this.transformBreadcrumb(breadcrumb)) : null,
+			children_count: category.children_count,
+			//todo: use optional chaining when possible
+			breadcrumbs: category.breadcrumbs ? 
+				category.breadcrumbs
+					.map(breadcrumb => this.transformBreadcrumb(breadcrumb))
+					.sort((a, b) => a.categoryLevel - b.categoryLevel) : 
+				null,
 			product_ids: category.products.items.map(product => product.sku),
 			total_products: category.products.items.length
     }
