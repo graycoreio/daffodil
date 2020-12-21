4350c454dbbb559558d86d1f7c6d63e2c60424b3
Peter Lauck <griest024@gmail.com>
2020-12-21 18:29:52 -0500

    feat(authorizenet): use accept.js loader service in Magento driver

--- libs/authorizenet/driver/magento/src/authorize-net.service.ts
+++ libs/authorizenet/driver/magento/src/authorize-net.service.ts
@@ -1,13 +1,11 @@
 import { Injectable, Inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { AcceptType, DaffAuthorizeNetTokenRequest, AuthorizeNetResponse } from '@daffodil/authorizenet';
+import { DaffAuthorizeNetTokenRequest, AuthorizeNetResponse, DaffAcceptJsLoadingService } from '@daffodil/authorizenet';
 import { DaffAuthorizeNetService, DaffAuthorizeNetConfigToken, DaffAuthorizeNetConfig, DAFF_AUTHORIZE_NET_ERROR_CODE_MAP } from '@daffodil/authorizenet/driver';
 
 import { transformMagentoAuthorizeNetRequest, transformMagentoAuthorizeNetResponse } from './transformers/authorize-net-transformer';
 import { MagentoAuthorizeNetPayment } from './models/authorize-net-payment';
-
-export declare var Accept: AcceptType;
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +13,13 @@ export declare var Accept: AcceptType;
 export class DaffMagentoAuthorizeNetService implements DaffAuthorizeNetService {
 
 	constructor(
-		@Inject(DaffAuthorizeNetConfigToken) public config: DaffAuthorizeNetConfig
+    @Inject(DaffAuthorizeNetConfigToken) public config: DaffAuthorizeNetConfig,
+    private acceptJsLoader: DaffAcceptJsLoadingService
 	) {}
 
 	generateToken(paymentTokenRequest: DaffAuthorizeNetTokenRequest): Observable<MagentoAuthorizeNetPayment> {
 		return new Observable(observer =>
-			Accept.dispatchData(transformMagentoAuthorizeNetRequest(paymentTokenRequest, this.config), (response: AuthorizeNetResponse) => {
+			this.acceptJsLoader.getAccept().dispatchData(transformMagentoAuthorizeNetRequest(paymentTokenRequest, this.config), (response: AuthorizeNetResponse) => {
 				if (response.messages.resultCode === 'Error') {
           const MappedError = DAFF_AUTHORIZE_NET_ERROR_CODE_MAP[response.messages.message[0].code];
           const message = response.messages.message[0].code + ': ' + response.messages.message[0].text;
