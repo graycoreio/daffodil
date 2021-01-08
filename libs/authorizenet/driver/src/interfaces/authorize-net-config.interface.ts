58f701ee59fdb7fdde1f45e7f0125e678240b232
Peter Lauck <griest024@gmail.com>
2021-01-08 16:12:20 -0500

    feat(authorizenet): add injection token types

--- libs/authorizenet/driver/src/interfaces/authorize-net-config.interface.ts
+++ libs/authorizenet/driver/src/interfaces/authorize-net-config.interface.ts
@@ -1,6 +1,6 @@
 import { InjectionToken } from '@angular/core';
 
-export const DaffAuthorizeNetConfigToken = new InjectionToken('DaffAuthorizeNetConfigToken');
+export const DaffAuthorizeNetConfigToken = new InjectionToken<DaffAuthorizeNetConfig>('DaffAuthorizeNetConfigToken');
 
 /**
  * An interface for providing @daffodil/authorizenet with needed configurations specific to your authorizenet
