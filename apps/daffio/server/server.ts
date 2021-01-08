cb6c10096b8babf6cab0ef7d817823a2af1c08ec
Peter Lauck <griest024@gmail.com>
2021-01-08 11:45:40 -0500

    chore(daffio): upgrade angular universal (#1229)

--- apps/daffio/server/server.ts
+++ apps/daffio/server/server.ts
@@ -1,41 +1,61 @@
 import 'zone.js/dist/zone-node';
-import { enableProdMode } from '@angular/core';
-// Express Engine
 
 import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
-
-
-
 import * as express from 'express';
 import { join } from 'path';
 
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
+import { AppServerModule } from '../src/main.server';
+import { APP_BASE_HREF } from '@angular/common';
+import { existsSync } from 'fs';
 
-// Express server
-export const app = express();
+// The Express app is exported so that it can be used by serverless Functions.
+export function app(): express.Express {
+  const server = express();
+  const BROWSER_FOLDER = join(__dirname, '../browser');
 
-const BROWSER_FOLDER = join(__dirname, 'browser');
+  const indexHtml = existsSync(join(BROWSER_FOLDER, 'index.original.html')) ? 'index.original.html' : 'index';
 
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./../../../dist/apps/daffio/server/main');
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  server.engine('html', ngExpressEngine({
+    bootstrap: AppServerModule,
+  }));
 
+  server.set('view engine', 'html');
+  server.set('views', BROWSER_FOLDER);
 
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get('*.*', express.static(BROWSER_FOLDER, {
+    maxAge: '1y'
+  }));
 
-app.set('view engine', 'html');
-app.set('views', join(BROWSER_FOLDER));
+  // All regular routes use the Universal engine
+  server.get('*', (req, res) => {
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+  });
 
-// Server static files from /browser
-app.get('*.*', express.static(BROWSER_FOLDER));
+  return server;
+}
 
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render(join(BROWSER_FOLDER, 'index.html'), { req });
-});
+function run(): void {
+  const port = process.env.PORT || 4000;
+
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
+
+export * from '../src/main.server';
