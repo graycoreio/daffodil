3abf6441911891c563b5026dc2521e7b511b9bd8
Peter Lauck <griest024@gmail.com>
2021-01-08 16:12:20 -0500

    feat(authorizenet): add injection token types

--- libs/authorizenet/driver/src/interfaces/authorize-net-service.interface.ts
+++ libs/authorizenet/driver/src/interfaces/authorize-net-service.interface.ts
@@ -3,7 +3,7 @@ import { InjectionToken } from '@angular/core';
 
 import { DaffAuthorizeNetTokenRequest } from '@daffodil/authorizenet';
 
-export const DaffAuthorizeNetDriver = new InjectionToken('DaffAuthorizeNetDriver');
+export const DaffAuthorizeNetDriver = new InjectionToken<DaffAuthorizeNetService>('DaffAuthorizeNetDriver');
 
 export interface DaffAuthorizeNetService<T extends DaffAuthorizeNetTokenRequest = DaffAuthorizeNetTokenRequest> {
 	generateToken(paymentTokenRequest: T): Observable<any>;
