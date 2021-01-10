b1b403976743735272dbe080e3f60a8c34d09d9a
Peter Lauck <griest024@gmail.com>
2021-01-09 20:31:08 -0500

    feat(contact): add injection token types

--- libs/contact/driver/src/interfaces/contact-service.interface.ts
+++ libs/contact/driver/src/interfaces/contact-service.interface.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { InjectionToken } from '@angular/core';
 
-export const DaffContactDriver = new InjectionToken('DaffContactDriver');
+export const DaffContactDriver = new InjectionToken<DaffContactServiceInterface<any, any>>('DaffContactDriver');
 export interface DaffContactServiceInterface<T, V> {
   send(payload: T): Observable<V>;
 }
