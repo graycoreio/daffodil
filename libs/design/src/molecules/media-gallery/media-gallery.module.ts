97cff63d7a81c6ae8b406b60a2890486861f5ff7
xelaint <xelaint@gmail.com>
2021-01-11 16:49:33 -0500

    UI updates

--- libs/design/src/molecules/media-gallery/media-gallery.module.ts
+++ libs/design/src/molecules/media-gallery/media-gallery.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DaffMediaGalleryComponent } from './media-gallery.component';
-import { DaffThumbnailDirective } from "./thumbnail/thumbnail.directive";
+import { DaffThumbnailDirective } from './thumbnail/thumbnail.directive';
 import { DaffMediaRendererComponent } from './media-renderer/media-renderer.component';
 import { DaffMediaGalleryRegistry } from './registry/media-gallery.registry';
 
--- libs/design/src/molecules/media-gallery/thumbnail/thumbnail.directive.ts
+++ libs/design/src/molecules/media-gallery/thumbnail/thumbnail.directive.ts
@@ -11,6 +11,7 @@ import {
 	OnInit,
 	OnDestroy
 } from '@angular/core';
+
 import { DaffMediaGalleryRegistry } from '../registry/media-gallery.registry';
 import { daffThumbnailCompatToken } from './thumbnail-compat.token';
 import { DaffMediaGalleryComponent } from '../media-gallery.component';
@@ -19,12 +20,6 @@ import { DaffMediaGalleryComponent } from '../media-gallery.component';
 	selector: '[daffThumbnail]',
 })
 export class DaffThumbnailDirective implements OnInit, OnDestroy {
-
-	/**
-	 * A prop for determining whether or not the media element is selected.
-	 */
-	@Input() selected = false;
-
 	/**
 	 * Adds a class for styling a gallery item
 	 */
@@ -37,20 +32,16 @@ export class DaffThumbnailDirective implements OnInit, OnDestroy {
 		return this.selected;
 	};
 	
+	/**
+	 * A prop for determining whether or not the media element is selected.
+	 */
+	@Input() selected = false;
 
 	/**
 	 * Adds a click event to trigger selection of the media element.
 	 * @param $event 
 	 */
 	@HostListener('click', ['$event']) onClick($event: MouseEvent) {
-		this.registry.select(this);
-	}
-
-	/**
-	 * Adds a click event to trigger selection of the media element.
-	 * @param $event 
-	 */
-	@HostListener('mouseover', ['$event']) onHover($event: MouseEvent) {
 		this.registry.select(this);
 	}
 
