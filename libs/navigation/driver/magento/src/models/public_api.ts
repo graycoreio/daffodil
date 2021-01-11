c769eec8cacbaaa575e5fa589489a3957a6fe78c
Luke <lderrickable@gmail.com>
2021-01-11 12:21:13 -0500

    feat(navigation): export the MagentoBreadcrumb interface

--- libs/navigation/driver/magento/src/models/public_api.ts
+++ libs/navigation/driver/magento/src/models/public_api.ts
@@ -1,3 +1,3 @@
-export { CategoryNode } from './category-node';
+export { CategoryNode, MagentoBreadcrumb } from './category-node';
 export { CategoryProductNode } from './category-product-node';
 export { GetCategoryTreeResponse } from './get-category-tree-response';
