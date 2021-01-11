a94557f2f6578bcac000e7b45035c0d08d2554af
Peter Lauck <griest024@gmail.com>
2021-01-11 11:23:00 -0500

    feat(paypal): add default generic types to transformer interface (#1229)

--- libs/paypal/src/drivers/interfaces/paypal-transformer.interface.ts
+++ libs/paypal/src/drivers/interfaces/paypal-transformer.interface.ts
@@ -1,7 +1,10 @@
 import { DaffPaypalTokenRequest } from '../../models/paypal-token-request';
 import { DaffPaypalTokenResponse } from '../../models/paypal-token-response';
 
-export interface DaffPaypalTransformerInterface<T extends DaffPaypalTokenRequest, V extends DaffPaypalTokenResponse> {
+export interface DaffPaypalTransformerInterface<
+  T extends DaffPaypalTokenRequest = DaffPaypalTokenRequest,
+  V extends DaffPaypalTokenResponse = DaffPaypalTokenResponse
+> {
 	transformOut(tokenRequest: T, config?: any): any;
 	transformIn(tokenResponse: any): V;
 }
