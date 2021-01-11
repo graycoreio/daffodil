891517a7e49f95b5b5114c3d6283c29f31b698c6
Peter Lauck <griest024@gmail.com>
2021-01-11 11:15:05 -0500

    feat(geography): add injection token types

--- libs/geography/driver/src/interfaces/geography-service.interface.ts
+++ libs/geography/driver/src/interfaces/geography-service.interface.ts
@@ -3,9 +3,9 @@ import { Observable } from 'rxjs';
 
 import { DaffCountry } from '@daffodil/geography';
 
-export const DaffGeographyDriver = new InjectionToken('DaffGeographyDriver');
+export const DaffGeographyDriver = new InjectionToken<DaffGeographyServiceInterface>('DaffGeographyDriver');
 
-export interface DaffGeographyServiceInterface<T extends DaffCountry> {
+export interface DaffGeographyServiceInterface<T extends DaffCountry = DaffCountry> {
 	/**
 	 * Retrieves the list of countries available to the given store.
 	 */
