cb55f353c18da72840cfe1c9c2b940e9b857571f
Peter Lauck <griest024@gmail.com>
2021-01-11 11:24:37 -0500

    feat(product): add injection token types

--- libs/product/src/drivers/injection-tokens/product-driver.token.ts
+++ libs/product/src/drivers/injection-tokens/product-driver.token.ts
@@ -1,8 +1,9 @@
 import { InjectionToken } from '@angular/core';
+
 import { DaffProductServiceInterface } from '../interfaces/product-service.interface';
 
 /**
  * Injection token that serves as a placeholder for any service that implements the DaffProductServiceInterface.
  */
-export const DaffProductDriver = 
-    new InjectionToken<DaffProductServiceInterface<any>>('DaffProductDriver');
+export const DaffProductDriver =
+    new InjectionToken<DaffProductServiceInterface>('DaffProductDriver');
