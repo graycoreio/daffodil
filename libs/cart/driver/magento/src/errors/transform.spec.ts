2e59eebdd968c6e7e95f881188413dfb3dd817d7
griest024 <griest024@gmail.com>
2021-01-12 14:55:15 -0500

    feat(cart): transform invalid coupon code error from message (#1199)

---
+++ libs/cart/driver/magento/src/errors/transform.spec.ts
@@ -0,0 +1,31 @@
+import { ApolloError } from 'apollo-client';
+import { GraphQLError } from 'graphql';
+
+import { DaffInvalidCouponCodeError } from '@daffodil/cart/driver';
+
+import { transformCartMagentoError } from './transform';
+
+describe('Cart | Driver | Magento | transformCartMagentoError', () => {
+  let apolloError: ApolloError;
+  let graphQlError: GraphQLError;
+  let transformedError: Error;
+
+  describe('when the GraphQL error is an invalid coupon code error', () => {
+    beforeEach(() => {
+      graphQlError = new GraphQLError('The coupon code isn\'t valid. Verify the code and try again.');
+      apolloError = new ApolloError({
+        graphQLErrors: [graphQlError]
+      })
+
+      transformedError = transformCartMagentoError(apolloError)
+    })
+
+    it('should return a DaffInvalidCouponCodeError', () => {
+      expect(transformedError).toEqual(jasmine.any(DaffInvalidCouponCodeError))
+    })
+
+    it('should return an error containing the GraphQL error message', () => {
+      expect(transformedError.message).toContain(graphQlError.message)
+    })
+  })
+})
--- libs/cart/driver/magento/src/errors/transform.ts
+++ libs/cart/driver/magento/src/errors/transform.ts
@@ -1,7 +1,30 @@
-import { daffTransformMagentoError } from '@daffodil/driver/magento';
+import { ApolloError } from 'apollo-client';
 
-import { DaffCartMagentoErrorMap } from './map';
+import { daffTransformMagentoError } from '@daffodil/driver/magento';
+import { DaffCartDriverErrorMap } from '@daffodil/cart/driver';
+
+import { DaffCartMagentoErrorMap, DaffCartMagentoErrorMessageRegexMap } from './map';
+
+
+function transformMagentoCartGraphQlError(error: ApolloError): Error {
+  // TODO: readdress this when we move to eslint
+  // tslint:disable-next-line
+  for (const code in DaffCartMagentoErrorMessageRegexMap) {
+    const matchIndex = error.graphQLErrors[0].message.search(DaffCartMagentoErrorMessageRegexMap[code]);
+
+    if (matchIndex > -1 && DaffCartDriverErrorMap[code]) {
+      return new DaffCartDriverErrorMap[code](error.message)
+    }
+  }
+
+  return daffTransformMagentoError(error, DaffCartMagentoErrorMap);
+};
 
 export function transformCartMagentoError(error) {
-  return daffTransformMagentoError(error, DaffCartMagentoErrorMap)
+  // TODO: optional chaining
+  if (error.graphQLErrors && error.graphQLErrors.length) {
+		return transformMagentoCartGraphQlError(error);
+	} else {
+		return daffTransformMagentoError(error, DaffCartMagentoErrorMap);
+	}
 }
