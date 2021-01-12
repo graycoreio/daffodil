e08fe3a0f5e9834a5c48b9896f60a1f5b576f515
Nolan Arnold <nolan.arnold@gmail.com>
2021-01-11 23:55:49 -0600

    build(contact): changed name of DaffContactStateModule and updated imports
    Signed-off-by: Nolan Arnold <nolan.arnold@gmail.com>

--- libs/contact/state/src/contact.module.ts
+++ libs/contact/state/src/contact.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 import { reducer } from './reducers/contact.reducer';
+import { DaffContactEffects } from './effects/contact.effects';
 
 @NgModule({
 	declarations: [],
 	imports: [
-		StoreModule.forFeature('contact', reducer)
+		StoreModule.forFeature('contact', reducer),
+		EffectsModule.forFeature([DaffContactEffects])
 	],
 	providers: [],
 })
-export class DaffContactModule {}
+export class DaffContactStateModule { }
--- libs/contact/state/src/facades/contact.facade.ts
+++ libs/contact/state/src/facades/contact.facade.ts
@@ -8,16 +8,16 @@ import {
 	selectDaffContactError,
 	selectDaffContactSuccess,
 } from '../selectors/contact.selector';
-import { DaffContactModule } from '../contact.module';
+import { DaffContactStateModule } from '../contact.module';
 import { DaffContactFacadeInterface } from './contact-facade.interface';
 
-@Injectable({ providedIn: DaffContactModule })
+@Injectable({ providedIn: DaffContactStateModule })
 export class DaffContactFacade implements DaffContactFacadeInterface {
 	success$: Observable<boolean> = this.store.select(selectDaffContactSuccess);
 	error$: Observable<string[]> = this.store.select(selectDaffContactError);
 	loading$: Observable<boolean> = this.store.select(selectDaffContactLoading);
 
-	constructor(private store: Store<DaffContactFeatureState>) {}
+	constructor(private store: Store<DaffContactFeatureState>) { }
 	dispatch(action: Action) {
 		this.store.dispatch(action);
 	}
