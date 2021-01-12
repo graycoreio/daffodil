f3d1197a3e23d4345687b9f89db735bb6af9ce02
Peter Lauck <griest024@gmail.com>
2021-01-12 12:14:50 -0500

    chore(driver): remove DaffCoreTestingModule from Magento testing (#1229)

--- libs/driver/magento/testing/src/testing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { DaffMockCurrencyPipe } from './angular/mocks/pipes/currency';
-
-@NgModule({
-  declarations: [
-    DaffMockCurrencyPipe
-  ]
-})
-export class DaffCoreTestingModule {}
