c8e9c52863627c4454f216437e87abe2390d4165
Peter Lauck <griest024@gmail.com>
2021-01-12 12:29:35 -0500

    feat(design): add checkbox CVA export

--- libs/design/src/atoms/form/checkbox/public_api.ts
+++ libs/design/src/atoms/form/checkbox/public_api.ts
@@ -1,3 +1,4 @@
 export { DaffCheckboxModule } from './checkbox.module';
 export { DaffCheckboxComponent } from './checkbox.component';
 export { DaffCheckboxSetComponent } from '../checkbox-set/checkbox-set.component';
+export { DaffCheckboxControlValueAccessorDirective } from './cva/checkbox-cva.directive';
