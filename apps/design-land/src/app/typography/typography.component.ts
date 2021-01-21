a799385bd3aa30f11661fe93b854877038ab47a7
xelaint <xelaint@gmail.com>
2021-01-21 17:33:14 -0500

    docs(design-land): create typography documentation page (#1186)

---
+++ apps/design-land/src/app/typography/typography.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'design-land-typography',
+  templateUrl: './typography.component.html',
+  styleUrls: ['./typography.component.scss']
+})
+export class DesignLandTypographyComponent {}
