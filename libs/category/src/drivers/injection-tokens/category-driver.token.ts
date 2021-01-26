aaf56b78c84547189ee26ba8b1a3d7a6e2d08184
Damien Retzinger <damienwebdev@gmail.com>
2021-01-26 00:58:21 -0500

    build(all): do not publish Ivy libraries to npm (#1250)
    Angular recommends not publishing Ivy built libraries yet, so we have to make sure that when we do a build, we don't accidentally publish the Ivy libraries. To do so, we've changed the tsconfigs of our packages to have a "prod" version that disables ivy. This ensures that we don't publish the package to npm as an Ivy package, while still allowing us to work with Ivy internally.

--- libs/category/src/drivers/injection-tokens/category-driver.token.ts
+++ libs/category/src/drivers/injection-tokens/category-driver.token.ts
@@ -2,4 +2,8 @@ import { InjectionToken } from '@angular/core';
 
 import { DaffCategoryServiceInterface } from '../interfaces/category-service.interface';
 
+//TODO(damienwebdev): This import is necessary until we ship Ivy packages, do not remove it.
+//See: https://github.com/ng-packagr/ng-packagr/issues/1844
+import { DaffProduct } from '@daffodil/product';
+
 export const DaffCategoryDriver = new InjectionToken<DaffCategoryServiceInterface>('DaffCategoryDriver');
