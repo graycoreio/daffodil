02cd0f25673e90e290836a1c588c80e541eabb4a
griest024 <griest024@gmail.com>
2021-02-09 15:10:51 -0500

    perf(demo): don't negate async pipes (#1268)

--- apps/demo/src/app/newsletter/newsletter.component.ts
+++ apps/demo/src/app/newsletter/newsletter.component.ts
@@ -1,38 +1,58 @@
-import { Component } from '@angular/core';
-import { DaffNewsletterFacade, DaffNewsletterActionTypes, DaffNewsletterSubmission, DaffNewsletterCancel, DaffNewsletterRetry } from '@daffodil/newsletter';
-import { DaffNewsletterSubscribe } from '@daffodil/newsletter';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+import {
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
-
+import {
+  DaffNewsletterFacade,
+  DaffNewsletterSubscribe,
+  DaffNewsletterSubmission,
+  DaffNewsletterCancel,
+  DaffNewsletterRetry,
+} from '@daffodil/newsletter';
 
 @Component({
   selector: 'demo-newsletter',
   templateUrl: './newsletter.component.html',
-  styleUrls: ['./newsletter.component.scss']
+  styleUrls: ['./newsletter.component.scss'],
 })
-export class NewsletterComponent {
+export class NewsletterComponent implements OnInit {
 
   success$ = this.newsletterFacade.success$;
   error$ = this.newsletterFacade.error$;
   loading$ = this.newsletterFacade.loading$;
+  hasError$: Observable<boolean>;
 
   email: FormControl = new FormControl('', Validators.email);
 
   constructor(public newsletterFacade: DaffNewsletterFacade) {
   }
+
+  ngOnInit() {
+    this.hasError$ = this.error$.pipe(
+      map(error => !!error),
+    );
+  }
+
   onNewsletterSubmit() {
     if (this.email.valid) {
       this.newsletterFacade.dispatch(new DaffNewsletterSubscribe<DaffNewsletterSubmission>(this._makeSubmission(this.email.value)));
     }
   }
   onNewsletterCancel() {
-    this.newsletterFacade.dispatch(new DaffNewsletterCancel);
+    this.newsletterFacade.dispatch(new DaffNewsletterCancel());
   }
   onNewsletterRetry() {
     this.newsletterFacade.dispatch(new DaffNewsletterRetry<DaffNewsletterSubmission>(this._makeSubmission(this.email.value)));
   }
 
   private _makeSubmission(email: string): DaffNewsletterSubmission {
-    return { email: email };
+    return { email };
   }
 }
