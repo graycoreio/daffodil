afcf8422cb4b69373c1da8d42173df674d41b15a
Luke <lderrickable@gmail.com>
2021-02-15 13:00:14 -0500

    add the provider functions for angular 9+

--- libs/driver/magento/src/graphql/public_api.ts
+++ libs/driver/magento/src/graphql/public_api.ts
@@ -1,2 +1,2 @@
 export { DaffMagentoApolloCacheableOperationsLinkGenerator } from './cacheable-operations/apollo-cacheable-operations-link-generator';
-export { DAFF_MAGENTO_CACHEABLE_OPERATIONS } from './cacheable-operations/cacheable-operations-token';
+export * from './cacheable-operations/cacheable-operations-token';
