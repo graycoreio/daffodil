54ba78c675fb6499923a417f56db27b54a1ea4bb
Damien Retzinger <damienwebdev@gmail.com>
2021-02-17 16:22:13 -0500

    docs

--- libs/external-router/src/model/type-route-pair.ts
+++ libs/external-router/src/model/type-route-pair.ts
@@ -5,6 +5,10 @@ import { DaffExternalRouterType } from './route-type';
  * A model used to describe the relationship between a {@link DaffExternalRouterType}
  * and its corresponding route.
  *
+ * The TypeRoutePair is the pre-provided "glue" that a developer will provide
+ * ahead-of-time so that that the External Router knows what the list of available
+ * "types" are (along with that type's corresponding route).
+ *
  * @docs-private
  */
 export interface TypeRoutePair {
