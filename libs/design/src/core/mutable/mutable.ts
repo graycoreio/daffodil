2f213fe464849b4ad53db90f2ff865c74261eb2e
xelaint <xelaint@gmail.com>
2021-02-25 09:37:40 -0500

    refactor(design): update mutable interface name

---
+++ libs/design/src/core/mutable/mutable.ts
@@ -0,0 +1,7 @@
+/**
+ * An interface for giving a component the ability to display a mutating UI.
+ * In order to be mutable, our class must implement this property.
+ */
+export interface DaffMutable {
+	mutating: boolean;
+}
