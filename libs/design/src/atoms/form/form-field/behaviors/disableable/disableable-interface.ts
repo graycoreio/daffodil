f4e746208b8e37e57f15d0fa63f664f6fa14b7de
xelaint <xelaint@gmail.com>
2021-03-01 15:05:14 -0500

    feat(design): create disableable and focusable interfaces for form field behaviors

---
+++ libs/design/src/atoms/form/form-field/behaviors/disableable/disableable-interface.ts
@@ -0,0 +1,9 @@
+import { DaffFormFieldControl } from '../../form-field-control';
+
+/**
+ * A behavior interface for tracking the disabled state of a form field.
+ */
+export interface DaffDisableable {
+	_control: DaffFormFieldControl;
+	disabled: boolean;
+}
