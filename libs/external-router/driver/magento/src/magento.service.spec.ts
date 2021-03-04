cb13d2fc08582d051b4ba0a0c511d66e51f0e10e
Peter Lauck <griest024@gmail.com>
2021-03-03 21:46:25 -0500

    add tests

--- libs/external-router/driver/magento/src/magento.service.spec.ts
+++ libs/external-router/driver/magento/src/magento.service.spec.ts
@@ -1,27 +1,70 @@
 import { TestBed } from '@angular/core/testing';
 import { TestScheduler } from 'rxjs/testing';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
+
+import { DaffExternallyResolvableUrl } from '@daffodil/external-router';
+import { MagentoResolution, MagentoUrlRewriteEntityTypeEnum } from '@daffodil/external-router/driver/magento';
 
 import { DaffExternalRouterMagentoDriver } from './magento.service';
 import { DaffExternalRouterDriverMagentoModule } from './magento.module';
+import { MagentoResolveUrl } from './graphql/queries/resolve-url';
 
 describe('@daffodil/external-router/driver/magento | DaffExternalRouterMagentoDriver', () => {
 	let service: DaffExternalRouterMagentoDriver;
+  let controller: ApolloTestingController;
 	let scheduler: TestScheduler;
+  let url: string;
+  let resolution: MagentoResolution;
+  let resolvableUrl: DaffExternallyResolvableUrl;
 
 	const setupTest = (
 	) => {
 		TestBed.configureTestingModule({
-			imports: [DaffExternalRouterDriverMagentoModule.forRoot()],
+			imports: [
+        DaffExternalRouterDriverMagentoModule.forRoot(),
+        ApolloTestingModule
+      ],
 		});
-		service = TestBed.get(DaffExternalRouterMagentoDriver);
+		service = TestBed.inject(DaffExternalRouterMagentoDriver);
+    controller = TestBed.inject(ApolloTestingController);
 
 		scheduler = new TestScheduler((actual, expected) => {
-			expect(actual).toEqual(expected);
+      expect(actual).toEqual(expected);
 		});
+
+    url = 'url';
+    resolution = {
+      relative_url: url,
+      type: MagentoUrlRewriteEntityTypeEnum.PRODUCT,
+      redirectCode: 0,
+      entity_uid: '',
+    };
+    resolvableUrl = {
+      url,
+      type: MagentoUrlRewriteEntityTypeEnum.PRODUCT
+    };
 	};
 
 	it('should be created', () => {
 		setupTest();
 		expect(service).toBeTruthy();
 	});
+
+  describe('resolve', () => {
+    it('should return a resolvable url', done => {
+      setupTest();
+
+      service.resolve(url).subscribe(result => {
+        expect(result.url).toEqual(url);
+        expect(result.type).toEqual(MagentoUrlRewriteEntityTypeEnum.PRODUCT);
+        done();
+      });
+
+      const op = controller.expectOne(MagentoResolveUrl);
+
+      op.flush({
+        data: resolution
+      });
+    });
+  });
 });
---
+++ libs/external-router/driver/magento/src/transforms/resolution-to-resolvable-url.spec.ts
@@ -0,0 +1,30 @@
+import { DaffExternallyResolvableUrl } from '@daffodil/external-router';
+import { MagentoResolution, MagentoUrlRewriteEntityTypeEnum } from '@daffodil/external-router/driver/magento';
+
+import { transformResolutionToResolvableUrl } from './resolution-to-resolvable-url';
+
+describe('@daffodil/external-router/driver/magento | transformResolutionToResolvableUrl', () => {
+  let url: string;
+  let resolution: MagentoResolution;
+  let resolvableUrl: DaffExternallyResolvableUrl;
+
+  beforeEach(() => {
+    url = 'url';
+    resolution = {
+      relative_url: url,
+      type: MagentoUrlRewriteEntityTypeEnum.PRODUCT,
+      redirectCode: 0,
+      entity_uid: '',
+    };
+    resolvableUrl = {
+      url,
+      type: MagentoUrlRewriteEntityTypeEnum.PRODUCT
+    };
+  });
+
+  it('should return a resolvable url with the correct values', () => {
+    const result = transformResolutionToResolvableUrl(resolution);
+    expect(result.type).toEqual(MagentoUrlRewriteEntityTypeEnum.PRODUCT);
+    expect(result.url).toEqual(url);
+  })
+});
