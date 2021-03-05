8187364cee2847fca782d10e45786cb281cbc68a
Peter Lauck <griest024@gmail.com>
2021-03-05 11:57:51 -0500

    docs(cart): using extra cart fragments forces increase to query complexity limit

--- libs/cart/driver/magento/src/injection-tokens/fragments/cart.ts
+++ libs/cart/driver/magento/src/injection-tokens/fragments/cart.ts
@@ -7,6 +7,11 @@ import { DocumentNode } from 'graphql';
  * The data will appear in DaffCart#extra_attributes.
  *
  * Fragment structure is platform-specific and this feature should be used with care.
+ *
+ * This can increase query complexity above the Magento default of 300.
+ * Daffodil only guarantees that stock queries don't exceed the limit of 300.
+ * Apps using this token should therefore increase the query complexity limit
+ * to accommodate the extra complexity contributed by the provided fragments.
  */
 export const DAFF_CART_MAGENTO_EXTRA_CART_FRAGMENTS = new InjectionToken<DocumentNode[]>(
   'DAFF_CART_MAGENTO_EXTRA_CART_FRAGMENTS',
