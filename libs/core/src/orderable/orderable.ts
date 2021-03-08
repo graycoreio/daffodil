408ab350b9318496c43bbd84b91d3a11a664936a
Damien Retzinger <damienwebdev@gmail.com>
2021-03-08 14:58:02 -0500

    linting

--- libs/core/src/orderable/orderable.ts
+++ libs/core/src/orderable/orderable.ts
@@ -1,7 +1,7 @@
 /**
  * An interface for models where the order of elements is important
- * and definable. 
- * 
+ * and definable.
+ *
  * Disambiguation: This is does not relate to ecommerce "Orders", e.g.a purchase.
  */
 export interface DaffOrderable {
