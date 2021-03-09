13ac9d76da6b2701bbadb83db4355f4edb9b8170
Damien Retzinger <damienwebdev@gmail.com>
2021-03-09 08:54:52 -0500

    add a space

--- libs/core/src/orderable/orderable.ts
+++ libs/core/src/orderable/orderable.ts
@@ -2,7 +2,7 @@
  * An interface for models where the order of elements is important
  * and definable.
  *
- * Disambiguation: This is does not relate to ecommerce "Orders", e.g.a purchase.
+ * Disambiguation: This is does not relate to ecommerce "Orders", e.g. a purchase.
  */
 export interface DaffOrderable {
 	order: number;
