18ebe25775e436c768587700d287a01dfff6cfd9
griest024 <griest024@gmail.com>
2021-03-10 09:00:16 -0500

    feat(contact): add store feature key (#1335)

--- libs/contact/state/src/selectors/contact.selector.ts
+++ libs/contact/state/src/selectors/contact.selector.ts
@@ -4,14 +4,15 @@ import {
   createSelector,
 } from '@ngrx/store';
 
+import { DAFF_CONTACT_STORE_FEATURE_KEY } from '../reducers/contact-store-feature-key';
 import { DaffContactState } from '../reducers/contact.reducer';
 
 export interface DaffContactFeatureState {
-  contact: DaffContactState;
+  [DAFF_CONTACT_STORE_FEATURE_KEY]: DaffContactState;
 }
 
 export const selectContactFeatureState:
-  MemoizedSelector<DaffContactFeatureState, DaffContactState> = createFeatureSelector<DaffContactState>('contact');
+  MemoizedSelector<DaffContactFeatureState, DaffContactState> = createFeatureSelector<DaffContactState>(DAFF_CONTACT_STORE_FEATURE_KEY);
 
 export const selectDaffContactLoading = createSelector(
   selectContactFeatureState, (state: DaffContactState) => state.loading,
