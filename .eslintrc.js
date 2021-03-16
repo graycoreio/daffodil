843827eeadde7e7c1d20fa6ead6cadba2c79244b
griest024 <griest024@gmail.com>
2021-03-16 09:42:33 -0400

    feat(tools): add eslint shareable config (#1359)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,6 @@ module.exports = {
       files: [
         '*.ts'
       ],
-      plugins: [
-        'modules-newline',
-        'jasmine'
-      ],
       parserOptions: {
         project: [
           'tsconfig.json',
@@ -16,30 +12,9 @@ module.exports = {
         createDefaultProgram: true
       },
       extends: [
-        'plugin:@angular-eslint/ng-cli-compat',
-        'plugin:@angular-eslint/ng-cli-compat--formatting-add-on',
-        'plugin:@angular-eslint/template/process-inline-templates',
-        'plugin:import/typescript',
-        'plugin:jasmine/recommended',
+        './tools/eslint/config/index.js',
       ],
       rules: {
-        'prefer-arrow/prefer-arrow-functions': [
-          'error',
-          {
-            allowStandaloneDeclarations: true
-          }
-        ],
-        '@typescript-eslint/naming-convention': [
-          'error',
-          {
-            selector: 'enumMember',
-            format: ['PascalCase', 'UPPER_CASE', 'camelCase']
-          }
-        ],
-        'no-shadow': 'off',
-        '@typescript-eslint/no-shadow': ['error'],
-        '@typescript-eslint/member-ordering': 'off',
-        'import/no-unassigned-import': ['error'],
         'import/order': [
           'error',
           {
@@ -61,75 +36,6 @@ module.exports = {
             }
           }
         ],
-        'modules-newline/import-declaration-newline': 'warn',
-        'modules-newline/export-declaration-newline': 'warn',
-        indent: [
-          'error',
-          2,
-          {
-            SwitchCase: 1
-          }
-        ],
-        'object-curly-newline': [
-          'error',
-          {
-            ImportDeclaration: {multiline: true, minProperties: 2},
-            ExportDeclaration: {multiline: true, minProperties: 2}
-          }
-        ],
-        'object-curly-spacing': [
-          'error',
-          'always',
-          {
-            arraysInObjects: false,
-            objectsInObjects: false
-          }
-        ],
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': [
-          'error',
-          {
-            functions: false
-          }
-        ],
-        '@angular-eslint/component-class-suffix': [
-          'error',
-          {
-            suffixes: [
-              'Component'
-            ]
-          }
-        ],
-        '@typescript-eslint/consistent-type-assertions': [
-          'error',
-          {
-            assertionStyle: 'angle-bracket'
-          }
-        ],
-        '@typescript-eslint/consistent-type-definitions': [ 'error', 'interface'],
-        '@typescript-eslint/dot-notation': 'off',
-        '@typescript-eslint/explicit-member-accessibility': [
-          'off',
-          {
-            accessibility: 'explicit'
-          }
-        ],
-        'brace-style': [
-          'error',
-          '1tbs'
-        ],
-        'no-trailing-spaces': 'error',
-        'comma-dangle': [
-          'error',
-          'always-multiline'
-        ],
-        'id-blacklist': 'off',
-        'id-match': 'off',
-        'no-underscore-dangle': 'off',
-        'max-len': 'off',
-        'jasmine/no-suite-dupes': ['warn', 'branch'],
-        'jasmine/no-spec-dupes': ['warn', 'branch'],
-        'jasmine/new-line-before-expect': 'off',
       }
     },
     {
