3450d7d406bc710068ac4494640006766d0255fb
Damien Retzinger <damienwebdev@gmail.com>
2021-03-18 09:58:11 -0400

    remove

--- libs/category/src/models/category-page-configuration-state.ts
+++ libs/category/src/models/category-page-configuration-state.ts
@@ -1,11 +1,13 @@
+import { DaffSortOption } from '@daffodil/core/state';
+
 import { DaffCategoryFilter } from './category-filter';
-import { DaffCategorySortOptions } from './category-sort-options';
 import { DaffCategoryRequest } from './requests/category-request';
 
 export type DaffCategoryPageConfigurationState = DaffCategoryRequest & {
 	filters: DaffCategoryFilter[];
-  sort_options: DaffCategorySortOptions;
+  sort_options: DaffSortOption[];
 	total_pages: number;
 	total_products: number;
 	product_ids: string[];
 };
+
