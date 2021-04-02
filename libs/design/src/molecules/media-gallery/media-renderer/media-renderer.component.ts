f21c50a3302ace83cf7889c47c1c83c5917ede60
xelaint <xelaint@gmail.com>
2021-04-02 10:12:24 -0400

    fix linting

--- libs/design/src/molecules/media-gallery/media-renderer/media-renderer.component.ts
+++ libs/design/src/molecules/media-gallery/media-renderer/media-renderer.component.ts
@@ -33,74 +33,75 @@ export class DaffMediaRendererComponent implements OnInit, OnDestroy {
   /**
    * Private tracker for indicating when the component is destroyed.
    */
-	 private _destroy$ = new Subject();
+   private _destroy$ = new Subject();
 
-	/**
-	 * The constructor function of the component to render.
-	 */
-	@Input() component: Type<unknown>;
+  /**
+   * The constructor function of the component to render.
+   */
+  @Input() component: Type<unknown>;
 
-	constructor(
-		private componentFactoryResolver: ComponentFactoryResolver,
-		private registry: DaffMediaGalleryRegistry,
-		private gallery: DaffMediaGalleryComponent,
-	) {}
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private registry: DaffMediaGalleryRegistry,
+    private gallery: DaffMediaGalleryComponent,
+  ) {}
 
-	/**
-	 * The slot that the "component" is rendered into.
-	 */
-	@ViewChild(TemplateRef, { static: true, read: ViewContainerRef })
-	slot: ViewContainerRef;
+  /**
+   * The slot that the "component" is rendered into.
+   */
+  @ViewChild(TemplateRef, { static: true, read: ViewContainerRef })
+  slot: ViewContainerRef;
 
-	ngOnInit() {
-	  this.registry.galleries[this.gallery.name]
-	    .pipe(takeUntil(this._destroy$))
-	    .subscribe((gallery) => {
+  ngOnInit() {
+    this.registry.galleries[this.gallery.name]
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((gallery) => {
 
-	    /**
-			 * Clear out the slot for the dynamically rendered thumbnail
-			 */
-	    this.slot.clear();
+        /**
+         * Clear out the slot for the dynamically rendered thumbnail
+         */
+        this.slot.clear();
 
-	    const _selectedThumbnail = gallery.thumbnails.filter(media => media.selected).shift();
+        const _selectedThumbnail = gallery.thumbnails.filter(media => media.selected).shift();
 
-	    /**
-			 * If there's no selected media, render nothing.
-			 */
-	    if(!_selectedThumbnail) {
-	      return;
-	    }
+        /**
+         * If there's no selected media, render nothing.
+         */
+        if(!_selectedThumbnail) {
+          return;
+        }
 
-	    const _selectedThumbnailComponent = _selectedThumbnail.component;
+        const _selectedThumbnailComponent = _selectedThumbnail.component;
 
-	    /**
-			 * Create the component to insert.
-			 */
-	    const component = this.componentFactoryResolver.resolveComponentFactory(
-				<Type<unknown>>_selectedThumbnailComponent.constructor,
-	    );
-	    const componentRef = this.slot.createComponent(component);
+        /**
+         * Create the component to insert.
+         */
+        const component = this.componentFactoryResolver.resolveComponentFactory(
+        <Type<unknown>>_selectedThumbnailComponent.constructor,
+        );
+        const componentRef = this.slot.createComponent(component);
 
-	    /**
-			 * Fill the component with it's values from the original component
-			 */
-	    component.inputs.forEach(input => {
-	      componentRef.instance[input.propName] = _selectedThumbnailComponent[input.propName];
-	    });
+        /**
+         * Fill the component with it's values from the original component
+         */
+        component.inputs.forEach(input => {
+          componentRef.instance[input.propName] = _selectedThumbnailComponent[input.propName];
+        });
 
-			/**
-			 * Trigger a change detection on the component tree, outside the cycle to address
-			 * the fact that the component was created outside a typical CD loop.
-			 */
-	    componentRef.changeDetectorRef.detectChanges();
-	  });
-	}
+        /**
+         * Trigger a change detection on the component tree, outside the cycle to address
+         * the fact that the component was created outside a typical CD loop.
+         */
+        componentRef.changeDetectorRef.detectChanges();
+      });
+  }
 
-	/**
-	 * Used to unsubscribe from the dynamic component.
-	 */
-	ngOnDestroy() {
-	  this._destroy$.next(true);
-	  this._destroy$.unsubscribe();
-	}
+  /**
+   * Used to unsubscribe from the dynamic component.
+   */
+  ngOnDestroy() {
+    this._destroy$.next(true);
+    this._destroy$.unsubscribe();
+  }
 }
+
