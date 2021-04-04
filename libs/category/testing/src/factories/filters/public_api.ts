9a2d4294d2233a6c59b68fd6aeaeb34ed0c3ffc4
Peter Lauck <griest024@gmail.com>
2021-04-04 18:13:35 -0400

    export factories

--- libs/category/testing/src/factories/filters/public_api.ts
+++ libs/category/testing/src/factories/filters/public_api.ts
@@ -1,1 +1,5 @@
 export { DaffCategoryFilterFactory } from './filter.factory';
+export { DaffCategoryFilterRequestFactory } from './request.factory';
+export { DaffCategoryFilterToggleRequestFactory } from './toggle-request';
+
+export * from './type/public_api';
---
+++ libs/category/testing/src/factories/filters/type/public_api.ts
@@ -0,0 +1,11 @@
+export * from './equal/public_api';
+export * from './range-numeric/public_api';
+
+export {
+  DaffCategoryFilterEqualFactory,
+  MockCategoryEqualFilter,
+} from './equal';
+export {
+  DaffCategoryFilterRangeNumericFactory,
+  MockCategoryFilterRangeNumeric,
+} from './range-numeric';
