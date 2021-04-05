116da6c4ed2fb55f0db719990019bd4c1f06616e
griest024 <griest024@gmail.com>
2021-04-05 09:19:05 -0400

    export factories (#1403)

--- libs/category/testing/src/public_api.ts
+++ libs/category/testing/src/public_api.ts
@@ -1,6 +1,3 @@
-export { DaffCategoryFactory } from './factories/category.factory';
-export {
-  DaffCategoryPageMetadataFactory,
-  MockCategoryPageMetadata,
-} from './factories/category-page-metadata.factory';
 export { isCategory } from './helpers/category-helper';
+
+export * from './factories/public_api';
