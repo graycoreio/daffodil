3f7da505c8998c789df9dd474ac283507b45bade
Damien Retzinger <damienwebdev@gmail.com>
2021-04-05 11:11:31 -0400

    add some magento driver factories

--- libs/category/driver/magento/testing/src/factories/aggregation/aggregation.factory.ts
+++ libs/category/driver/magento/testing/src/factories/aggregation/aggregation.factory.ts
@@ -5,14 +5,13 @@ import { MagentoAggregation } from '@daffodil/category/driver/magento';
 import { DaffModelFactory } from '@daffodil/core/testing';
 
 import { DaffCategoryDriverMagentoAggregationPriceFactory } from './type/price.factory';
+import { DaffCategoryDriverMagentoAggregationSelectFactory } from './type/select.factory';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DaffCategoryDriverMagentoAggregationFactory extends DaffModelFactory<MagentoAggregation> {
-  constructor(
-    private priceFactory: DaffCategoryDriverMagentoAggregationPriceFactory,
-  ){
-    super(priceFactory.type);
+  constructor(private selectFactory: DaffCategoryDriverMagentoAggregationSelectFactory, private priceFactory: DaffCategoryDriverMagentoAggregationPriceFactory){
+    super(faker.random.number({ min: 1, max: 2 }) === 2 ? selectFactory.type : priceFactory.type);
   }
 }
