8c0965c72fe55757f8e99aeb45e284755f17bfe0
Damien Retzinger <damienwebdev@gmail.com>
2021-04-06 09:47:55 -0400

    expand the public api for range filter label computation and computing option dictionaries

--- libs/category/src/filters/public_api.ts
+++ libs/category/src/filters/public_api.ts
@@ -6,4 +6,6 @@ export * from './is-applied/public_api';
 export * from './to-request/public_api';
 export { daffCategoryFilterArrayToDict } from './array-to-dict/array-to-dict';
 export { daffCategoryFilterRangePairArrayToDict } from './type/range/array-to-dict/pair';
+export { daffCategoryFilterEqualOptionArrayToDict } from './type/equal/array-to-dict/option';
+export { daffCategoryComputeFilterRangePairLabel } from './type/range/compute-pair-label';
 export * from './validators/public_api';
