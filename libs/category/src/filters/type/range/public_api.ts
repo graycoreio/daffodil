0f3ffb13361b981033a5ec50d049cacc1e2531d9
Damien Retzinger <damienwebdev@gmail.com>
2021-04-06 09:47:55 -0400

    expand the public api for range filter label computation and computing option dictionaries

--- libs/category/src/filters/type/range/public_api.ts
+++ libs/category/src/filters/type/range/public_api.ts
@@ -4,3 +4,4 @@ export { daffIsFilterRangeApplied } from './is-applied/is-applied';
 export { daffIsRequestedFilterRangeOptionApplied } from './is-applied/is-requested-option-applied';
 export { daffRemoveFilterRange } from './behaviors/remove/remove';
 export { daffToggleFilterRange } from './behaviors/toggle/toggle';
+export { daffCategoryComputeFilterRangePairLabel } from './compute-pair-label';
