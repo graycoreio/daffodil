c0b9897a13be2dff8416249881067703a723fdfe
Damien Retzinger <damienwebdev@gmail.com>
2021-04-09 15:27:57 -0400

    move request up out of requests folder

--- libs/category/src/models/public_api.ts
+++ libs/category/src/models/public_api.ts
@@ -8,7 +8,7 @@ export { DaffCategoryPageConfigurationState } from './category-page-configuratio
 export { DaffCategoryBreadcrumb } from './category-breadcrumb';
 export { DaffCategory } from './category';
 export { DaffGenericCategory } from './generic-category';
-export { DaffCategoryRequest } from './requests/category-request';
+export { DaffCategoryRequest } from './category-request';
 export * from './requests/filter-request';
 export * from './category-filter-base';
 export * from './category-applied-filter';
