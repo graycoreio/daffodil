66060b9aefc6ee81eb2a42e0104bd562ad9610a9
griest024 <griest024@gmail.com>
2021-04-09 09:22:38 -0400

    add array to dict tests and docs (#1419)

--- libs/category/src/filters/array-to-dict/array-to-dict.ts
+++ libs/category/src/filters/array-to-dict/array-to-dict.ts
@@ -2,6 +2,9 @@ import { Dict } from '@daffodil/core';
 
 import { DaffCategoryFilter } from '../../models/public_api';
 
+/**
+ * Converts a list of category filters to a dict of filters keyed by filter name.
+ */
 export const daffCategoryFilterArrayToDict = (filters: DaffCategoryFilter[]): Dict<DaffCategoryFilter> =>
   filters.reduce((acc, filter) => {
     acc[filter.name] = filter;
