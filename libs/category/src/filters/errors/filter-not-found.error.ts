52a71e09b3ecb2ee0d9df8037f479c3128c4f37b
Damien Retzinger <damienwebdev@gmail.com>
2021-04-12 16:47:51 -0400

    adding docs to filter errors

--- libs/category/src/filters/errors/filter-not-found.error.ts
+++ libs/category/src/filters/errors/filter-not-found.error.ts
@@ -5,6 +5,9 @@ import {
 
 import { DaffCategoryErrorCodes } from '../../errors/codes.enum';
 
+/**
+ * An error thrown when a lookup for a filter.
+ */
 export class DaffCategoryFilterNotFound extends DaffInheritableError implements DaffError {
   public readonly code: string = DaffCategoryErrorCodes.CATEGORY_FILTER_NOT_FOUND;
 
