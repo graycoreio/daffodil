5ba302371328f273e45e83a8561e77ded21445af
Damien Retzinger <damienwebdev@gmail.com>
2021-04-13 10:27:35 -0400

    fix a comma splice

--- libs/category/src/models/filters/category-filter.ts
+++ libs/category/src/models/filters/category-filter.ts
@@ -2,8 +2,8 @@ import { DaffCategoryFilterEqual } from './type/equal/filter';
 import { DaffCategoryFilterRangeNumeric } from './type/range-numeric/filter';
 
 /**
- * A single category filter, along with its options. For example, a filter may be
- * something like "Color", and have the filter options of "Blue", "Green", etc.
+ * A single category filter along with its options. For example, a filter may be
+ * something like "Color" with options of "Red", "Blue", and "Green".
  * Additionally, filters have "types", see {@link DaffCategoryFilterType} for a
  * complete list.
  */
