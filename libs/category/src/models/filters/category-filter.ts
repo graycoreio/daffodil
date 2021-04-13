986091c6a5fac2258c114a82fdd8d05d38b18ea6
Damien Retzinger <damienwebdev@gmail.com>
2021-04-13 10:26:18 -0400

    fix a bad quote

--- libs/category/src/models/filters/category-filter.ts
+++ libs/category/src/models/filters/category-filter.ts
@@ -3,7 +3,7 @@ import { DaffCategoryFilterRangeNumeric } from './type/range-numeric/filter';
 
 /**
  * A single category filter, along with its options. For example, a filter may be
- * something like "Color", and have the filter options of "Blue", "Green,", etc.
+ * something like "Color", and have the filter options of "Blue", "Green", etc.
  * Additionally, filters have "types", see {@link DaffCategoryFilterType} for a
  * complete list.
  */
