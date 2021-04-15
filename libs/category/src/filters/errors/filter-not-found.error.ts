098bb0a668cbe7fc09a1f65ef8ed842f02260190
Peter Lauck <griest024@gmail.com>
2021-04-15 12:07:30 -0400

    feat(category): remove category prefix from error codes

--- libs/category/src/filters/errors/filter-not-found.error.ts
+++ libs/category/src/filters/errors/filter-not-found.error.ts
@@ -9,7 +9,7 @@ import { DaffCategoryErrorCodes } from '../../errors/codes.enum';
  * An error thrown when a lookup for a filter.
  */
 export class DaffCategoryFilterNotFound extends DaffInheritableError implements DaffError {
-  public readonly code: string = DaffCategoryErrorCodes.CATEGORY_FILTER_NOT_FOUND;
+  public readonly code: string = DaffCategoryErrorCodes.FILTER_NOT_FOUND;
 
   constructor(message?: string) {
 	  super(message);
--- libs/category/src/filters/errors/request-type-mismatch.error.ts
+++ libs/category/src/filters/errors/request-type-mismatch.error.ts
@@ -10,7 +10,7 @@ import { DaffCategoryErrorCodes } from '../../errors/codes.enum';
  * matches a filter of another type.
  */
 export class DaffCategoryFilterRequestTypeMismatch extends DaffInheritableError implements DaffError {
-  public readonly code: string = DaffCategoryErrorCodes.CATEGORY_FILTER_REQUEST_TYPE_MISMATCH;
+  public readonly code: string = DaffCategoryErrorCodes.FILTER_REQUEST_TYPE_MISMATCH;
 
   constructor(message?: string) {
 	  super(message);
