1807a953d4bc1f581ee444da05f77eb47d56334f
Damien Retzinger <damienwebdev@gmail.com>
2021-04-19 18:41:47 -0400

    build(design): add a watcher-style runner to build docs and design-land at the same time (#1431)

--- tools/dgeni/build.ts
+++ tools/dgeni/build.ts
@@ -1,12 +1,13 @@
 import { Dgeni } from 'dgeni';
-import { apiDocs } from './src/transforms/daffodil-api-package';
-import { guideDocPackage } from './src/transforms/daffodil-guides-package';
-import { designExamplePackage } from './src/transforms/daffodil-design-examples-package';
-
 import rimraf from 'rimraf';
 
-rimraf('../../dist/docs/**/*', function() {
-	new Dgeni([guideDocPackage]).generate().catch(() => process.exit(1));
-	new Dgeni([apiDocs]).generate().catch(() => process.exit(1));
-	new Dgeni([designExamplePackage]).generate().catch(() => process.exit(1));
+import { apiDocs } from './src/transforms/daffodil-api-package';
+import { designExamplePackage } from './src/transforms/daffodil-design-examples-package';
+import { guideDocPackage } from './src/transforms/daffodil-guides-package';
+
+
+rimraf('../../dist/docs/**/*', () => {
+  new Dgeni([guideDocPackage]).generate().catch(() => process.exit(1));
+  new Dgeni([apiDocs]).generate().catch(() => process.exit(1));
+  new Dgeni([designExamplePackage]).generate().catch(() => process.exit(1));
 });
