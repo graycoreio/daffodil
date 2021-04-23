e1d9dc456486a7ba34ebebe4d62642108175f290
Peter Lauck <griest024@gmail.com>
2021-04-22 22:02:20 -0400

    feat(category): append url_suffix to category url key

--- libs/category/driver/magento/src/transformers/category-transformer.service.ts
+++ libs/category/driver/magento/src/transformers/category-transformer.service.ts
@@ -18,7 +18,7 @@ export class DaffMagentoCategoryTransformerService {
   transform(category: MagentoCategory): DaffCategory {
     return {
       id: String(category.id),
-      uri: category.url_path,
+      uri: `${category.url_path}${category.url_suffix}`,
       name: category.name,
       description: category.description,
       children_count: category.children_count,
