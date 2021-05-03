a8ede25c6b95b0e3f8054a96baf25b6a755d0081
griest024 <griest024@gmail.com>
2021-05-02 22:03:39 -0400

    feat(category): add URI resolver (#1477)

---
+++ libs/category/routing/src/resolvers/category-page-uri/category-page-uri.resolver.spec.ts
@@ -0,0 +1,206 @@
+import {
+  ɵPLATFORM_BROWSER_ID,
+  ɵPLATFORM_SERVER_ID,
+} from '@angular/common';
+import {
+  PLATFORM_ID,
+  Component,
+} from '@angular/core';
+import {
+  TestBed,
+  waitForAsync,
+  tick,
+  fakeAsync,
+} from '@angular/core/testing';
+import {
+  ActivatedRoute,
+  Router,
+} from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import {
+  StoreModule,
+  combineReducers,
+  Store,
+} from '@ngrx/store';
+import { fail } from 'assert';
+import { Observable } from 'rxjs';
+
+import {
+  DaffCategory,
+  DaffCategoryRequestKind,
+} from '@daffodil/category';
+import {
+  daffCategoryReducers,
+  DaffCategoryPageLoadByUri,
+  DaffCategoryPageLoadSuccess,
+  DaffCategoryPageLoadFailure,
+  DaffCategoryReducersState,
+  DAFF_CATEGORY_STORE_FEATURE_KEY,
+  DaffDefaultCategoryPageSize,
+} from '@daffodil/category/state';
+import {
+  DaffCategoryFactory,
+  DaffCategoryPageMetadataFactory,
+} from '@daffodil/category/testing';
+import { DaffProductFactory } from '@daffodil/product/testing';
+
+import { DaffCategoryPageUriResolver } from './category-page-uri.resolver';
+
+@Component({ template: '' })
+class TestComponent {}
+
+describe('DaffCategoryPageUriResolver', () => {
+  const actions$: Observable<any> = null;
+  let categoryResolver: DaffCategoryPageUriResolver;
+  let store: Store<DaffCategoryReducersState>;
+  let categoryFactory: DaffCategoryFactory;
+  let productFactory: DaffProductFactory;
+  let stubCategory: DaffCategory;
+  let route: ActivatedRoute;
+  let router: Router;
+  let path: string;
+
+  describe('resolve - on server', () => {
+
+    beforeEach(waitForAsync(() => {
+      path = '123';
+      TestBed.configureTestingModule({
+        imports: [
+          StoreModule.forRoot({
+            [DAFF_CATEGORY_STORE_FEATURE_KEY]: combineReducers(daffCategoryReducers),
+          }),
+          RouterTestingModule.withRoutes([
+            {
+              path,
+              component: TestComponent,
+            },
+            {
+              path: '**',
+              component: TestComponent,
+            },
+          ]),
+        ],
+        providers: [
+          provideMockActions(() => actions$),
+          { provide: DaffDefaultCategoryPageSize, useValue: 12 },
+          { provide: PLATFORM_ID, useValue: ɵPLATFORM_SERVER_ID },
+        ],
+      });
+
+      categoryResolver = TestBed.inject(DaffCategoryPageUriResolver);
+      categoryFactory = TestBed.inject(DaffCategoryFactory);
+      productFactory = TestBed.inject(DaffProductFactory);
+      stubCategory = categoryFactory.create();
+      store = TestBed.inject(Store);
+      route = TestBed.inject(ActivatedRoute);
+      router = TestBed.inject(Router);
+      router.initialNavigation();
+    }));
+
+    it('should dispatch a DaffCategoryPageLoadByUri action with the correct category url', fakeAsync(() => {
+      const url = `/${path}?asfgsfd=afdgsdf#asadiufy`;
+      router.navigateByUrl(url);
+      tick();
+
+      spyOn(store, 'dispatch');
+      categoryResolver.resolve(route.snapshot, router.routerState.snapshot);
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new DaffCategoryPageLoadByUri({ uri: url, page_size: 12, kind: DaffCategoryRequestKind.URI }),
+      );
+    }));
+
+    it('should resolve when DaffCategoryPageLoadSuccess is dispatched', fakeAsync(() => {
+      router.navigateByUrl(path);
+      tick();
+
+      categoryResolver.resolve(route.snapshot, router.routerState.snapshot).subscribe(value => {
+        expect(value).toEqual(true);
+      });
+
+      store.dispatch(new DaffCategoryPageLoadSuccess({
+        products: [productFactory.create()],
+        category: stubCategory,
+        categoryPageMetadata: new DaffCategoryPageMetadataFactory().create(),
+      }));
+    }));
+
+    it('should resolve when DaffCartLoadFailure is dispatched', fakeAsync(() => {
+      router.navigateByUrl(path);
+      tick();
+
+      categoryResolver.resolve(route.snapshot, router.routerState.snapshot).subscribe(value => {
+        expect(value).toEqual(true);
+      });
+
+      store.dispatch(new DaffCategoryPageLoadFailure(null));
+    }));
+
+    it('should not resolve without a category load success or failure', fakeAsync(() => {
+      router.navigateByUrl(path);
+      tick();
+
+      categoryResolver.resolve(route.snapshot, router.routerState.snapshot).subscribe(() => {
+        fail();
+      });
+      expect(true).toBeTruthy();
+    }));
+  });
+
+  describe('resolve - in the browser', () => {
+
+    beforeEach(waitForAsync(() => {
+      path = '123';
+      TestBed.configureTestingModule({
+        imports: [
+          StoreModule.forRoot({
+            [DAFF_CATEGORY_STORE_FEATURE_KEY]: combineReducers(daffCategoryReducers),
+          }),
+          RouterTestingModule.withRoutes([
+            {
+              path,
+              component: TestComponent,
+            },
+            {
+              path: '**',
+              component: TestComponent,
+            },
+          ]),
+        ],
+        providers: [
+          provideMockActions(() => actions$),
+          { provide: DaffDefaultCategoryPageSize, useValue: 12 },
+          { provide: PLATFORM_ID, useValue: ɵPLATFORM_BROWSER_ID },
+        ],
+      });
+
+      categoryResolver = TestBed.inject(DaffCategoryPageUriResolver);
+      categoryFactory = TestBed.inject(DaffCategoryFactory);
+      stubCategory = categoryFactory.create();
+      store = TestBed.inject(Store);
+      route = TestBed.inject(ActivatedRoute);
+      router = TestBed.inject(Router);
+      router.initialNavigation();
+    }));
+
+    it('should dispatch a DaffCategoryPageLoadByUri action with the correct category id', fakeAsync(() => {
+      const url = `/${path}?asfgsfd=afdgsdf#asadiufy`;
+      router.navigateByUrl(url);
+      tick();
+
+      spyOn(store, 'dispatch');
+      categoryResolver.resolve(route.snapshot, router.routerState.snapshot);
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new DaffCategoryPageLoadByUri({ uri: url, page_size: 12, kind: DaffCategoryRequestKind.URI }),
+      );
+    }));
+
+    it('should resolve without a category load success or failure', fakeAsync(() => {
+      router.navigateByUrl(path);
+      tick();
+      categoryResolver.resolve(route.snapshot, router.routerState.snapshot).subscribe((value) => {
+        expect(value).toBeTruthy();
+      });
+    }));
+  });
+});
--- libs/category/routing/src/resolvers/public_api.ts
+++ libs/category/routing/src/resolvers/public_api.ts
@@ -1,2 +1,3 @@
 export { DaffCategoryPageResolver } from './category-page/category-page.resolver';
 export { DaffCategoryPageIdResolver } from './category-page-id/category-page-id.resolver';
+export { DaffCategoryPageUriResolver } from './category-page-uri/category-page-uri.resolver';
