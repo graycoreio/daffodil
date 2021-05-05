5496f396ce0863d06bc6051f085e81f3c1164c6c
Peter Lauck <griest024@gmail.com>
2021-05-05 18:00:28 -0400

    docs

--- libs/core/src/locateable/locateable.interface.ts
+++ libs/core/src/locateable/locateable.interface.ts
@@ -1,4 +1,7 @@
-export interface DaffLocatable {
+/**
+ * An object that has a location referenced by a URI.
+ */
+export interface DaffLocateable {
   /**
    * The URI describing the location of the entity.
    */
