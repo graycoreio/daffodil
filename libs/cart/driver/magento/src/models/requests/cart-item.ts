25be970953ef8fb5b0d8b34ec95e37a23021a9ce
Peter Lauck <griest024@gmail.com>
2021-05-17 15:09:18 -0400

    fix entered options model

--- libs/cart/driver/magento/src/models/requests/cart-item.ts
+++ libs/cart/driver/magento/src/models/requests/cart-item.ts
@@ -3,5 +3,10 @@ export interface MagentoCartItemInput {
   sku: string;
   parent_sku?: string;
   selected_options?: string[];
-  entered_options?: string[];
+  entered_options?: MagentoEnteredOptionInput[];
+}
+
+export interface MagentoEnteredOptionInput {
+  uid: string;
+  value: string;
 }
