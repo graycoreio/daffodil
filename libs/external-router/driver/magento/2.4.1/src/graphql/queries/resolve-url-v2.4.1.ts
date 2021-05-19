8d552c781f98aa20602cbeda5572c61d853e8622
Damien Retzinger <damienwebdev@gmail.com>
2021-05-19 10:07:40 -0400

    feat(external-router): cache magento 2 urlResolver queries (#1552)

--- libs/external-router/driver/magento/2.4.1/src/graphql/queries/resolve-url-v2.4.1.ts
+++ libs/external-router/driver/magento/2.4.1/src/graphql/queries/resolve-url-v2.4.1.ts
@@ -1,11 +1,13 @@
 import { gql } from 'apollo-angular';
 
+export const DAFF_MAGENTO_RESOLVE_URL_QUERY_NAME = 'MagentoResolveUrlv241';
+
 /**
  * This query retrieves a URL resolution from Magento and informs you about
  * what type of route the URL is.
  */
 export const MagentoResolveUrlv241 = gql`
-	query MagentoResolveUrlv241($url: String!) {
+	query ${DAFF_MAGENTO_RESOLVE_URL_QUERY_NAME}($url: String!) {
 		urlResolver(url: $url) {
 			id
 			relative_url
