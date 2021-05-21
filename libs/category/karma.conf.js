496e9b3e587932b53308acbb7abc3104f40568ac
Peter Lauck <griest024@gmail.com>
2021-05-21 11:03:58 -0400

    test(all): extend from base karma config

--- libs/category/karma.conf.js
+++ libs/category/karma.conf.js
@@ -1,31 +1,10 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+baseConfiguration = require('../../tools/karma/karma.conf');
 
 module.exports = function (config) {
-    config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-coverage-istanbul-reporter'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      client: {
-        clearContext: false // leave Jasmine Spec Runner output visible in browser
-      },
-      coverageIstanbulReporter: {
-        dir: require('path').join(__dirname, '../../coverage/libs/category'),
-        reports: ['html', 'lcovonly'],
-        fixWebpackSourcePaths: true
-      },
-      reporters: ['progress', 'kjhtml'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: ['Chrome'],
-      singleRun: false
-    });
-  };
+  baseConfiguration(config);
+  config.set({
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../../coverage/libs/category'),
+    },
+  });
+};
