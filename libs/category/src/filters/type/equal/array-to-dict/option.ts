339ac781db465b8edf7b218d9328c1bd4db0d872
Peter Lauck <griest024@gmail.com>
2021-06-03 15:33:28 -0400

    feat(category): use core array to dict

--- libs/category/src/filters/type/equal/array-to-dict/option.ts
+++ libs/category/src/filters/type/equal/array-to-dict/option.ts
@@ -1,15 +1,13 @@
-import { Dict } from '@daffodil/core';
+import {
+  Dict,
+  daffArrayToDict,
+} from '@daffodil/core';
 
 import { DaffCategoryFilterEqualOption } from '../../../../models/public_api';
-
-const reducer = (acc, current: DaffCategoryFilterEqualOption): Dict<DaffCategoryFilterEqualOption>  => {
-  acc[current.value] = current;
-  return acc;
-};
 
 /**
  * Converts a list of category equal filter options to a dict of options keyed by option value.
  */
 export const daffCategoryFilterEqualOptionArrayToDict =
   (options: DaffCategoryFilterEqualOption[]): Dict<DaffCategoryFilterEqualOption> =>
-    options.reduce(reducer, <Dict<DaffCategoryFilterEqualOption>>{});
+    daffArrayToDict(options, option => option.value);
