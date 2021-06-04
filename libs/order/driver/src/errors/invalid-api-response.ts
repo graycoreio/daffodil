5d5eb2d90cfb9a9bf1f43748ad5bafa188f126c1
Luke <lderrickable@gmail.com>
2021-06-03 20:22:46 -0400

    revert error docs changes

--- libs/order/driver/src/errors/invalid-api-response.ts
+++ libs/order/driver/src/errors/invalid-api-response.ts
@@ -3,9 +3,6 @@ import {
   DaffInheritableError,
 } from '@daffodil/core';
 
-/**
- * @inheritdoc
- */
 export class DaffOrderInvalidAPIResponseError extends DaffInheritableError implements DaffError {
   public readonly code: string = 'DAFF_ORDER_INVALID_API_RESPONSE';
 
--- libs/order/driver/src/errors/order-not-found.ts
+++ libs/order/driver/src/errors/order-not-found.ts
@@ -3,9 +3,6 @@ import {
   DaffInheritableError,
 } from '@daffodil/core';
 
-/**
- * @inheritdoc
- */
 export class DaffOrderNotFoundError extends DaffInheritableError implements DaffError {
   public readonly code: string = 'DAFF_ORDER_NOT_FOUND';
 
