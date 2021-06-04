b52f24e947f2c9d3bae28c192cfc2b2bd07eb759
Luke <lderrickable@gmail.com>
2021-06-03 20:21:07 -0400

    revert error docs changes

--- libs/geography/driver/src/errors/country-not-found.ts
+++ libs/geography/driver/src/errors/country-not-found.ts
@@ -3,9 +3,6 @@ import {
   DaffInheritableError,
 } from '@daffodil/core';
 
-/**
- * @inheritdoc
- */
 export class DaffCountryNotFoundError extends DaffInheritableError implements DaffError {
   public readonly code: string = 'DAFF_GEOGRAPHY_COUNTRY_NOT_FOUND';
 
--- libs/geography/driver/src/errors/invalid-api-response.ts
+++ libs/geography/driver/src/errors/invalid-api-response.ts
@@ -3,9 +3,6 @@ import {
   DaffInheritableError,
 } from '@daffodil/core';
 
-/**
- * @inheritdoc
- */
 export class DaffGeographyInvalidAPIResponseError extends DaffInheritableError implements DaffError {
   public readonly code: string = 'DAFF_GEOGRAPHY_INVALID_API_RESPONSE';
 
