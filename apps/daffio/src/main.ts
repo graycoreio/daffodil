62b52f0aaacf6e4fdda3fab612b83bb81797d3e5
xelaint <xelaint@gmail.com>
2021-06-09 12:28:52 -0400

    feat(all): update all apps to use system fonts

--- apps/daffio/src/main.ts
+++ apps/daffio/src/main.ts
@@ -1,16 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as WebFont from 'webfontloader';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-
-WebFont.load({
-  typekit: {
-    id: 'bvl8ycj',
-  },
-});
 
 if (environment.production) {
   enableProdMode();
--- apps/design-land/src/main.ts
+++ apps/design-land/src/main.ts
@@ -4,14 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import * as WebFont from 'webfontloader';
-
-WebFont.load({
-  typekit: {
-    id: 'bvl8ycj',
-  },
-});
-
 if (environment.production) {
   enableProdMode();
 }
