2201e26fbdbda8953cede6deb2b6018d1c0cd0a2
Peter Lauck <griest024@gmail.com>
2021-06-21 11:09:47 -0400

    add return type

--- libs/core/src/identifiable/array-to-dict.ts
+++ libs/core/src/identifiable/array-to-dict.ts
@@ -1,6 +1,7 @@
+import { Dict } from '../types/dict.type';
 import { daffArrayToDict } from '../utils/array-to-dict';
 import { DaffIdentifiable } from './identifiable.interface';
 
 export const daffIdentifiableArrayToDict =
-  <T extends DaffIdentifiable = DaffIdentifiable>(items: T[]) =>
+  <T extends DaffIdentifiable = DaffIdentifiable>(items: T[]): Dict<T> =>
     daffArrayToDict(items, e => e.id);
