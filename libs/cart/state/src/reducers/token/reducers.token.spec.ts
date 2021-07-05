a87e23510b523911b1686173f355faa480d25896
Peter Lauck <griest024@gmail.com>
2021-07-05 10:19:58 -0400

    improve test

--- libs/cart/state/src/reducers/token/reducers.token.spec.ts
+++ libs/cart/state/src/reducers/token/reducers.token.spec.ts
@@ -1,20 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { ActionReducer } from '@ngrx/store';
 
+import { DaffCartPaymentMethod } from '@daffodil/cart';
 import {
-  DaffCartBillingAddressLoadSuccess,
   daffCartProvideExtraReducers,
-  daffCartProvideBeforeReducers,
   DaffCartReducersState,
   initialState as cartInitialState,
+  DaffCartPaymentLoadSuccess,
 } from '@daffodil/cart/state';
+import { DaffCartPaymentFactory } from '@daffodil/cart/testing';
 
 import { DAFF_CART_REDUCERS } from './reducers.token';
 
 describe('daffCartProvideExtraReducers', () => {
-  let beforeReducer: ActionReducer<DaffCartReducersState>;
-  let extraReducer: ActionReducer<DaffCartReducersState>;
+  let paymentFactory: DaffCartPaymentFactory;
+  let payment: DaffCartPaymentMethod;
 
+  let extraReducer: ActionReducer<DaffCartReducersState>;
   let reducer: ActionReducer<DaffCartReducersState>;
   let result: DaffCartReducersState;
 
@@ -30,27 +32,10 @@ describe('daffCartProvideExtraReducers', () => {
         ...state.cart,
         cart: {
           ...state.cart.cart,
-          available_payment_methods: [
-            ...state.cart.cart.available_payment_methods,
-            {
-              method: 'extra reducer',
-            },
-          ],
-        },
-      },
-    });
-    beforeReducer = (state, action) => ({
-      ...state,
-      cart: {
-        ...state.cart,
-        cart: {
-          ...state.cart.cart,
-          available_payment_methods: [
-            ...state.cart.cart.available_payment_methods,
-            {
-              method: 'before reducer',
-            },
-          ],
+          payment: {
+            ...state.cart.cart.payment,
+            method: `${state.cart.cart.payment.method} extra reducer`,
+          },
         },
       },
     });
@@ -58,20 +43,18 @@ describe('daffCartProvideExtraReducers', () => {
     TestBed.configureTestingModule({
       providers: [
         ...daffCartProvideExtraReducers(extraReducer),
-        ...daffCartProvideBeforeReducers(beforeReducer),
       ],
     });
 
+    paymentFactory = TestBed.inject(DaffCartPaymentFactory);
     reducer = TestBed.inject(DAFF_CART_REDUCERS);
 
-    result = reducer(initialState, new DaffCartBillingAddressLoadSuccess(null));
+    payment = paymentFactory.create();
+
+    result = reducer(initialState, new DaffCartPaymentLoadSuccess(payment));
   });
 
-  it('should run the before reducer first', () => {
-    expect(result.cart.cart.available_payment_methods[0].method).toEqual('before reducer');
-  });
-
-  it('should run the extra reducer last', () => {
-    expect(result.cart.cart.available_payment_methods[1].method).toEqual('extra reducer');
+  it('should run the extra reducer after the daffodil reducers', () => {
+    expect(result.cart.cart.payment.method).toEqual(`${payment.method} extra reducer`);
   });
 });
