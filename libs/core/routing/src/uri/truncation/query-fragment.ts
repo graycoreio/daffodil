2b8f4f8b78c37332f6df428ad74349ab87104337
Luke Derrick <lderrickable@gmail.com>
2021-07-06 10:52:30 -0400

    refactor(core): daffUriTruncateQueryFragment now supports ie11 (#1663)

--- libs/core/routing/src/uri/truncation/query-fragment.ts
+++ libs/core/routing/src/uri/truncation/query-fragment.ts
@@ -2,5 +2,9 @@
  * Truncates query parameters and fragments from a URI.
  * e.g. /foo/bar/baz.html?thing=test#id -> /foo/bar/baz.html
  */
-export const daffUriTruncateQueryFragment = (uri: string): string =>
-  (new URL(`http://fakedomain.com${uri.charAt(0) === '/' ? '' : '/'}${uri}`)).pathname;
+export const daffUriTruncateQueryFragment = (uri: string): string => {
+  if(uri[0] !== '/') {
+    uri = `/${uri}`;
+  }
+  return uri.replace(/(\?.*)|(#.*)/, '');
+};
