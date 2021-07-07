978971388fd6b599b288ed07ff0a04af8828a072
Peter Lauck <griest024@gmail.com>
2021-07-06 23:17:22 -0400

    export

--- libs/product/driver/magento/src/public_api.ts
+++ libs/product/driver/magento/src/public_api.ts
@@ -19,3 +19,5 @@ export { DaffProductMagentoDriverModule } from './product-driver.module';
 export * from './transforms/public_api';
 export * from './interfaces/public_api';
 export * from './config/public_api';
+export * from './injection-tokens/public_api';
+export * from './interfaces/public_api';
