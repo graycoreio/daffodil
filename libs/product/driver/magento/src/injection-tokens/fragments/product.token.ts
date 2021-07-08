875de8e591604dda4ad59fc32645c470cb484b5c
Peter Lauck <griest024@gmail.com>
2021-07-08 12:16:20 -0400

    docs

--- libs/product/driver/magento/src/injection-tokens/fragments/product.token.ts
+++ libs/product/driver/magento/src/injection-tokens/fragments/product.token.ts
@@ -5,9 +5,8 @@ import {
 import { DocumentNode } from 'graphql';
 
 /**
- * An multi-provider injection token for providing extra GraphQL fragments that will be spread into product queries.
+ * A multi-provider injection token for providing extra GraphQL fragments that will be spread into product queries.
  * This can be used to retrieve additional data that is not covered by the standard Daffodil interfaces.
- * The data will appear in DaffCart#extra_attributes.
  *
  * Fragment structure is platform-specific and this feature should be used with care.
  *
