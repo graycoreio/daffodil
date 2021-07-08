6836b8f1f2132bd72eda6136e6e95b399d9bf222
Luke Derrick <lderrickable@gmail.com>
2021-07-08 14:02:48 -0400

    refactor(core): replace regex.group in daffUriTruncateFileExtension f… (#1666)
    * refactor(core): replace regex.group in daffUriTruncateFileExtension for ie compatibility

--- libs/core/routing/src/uri/truncation/file-extension.ts
+++ libs/core/routing/src/uri/truncation/file-extension.ts
@@ -2,9 +2,8 @@
  * Truncates the file extension from the end of the URI.
  * Captures the truncated URI in the `uri` named group.
  */
-export const DAFF_TRUNCATE_FILE_EXTENSION_REGEX = /(?<uri>.+)\.(.*)$/;
+export const DAFF_TRUNCATE_FILE_EXTENSION_REGEX = /(.*)(?=\.)/;
 /**
  * Truncates the file extension from the end of the URI.
  */
-export const daffUriTruncateFileExtension = (uri: string): string =>
-  uri.match(DAFF_TRUNCATE_FILE_EXTENSION_REGEX)?.groups.uri || uri;
+export const daffUriTruncateFileExtension = (uri: string): string => uri.match(DAFF_TRUNCATE_FILE_EXTENSION_REGEX)?.[0] || uri;
