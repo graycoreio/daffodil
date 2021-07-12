07ccc78d3a71bac00bb415084c0467cb261c4d09
Peter Lauck <griest024@gmail.com>
2021-07-12 11:10:26 -0400

    add select test

--- libs/design/src/atoms/form/quantity-field/quantity-select/quantity-select.component.spec.ts
+++ libs/design/src/atoms/form/quantity-field/quantity-select/quantity-select.component.spec.ts
@@ -1,3 +1,5 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import {
   waitForAsync,
   ComponentFixture,
@@ -6,26 +8,60 @@ import {
 import {
   NgControl,
   FormControl,
+  ReactiveFormsModule,
 } from '@angular/forms';
-import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { Subject } from 'rxjs';
 
+import { DaffNativeSelectComponent } from '@daffodil/design';
+
+import { DaffNativeSelectModule } from '../../select/public_api';
 import { DaffQuantitySelectComponent } from './quantity-select.component';
 
-describe('DaffQuantitySelectComponent', () => {
+@Component({
+  template: `
+    <daff-quantity-select
+      [min]="minValue"
+      [max]="maxValue"
+      [extendable]="extendableValue"
+    ></daff-quantity-select>
+  `,
+})
+class WrapperComponent {
+  minValue = 2;
+  maxValue = 7;
+  extendableValue = true;
+}
+
+fdescribe('DaffQuantitySelectComponent', () => {
+  let wrapper: WrapperComponent;
   let component: DaffQuantitySelectComponent;
-  let fixture: ComponentFixture<DaffQuantitySelectComponent>;
+  let fixture: ComponentFixture<WrapperComponent>;
+  let selectComponent: DaffNativeSelectComponent;
+  let control;
 
   beforeEach(waitForAsync(() => {
+    control = {
+      statusChanges: new Subject(),
+      disabled: false,
+      control: new FormControl(1),
+      value: null,
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ DaffQuantitySelectComponent ],
+      imports: [
+        CommonModule,
+        DaffNativeSelectModule,
+        ReactiveFormsModule,
+      ],
+      declarations: [
+        DaffQuantitySelectComponent,
+        WrapperComponent,
+      ],
       providers: [
         {
           provide: NgControl,
-          useValue: {
-            statusChanges: of(),
-            value: null,
-            control: new FormControl(),
-          },
+          useValue: control,
         },
       ],
     })
@@ -33,12 +69,68 @@ describe('DaffQuantitySelectComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DaffQuantitySelectComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(WrapperComponent);
+    wrapper = fixture.componentInstance;
     fixture.detectChanges();
+
+    component = fixture.debugElement.query(By.css('daff-quantity-select')).componentInstance;
+    selectComponent = fixture.debugElement.query(By.css('[daff-native-select]')).componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when the select is extendable', () => {
+    beforeEach(() => {
+      wrapper.extendableValue = true;
+      fixture.detectChanges();
+    });
+
+    it('should create options from min to max including a max+ option', () => {
+      const expected = ['2', '3', '4', '5', '6', '7+'];
+      const optionTexts = fixture.debugElement.queryAll(By.css('select option')).map(el => el.nativeElement.innerText);
+
+      expect(optionTexts).toEqual(expected);
+    });
+  });
+
+  describe('when the select is not extendable', () => {
+    beforeEach(() => {
+      wrapper.extendableValue = false;
+      fixture.detectChanges();
+    });
+
+    it('should create options from min to max excluding a max+ option', () => {
+      const expected = ['2', '3', '4', '5', '6', '7'];
+      const optionTexts = fixture.debugElement.queryAll(By.css('select option')).map(el => el.nativeElement.innerText);
+
+      expect(optionTexts).toEqual(expected);
+    });
+  });
+
+  describe('when a value is set on the component', () => {
+    let value;
+
+    beforeEach(() => {
+      value = 5;
+      component.value = value;
+      fixture.detectChanges();
+    });
+
+    it('should set that value on the form control', () => {
+      expect(control.control.value).toEqual(value);
+    });
+  });
+
+  describe('when the component is focused', () => {
+    beforeEach(() => {
+      component.focus();
+      fixture.detectChanges();
+    });
+
+    it('should focus the select component', () => {
+      expect(selectComponent.focused).toBeTrue();
+    });
   });
 });
