68ff238417473c019f14e549242478e48a83ede6
Peter Lauck <griest024@gmail.com>
2021-07-15 16:12:23 -0400

    feat(product): export preview fragment

---
+++ libs/product/driver/magento/src/queries/public_api.ts
@@ -0,0 +1,6 @@
+export * from './get-product';
+export * from './get-product-by-url';
+export * from './get-all-products';
+export { magentoBundledProductFragment } from './fragments/bundled-product';
+export { magentoProductFragment } from './fragments/product';
+export { magentoProductPreviewFragment } from './fragments/product-preview';
