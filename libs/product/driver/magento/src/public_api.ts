91efecb66967f5cbe336ba9fd2903a0d84c622fd
griest024 <griest024@gmail.com>
2021-07-15 20:29:13 -0400

    feat(product): export magento preview fragment (#1693)

--- libs/product/driver/magento/src/public_api.ts
+++ libs/product/driver/magento/src/public_api.ts
@@ -1,11 +1,7 @@
-export * from './queries/get-product';
-export * from './queries/get-product-by-url';
-export * from './queries/get-all-products';
-export { magentoBundledProductFragment } from './queries/fragments/bundled-product';
-export { magentoProductFragment } from './queries/fragments/product';
 export { DaffMagentoProductService } from './product.service';
 export { DaffProductMagentoDriverModule } from './product-driver.module';
 
+export * from './queries/public_api';
 export * from './transforms/public_api';
 export * from './interfaces/public_api';
 export * from './config/public_api';
