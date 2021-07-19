6c051b8cb085c3c8ee6ec1af31462231fe7b273e
Peter Lauck <griest024@gmail.com>
2021-07-19 14:27:08 -0400

    add alias

--- libs/product/driver/magento/testing/src/factories/public_api.ts
+++ libs/product/driver/magento/testing/src/factories/public_api.ts
@@ -3,3 +3,5 @@ export * from './bundle/bundle.factory';
 export * from './configurable/configurable.factory';
 export * from './core/product.factory';
 export * from './core/product-preview.factory';
+
+export { MagentoSimpleProductFactory as MagentoProductFactory } from './simple/simple.factory';
