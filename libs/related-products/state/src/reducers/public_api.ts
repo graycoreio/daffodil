4fc84c3e19684fba38ddfa88351d704e80e97bfa
Peter Lauck <griest024@gmail.com>
2021-07-28 15:05:40 -0400

    provide directly to token

--- libs/related-products/state/src/reducers/public_api.ts
+++ libs/related-products/state/src/reducers/public_api.ts
@@ -1,9 +1,10 @@
-export { daffRelatedProductsReducers } from './reducers';
+export { DaffRelatedProductsReducerState } from './related-products/reducer-state.interface';
+export * from './related-products/reducer';
+
+export { daffRelatedProductsDedupeMetaReducer } from './meta-reducers';
 export {
   DaffRelatedProductsReducersState,
   DaffRelatedProductStateRootSlice,
 } from './reducers-state.interface';
-export { DaffRelatedProductsReducerState } from './related-products/reducer-state.interface';
-export { daffRelatedProductsReducer } from './related-products/reducer';
+export { daffRelatedProductsReducers } from './reducers';
 export { DAFF_RELATED_PRODUCTS_STORE_FEATURE_KEY } from './store-feature-key';
-export { daffRelatedProductsDedupeMetaReducer } from './meta-reducers';
--- libs/related-products/state/src/state.module.ts
+++ libs/related-products/state/src/state.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 
-import { daffProductProvideMetaReducers } from '@daffodil/product/state';
+import { DAFF_PRODUCT_META_REDUCERS } from '@daffodil/product/state';
 
 import {
   DAFF_RELATED_PRODUCTS_STORE_FEATURE_KEY,
   daffRelatedProductsDedupeMetaReducer,
+  daffRelatedProductsReducers,
 } from './reducers/public_api';
-import { daffRelatedProductsReducers } from './reducers/reducers';
 
 /**
  * A module that provides the default reducers for the related product redux state.
@@ -17,7 +17,11 @@ import { daffRelatedProductsReducers } from './reducers/reducers';
     StoreModule.forFeature(DAFF_RELATED_PRODUCTS_STORE_FEATURE_KEY, daffRelatedProductsReducers),
   ],
   providers: [
-    ...daffProductProvideMetaReducers(daffRelatedProductsDedupeMetaReducer),
+    {
+      provide: DAFF_PRODUCT_META_REDUCERS,
+      multi: true,
+      useValue: daffRelatedProductsDedupeMetaReducer,
+    },
   ],
 })
 export class DaffRelatedProductStateModule {}
