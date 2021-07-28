aeb184fee0c89dd3788b06b7d73bb46a3b9e63ab
Damien Retzinger <damienwebdev@gmail.com>
2021-07-28 15:44:23 -0400

    feat(docs-gen): compute the docGroup of an example from the folder path, not the filename

--- tools/dgeni/src/transforms/daffodil-design-examples-package/processors/designExampleDocumentCreator.ts
+++ tools/dgeni/src/transforms/daffodil-design-examples-package/processors/designExampleDocumentCreator.ts
@@ -1,4 +1,9 @@
-import { Processor, Document } from 'dgeni';
+import {
+  Processor,
+  Document,
+} from 'dgeni';
+
+export const docGroupRegex = /examples\/src\/(.*)\//;
 
 export class DesignExampleDocumentCreatorProcessor implements Processor {
   name = 'examples';
@@ -7,14 +12,14 @@ export class DesignExampleDocumentCreatorProcessor implements Processor {
 
   private convertExtensionToLanguage(extension: string) {
     switch(extension){
-      case "ts":
-        return "typescript";
-      case "html":
-        return "html";
-      case "scss":
-          return "css";
+      case 'ts':
+        return 'typescript';
+      case 'html':
+        return 'html';
+      case 'scss':
+        return 'css';
       default:
-          return "";
+        return '';
     }
   }
 
@@ -22,21 +27,32 @@ export class DesignExampleDocumentCreatorProcessor implements Processor {
     return {
       name: doc.fileInfo.baseName + '.' + doc.fileInfo.extension,
       content: doc.fileInfo.content,
-      language: this.convertExtensionToLanguage(doc.fileInfo.extension)
+      language: this.convertExtensionToLanguage(doc.fileInfo.extension),
+    };
+  }
+
+  private getDocGroup(doc: Document) {
+    const path: string = doc.fileInfo.relativePath;
+    const match = path.match(docGroupRegex);
+
+    if(match){
+      return match[1];
     }
   }
 
   $process(docs: Document[]): Document[] {
-    let designExampleDocs = [];
+    const designExampleDocs = [];
     docs.forEach((doc) => {
-      doc.docGroup = doc.fileInfo.baseName
-        .replace(/\.component$/, '')
-        .replace(/\.module$/, '')
+      doc.docGroup = this.getDocGroup(doc);
+      if(!doc.docGroup){
+        console.warn(`Design Land Docs Generation - Group is missing for ${doc.fileInfo.relativePath}`);
+        return;
+      }
       const designExample = designExampleDocs.find((designDoc) => designDoc.id === doc.docGroup);
 
       /**
        * We need to add a new type of doc for the examples.
-       * This doc is called a `design-example`. It houses the files that make 
+       * This doc is called a `design-example`. It houses the files that make
        * up the example as well as the element name of the example.
        */
       if(!designExample){
@@ -45,15 +61,14 @@ export class DesignExampleDocumentCreatorProcessor implements Processor {
           docType: 'design-example',
           name: doc.docGroup,
           element: doc.docGroup,
-          files: [this.convertDocToDesignExampleFile(doc)]
-        })
-      } 
-      else {
+          files: [this.convertDocToDesignExampleFile(doc)],
+        });
+      } else {
         // The example already exists, so we just need to append the new file.
-        designExample.files.push(this.convertDocToDesignExampleFile(doc))
+        designExample.files.push(this.convertDocToDesignExampleFile(doc));
       }
-    });
 
+    });
     return docs.concat(designExampleDocs);
   }
 };
