a459492117f2f715846932ab6169c725d9856bec
griest024 <griest024@gmail.com>
2021-07-30 09:39:39 -0400

    feat(core): add canonically locatable interface (#1715)

---
+++ libs/core/src/canonically-locateable/canonically-locateable.interface.ts
@@ -0,0 +1,9 @@
+/**
+ * An object that has a canonical location referenced by a URL.
+ */
+export interface DaffCanonicallyLocatable {
+  /**
+   * The URL describing the canonical location of the entity.
+   */
+  canonicalUrl: string;
+}
