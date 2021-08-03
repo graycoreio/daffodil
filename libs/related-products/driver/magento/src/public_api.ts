72ea8a371040f27ce4f03c82b36845e05ccff543
griest024 <griest024@gmail.com>
2021-08-03 12:29:41 -0400

    fix(related-products): arrow functions in provider factory (#1727)

--- libs/related-products/driver/magento/src/public_api.ts
+++ libs/related-products/driver/magento/src/public_api.ts
@@ -2,3 +2,5 @@ export { magentoRelatedProductsFragment } from './queries/fragments/related-prod
 export { DaffMagentoRelatedProductsTransformers } from './transforms/product-response.service';
 
 export * from './models/public_api';
+
+export { DaffRelatedProductsMagentoDriverModule } from './related-products-driver.module';
