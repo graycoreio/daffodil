11c1abb40e9009fa668fdf0b46f261f331ce61a8
Peter Lauck <griest024@gmail.com>
2021-08-08 15:16:02 -0400

    feat(seo): add meta update model

--- libs/seo/state/src/models/public_api.ts
+++ libs/seo/state/src/models/public_api.ts
@@ -1,2 +1,3 @@
 export { DaffSeoUpdateActionPair } from './update-action-pair.interface';
 export { DaffSeoCanonicalUrlUpdate } from './canonical-url-update.interface';
+export { DaffSeoMetaUpdate } from './meta-update.interface';
