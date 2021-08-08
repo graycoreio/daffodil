fc127abeea392b4f1119f94ba11167a739d5e478
Peter Lauck <griest024@gmail.com>
2021-08-08 15:19:22 -0400

    feat(seo): add meta page hook effects

--- libs/seo/state/src/state.module.ts
+++ libs/seo/state/src/state.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 
 import { DaffSeoCanonicalUrlEffects } from './effects/canonical.effects';
+import { DaffSeoMetaEffects } from './effects/meta.effects';
 
 @NgModule({
   imports: [
     EffectsModule.forFeature([
       DaffSeoCanonicalUrlEffects,
+      DaffSeoMetaEffects,
     ]),
   ],
 })
