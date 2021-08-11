0743b41f35ff9cd0866334f23e84a4627d47b821
Peter Lauck <griest024@gmail.com>
2021-08-11 15:24:23 -0400

    feat(seo): add title update model

--- libs/seo/state/src/models/public_api.ts
+++ libs/seo/state/src/models/public_api.ts
@@ -1,3 +1,4 @@
 export { DaffSeoUpdateActionPair } from './update-action-pair.interface';
 export { DaffSeoCanonicalUrlUpdate } from './canonical-url-update.interface';
+export { DaffSeoTitleUpdate } from './title-update.interface';
 export { DaffSeoMetaUpdate } from './meta-update.interface';
