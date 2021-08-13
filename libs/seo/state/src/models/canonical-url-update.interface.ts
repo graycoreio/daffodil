2a34672b47fe71e0ffe48cf1342f1de67372ab83
Peter Lauck <griest024@gmail.com>
2021-08-13 10:53:30 -0400

    docs

--- libs/seo/state/src/models/canonical-url-update.interface.ts
+++ libs/seo/state/src/models/canonical-url-update.interface.ts
@@ -2,4 +2,7 @@ import { Action } from '@ngrx/store';
 
 import { DaffSeoUpdateActionPair } from './update-action-pair.interface';
 
+/**
+ * Describes how to set canonical URLs based on dispatched actions.
+ */
 export type DaffSeoCanonicalUrlUpdate<T extends Action = Action> = DaffSeoUpdateActionPair<T, string>;
