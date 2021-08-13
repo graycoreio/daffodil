c34c19fd10a3bfa4e439d53ab29c4032516dcea2
Peter Lauck <griest024@gmail.com>
2021-08-13 10:52:47 -0400

    docs

--- libs/seo/state/src/models/title-update.interface.ts
+++ libs/seo/state/src/models/title-update.interface.ts
@@ -2,4 +2,7 @@ import { Action } from '@ngrx/store';
 
 import { DaffSeoUpdateActionPair } from './update-action-pair.interface';
 
+/**
+ * Describes how to set page titles based on dispatched actions.
+ */
 export type DaffSeoTitleUpdate<T extends Action = Action> = DaffSeoUpdateActionPair<T, string>;
