5c1d735b430e80a9d78179279c1fbee65a49df61
griest024 <griest024@gmail.com>
2021-08-16 10:19:03 -0400

    feat(seo): add meta definition models (#1744)

--- libs/seo/src/models/public_api.ts
+++ libs/seo/src/models/public_api.ts
@@ -1,1 +1,6 @@
 export { DaffSeoRestoreableServiceInterface } from './restoreable-service.interface';
+export {
+  DaffSeoMetaDefinition,
+  DaffSeoNameMetaDefinition,
+  DaffSeoPropertyMetaDefinition,
+} from './meta-definition.interface';
