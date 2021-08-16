628a73d02c2450dd50968e67d70df96a9e38f51c
Peter Lauck <griest024@gmail.com>
2021-08-16 12:13:02 -0400

    docs

--- libs/seo/state/src/models/meta-update.interface.ts
+++ libs/seo/state/src/models/meta-update.interface.ts
@@ -4,4 +4,7 @@ import { DaffSeoMetaDefinition } from '@daffodil/seo';
 
 import { DaffSeoUpdateActionPair } from './update-action-pair.interface';
 
+/**
+ * Describes how to set meta tags based on dispatched actions.
+ */
 export type DaffSeoMetaUpdate<T extends Action = Action> = DaffSeoUpdateActionPair<T, DaffSeoMetaDefinition>;
