da381691ea0092e64169fe1c4250f3f09e65d43b
Peter Lauck <griest024@gmail.com>
2021-08-18 13:38:19 -0400

    docs

--- libs/seo/state/src/effects/title.effects.ts
+++ libs/seo/state/src/effects/title.effects.ts
@@ -10,6 +10,9 @@ import { DAFF_SEO_TITLE_UPDATES } from '../injection-tokens/public_api';
 import { DaffSeoTitleUpdate } from '../models/public_api';
 import { DaffSeoPageHookEffects } from './page-hook.effects';
 
+/**
+ * Page hook effects for the page title.
+ */
 @Injectable()
 export class DaffSeoTitleEffects extends DaffSeoPageHookEffects<DaffRestoreableTitleService, DaffSeoTitleUpdate, string> {
   constructor(
