00c33a5cb4ae981a9a497742c2709061ebb0dd86
griest024 <griest024@gmail.com>
2021-08-18 09:54:14 -0400

    feat(seo): add meta update model (#1739)

--- libs/seo/state/src/models/public_api.ts
+++ libs/seo/state/src/models/public_api.ts
@@ -1,2 +1,3 @@
 export { DaffSeoUpdateActionPair } from './update-action-pair.interface';
 export { DaffSeoTitleUpdate } from './title-update.interface';
+export { DaffSeoMetaUpdate } from './meta-update.interface';
