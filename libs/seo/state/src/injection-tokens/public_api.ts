335403385a08d2cc825a9f04cc30536a598373d1
griest024 <griest024@gmail.com>
2021-08-18 13:33:36 -0400

    feat(seo): add `daffProvideMetaUpdates` and `DAFF_SEO_META_UPDATES` token (#1757)

--- libs/seo/state/src/injection-tokens/public_api.ts
+++ libs/seo/state/src/injection-tokens/public_api.ts
@@ -2,3 +2,7 @@ export {
   daffProvideTitleUpdates,
   DAFF_SEO_TITLE_UPDATES,
 } from './title/updates.token';
+export {
+  daffProvideMetaUpdates,
+  DAFF_SEO_META_UPDATES,
+} from './meta/updates.token';
