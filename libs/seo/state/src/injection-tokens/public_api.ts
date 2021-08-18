39b82d6deb663fa8258516e0c9095f098d14647e
Peter Lauck <griest024@gmail.com>
2021-08-18 13:35:11 -0400

    feat(seo): add `daffProvideCanonicalUrlUpdates` and `DAFF_SEO_CANONICAL_URL_UPDATES` token

--- libs/seo/state/src/injection-tokens/public_api.ts
+++ libs/seo/state/src/injection-tokens/public_api.ts
@@ -6,3 +6,7 @@ export {
   daffProvideMetaUpdates,
   DAFF_SEO_META_UPDATES,
 } from './meta/updates.token';
+export {
+  daffProvideCanonicalUrlUpdates,
+  DAFF_SEO_CANONICAL_URL_UPDATES,
+} from './canonical/updates.token';
