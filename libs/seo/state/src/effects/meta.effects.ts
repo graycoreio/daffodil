926008d55c90f4795864c110c4475b7eb2883919
griest024 <griest024@gmail.com>
2021-08-19 11:55:30 -0400

    feat(seo): add `DaffSeoMetaEffects` (#1761)

---
+++ libs/seo/state/src/effects/meta.effects.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  Inject,
+} from '@angular/core';
+import { MetaDefinition } from '@angular/platform-browser';
+import { Actions } from '@ngrx/effects';
+
+import { DaffRestoreableMetaService } from '@daffodil/seo';
+
+import { DAFF_SEO_META_UPDATES } from '../injection-tokens/public_api';
+import { DaffSeoMetaUpdate } from '../models/public_api';
+import { DaffSeoPageHookEffects } from './page-hook.effects';
+
+/**
+ * Page hook effects for the page meta tags.
+ */
+@Injectable()
+export class DaffSeoMetaEffects extends DaffSeoPageHookEffects<DaffRestoreableMetaService, DaffSeoMetaUpdate, MetaDefinition> {
+  constructor(
+    actions$: Actions,
+    @Inject(DAFF_SEO_META_UPDATES) updates: DaffSeoMetaUpdate[],
+    service: DaffRestoreableMetaService,
+  ) {
+    super(
+      actions$,
+      updates,
+      service,
+    );
+  }
+}
--- libs/seo/state/src/state.module.ts
+++ libs/seo/state/src/state.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 
+import { DaffSeoMetaEffects } from './effects/meta.effects';
 import { DaffSeoTitleEffects } from './effects/title.effects';
 
 @NgModule({
   imports: [
     EffectsModule.forFeature([
       DaffSeoTitleEffects,
+      DaffSeoMetaEffects,
     ]),
   ],
 })
