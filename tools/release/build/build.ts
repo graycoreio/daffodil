2766234b594a90a9a277d750d611881c9c1e8b38
griest024 <griest024@gmail.com>
2021-08-20 09:22:51 -0400

    fix(tools): release build task fails (#1766)

--- tools/release/build/build.ts
+++ tools/release/build/build.ts
@@ -2,6 +2,6 @@ import { series } from 'gulp';
 import { exec } from 'gulp-execa';
 import { RELEASE_CONFIG } from '../config';
 
-const lernaBuild = async () => await exec('lerna run build --stream', { cwd: RELEASE_CONFIG.PROJECT_PATH});
+const lernaBuild = async () => await exec('npx lerna run build --stream', { cwd: RELEASE_CONFIG.PROJECT_PATH});
 
 export const build = series(lernaBuild);
