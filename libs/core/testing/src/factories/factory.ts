04c0d37cf31ca5de0e5f602d62a1b3da2feb4c0c
griest024 <griest024@gmail.com>
2021-08-24 15:20:06 -0400

    feat(core): accept mock class instantiation args in constructor (#1777)
    * fix(checkout): improper creation of partial in testing driver
    * feat(core): accept mock class instantiation args in constructor

--- libs/core/testing/src/factories/factory.ts
+++ libs/core/testing/src/factories/factory.ts
@@ -1,18 +1,55 @@
-import { range } from '@daffodil/core';
+import {
+  range,
+  Constructable,
+} from '@daffodil/core';
 
 import { IDaffModelFactory } from './factory.interface';
 
-type ArglessConstructable<T> = new()  => T;
-
-
+/**
+ * The base class for model factories.
+ *
+ * The mock class is passed as the first constructor arg
+ * and any additional args are passed to the mock class constructor.
+ *
+ * The following example demonstrates using this feature to inject
+ * a different factory into a mock class.
+ *
+ * ```ts
+ * class MyMockModel {
+ *  constructor(
+ *    private otherFactory: MyOtherFactory
+ *  ) {}
+ *
+ *  private createOtherModel() {
+ *    return this.otherFactory.create()
+ *  }
+ *
+ *  otherModel = this.createOtherModel()
+ * }
+ *
+ * @Injectable()
+ * class TestFactory extends DaffModelFactory<MyMockModel> {
+ *  constructor(
+ *    otherFactory: MyOtherFactory
+ *  ) {
+ *    super(MyMockModel, otherFactory)
+ *  }
+ * }
+ * ```
+ */
 export abstract class DaffModelFactory<T extends Record<string, any>> implements IDaffModelFactory<T> {
-  constructor(public type: ArglessConstructable<T>){
+  _instantiationArgs: ConstructorParameters<Constructable<T>>;
 
+  constructor(
+    public type: Constructable<T>,
+    ...args: ConstructorParameters<Constructable<T>>
+  ) {
+    this._instantiationArgs = args;
   }
 
   create(partial = {}): T {
     return {
-      ...<any>new this.type(), // TODO: remove in TS 3.3
+      ...new this.type(...this._instantiationArgs),
       ...partial,
     };
   }
