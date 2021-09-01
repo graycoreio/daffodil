d020307cc818849532d97c9a4fde7118e7a4985b
Peter Lauck <griest024@gmail.com>
2021-09-01 13:11:03 -0400

    feat(core): add length param to `randomSlice`

--- libs/core/src/utils/random-slice.ts
+++ libs/core/src/utils/random-slice.ts
@@ -3,11 +3,15 @@
  *
  * @param array
  */
-export const randomSlice = <T>(array: T[]): T[] => {
+export const randomSlice = <T>(array: T[], length?: number): T[] => {
   if(array.length === 0) {
     return [];
   };
-  const start = Math.floor(Math.random() * Math.floor(array.length - 1));
-  const end =  start + Math.floor(Math.random() * (array.length - 1 - start));
+  if(length > array.length) {
+    throw new Error('Requested length is longer than array length.');
+  }
+  const arrayLength = length || Math.floor(Math.random() * array.length);
+  const start = Math.floor(Math.random() * (array.length - arrayLength));
+  const end = start + arrayLength;
   return array.slice(start, end);
 };
