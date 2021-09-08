1055034a54e855678fbbae18b8ae1993062f8c70
Peter Lauck <griest024@gmail.com>
2021-09-07 21:19:40 -0400

    implement with `randomSlice`

--- libs/core/src/utils/sample.ts
+++ libs/core/src/utils/sample.ts
@@ -1,7 +1,6 @@
+import { randomSlice } from './random-slice';
+
 /**
  * Returns a random element of an array.
  */
-export const sample = <T>(array: T[]): T => {
-  const index = Math.floor(Math.random() * array.length);
-  return array[index];
-};
+export const sample = <T>(array: T[]): T => randomSlice(array, 1)[0];
