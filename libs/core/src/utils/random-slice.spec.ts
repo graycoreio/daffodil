0465b21f8a5762ad1518c83327d00289b7e529bf
griest024 <griest024@gmail.com>
2021-09-07 21:08:27 -0400

    feat(core): add length param to `randomSlice` (#1789)

--- libs/core/src/utils/random-slice.spec.ts
+++ libs/core/src/utils/random-slice.spec.ts
@@ -8,7 +8,22 @@ describe('Core | Utils | random-slice', () => {
     expect(result).toEqual([]);
   });
 
-  it('should return a slice of the given array', () => {
+  it('should return a slice of the given array of the specified length', () => {
+    const length = 4;
+    const initialArray = [1,2,3,4,5,6,7,8,9];
+    const result = randomSlice(initialArray, length);
+    const start = initialArray.indexOf(result[0]);
+    const end = start + length;
+    expect(initialArray.slice(start, end)).toEqual(result);
+  });
+
+  it('should throw an error if the given length is greater than the length of the array', () => {
+    const initialArray = [1,2,3,4,5,6,7,8,9];
+    expect(() => randomSlice(initialArray, initialArray.length + 1))
+      .toThrowError('Requested length is longer than array length.');
+  });
+
+  it('should return a random slice if a length is not given', () => {
     const initialArray = [1,2,3,4,5,6,7,8,9];
     const result = randomSlice(initialArray);
     const start = initialArray.indexOf(result[0]);
--- libs/core/src/utils/random-slice.ts
+++ libs/core/src/utils/random-slice.ts
@@ -1,13 +1,18 @@
 /**
  * Returns a random slice of an array.
+ * An optional length may be specified.
  *
  * @param array
  */
-export const randomSlice = <T>(array: T[]): T[] => {
+export const randomSlice = <T>(array: T[], length?: number): T[] => {
   if(array.length === 0) {
     return [];
   };
-  const start = Math.floor(Math.random() * Math.floor(array.length - 1));
-  const end =  start + Math.floor(Math.random() * (array.length - 1 - start));
+  if(length > array.length) {
+    throw new Error('Requested length is longer than array length.');
+  }
+  const arrayLength = length || Math.floor(Math.random() * array.length);
+  const start = Math.floor(Math.random() * (array.length - arrayLength));
+  const end = start + arrayLength;
   return array.slice(start, end);
 };
