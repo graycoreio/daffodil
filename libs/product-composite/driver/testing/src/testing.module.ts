e292058e2c318af25bd68cd3c15e61cc5e4e7067
Peter Lauck <griest024@gmail.com>
2021-09-09 21:27:20 -0400

    feat(product-composite): add testing driver package

---
+++ libs/product-composite/driver/testing/src/testing.module.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import {
+  NgModule,
+  ModuleWithProviders,
+} from '@angular/core';
+
+import { DaffCompositeProductFactory } from '@daffodil/product-composite/testing';
+import { DAFF_PRODUCT_TYPE_FACTORIES } from '@daffodil/product/testing';
+
+/**
+ * Module for providing the related product fields to the product factory.
+ */
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+})
+export class DaffCompositeProductTestingDriverModule {
+  static forRoot(): ModuleWithProviders<DaffCompositeProductTestingDriverModule> {
+    return {
+      ngModule: DaffCompositeProductTestingDriverModule,
+      providers: [
+        {
+          provide: DAFF_PRODUCT_TYPE_FACTORIES,
+          useExisting: DaffCompositeProductFactory,
+          multi: true,
+        },
+      ],
+    };
+  }
+}
