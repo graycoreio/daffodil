a40d4a7f06b088926e3f9cc9fb4588a458e18f40
Peter Lauck <griest024@gmail.com>
2021-09-10 14:49:05 -0400

    docs

--- libs/product/testing/src/testing.module.ts
+++ libs/product/testing/src/testing.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { DaffDefaultProductFactory } from './factories/public_api';
 import { DAFF_PRODUCT_TYPE_FACTORIES } from './injection-tokens/public_api';
 
+/**
+ * Provides the default product factory as a product type factory.
+ */
 @NgModule({
   providers: [
     {
