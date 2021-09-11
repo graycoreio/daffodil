1690159f4396ceb2bd3b7cddc97a7f05e87788ef
Peter Lauck <griest024@gmail.com>
2021-09-11 16:37:08 -0400

    fix lint

--- libs/upsell-products/state/src/reducers/reducers-state.interface.ts
+++ libs/upsell-products/state/src/reducers/reducers-state.interface.ts
@@ -1,8 +1,8 @@
 import { DaffProduct } from '@daffodil/product';
 import { DaffProductStateRootSlice } from '@daffodil/product/state';
 
-import { DaffUpsellProductsReducerState } from './upsell-products/reducer-state.interface';
 import { DAFF_UPSELL_PRODUCTS_STORE_FEATURE_KEY } from './store-feature-key';
+import { DaffUpsellProductsReducerState } from './upsell-products/reducer-state.interface';
 
 /**
  * Interface for the redux store of the upsell product feature area.
