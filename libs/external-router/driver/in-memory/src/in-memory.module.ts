9ec846ca0579106572befd9570e67f53bf357fc0
griest024 <griest024@gmail.com>
2021-09-28 18:17:02 -0600

    feat(external-router): add in-memory driver (#1838)
    * feat(external-router): add in-memory driver
    
    * allow for config token

---
+++ libs/external-router/driver/in-memory/src/in-memory.module.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import {
+  NgModule,
+  ModuleWithProviders,
+  InjectionToken,
+  inject,
+} from '@angular/core';
+
+import { DaffExternalRouterDriver } from '@daffodil/external-router/driver';
+
+import {
+  DaffExternalRouterDriverInMemoryConfig,
+  DAFF_EXTERNAL_ROUTER_DRIVER_IN_MEMORY_CONFIG,
+} from './config';
+import { DaffExternalRouterInMemoryDriver } from './in-memory.service';
+
+/**
+ * The `DaffExternalRouterDriverInMemoryModule` is an importable NgModule that can
+ * be used to configure the {@link DaffExternalRouterInMemoryDriver}.
+ */
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class DaffExternalRouterDriverInMemoryModule {
+  static forRoot(
+    config: DaffExternalRouterDriverInMemoryConfig | InjectionToken<DaffExternalRouterDriverInMemoryConfig>,
+  ): ModuleWithProviders<DaffExternalRouterDriverInMemoryModule> {
+    return {
+      ngModule: DaffExternalRouterDriverInMemoryModule,
+      providers: [
+        {
+          provide: DaffExternalRouterDriver,
+          useExisting: DaffExternalRouterInMemoryDriver,
+        },
+        {
+          provide: DAFF_EXTERNAL_ROUTER_DRIVER_IN_MEMORY_CONFIG,
+          // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+          useFactory() {
+            return config instanceof InjectionToken ? inject(config) : config;
+          },
+        },
+      ],
+    };
+  }
+}
