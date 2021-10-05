6e6bcd43a214922de60bf1385440e7009cfc4c40
griest024 <griest024@gmail.com>
2021-10-05 06:41:12 -0600

    docs(core): clarify `DaffLocatable#url` format (#1849)

--- libs/core/src/locateable/locateable.interface.ts
+++ libs/core/src/locateable/locateable.interface.ts
@@ -4,6 +4,7 @@
 export interface DaffLocatable {
   /**
    * The URL describing the location of the entity.
+   * It should be relative to the domain root with a leading slash, i.e. `/path/to/entity`.
    */
   url: string;
 }
