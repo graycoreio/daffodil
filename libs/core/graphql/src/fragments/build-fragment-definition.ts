379ca1f246ea023296142a6ed9bf9475929f9b78
griest024 <griest024@gmail.com>
2021-10-14 02:18:11 -0600

    feat(core): only ensure uniqueness for GraphQL fragment names (#1868)
    * feat(core): add unique util
    
    * feat(core): only ensure uniqueness for fragment names
    
    * docs

--- libs/core/graphql/src/fragments/build-fragment-definition.ts
+++ libs/core/graphql/src/fragments/build-fragment-definition.ts
@@ -1,7 +1,5 @@
 import { DocumentNode } from 'graphql';
 
-import { daffFilterDuplicateFragments } from './filter-duplicate-fragments';
-
 /**
  * Builds a string of fragment definitions that can be interpolated into a GraphQL query.
  * Each definition is separated by a newline character: '\n'.
@@ -9,4 +7,4 @@ import { daffFilterDuplicateFragments } from './filter-duplicate-fragments';
  * @param documents A list of GraphQL documents that should only contain fragments.
  */
 export const daffBuildFragmentDefinition = (...documents: DocumentNode[]): string =>
-  daffFilterDuplicateFragments(...documents).reduce((acc, fragment) => acc.concat(`${fragment.loc.source.body}\n`), '');
+  documents.reduce((acc, fragment) => acc.concat(`${fragment.loc.source.body}\n`), '');
--- libs/core/graphql/src/fragments/filter-duplicate-fragments.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentNode } from 'graphql';
-
-/**
- * Filters a list of fragments, removing duplicates.
- */
-export const daffFilterDuplicateFragments = (...documents: DocumentNode[]): DocumentNode[] =>
-  // TODO(griest024): extract to core util
-  documents.filter((doc, index) => documents.slice(index + 1).indexOf(doc) === -1);
