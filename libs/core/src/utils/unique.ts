67ac3b568069f900c6b8e4e80f4cf3d8fb245c35
Peter Lauck <griest024@gmail.com>
2021-10-14 03:45:20 -0400

    docs

--- libs/core/src/utils/unique.ts
+++ libs/core/src/utils/unique.ts
@@ -1,2 +1,9 @@
+/**
+ * Filters an array of elements, removing duplicates.
+ * Does not guarantee order of filtered elements.
+ *
+ * An optional comparator function may be passed for arbitrary comparison behavior.
+ * The default is a strict equality comparison: `a === b`.
+ */
 export const unique = <T>(array: T[], comparator: (a: T, b: T) => boolean = (a, b) => a === b): T[] =>
   array.filter((a, index) => array.slice(index + 1).filter(b => comparator(a, b)).length === 0);
