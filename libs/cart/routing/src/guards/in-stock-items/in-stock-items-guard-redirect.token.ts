190818116bb930a8bbd1cd8038729ab322e43af2
Peter Lauck <griest024@gmail.com>
2022-01-21 13:22:45 -0500

    feat(cart): add `DaffCartInStockItemsGuard`

---
+++ libs/cart/routing/src/guards/in-stock-items/in-stock-items-guard-redirect.token.ts
@@ -0,0 +1,6 @@
+import { InjectionToken } from '@angular/core';
+
+/**
+ * The path to which the user should be redirected if the cart has no items when {@link DaffCartInStockItemsGuard} is invoked.
+ */
+export const DaffCartInStockItemsGuardRedirectUrl = new InjectionToken<string>('DaffCartInStockItemsGuardRedirectUrl');
