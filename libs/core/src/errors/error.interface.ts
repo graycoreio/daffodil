5c5efff6f851738b2731d5841b34c413cd8db48d
Peter Lauck <griest024@gmail.com>
2022-02-14 16:51:26 -0500

    feat(core): add `recoverable` field to `DaffError`

--- libs/core/src/errors/error.interface.ts
+++ libs/core/src/errors/error.interface.ts
@@ -2,6 +2,16 @@
  * An error object containing info about the exception that generated it.
  */
 export interface DaffError extends Error {
+  /**
+   * A string that uniquely identifies the error.
+   */
   code: string;
+  /**
+   * Contains additional relevant information to the cause and/or solution of the error.
+   */
   message: string;
+  /**
+   * Whether the error is non-critical and recoverable or catastrophic and unrecoverable.
+   */
+  recoverable: boolean;
 }
