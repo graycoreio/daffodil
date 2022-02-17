92ad274b533698e6fbf18794201bd7cb33a6bb9d
Peter Lauck <griest024@gmail.com>
2022-02-17 15:40:08 -0500

    c mage

--- libs/search-category/driver/magento/src/driver.module.ts
+++ libs/search-category/driver/magento/src/driver.module.ts
@@ -4,9 +4,11 @@ import {
   ModuleWithProviders,
 } from '@angular/core';
 
+import { DAFF_MAGENTO_CACHEABLE_OPERATIONS } from '@daffodil/driver/magento';
 import { DAFF_SEARCH_FEDERATED_DRIVERS } from '@daffodil/search/driver/federated';
 
 import { DaffSearchCategoryMagentoDriver } from './category-search.service';
+import { DAFF_MAGENTO_SEARCH_FOR_CATEGORIES_QUERY_NAME } from './queries/category-search';
 
 /**
  * A module that provides the {@link DaffSearchCategoryMagentoDriver} to the federated search driver.
@@ -25,6 +27,11 @@ export class DaffSearchCategoryMagentoDriverModule {
           provide: DAFF_SEARCH_FEDERATED_DRIVERS,
           multi: true,
           useExisting: DaffSearchCategoryMagentoDriver,
+        },
+        {
+          provide: DAFF_MAGENTO_CACHEABLE_OPERATIONS,
+          useValue: DAFF_MAGENTO_SEARCH_FOR_CATEGORIES_QUERY_NAME,
+          multi: true,
         },
       ],
     };
