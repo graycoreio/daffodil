2fc86a4b59824300751cdc567e177b0689730f5d
Peter Lauck <griest024@gmail.com>
2022-02-17 15:47:35 -0500

    p mage

--- libs/search-product/driver/magento/src/queries/product-search.ts
+++ libs/search-product/driver/magento/src/queries/product-search.ts
@@ -5,7 +5,7 @@ import { magentoProductSearchResultFragment } from './fragments/product-result';
 export const DAFF_MAGENTO_SEARCH_FOR_PRODUCTS_QUERY_NAME = 'MagentoSearchForProducts';
 
 export const productSearch = gql`
-  query {DAFF_MAGENTO_SEARCH_FOR_PRODUCTS_QUERY_NAME}($query: String!){
+  query ${DAFF_MAGENTO_SEARCH_FOR_PRODUCTS_QUERY_NAME}($query: String!){
     products(search: $query) {
       items {
         ...magentoProductSearchResult
