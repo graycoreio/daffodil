a2e6a258054bcd7763ef3fb4548981714538c045
Peter Lauck <griest024@gmail.com>
2022-02-17 15:40:10 -0500

    p mage

--- libs/search-product/driver/magento/src/driver.module.ts
+++ libs/search-product/driver/magento/src/driver.module.ts
@@ -4,9 +4,11 @@ import {
   ModuleWithProviders,
 } from '@angular/core';
 
+import { DAFF_MAGENTO_CACHEABLE_OPERATIONS } from '@daffodil/driver/magento';
 import { DAFF_SEARCH_FEDERATED_DRIVERS } from '@daffodil/search/driver/federated';
 
 import { DaffSearchProductMagentoDriver } from './product-search.service';
+import { DAFF_MAGENTO_SEARCH_FOR_PRODUCTS_QUERY_NAME } from './queries/product-search';
 
 /**
  * A module that provides the {@link DaffSearchProductMagentoDriver} to the federated search driver.
@@ -25,6 +27,11 @@ export class DaffSearchProductMagentoDriverModule {
           provide: DAFF_SEARCH_FEDERATED_DRIVERS,
           multi: true,
           useExisting: DaffSearchProductMagentoDriver,
+        },
+        {
+          provide: DAFF_MAGENTO_CACHEABLE_OPERATIONS,
+          useValue: DAFF_MAGENTO_SEARCH_FOR_PRODUCTS_QUERY_NAME,
+          multi: true,
         },
       ],
     };
--- libs/search-product/driver/magento/src/queries/product-search.ts
+++ libs/search-product/driver/magento/src/queries/product-search.ts
@@ -2,8 +2,10 @@ import { gql } from 'apollo-angular';
 
 import { magentoProductSearchResultFragment } from './fragments/product-result';
 
+export const DAFF_MAGENTO_SEARCH_FOR_PRODUCTS_QUERY_NAME = 'MagentoSearchForProducts';
+
 export const productSearch = gql`
-  query MagentoSearchForProducts($query: String!){
+  query {DAFF_MAGENTO_SEARCH_FOR_PRODUCTS_QUERY_NAME}($query: String!){
     products(search: $query) {
       items {
         ...magentoProductSearchResult
