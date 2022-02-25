95b2a951596e7efa315f4a2c885a9f5a370a209b
Damien Retzinger <damienwebdev@gmail.com>
2022-02-25 08:34:41 -0500

    fix(contact, newsletter): allow hubspot drivers to be used together
    Previously, if you tried to use the two Daff*HubspotDriverModules in the same injection heirarchy, whichever one was imported last would win. This would mean rather unexpected behaviors for users. This makes the two imports distinct.

--- libs/newsletter/driver/hubspot/src/newsletter.service.ts
+++ libs/newsletter/driver/hubspot/src/newsletter.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@angular/core';
+import {
+  Inject,
+  Injectable,
+} from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { DaffHubspotFormsService } from '@daffodil/driver/hubspot';
 import { DaffNewsletterUnion } from '@daffodil/newsletter';
 import { DaffNewsletterServiceInterface } from '@daffodil/newsletter/driver';
 
+import { DAFF_NEWSLETTER_HUBSPOT_FORMS_TOKEN } from './token/hubspot-forms.token';
+
 @Injectable()
 export class DaffNewsletterHubspotService implements DaffNewsletterServiceInterface<DaffNewsletterUnion, any> {
 
-  constructor(private hubspotService: DaffHubspotFormsService) {}
+  constructor(@Inject(DAFF_NEWSLETTER_HUBSPOT_FORMS_TOKEN) private hubspotService: DaffHubspotFormsService) {}
 
   send(payload: DaffNewsletterUnion): Observable<any> {
     return this.hubspotService.submit(payload);
