3034f20ba433ab5e30c3f800ff58a9280f0be253
Peter Lauck <griest024@gmail.com>
2022-03-01 12:59:02 -0500

    feat(authorizenet): add `DaffAuthorizeNeUnconfiguredError`

---
+++ libs/authorizenet/driver/src/errors/unconfigured.ts
@@ -0,0 +1,17 @@
+import {
+  DaffError,
+  DaffInheritableError,
+} from '@daffodil/core';
+
+import { DaffAuthorizeNetDriverErrorCodes } from './error-codes.enum';
+
+/**
+ * An error thrown when the driver config is missing required information for the driver to function.
+ */
+export class DaffAuthorizeNeUnconfiguredError extends DaffInheritableError implements DaffError {
+  public readonly code: string = DaffAuthorizeNetDriverErrorCodes.UNCONFIGURED;
+
+  constructor(public message: string) {
+    super(message);
+  }
+}
