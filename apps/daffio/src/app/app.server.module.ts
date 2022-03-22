c7c061bea6e4cae9efced327b2baed978d898bf9
griest024 <griest024@gmail.com>
2022-03-22 16:24:48 -0400

    fix(daffio): broken assets paths on server (#2028)

--- apps/daffio/src/app/app.server.module.ts
+++ apps/daffio/src/app/app.server.module.ts
@@ -3,6 +3,7 @@ import { ServerModule } from '@angular/platform-server';
 
 import { DaffioAppComponent } from './app.component';
 import { AppModule } from './app.module';
+import { DAFFIO_DOCS_PATH_TOKEN } from './docs/services/docs-path.token';
 
 @NgModule({
   imports: [
@@ -10,5 +11,11 @@ import { AppModule } from './app.module';
     ServerModule,
   ],
   bootstrap: [DaffioAppComponent],
+  providers: [
+    {
+      provide: DAFFIO_DOCS_PATH_TOKEN,
+      useValue: process.env.DAFFIO_DOCS_PATH || '',
+    },
+  ],
 })
 export class AppServerModule {}
