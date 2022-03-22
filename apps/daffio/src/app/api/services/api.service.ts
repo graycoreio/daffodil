3dc8b7a94f5151c1ee45da537b67464ce69b6722
Peter Lauck <griest024@gmail.com>
2022-03-22 10:40:04 -0400

    fix(daffio): broken assets paths on server

--- apps/daffio/src/app/api/services/api.service.ts
+++ apps/daffio/src/app/api/services/api.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {
+  Inject,
+  Injectable,
+} from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { DAFFIO_DOCS_PATH_TOKEN } from '../../docs/services/docs-path.token';
 import { DaffioApiReference } from '../models/api-reference';
 import { DaffioApiServiceInterface } from './api-service.interface';
-
-
-const DOCS_BASE_URL = '/assets/daffio/docs/';
 
 @Injectable({ providedIn: 'root' })
 export class DaffioApiService implements DaffioApiServiceInterface {
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(DAFFIO_DOCS_PATH_TOKEN) private docsPath: string,
+  ) {}
 
   list(): Observable<DaffioApiReference[]> {
-    return this.http.get<DaffioApiReference[]>(DOCS_BASE_URL + 'api/api-list.json');
+    return this.http.get<DaffioApiReference[]>(this.docsPath + 'api/api-list.json');
   }
 }
--- apps/daffio/src/app/docs/services/docs.service.ts
+++ apps/daffio/src/app/docs/services/docs.service.ts
@@ -1,24 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {
+  Inject,
+  Injectable,
+} from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { DaffioDoc } from '../models/doc';
 import { DaffioGuideList } from '../models/guide-list';
+import { DAFFIO_DOCS_PATH_TOKEN } from './docs-path.token';
 import { DaffioDocsServiceInterface } from './docs-service.interface';
-
-const DOCS_BASE_URL = '/assets/daffio/docs/';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DaffioDocsService<T extends DaffioDoc, V extends DaffioGuideList> implements DaffioDocsServiceInterface<DaffioDoc, DaffioGuideList> {
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(DAFFIO_DOCS_PATH_TOKEN) private docsPath: string,
+  ) {}
 
   get(path: string): Observable<T> {
-    return this.http.get<T>(DOCS_BASE_URL + path + '.json');
+    console.log(this.docsPath);
+
+    return this.http.get<T>(this.docsPath + path + '.json');
   }
   getGuideList(): Observable<DaffioGuideList> {
-    return this.http.get<V>(DOCS_BASE_URL + 'guides/guide-list.json');
+    console.log(this.docsPath);
+    return this.http.get<V>(this.docsPath + 'guides/guide-list.json');
   }
 }
