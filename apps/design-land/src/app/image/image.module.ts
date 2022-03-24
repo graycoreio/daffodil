2cb897dd83162fb34b64a68d32a6e2b4fba4a935
xelaint <xelaint@gmail.com>
2022-03-24 10:24:47 -0400

    fix linting

--- apps/design-land/src/app/image/image.module.ts
+++ apps/design-land/src/app/image/image.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
 import { DaffArticleModule } from '@daffodil/design';
+
 import { DesignLandExampleViewerModule } from '../core/code-preview/container/example-viewer.module';
 import { DesignLandImageRoutingModule } from './image-routing-module';
 import { DesignLandImageComponent } from './image.component';
@@ -13,9 +15,7 @@ import { DesignLandImageComponent } from './image.component';
     CommonModule,
     DesignLandExampleViewerModule,
     DesignLandImageRoutingModule,
-    DaffArticleModule
-  ]
+    DaffArticleModule,
+  ],
 })
-export class DesignLandImageModule {
-  
-}
+export class DesignLandImageModule { }
