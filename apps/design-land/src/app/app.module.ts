c51d9b4f7231c110c09c9e317c83268192cf7f7f
Elain T <xelaint@gmail.com>
2022-03-30 13:41:45 -0400

    feat(design-land): add theme switch functionality to design land (#2059)

--- apps/design-land/src/app/app.module.ts
+++ apps/design-land/src/app/app.module.ts
@@ -7,11 +7,12 @@ import {
   DaffSidebarModule,
   DaffLinkSetModule,
   DaffArticleModule,
+  DAFF_THEME_INITIALIZER,
 } from '@daffodil/design';
 
 import { DesignLandAppRoutingModule } from './app-routing.module';
 import { DesignLandAppComponent } from './app.component';
-
+import { DesignLandThemeSwitchModule } from './core/theme-switch/theme-switch.module';
 
 @NgModule({
   imports: [
@@ -22,12 +23,16 @@ import { DesignLandAppComponent } from './app.component';
     DaffSidebarModule,
     DaffLinkSetModule,
     DaffArticleModule,
+    DesignLandThemeSwitchModule,
   ],
   declarations: [
     DesignLandAppComponent,
   ],
   bootstrap: [
     DesignLandAppComponent,
+  ],
+  providers: [
+    DAFF_THEME_INITIALIZER,
   ],
 })
 export class AppModule { }
