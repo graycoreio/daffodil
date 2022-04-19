af8465ba5cbd7320635f05d7a732da16d33253bb
Elain T <xelaint@gmail.com>
2022-04-19 12:22:22 -0400

    feat(design-land): update code preview UI (#2072)

--- apps/design-land/src/app/core/code-preview/component/code-preview.component.ts
+++ apps/design-land/src/app/core/code-preview/component/code-preview.component.ts
@@ -5,12 +5,26 @@ import {
   Input,
   ChangeDetectionStrategy,
   OnChanges,
+  HostBinding,
+  Renderer2,
 } from '@angular/core';
+
+import { daffArticleEncapsulatedMixin } from '@daffodil/design';
 
 import {
   DesignLandCodeExample,
   DesignLandCodeExampleFile,
 } from '../model/code-example';
+
+/**
+ * An _elementRef and an instance of renderer2 are needed for the code preview mixins
+ */
+class CodePreviewBase {
+  constructor(public _elementRef: ElementRef, public _renderer: Renderer2) {}
+}
+
+const _codePreviewBase = daffArticleEncapsulatedMixin((CodePreviewBase));
+
 
 @Component({
   selector: 'design-land-code-preview',
@@ -18,7 +32,9 @@ import {
   styleUrls: ['./code-preview.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CodePreviewComponent implements OnChanges {
+export class CodePreviewComponent extends _codePreviewBase implements OnChanges {
+  @HostBinding('class.design-land-code-preview') class = true;
+
   /**
    * The ref for the slot of the custom element for the example
    */
@@ -34,13 +50,17 @@ export class CodePreviewComponent implements OnChanges {
    */
   @Input() example: DesignLandCodeExample;
 
-  selectedFile: DesignLandCodeExampleFile;
+  file: DesignLandCodeExampleFile;
 
   ngOnChanges() {
     if(this.example){
       this.content.nativeElement.innerHtml = '';
       this.content.nativeElement.appendChild(document.createElement(this.example.element));
-      this.selectedFile = this.example.files[0];
+      this.file = this.example.files[0];
     }
+  }
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    super(elementRef, renderer);
   }
 }
--- apps/design-land/src/app/core/code-preview/component/code-preview.module.ts
+++ apps/design-land/src/app/core/code-preview/component/code-preview.module.ts
@@ -1,17 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { DaffNavbarModule } from '@daffodil/design';
-
 import { CodePreviewComponent } from './code-preview.component';
 
-
-
 @NgModule({
-  declarations: [CodePreviewComponent],
+  declarations: [
+    CodePreviewComponent,
+  ],
   imports: [
     CommonModule,
-    DaffNavbarModule,
   ],
   exports: [
     CodePreviewComponent,
