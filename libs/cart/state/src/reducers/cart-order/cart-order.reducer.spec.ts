a586a7f6d97f04468eca4ee2c6e46ba5d7f21366
Peter Lauck <griest024@gmail.com>
2022-04-22 13:49:19 -0400

    feat(cart): reset order errors when cart is successfully mutated

--- libs/cart/state/src/reducers/cart-order/cart-order.reducer.spec.ts
+++ libs/cart/state/src/reducers/cart-order/cart-order.reducer.spec.ts
@@ -4,6 +4,32 @@ import {
   DaffCartPlaceOrderSuccess,
   DaffCartPlaceOrderFailure,
   daffCartOrderInitialState as initialState,
+  DaffCartActionTypes,
+  DaffCartClearSuccess,
+  DaffCartCreateSuccess,
+  DaffAddToCartSuccess,
+  DaffCartBillingAddressActionTypes,
+  DaffCartBillingAddressUpdateSuccess,
+  DaffCartShippingAddressUpdateSuccess,
+  DaffCartShippingAddressActionTypes,
+  DaffCartAddressActionTypes,
+  DaffCartAddressUpdateSuccess,
+  DaffCartCouponApplySuccess,
+  DaffCartCouponActionTypes,
+  DaffCartCouponRemoveSuccess,
+  DaffCartCouponRemoveAllSuccess,
+  DaffCartItemUpdateSuccess,
+  DaffCartItemActionTypes,
+  DaffCartItemAddSuccess,
+  DaffCartItemDeleteSuccess,
+  DaffCartItemDeleteOutOfStockSuccess,
+  DaffCartPaymentRemoveSuccess,
+  DaffCartPaymentActionTypes,
+  DaffCartPaymentUpdateSuccess,
+  DaffCartPaymentUpdateWithBillingSuccess,
+  DaffCartShippingInformationUpdateSuccess,
+  DaffCartShippingInformationActionTypes,
+  DaffCartShippingInformationDeleteSuccess,
 } from '@daffodil/cart/state';
 import {
   DaffState,
@@ -94,6 +120,402 @@ describe('Cart | Reducer | CartOrder', () => {
 
     it('should add an error to state', () => {
       expect(result.errors.length).toEqual(2);
+    });
+  });
+
+  describe('when CartClearSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartClearSuccess>{
+        type: DaffCartActionTypes.CartClearSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartCreateSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartCreateSuccess>{
+        type: DaffCartActionTypes.CartCreateSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when AddToCartSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffAddToCartSuccess>{
+        type: DaffCartActionTypes.AddToCartSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartBillingAddressUpdateSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartBillingAddressUpdateSuccess>{
+        type: DaffCartBillingAddressActionTypes.CartBillingAddressUpdateSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartShippingAddressUpdateSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartShippingAddressUpdateSuccess>{
+        type: DaffCartShippingAddressActionTypes.CartShippingAddressUpdateSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartAddressUpdateSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartAddressUpdateSuccess>{
+        type: DaffCartAddressActionTypes.CartAddressUpdateSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartCouponApplySuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartCouponApplySuccess>{
+        type: DaffCartCouponActionTypes.CartCouponApplySuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartCouponRemoveSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartCouponRemoveSuccess>{
+        type: DaffCartCouponActionTypes.CartCouponRemoveSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartCouponRemoveAllSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartCouponRemoveAllSuccess>{
+        type: DaffCartCouponActionTypes.CartCouponRemoveAllSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartItemUpdateSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartItemUpdateSuccess>{
+        type: DaffCartItemActionTypes.CartItemUpdateSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartItemAddSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartItemAddSuccess>{
+        type: DaffCartItemActionTypes.CartItemAddSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartItemDeleteSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartItemDeleteSuccess>{
+        type: DaffCartItemActionTypes.CartItemDeleteSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartItemDeleteOutOfStockSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartItemDeleteOutOfStockSuccess>{
+        type: DaffCartItemActionTypes.CartItemDeleteOutOfStockSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartPaymentRemoveSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartPaymentRemoveSuccess>{
+        type: DaffCartPaymentActionTypes.CartPaymentRemoveSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartPaymentUpdateSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartPaymentUpdateSuccess>{
+        type: DaffCartPaymentActionTypes.CartPaymentUpdateSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartPaymentUpdateWithBillingSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartPaymentUpdateWithBillingSuccess>{
+        type: DaffCartPaymentActionTypes.CartPaymentUpdateWithBillingSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartShippingInformationUpdateSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartShippingInformationUpdateSuccess>{
+        type: DaffCartShippingInformationActionTypes.CartShippingInformationUpdateSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
+    });
+  });
+
+  describe('when CartShippingInformationDeleteSuccessAction is triggered', () => {
+    let result;
+    let state: DaffCartOrderReducerState;
+
+    beforeEach(() => {
+      state = {
+        ...initialState,
+        loading: DaffState.Resolving,
+      };
+
+      const action = <DaffCartShippingInformationDeleteSuccess>{
+        type: DaffCartShippingInformationActionTypes.CartShippingInformationDeleteSuccessAction,
+      };
+
+      result = reducer(state, action);
+    });
+
+    it('should reset the errors in state', () => {
+      expect(result.errors).toEqual([]);
     });
   });
 });
