9bb49238245f339d1542795b4892557a2a2d192d
Peter Lauck <griest024@gmail.com>
2022-04-25 14:10:02 -0400

    feat(driver): export error codes

--- libs/driver/src/errors/bad-input.ts
+++ libs/driver/src/errors/bad-input.ts
@@ -3,8 +3,10 @@ import {
   DaffInheritableError,
 } from '@daffodil/core';
 
+import { DaffDriverErrorCodes } from './codes';
+
 export class DaffBadInputError extends DaffInheritableError implements DaffError {
-  public readonly code: string = 'DAFF_BAD_INPUT';
+  public readonly code: string = DaffDriverErrorCodes.BAD_INPUT;
 
   constructor(public message: string) {
     super(message);
