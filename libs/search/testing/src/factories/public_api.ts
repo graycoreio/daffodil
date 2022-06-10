64837fffcca7bba9484b04ddc590e9820290f569
Peter Lauck <griest024@gmail.com>
2022-06-10 15:03:33 -0400

    feat(search): add `@daffodil/search/testing`

---
+++ libs/search/testing/src/factories/public_api.ts
@@ -0,0 +1,2 @@
+export * from './search-result.factory';
+export * from './kind.factory';
---
+++ libs/search/testing/src/factories/search-result.factory.spec.ts
@@ -0,0 +1,41 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DaffSearchResult } from '@daffodil/search';
+
+import { DaffSearchResultFactory } from './search-result.factory';
+
+describe('@daffodil/search/testing | DaffSearchResultFactory', () => {
+  let searchResultFactory: DaffSearchResultFactory;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DaffSearchResultFactory],
+    });
+
+    searchResultFactory = TestBed.inject(DaffSearchResultFactory);
+  });
+
+  it('should be created', () => {
+    expect(searchResultFactory).toBeTruthy();
+  });
+
+  describe('create', () => {
+    let result: DaffSearchResult;
+
+    beforeEach(() => {
+      result = searchResultFactory.create();
+    });
+
+    it('should return a Search', () => {
+      expect(result).toBeDefined();
+    });
+
+    it('should define all the required fields', () => {
+      expect(result.id).toBeDefined();
+      expect(result.url).toBeDefined();
+      expect(result.name).toBeDefined();
+      expect(result.kind).toBeDefined();
+      expect(result.description).toBeDefined();
+    });
+  });
+});
