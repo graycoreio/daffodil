d9956777ffb36cbe6d193942f310eb26dd26f0ac
Peter Lauck <griest024@gmail.com>
2022-06-10 15:03:33 -0400

    feat(search-product): add `@daffodil/search-product/driver` (#2149)

---
+++ libs/search-product/driver/src/interfaces/search-product-service.interface.ts
@@ -0,0 +1,23 @@
+import { Observable } from 'rxjs';
+
+import {
+  DaffSearchProductResult,
+  DAFF_SEARCH_PRODUCT_RESULT_KIND,
+} from '@daffodil/search-product';
+import { DaffSearchDriverKindedInterface } from '@daffodil/search/driver';
+
+import { DaffSearchProductDriverOptions } from './options.interface';
+import { DaffSearchProductDriverResponse } from './response.interface';
+
+/**
+ * A driver interface for searching products.
+ *
+ * @inheritdoc
+ */
+export interface DaffSearchProductDriverInterface<
+  T extends DaffSearchProductResult = DaffSearchProductResult,
+> extends DaffSearchDriverKindedInterface<T> {
+  readonly kind: typeof DAFF_SEARCH_PRODUCT_RESULT_KIND;
+
+  search(query: string, options?: DaffSearchProductDriverOptions): Observable<DaffSearchProductDriverResponse<T>>;
+}
