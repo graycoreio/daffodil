7b00ccc33485be776723bba83fff170e623c72c5
Peter Lauck <griest024@gmail.com>
2022-06-10 15:03:33 -0400

    feat(search): add `@daffodil/search/driver` (#2149)

---
+++ libs/search/driver/src/errors/public_api.ts
@@ -0,0 +1,9 @@
+export {
+  DaffSearchInvalidAPIResponseError,
+  DAFF_SEARCH_INVALID_API_RESPONSE_ERROR_CODE,
+} from './invalid-api-response';
+export { DaffSearchDriverErrorMap } from './map';
+export {
+  DaffSearchQueryTooShortError,
+  DAFF_SEARCH_QUERY_TOO_SHORT_ERROR_CODE,
+} from './query-too-short';
