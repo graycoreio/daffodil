96cd4922c177e550efc21184ea833729c9dda01a
Peter Lauck <griest024@gmail.com>
2022-06-16 14:29:24 -0400

    feat(core): add `DaffCollectionPageOutOfBoundsError`

---
+++ libs/core/src/collection/page-out-of-bounds.error.ts
@@ -0,0 +1,17 @@
+import {
+  DaffError,
+  DaffInheritableError,
+} from '../errors/public_api';
+
+export const DAFF_COLLECTION_PAGE_OUT_OF_BOUNDS_ERROR_CODE = 'DAFF_COLLECTION_PAGE_OUT_OF_BOUNDS';
+
+/**
+ * An error thrown when the requested page is greater than the number of available pages.
+ */
+export class DaffCollectionPageOutOfBoundsError extends DaffInheritableError implements DaffError {
+  public readonly code: string = DAFF_COLLECTION_PAGE_OUT_OF_BOUNDS_ERROR_CODE;
+
+  constructor(message?: string) {
+	  super(message);
+  }
+}
