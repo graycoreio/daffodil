a6cba18a96f129a731b07e8b4b97f7b8f2e30651
griest024 <griest024@gmail.com>
2022-06-21 13:06:39 -0400

    feat(core): add `DaffCollectionPageOutOfBoundsError` (#2118)

---
+++ libs/core/src/collection/error-map.ts
@@ -0,0 +1,8 @@
+import {
+  DaffCollectionPageOutOfBoundsError,
+  DAFF_COLLECTION_PAGE_OUT_OF_BOUNDS_ERROR_CODE,
+} from './page-out-of-bounds.error';
+
+export const daffCollectionErrorMap = {
+  [DAFF_COLLECTION_PAGE_OUT_OF_BOUNDS_ERROR_CODE]: DaffCollectionPageOutOfBoundsError,
+};
--- libs/core/src/collection/public_api.ts
+++ libs/core/src/collection/public_api.ts
@@ -1,2 +1,4 @@
 export * from './paginable';
 export * from './sortable';
+export * from './page-out-of-bounds.error';
+export * from './error-map';
