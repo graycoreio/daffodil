def0fc606eaa2f2b6ce1bb261d33b4bdb8e5d07a
Peter Lauck <griest024@gmail.com>
2022-07-07 11:38:11 -0400

    feat(product): add `DaffProductCollectionRequestFactory`

--- libs/product/testing/src/factories/public_api.ts
+++ libs/product/testing/src/factories/public_api.ts
@@ -3,6 +3,7 @@ export * from './extension.factory';
 export * from './kind.factory';
 export * from './default-product.factory';
 export * from './collection/metadata.factory';
+export * from './collection/request.factory';
 export * from './filters/public_api';
 
 export { DaffDefaultProductFactory as DaffProductFactory } from './default-product.factory';
