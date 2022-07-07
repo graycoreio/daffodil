815bdc806872f423e91af3b288172e94fc9b6d49
Peter Lauck <griest024@gmail.com>
2022-07-07 12:18:36 -0400

    feat(category): use parent factories

--- libs/category/testing/src/factories/category-page-metadata.factory.spec.ts
+++ libs/category/testing/src/factories/category-page-metadata.factory.spec.ts
@@ -30,13 +30,6 @@ describe('Category | Testing | Factories | DaffCategoryPageMetadataFactory', () 
 
     it('should return a DaffCategoryPageMetadata with all required fields defined', () => {
       expect(result.id).toBeDefined();
-      expect(result.sortOptions.default).toBeDefined();
-      expect(result.sortOptions.options[0].label).toBeDefined();
-      expect(result.sortOptions.options[0].value).toBeDefined();
-      expect(result.totalPages).toBeDefined();
-      expect(result.filters).toBeDefined();
-      expect(result.ids).toBeDefined();
-      expect(result.count).toBeDefined();
     });
   });
 });
--- libs/category/testing/src/factories/category-page-metadata.factory.ts
+++ libs/category/testing/src/factories/category-page-metadata.factory.ts
@@ -2,38 +2,18 @@ import { Injectable } from '@angular/core';
 import * as faker from '@faker-js/faker/locale/en_US';
 
 import { DaffCategoryPageMetadata } from '@daffodil/category';
-import { DaffModelFactory } from '@daffodil/core/testing';
+import {
+  DaffModelFactory,
+  DaffNumericallyPaginableFactory,
+  DaffSortableFactory,
+} from '@daffodil/core/testing';
+import {
+  DaffProductFilterFactory,
+  MockProductCollectionMetadata,
+} from '@daffodil/product/testing';
 
-export class MockCategoryPageMetadata implements DaffCategoryPageMetadata {
+export class MockCategoryPageMetadata extends MockProductCollectionMetadata implements DaffCategoryPageMetadata {
   id = faker.datatype.uuid();
-  count = faker.datatype.number({ min: 1, max: 3 });
-  ids = [faker.datatype.number({ min: 1, max: 100 }).toString()];
-
-  sortOptions = {
-    default: 'position',
-    options: [
-      {
-        label: 'Position',
-        value: 'position',
-      },
-      {
-        label: 'Name',
-        value: 'name',
-      },
-      {
-        label: 'Price',
-        value: 'price',
-      },
-  	],
-  };
-  appliedSortOption = 'position';
-  appliedSortDirection = null;
-
-  currentPage = 1;
-  totalPages = faker.datatype.number({ min: 1, max: 4 });
-  pageSize = 20;
-
-  filters = {};
 }
 
 /**
@@ -43,7 +23,11 @@ export class MockCategoryPageMetadata implements DaffCategoryPageMetadata {
   providedIn: 'root',
 })
 export class DaffCategoryPageMetadataFactory extends DaffModelFactory<DaffCategoryPageMetadata>{
-  constructor(){
-    super(MockCategoryPageMetadata);
+  constructor(
+    pageInfoFactory: DaffNumericallyPaginableFactory,
+    sortFieldsFactory: DaffSortableFactory,
+    filterFactory: DaffProductFilterFactory,
+  ) {
+    super(MockCategoryPageMetadata, pageInfoFactory, sortFieldsFactory, filterFactory);
   }
 }
