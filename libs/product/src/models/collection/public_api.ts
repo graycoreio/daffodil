7933a4534e7a648b49698e5e574fcb396efa49cc
Peter Lauck <griest024@gmail.com>
2022-07-25 14:25:32 -0400

    feat(product)!: replace `DaffProductCollectionResponse` with `DaffProductCollection`
    BREAKING CHANGE: removes `DaffProductCollectionResponse`

--- libs/product/src/models/collection/public_api.ts
+++ libs/product/src/models/collection/public_api.ts
@@ -1,2 +1,3 @@
 export { DaffProductCollectionMetadata } from './metadata.interface';
+export { DaffProductCollection } from './collection.interface';
 export { DaffProductCollectionRequest } from './request.interface';
--- libs/product/testing/src/factories/public_api.ts
+++ libs/product/testing/src/factories/public_api.ts
@@ -4,6 +4,7 @@ export * from './kind.factory';
 export * from './default-product.factory';
 export * from './collection/metadata.factory';
 export * from './collection/request.factory';
+export * from './collection/collection.factory';
 export * from './filters/public_api';
 
 export { DaffDefaultProductFactory as DaffProductFactory } from './default-product.factory';
