a6d1eb280ba089cd93bb820623526a2f5768bb7d
griest024 <griest024@gmail.com>
2022-07-26 13:40:13 -0400

    feat(core): add collection action interfaces (#2133)

--- libs/core/state/src/public_api.ts
+++ libs/core/state/src/public_api.ts
@@ -2,5 +2,6 @@ export * from './operators/public_api';
 export * from './states/public_api';
 export * from './errors/public_api';
 export * from './reducers/public_api';
+export * from './collection/public_api';
 
 export { DaffStoreFacade } from './store/facade';
