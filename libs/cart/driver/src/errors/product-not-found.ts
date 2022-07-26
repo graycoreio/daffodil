f61968b25054d568d9ca5ffd904098cc50f63b86
Peter Lauck <griest024@gmail.com>
2022-07-26 14:08:09 -0400

    chore(cart): deprecate `DaffProductNotFoundError`

--- libs/cart/driver/src/errors/product-not-found.ts
+++ libs/cart/driver/src/errors/product-not-found.ts
@@ -8,6 +8,8 @@ import { DaffCartDriverErrorCodes } from './codes.enum';
 /**
  * An error thrown when an add to cart request is sent
  * for a product that cannot be found.
+ *
+ * @deprecated Use the `DaffProductNotFoundError` from `@daffodil/product/driver` instead.
  */
 export class DaffProductNotFoundError extends DaffInheritableError implements DaffError {
   public readonly code: string = DaffCartDriverErrorCodes.PRODUCT_NOT_FOUND;
