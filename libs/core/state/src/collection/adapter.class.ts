94fb8341d7c7069f106339ce8b2447586cfd9a38
Peter Lauck <griest024@gmail.com>
2022-07-27 10:23:35 -0400

    feat(core): add collection initial state and adapter

---
+++ libs/core/state/src/collection/adapter.class.ts
@@ -0,0 +1,78 @@
+import { defaultMemoize } from '@ngrx/store';
+
+import {
+  daffCollectionBuildMetadataFromRequest,
+  DaffCollectionMetadata,
+  DaffCollectionRequest,
+} from '@daffodil/core';
+
+/**
+ * Provides an abstracted way to manage a collection state.
+ */
+export class DaffCollectionStateAdapter<T extends DaffCollectionMetadata = DaffCollectionMetadata> {
+  /**
+   * Stores a collection request in state.
+   * This is useful when you want to preemptively reduce a request before waiting for a response.
+   */
+  storeRequest(request: DaffCollectionRequest, state: T): T {
+    return {
+      ...state,
+      ...daffCollectionBuildMetadataFromRequest(request),
+    };
+  }
+
+  /**
+   * Sets the page size.
+   */
+  setPageSize(size: number, state: T): T {
+    return {
+      ...state,
+      pageSize: size,
+      currentPage: 1,
+    };
+  }
+
+  /**
+   * Sets the current page.
+   */
+  setCurrentPage(page: number, state: T): T {
+    return {
+      ...state,
+      currentPage: page,
+    };
+  }
+
+  /**
+   * Sets the sorting option and direction.
+   */
+  setSort(option: string, direction: string, state: T): T {
+    return {
+      ...state,
+      appliedSortOption: option,
+      appliedSortDirection: direction,
+    };
+  }
+
+  /**
+   * Set the entire collection metadata at once.
+   */
+  setMetadata(metadata: DaffCollectionMetadata, state: T): T {
+    return {
+      ...state,
+      ids: metadata.ids,
+      count: metadata.count,
+      currentPage: metadata.currentPage,
+      pageSize: metadata.pageSize,
+      sortOptions: metadata.sortOptions,
+      totalPages: metadata.totalPages,
+      appliedSortOption: metadata.appliedSortOption || state.appliedSortOption,
+      appliedSortDirection: metadata.appliedSortDirection || state.appliedSortDirection,
+    };
+  }
+}
+
+/**
+ * Create the adapter for the collection state.
+ */
+export const getCollectionStateAdapter: <T extends DaffCollectionMetadata = DaffCollectionMetadata>() => DaffCollectionStateAdapter<T>
+  = defaultMemoize(() => new DaffCollectionStateAdapter()).memoized;
---
+++ libs/core/state/src/collection/initial-state.ts
@@ -0,0 +1,15 @@
+import { DaffCollectionMetadata } from '@daffodil/core';
+
+export const daffCollectionReducerInitialState: DaffCollectionMetadata = {
+  count: 0,
+  appliedSortOption: null,
+  appliedSortDirection: null,
+  currentPage: null,
+  pageSize: null,
+  totalPages: null,
+  sortOptions: {
+    default: null,
+    options: [],
+  },
+  ids: [],
+};
