23aa4ac26666e190fd520c9414286400236cd895
griest024 <griest024@gmail.com>
2022-08-02 11:01:00 -0500

    feat(core): add collection selector factory (#2140)

--- libs/core/state/src/collection/public_api.ts
+++ libs/core/state/src/collection/public_api.ts
@@ -1,3 +1,4 @@
 export * from './actions';
 export * from './adapter.class';
 export * from './initial-state';
+export * from './selector-factory';
---
+++ libs/core/state/src/collection/selector-factory.ts
@@ -0,0 +1,139 @@
+import {
+  createSelector,
+  MemoizedSelector,
+} from '@ngrx/store';
+
+import {
+  DaffCollectionMetadata,
+  DaffCollectionRequest,
+} from '@daffodil/core';
+
+/**
+ * An interface to describe all selectors related to the collection metadata.
+ */
+export interface DaffCollectionMemoizedSelectors<
+  TState,
+  TMetadata extends DaffCollectionMetadata = DaffCollectionMetadata
+> {
+  /**
+   * Selects the metadata for the collection.
+   */
+  selectCollectionMetadata: MemoizedSelector<TState, TMetadata>;
+  /**
+   * Builds a request that matches the current collection.
+   */
+  selectCollectionRequest: MemoizedSelector<TState, DaffCollectionRequest>;
+  /**
+   * Selects the total number of items of the collection.
+   */
+  selectCollectionCount: MemoizedSelector<TState, TMetadata['count']>;
+  /**
+   * Selects the current page of items of the collection.
+   */
+  selectCollectionCurrentPage: MemoizedSelector<TState, TMetadata['currentPage']>;
+  /**
+   * Selects the total number of pages of items that exist in the collection.
+   */
+  selectCollectionTotalPages: MemoizedSelector<TState, TMetadata['totalPages']>;
+  /**
+   * Selects the number of items on each collection.
+   */
+  selectCollectionPageSize: MemoizedSelector<TState, TMetadata['pageSize']>;
+  /**
+   * Selects the sort options that may be applied to the collection.
+   */
+  selectCollectionSortOptions: MemoizedSelector<TState, TMetadata['sortOptions']['options']>;
+  /**
+   * Selects the applied sorting option if one is applied.
+   */
+  selectCollectionAppliedSortOption: MemoizedSelector<TState, TMetadata['appliedSortOption']>;
+  /**
+   * Selects the applied sorting direction if a sorting option is applied.
+   */
+  selectCollectionAppliedSortDirection: MemoizedSelector<TState, TMetadata['appliedSortDirection']>;
+  /**
+   * Selects the applied sorting direction if a sorting option is applied.
+   */
+  selectCollectionIds: MemoizedSelector<TState, TMetadata['ids']>;
+}
+
+/**
+ * Creates collection selectors.
+ *
+ * @param selectCollectionState A selector for the particular collection state upon which the returned selectors should operate.
+ */
+export const daffCollectionSelectorFactory = <
+  TState,
+  TMetadata extends DaffCollectionMetadata = DaffCollectionMetadata
+>(
+  selectCollectionState: MemoizedSelector<TState, TMetadata>,
+): DaffCollectionMemoizedSelectors<TState, TMetadata> => {
+
+  const selectCollectionMetadata = createSelector(
+    selectCollectionState,
+    state => state,
+  );
+
+  const selectCollectionRequest = createSelector(
+    selectCollectionMetadata,
+    metadata => ({
+      appliedSortOption: metadata.appliedSortOption,
+      appliedSortDirection: metadata.appliedSortDirection,
+      currentPage: metadata.currentPage,
+      pageSize: metadata.pageSize,
+    }),
+  );
+
+  const selectCollectionCount = createSelector(
+    selectCollectionMetadata,
+    state => state.count,
+  );
+
+  const selectCollectionCurrentPage = createSelector(
+    selectCollectionMetadata,
+    state => state.currentPage,
+  );
+
+  const selectCollectionTotalPages = createSelector(
+    selectCollectionMetadata,
+    state => state.totalPages,
+  );
+
+  const selectCollectionPageSize = createSelector(
+    selectCollectionMetadata,
+    state => state.pageSize,
+  );
+
+  const selectCollectionSortOptions = createSelector(
+    selectCollectionMetadata,
+    state => state.sortOptions.options,
+  );
+
+  const selectCollectionAppliedSortOption = createSelector(
+    selectCollectionMetadata,
+    state => state.appliedSortOption,
+  );
+
+  const selectCollectionAppliedSortDirection = createSelector(
+    selectCollectionMetadata,
+    state => state.appliedSortDirection,
+  );
+
+  const selectCollectionIds = createSelector(
+    selectCollectionMetadata,
+    state => state.ids,
+  );
+
+  return {
+    selectCollectionMetadata,
+    selectCollectionRequest,
+    selectCollectionCount,
+    selectCollectionCurrentPage,
+    selectCollectionTotalPages,
+    selectCollectionPageSize,
+    selectCollectionSortOptions,
+    selectCollectionAppliedSortOption,
+    selectCollectionAppliedSortDirection,
+    selectCollectionIds,
+  };
+};
