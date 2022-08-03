d0e0155bb49dbc546c805e8ddc98d4fe4465f494
Peter Lauck <griest024@gmail.com>
2022-08-03 11:38:07 -0400

    (product): add `DaffProductGetQueryParamsFromRequest`

---
+++ libs/product/routing/src/services/get-query-params-from-request.service.spec.ts
@@ -0,0 +1,130 @@
+import { Component } from '@angular/core';
+import {
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { Params } from '@angular/router';
+
+import {
+  DaffProductCollectionRequest,
+  daffProductFiltersToRequests,
+} from '@daffodil/product';
+import { DaffProductCollectionRequestQueryParamTransform } from '@daffodil/product/routing';
+import { DaffProductCollectionMetadataFactory } from '@daffodil/product/testing';
+
+import { DAFF_PRODUCT_ROUTING_CONFIG } from '../config/token';
+import { DaffProductGetQueryParamsFromRequest } from './get-query-params-from-request.service';
+
+describe('@daffodil/product/routing | DaffProductGetQueryParamsFromRequest', () => {
+  let customPageSizeQp: string;
+  let customCurrentPageTransform: DaffProductCollectionRequestQueryParamTransform<number>;
+
+  let service: DaffProductGetQueryParamsFromRequest;
+  let productCollectionMetadataFactory: DaffProductCollectionMetadataFactory;
+
+  let mockRequest: DaffProductCollectionRequest;
+  let result: Params;
+
+  beforeEach(() => {
+    customPageSizeQp = 'custom_pageSize';
+    customCurrentPageTransform = {
+      request: val => Number(val),
+      queryParam: val => String(val),
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        DaffProductGetQueryParamsFromRequest,
+        {
+          provide: DAFF_PRODUCT_ROUTING_CONFIG,
+          useValue: {
+            params: {
+              pageSize: customPageSizeQp,
+            },
+            transforms:  {
+              currentPage: customCurrentPageTransform,
+            },
+          },
+        },
+      ],
+    });
+
+    productCollectionMetadataFactory = TestBed.inject(DaffProductCollectionMetadataFactory);
+    service = TestBed.inject(DaffProductGetQueryParamsFromRequest);
+
+    const metadata = productCollectionMetadataFactory.create();
+    mockRequest = {
+      appliedSortOption: metadata.appliedSortOption,
+      appliedSortDirection: metadata.appliedSortDirection,
+      currentPage: metadata.currentPage,
+      pageSize: metadata.pageSize,
+      filterRequests: daffProductFiltersToRequests(metadata.filters),
+    };
+  });
+
+  describe('when the value should be set to a custom query param', () => {
+    let pageSizeValue: number;
+
+    beforeEach(() => {
+      pageSizeValue = 5;
+      result = service.getQueryParams({
+        ...mockRequest,
+        pageSize: pageSizeValue,
+      });
+    });
+
+    it('should set the query param the corresponding request fields', () => {
+      expect(Number(result[customPageSizeQp])).toEqual(pageSizeValue);
+    });
+  });
+
+  describe('when there is not custom query param', () => {
+    let sortOptionValue: string;
+
+    beforeEach(() => {
+      sortOptionValue = 'name';
+      result = service.getQueryParams({
+        ...mockRequest,
+        appliedSortOption: sortOptionValue,
+      });
+    });
+
+    it('should set the query param field from the request without performing a transform', () => {
+      expect(result.appliedSortOption).toEqual(sortOptionValue);
+    });
+  });
+
+  describe('when the non request fields are set', () => {
+    let value: string;
+
+    beforeEach(() => {
+      value = 'anything';
+      result = service.getQueryParams(<any>{
+        ...mockRequest,
+        ignore_me: value,
+      });
+    });
+
+    it('should not set a query param from that non request field', () => {
+      expect((<any>result).ignore_me).toBeUndefined();
+      expect(Object.values(result)).not.toContain(value);
+    });
+  });
+
+  describe('when the value is passed through a default query param with a custom transform', () => {
+    let currentPageValue: number;
+
+    beforeEach(() => {
+      currentPageValue = 2;
+      result = service.getQueryParams({
+        ...mockRequest,
+        currentPage: currentPageValue,
+      });
+    });
+
+    it('should set the request field to the value returned by the custom transform', () => {
+      expect(result.currentPage).toEqual(customCurrentPageTransform.queryParam(currentPageValue));
+    });
+  });
+});
---
+++ libs/product/routing/src/services/get-query-params-from-request.service.ts
@@ -0,0 +1,34 @@
+import {
+  Inject,
+  Injectable,
+} from '@angular/core';
+import { Params } from '@angular/router';
+
+import { DaffProductCollectionRequest } from '@daffodil/product';
+
+import {
+  DaffProductRoutingConfig,
+  DAFF_PRODUCT_ROUTING_CONFIG,
+} from '../config/public_api';
+import { DAFF_PRODUCT_COLLECTION_REQUEST_FIELDS } from '../constants/public_api';
+
+/**
+ * Builds a query param map from a {@link DaffProductCollectionRequest}.
+ * Its behavior can be configured via the {@link DAFF_PRODUCT_ROUTING_CONFIG} injection token.
+ */
+@Injectable()
+export class DaffProductGetQueryParamsFromRequest {
+  constructor(
+    @Inject(DAFF_PRODUCT_ROUTING_CONFIG) private config: DaffProductRoutingConfig,
+  ) {}
+
+  getQueryParams(request: DaffProductCollectionRequest): Params {
+    return DAFF_PRODUCT_COLLECTION_REQUEST_FIELDS.reduce<Params>((acc, field) => {
+      const val = request[field];
+      if (val) {
+        acc[this.config.params[field] || field] = this.config.transforms?.[field]?.queryParam?.(val) || String(val);
+      }
+      return acc;
+    }, {});
+  }
+}
