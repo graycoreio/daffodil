0d5d06bdd0d1d6e7628589b60160101801eae897
griest024 <griest024@gmail.com>
2022-08-03 12:34:54 -0500

    feat(product): add `DaffProductRoutingModule` (#2148)

---
+++ libs/product/routing/src/module.ts
@@ -0,0 +1,37 @@
+import {
+  inject,
+  ModuleWithProviders,
+  NgModule,
+} from '@angular/core';
+
+import { DaffBase64ServiceToken } from '@daffodil/core';
+
+import {
+  DaffProductRoutingConfig,
+  daffProductRoutingConfigDefaultFactory,
+  DAFF_PRODUCT_ROUTING_CONFIG,
+} from './config/public_api';
+import {
+  DaffProductGetCollectionRequestFromRoute,
+  DaffProductGetQueryParamsFromRequest,
+} from './services/public_api';
+
+@NgModule({
+  providers: [
+    DaffProductGetCollectionRequestFromRoute,
+    DaffProductGetQueryParamsFromRequest,
+  ],
+})
+export class DaffProductRoutingModule {
+  static withConfig(config?: DaffProductRoutingConfig): ModuleWithProviders<DaffProductRoutingModule> {
+    return {
+      ngModule: DaffProductRoutingModule,
+      providers: [
+        {
+          provide: DAFF_PRODUCT_ROUTING_CONFIG,
+          useFactory: () => config || daffProductRoutingConfigDefaultFactory(inject(DaffBase64ServiceToken)),
+        },
+      ],
+    };
+  }
+}
