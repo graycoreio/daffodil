3977744d05aaf1082fe24eb18606c5dbdb9fb05a
Peter Lauck <griest024@gmail.com>
2022-08-23 18:52:57 -0400

    feat(reviews): add `@daffodil/reviews/state/testing` (#2139)

---
+++ libs/reviews/state/testing/src/mock-product-reviews-facade.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { DaffProductReview } from '@daffodil/reviews';
+import { DaffProductReviewsFacadeInterface } from '@daffodil/reviews/state';
+
+/**
+ * @inheritdoc
+ */
+@Injectable({ providedIn: 'root' })
+export class MockDaffProductReviewsFacade implements DaffProductReviewsFacadeInterface {
+  getProductReview(id: DaffProductReview['id']): BehaviorSubject<DaffProductReview> {
+	  return new BehaviorSubject(null);
+  }
+
+  dispatch(action) {};
+}
---
+++ libs/reviews/state/testing/src/public_api.ts
@@ -0,0 +1,3 @@
+export { MockDaffProductPageReviewsFacade } from './mock-product-page-reviews-facade';
+export { MockDaffProductReviewsFacade } from './mock-product-reviews-facade';
+export { DaffReviewsStateTestingModule } from './state-testing.module';
