3d34f46ccb4802becd85bcac13d5625efe8c5fc8
Peter Lauck <griest024@gmail.com>
2022-09-06 09:31:45 -0400

    fix(reviews): magento query not cacheable

--- libs/reviews/driver/magento/src/queries/get-product-reviews.ts
+++ libs/reviews/driver/magento/src/queries/get-product-reviews.ts
@@ -4,8 +4,10 @@ import { magentoProductPageInfoFragment } from '@daffodil/product/driver/magento
 
 import { magentoProductReviewFragment } from './fragments/public_api';
 
+export const MAGENTO_REVIEWS_LIST_QUERY_NAME = 'MagentoProductReviews';
+
 export const getProductReviews = gql`
-  query MagentoProductReviews($id: String!, $pageSize: Int, $currentPage: Int) {
+  query ${MAGENTO_REVIEWS_LIST_QUERY_NAME}($id: String!, $pageSize: Int, $currentPage: Int) {
     products(filter: {
       sku: {
         eq: $id
--- libs/reviews/driver/magento/src/reviews-driver.module.ts
+++ libs/reviews/driver/magento/src/reviews-driver.module.ts
@@ -4,13 +4,17 @@ import {
   ModuleWithProviders,
 } from '@angular/core';
 
+import { DAFF_MAGENTO_CACHEABLE_OPERATIONS } from '@daffodil/driver/magento';
 import {
   daffProvideProductMagentoExtraProductPreviewFragments,
   daffProvideProductMagentoExtraProductPreviewTransforms,
 } from '@daffodil/product/driver/magento';
 import { DaffReviewsDriver } from '@daffodil/reviews/driver';
 
-import { magentoReviewedProductFragment } from './queries/public_api';
+import {
+  magentoReviewedProductFragment,
+  MAGENTO_REVIEWS_LIST_QUERY_NAME,
+} from './queries/public_api';
 import { DaffReviewsMagentoService } from './reviews.service';
 import { magentoReviewedProductTransform } from './transforms/public_api';
 
@@ -34,6 +38,11 @@ export class DaffReviewsMagentoDriverModule {
         ...daffProvideProductMagentoExtraProductPreviewTransforms(
           magentoReviewedProductTransform,
         ),
+        {
+          provide: DAFF_MAGENTO_CACHEABLE_OPERATIONS,
+          useValue: MAGENTO_REVIEWS_LIST_QUERY_NAME,
+          multi: true,
+        },
       ],
     };
   }
