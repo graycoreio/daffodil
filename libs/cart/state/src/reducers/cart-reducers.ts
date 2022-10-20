4774684801a7c1c043dc26b6e99063cb99fffa21
Peter Lauck <griest024@gmail.com>
2022-10-19 20:23:03 -0400

    fix build

--- libs/cart/state/src/reducers/cart-reducers.ts
+++ libs/cart/state/src/reducers/cart-reducers.ts
@@ -2,10 +2,10 @@ import { ActionReducerMap } from '@ngrx/store';
 
 import { daffCartItemEntitiesReducer } from './cart-item-entities/cart-item-entities.reducer';
 import { daffCartOrderReducer } from './cart-order/cart-order.reducer';
-import { DaffCartReducerState } from './cart-state.interface';
+import { DaffCartReducersState } from './cart-reducers-state.interface';
 import { daffCartReducer } from './cart.reducer';
 
-export const daffCartReducers: ActionReducerMap<DaffCartReducerState> = {
+export const daffCartReducers: ActionReducerMap<DaffCartReducersState> = {
   cart: daffCartReducer,
   cartItems: daffCartItemEntitiesReducer,
   order: daffCartOrderReducer,
