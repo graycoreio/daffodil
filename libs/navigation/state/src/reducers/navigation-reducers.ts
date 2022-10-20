b50b5e0d19b942d4f32871e0e4282c78f747311c
Peter Lauck <griest024@gmail.com>
2022-10-19 20:01:06 -0400

    fix build

--- libs/navigation/state/src/reducers/navigation-reducers.ts
+++ libs/navigation/state/src/reducers/navigation-reducers.ts
@@ -1,8 +1,10 @@
 import { ActionReducerMap } from '@ngrx/store';
 
+import { DaffNavigationTree } from '@daffodil/navigation';
+
 import { DaffNavigationReducersState } from './navigation-reducers.interface';
 import { daffNavigationReducer } from './navigation/navigation.reducer';
 
-export const daffNavigationReducers: ActionReducerMap<DaffNavigationReducersState> = {
+export const daffNavigationReducers: ActionReducerMap<DaffNavigationReducersState<DaffNavigationTree>> = {
   navigation: daffNavigationReducer,
 };
