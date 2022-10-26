793344a9246cc9a5586f31bc96dbf6093d69cc9f
griest024 <griest024@gmail.com>
2022-10-26 17:52:22 -0500

    feat(auth): add redirect URLs for member and guest only guards (#2233)

--- libs/auth/routing/src/guards/guest-only.guard.ts
+++ libs/auth/routing/src/guards/guest-only.guard.ts
@@ -1,35 +1,59 @@
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import {
+  Inject,
+  Injectable,
+} from '@angular/core';
+import {
+  CanActivate,
+  Router,
+} from '@angular/router';
+import {
+  Actions,
+  ofType,
+} from '@ngrx/effects';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {
+  map,
+  tap,
+} from 'rxjs/operators';
 
 import {
+  DaffAuthActionTypes,
   DaffAuthFacade,
   DaffAuthGuardCheck,
+  DaffAuthGuardCheckCompletion,
 } from '@daffodil/auth/state';
 
-import { MemberOnlyGuard } from './member-only.guard';
+import { DaffAuthGuestOnlyGuardRedirectUrl } from './guest-only-guard-redirect.token';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'any',
 })
 export class GuestOnlyGuard implements CanActivate {
   constructor(
     private facade: DaffAuthFacade,
-    private memberOnlyGuard: MemberOnlyGuard,
+    private actions$: Actions,
+    private router: Router,
+    @Inject(DaffAuthGuestOnlyGuardRedirectUrl) private redirectUrl: string,
   ) {}
 
   canActivate(): Observable<boolean> {
-    const ret = this.isUnauthenticated();
+    const ret = this.isUnauthenticated().pipe(
+      tap(result => {
+        if (!result) {
+          this.router.navigateByUrl(this.redirectUrl);
+        }
+      }),
+    );
 
     this.facade.dispatch(new DaffAuthGuardCheck());
 
     return ret;
   }
 
-  isUnauthenticated(): Observable<boolean> {
-    return this.memberOnlyGuard.isAuthenticated().pipe(
-      map(authenticated => !authenticated),
+  private isUnauthenticated(): Observable<boolean> {
+    return this.actions$.pipe(
+      ofType(DaffAuthActionTypes.AuthGuardCheckCompletionAction),
+      map((action: DaffAuthGuardCheckCompletion) => !action.result),
     );
   }
 }
--- libs/auth/routing/src/module.ts
+++ libs/auth/routing/src/module.ts
@@ -8,11 +8,17 @@ import {
   DAFF_AUTH_ROUTING_CONFIG,
   DAFF_AUTH_ROUTING_CONFIG_DEFAULT,
 } from './config/public_api';
-import { DaffAuthResetPasswordGuard } from './guards/public_api';
+import {
+  DaffAuthResetPasswordGuard,
+  GuestOnlyGuard,
+  MemberOnlyGuard,
+} from './guards/public_api';
 
 @NgModule({
   providers: [
     DaffAuthResetPasswordGuard,
+    MemberOnlyGuard,
+    GuestOnlyGuard,
   ],
 })
 export class DaffAuthRoutingModule {
