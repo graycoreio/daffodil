a056010019ff4498c4d4a60496d1fb2a4a7768aa
griest024 <griest024@gmail.com>
2022-11-02 15:27:29 -0500

    fix(auth): magento driver not throwing errors (#2241)

--- libs/auth/driver/magento/src/errors/transform.ts
+++ libs/auth/driver/magento/src/errors/transform.ts
@@ -1,5 +1,6 @@
+import { DaffAuthInvalidAPIResponseError } from '@daffodil/auth/driver';
 import { daffTransformMagentoError } from '@daffodil/driver/magento';
 
 import { DaffAuthMagentoErrorMap } from './map';
 
-export const transformMagentoAuthError = (error: any): Error => daffTransformMagentoError(error, DaffAuthMagentoErrorMap);
+export const transformMagentoAuthError = (error: any): Error => daffTransformMagentoError(error, DaffAuthMagentoErrorMap) || new DaffAuthInvalidAPIResponseError(error.message);
