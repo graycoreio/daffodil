17ba32510d951e313e0fb726d37646f54a3f728e
Peter Lauck <griest024@gmail.com>
2022-12-01 12:18:05 -0500

    replace with type policies constant

--- libs/driver/magento/src/graphql/public_api.ts
+++ libs/driver/magento/src/graphql/public_api.ts
@@ -1,4 +1,3 @@
 export { DaffMagentoApolloCacheableOperationsLinkGenerator } from './cacheable-operations/apollo-cacheable-operations-link-generator';
 export * from './cacheable-operations/cacheable-operations-token';
-
-export * from './type-policies/public_api';
+export * from './type-policies';
