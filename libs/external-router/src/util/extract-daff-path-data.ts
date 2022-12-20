717146b5a0a4688612eec7d078856ebf1cbef0f7
Peter Lauck <griest024@gmail.com>
2022-12-20 15:03:22 -0500

    feat(external-router): improve the typing of `daffExtractDaffPathData`

--- libs/external-router/src/util/extract-daff-path-data.ts
+++ libs/external-router/src/util/extract-daff-path-data.ts
@@ -7,7 +7,7 @@ import {
  * Extracts a key from DaffPath data based upon the currently activated
  * RouteSnapshot.
  */
-export const daffExtractDaffPathData = <T extends Data = Data>(snapshot: ActivatedRouteSnapshot, key: keyof T): unknown => {
+export const daffExtractDaffPathData = <T extends Data = Data, K extends keyof T = keyof T>(snapshot: ActivatedRouteSnapshot, key: K): T[K] => {
   const pathFromRoot = snapshot.pathFromRoot[snapshot.pathFromRoot.length - 1].url.map((seg) => seg.path).join('/');
   return snapshot.data?.daffPaths?.[pathFromRoot]?.[key] ?? null;
 };
