3658d6c04e46a6bc688005468785a967388c63e3
Peter Lauck <griest024@gmail.com>
2022-12-23 14:47:21 -0500

    docs

--- libs/customer/routing/src/guards/clear-errors.guard.ts
+++ libs/customer/routing/src/guards/clear-errors.guard.ts
@@ -8,6 +8,9 @@ import { Observable } from 'rxjs';
 
 import { DaffCustomerClearErrors } from '@daffodil/customer/state';
 
+/**
+ * A guard that will clear customer state errors before allowing route deactivation.
+ */
 @Injectable({ providedIn: 'root' })
 export class DaffClearErrorsGuard implements CanDeactivate<unknown> {
   constructor(private store: Store<any>) {}
