87f56ff22f14eee0704a0fbf9734a1fe798da416
Peter Lauck <griest024@gmail.com>
2022-12-23 11:48:44 -0500

    change to functions
    this is for aot purposes

--- libs/core/state/src/operation/adapter.ts
+++ libs/core/state/src/operation/adapter.ts
@@ -5,35 +5,43 @@ import { DaffOperationState } from './state';
  * Puts the state in a "resolving" state that correspondes to loading platform data in a GET fashion.
  * Sets loading to `DaffState.Resolving`
  */
-export const startResolution = <T extends DaffOperationState = DaffOperationState>(state: T): T => ({
-  ...state,
-  loading: DaffState.Resolving,
-});
+export function startResolution <T extends DaffOperationState = DaffOperationState>(state: T): T {
+  return {
+    ...state,
+    loading: DaffState.Resolving,
+  };
+};
 
 /**
  * Puts the state in a "mutating" state that correspondes to loading platform data in a POST or PUT fashion.
  */
-export const startMutation = <T extends DaffOperationState = DaffOperationState>(state: T): T => ({
-  ...state,
-  loading: DaffState.Mutating,
-});
+export function startMutation <T extends DaffOperationState = DaffOperationState>(state: T): T {
+  return {
+    ...state,
+    loading: DaffState.Mutating,
+  };
+};
 
 /**
  * Indicates a successfully completed operation.
  * Sets loading to stable and resets errors.
  */
-export const completeOperation = <T extends DaffOperationState = DaffOperationState>(state: T): T => ({
-  ...state,
-  loading: DaffState.Stable,
-  errors: [],
-});
+export function completeOperation <T extends DaffOperationState = DaffOperationState>(state: T): T {
+  return {
+    ...state,
+    loading: DaffState.Stable,
+    errors: [],
+  };
+};
 
 /**
  * Indicates a failed operation.
  * Sets loading to stable and stores errors.
  */
-export const operationFailed = <T extends DaffOperationState = DaffOperationState>(errors: T['errors'], state: T): T => ({
-  ...state,
-  loading: DaffState.Stable,
-  errors,
-});
+export function operationFailed <T extends DaffOperationState = DaffOperationState>(errors: T['errors'], state: T): T {
+  return {
+    ...state,
+    loading: DaffState.Stable,
+    errors,
+  };
+};
