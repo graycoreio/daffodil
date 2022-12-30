5bbd986916f0a90f19c2f25f90d574a07fccf7ee
Peter Lauck <griest024@gmail.com>
2022-12-29 22:32:07 -0500

    feat(geography): send Magento driver calls as `GET`

--- libs/geography/driver/magento/src/queries/get-countries.ts
+++ libs/geography/driver/magento/src/queries/get-countries.ts
@@ -2,8 +2,10 @@ import { gql } from 'apollo-angular';
 
 import { countryFragment } from './fragments/public_api';
 
+export const MAGENTO_GET_COUNTRIES_QUERY_NAME = 'MagentoGetCountries';
+
 export const getCountries = gql`
-  query MagentoGetCountries {
+  query ${MAGENTO_GET_COUNTRIES_QUERY_NAME} {
     countries {
       ...country
     }
