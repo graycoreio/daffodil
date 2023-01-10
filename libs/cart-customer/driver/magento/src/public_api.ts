67b6fbe752cad877f9ef42375404063b0ec240e4
Peter Lauck <griest024@gmail.com>
2023-01-10 17:33:42 -0500

    feat(cart-customer): add magento payment driver

--- libs/cart-customer/driver/magento/src/public_api.ts
+++ libs/cart-customer/driver/magento/src/public_api.ts
@@ -2,6 +2,7 @@ export { DaffMagentoCartCustomerBillingAddressService } from './cart-billing-add
 export { DaffMagentoCartCustomerShippingAddressService } from './cart-shipping-address.service';
 export { DaffMagentoCartCustomerAddressService } from './cart-address.service';
 export { DaffMagentoCartCustomerService } from './cart.service';
+export { DaffMagentoCartCustomerPaymentService } from './payment.service';
 
 export { DaffCartCustomerMagentoDriverModule } from './cart-driver.module';
 
