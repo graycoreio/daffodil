722807b943f191215a0cb3477ff0c6f8e28a3227
griest024 <griest024@gmail.com>
2023-01-31 11:26:17 -0500

    feat(driver): add magento filter input model (#2325)

--- libs/driver/magento/src/models/responses/public_api.ts
+++ libs/driver/magento/src/models/responses/public_api.ts
@@ -1,3 +1,4 @@
 export { MagentoMoney } from './money';
 export { MagentoDiscount } from './discount';
 export { MagentoSearchResultPageInfo } from './page-info';
+export { MagentoFilterInput } from './filter-input.type';
