e4f15c578540c885e93d1a287a58492c887c5d92
Peter Lauck <griest024@gmail.com>
2023-02-01 11:30:47 -0500

    feat(customer-order): add `@daffodil/customer-order/driver/magento/testing` (#2328)

---
+++ libs/customer-order/driver/magento/testing/src/factories/orders.factory.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MagentoCustomerOrders } from '@daffodil/customer-order/driver/magento';
+
+import { MagentoCustomerOrdersFactory } from './orders.factory';
+
+describe('@daffodil/customer-order/driver/magento/testing | MagentoCustomerOrdersFactory', () => {
+  let factory: MagentoCustomerOrdersFactory;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MagentoCustomerOrdersFactory],
+    });
+
+    factory = TestBed.inject(MagentoCustomerOrdersFactory);
+  });
+
+  it('should be created', () => {
+    expect(factory).toBeTruthy();
+  });
+
+  describe('create', () => {
+    let result: MagentoCustomerOrders;
+
+    beforeEach(() => {
+      result = factory.create();
+    });
+
+    it('should return', () => {
+      expect(result.items).toBeDefined();
+      expect(result.page_info).toBeDefined();
+    });
+  });
+});
