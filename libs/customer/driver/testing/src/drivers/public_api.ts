23919383bde0bd3da4325d2bcdf544399ebf2a30
griest024 <griest024@gmail.com>
2023-02-02 23:08:55 -0500

    feat(customer): add mock address facade and export testing helpers (#2333)

--- libs/customer/driver/testing/src/drivers/public_api.ts
+++ libs/customer/driver/testing/src/drivers/public_api.ts
@@ -1,2 +1,3 @@
 export { DaffCustomerTestingDriverModule } from './customer-driver.module';
 export { DaffCustomerTestingDriver } from './customer.service';
+export { DaffCustomerAddressTestingDriver } from './address.service';
--- libs/customer/state/testing/src/public_api.ts
+++ libs/customer/state/testing/src/public_api.ts
@@ -1,2 +1,5 @@
 export { MockDaffCustomerPageFacade } from './mock-customer-facade';
+export { MockDaffCustomerAddressPageFacade } from './mock-address-facade';
 export { DaffCustomerStateTestingModule } from './customer-testing.module';
+
+export * from './factories/public_api';
