52bb06beb9cdeb1608ea0ad74e79911135ea0b82
Peter Lauck <griest024@gmail.com>
2023-02-24 12:06:38 -0500

    feat(authorizenet): export Magento driver services

--- libs/authorizenet/driver/magento/src/public_api.ts
+++ libs/authorizenet/driver/magento/src/public_api.ts
@@ -1,3 +1,5 @@
 export { DaffMagentoAuthorizeNetDriverModule } from './authorize-net.module';
 export { MagentoAuthorizeNetPayment } from './models/authorize-net-payment';
 export { MAGENTO_AUTHORIZE_NET_PAYMENT_ID } from './authorize-net-payment-id';
+export { DaffMagentoAuthorizeNetService } from './authorize-net.service';
+export { DaffMagentoAuthorizeNetPaymentService } from './payment.service';
