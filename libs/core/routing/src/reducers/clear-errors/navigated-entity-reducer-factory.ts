faf18c7ce8dd94f20abc33b924341eede5dce63a
Peter Lauck <griest024@gmail.com>
2023-02-25 10:53:43 -0500

    docs

--- libs/core/routing/src/reducers/clear-errors/navigated-entity-reducer-factory.ts
+++ libs/core/routing/src/reducers/clear-errors/navigated-entity-reducer-factory.ts
@@ -10,6 +10,9 @@ import {
 
 import { DaffErrorable } from '@daffodil/core/state';
 
+/**
+ * Creates a reducer that will clear all contextual errors from entities in state when `ROUTER_NAVIGATED` is dispatched.
+ */
 export function daffRouterStateNavigatedClearEntityErrorsReducerFactory<T extends DaffErrorable = DaffErrorable>(adapter: EntityAdapter<T> = createEntityAdapter<T>()) {
   return (state: EntityState<T>, action: RouterAction<unknown>): EntityState<T> => {
     switch (action.type) {
--- libs/core/routing/src/reducers/clear-errors/navigated.reducer.ts
+++ libs/core/routing/src/reducers/clear-errors/navigated.reducer.ts
@@ -5,6 +5,9 @@ import {
 
 import { DaffErrorable } from '@daffodil/core/state';
 
+/**
+ * A reducer that will clear all errors from state when `ROUTER_NAVIGATED` is dispatched.
+ */
 export function daffRouterStateNavigatedClearErrorsReducer<T extends DaffErrorable = DaffErrorable>(state: T, action: RouterAction<unknown>): T {
   switch (action.type) {
     case ROUTER_NAVIGATED:
