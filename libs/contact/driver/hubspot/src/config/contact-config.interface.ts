c3be39b0e546e8d1104c20165a9bb2082fbf6a86
Damien Retzinger <damienwebdev@gmail.com>
2023-03-04 19:43:52 -0500

    doc(contact): small docs on ContactHubspot config.

--- libs/contact/driver/hubspot/src/config/contact-config.interface.ts
+++ libs/contact/driver/hubspot/src/config/contact-config.interface.ts
@@ -2,6 +2,9 @@ import { InjectionToken } from '@angular/core';
 
 import { DaffHubspotConfig } from '@daffodil/driver/hubspot';
 
+/**
+ * The injection token that holds the configuration of the DaffContact feature.
+ */
 export const DaffContactConfigToken = new InjectionToken<DaffHubspotConfig>(
   'DaffContactConfig',
 );
