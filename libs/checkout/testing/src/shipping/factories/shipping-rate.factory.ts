fa3598de09c6eaf20e10425b2520a395dd7a4ca7
Peter Lauck <griest024@gmail.com>
2023-03-10 21:47:28 -0500

    use `en_US` locale

--- libs/checkout/testing/src/shipping/factories/shipping-rate.factory.ts
+++ libs/checkout/testing/src/shipping/factories/shipping-rate.factory.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker/locale/en_US';
 
 import { ShippingRate } from '@daffodil/checkout';
 import { DaffModelFactory } from '@daffodil/core/testing';
--- libs/customer/state/testing/src/factories/customer-address.factory.ts
+++ libs/customer/state/testing/src/factories/customer-address.factory.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker/locale/en_US';
 
 import { DaffState } from '@daffodil/core/state';
 import { DaffModelFactory } from '@daffodil/core/testing';
