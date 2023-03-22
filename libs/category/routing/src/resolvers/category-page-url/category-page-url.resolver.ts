ddf6eabac45f84f2aefebb8603e9f2a7b2e99dfe
griest024 <griest024@gmail.com>
2023-03-22 13:15:23 -0400

    feat(category): add service for build category URL requests (#2368)

--- libs/category/routing/src/resolvers/category-page-url/category-page-url.resolver.ts
+++ libs/category/routing/src/resolvers/category-page-url/category-page-url.resolver.ts
@@ -23,16 +23,13 @@ import {
   take,
 } from 'rxjs/operators';
 
-import { DaffCategoryRequestKind } from '@daffodil/category';
 import {
   DaffCategoryReducersState,
   DaffCategoryPageActionTypes,
   DaffCategoryPageLoadByUrl,
 } from '@daffodil/category/state';
-import { DaffRoutingUriNormalizer } from '@daffodil/core/routing';
 
-import { DaffCategoryRoutingRequestBuilder } from '../../injection-tokens/public_api';
-import { DAFF_CATEGORY_ROUTING_OPTIONS_BUILDER } from '../../injection-tokens/request/builder.token';
+import { DaffCategoryRoutingUrlRequestBuilder } from '../../services/public_api';
 
 /**
  * Resolves category data for category pages, and will only resolve the url
@@ -44,18 +41,13 @@ import { DAFF_CATEGORY_ROUTING_OPTIONS_BUILDER } from '../../injection-tokens/re
 export class DaffCategoryPageUrlResolver implements Resolve<Observable<boolean>> {
   constructor(
     @Inject(PLATFORM_ID) private platformId: string,
-    @Inject(DAFF_CATEGORY_ROUTING_OPTIONS_BUILDER) private requestBuilder: DaffCategoryRoutingRequestBuilder,
     private store: Store<DaffCategoryReducersState>,
     private dispatcher: ActionsSubject,
-    private urlNormalizer: DaffRoutingUriNormalizer,
+    private requestBuilder: DaffCategoryRoutingUrlRequestBuilder,
   ) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    this.store.dispatch(new DaffCategoryPageLoadByUrl({
-      ...this.requestBuilder(route),
-      url: this.urlNormalizer.normalize(state.url),
-      kind: DaffCategoryRequestKind.URL,
-    }));
+    this.store.dispatch(new DaffCategoryPageLoadByUrl(this.requestBuilder.build(route, state)));
 
     return isPlatformBrowser(this.platformId) ? of(true) : this.dispatcher.pipe(
       ofType(DaffCategoryPageActionTypes.CategoryPageLoadSuccessAction, DaffCategoryPageActionTypes.CategoryPageLoadFailureAction),
---
+++ libs/category/routing/src/services/public_api.ts
@@ -0,0 +1,1 @@
+export { DaffCategoryRoutingUrlRequestBuilder } from './url-request-builder.service';
