24a1d11153b83e15d6d67dbb99911066045eb8ed
Peter Lauck <griest024@gmail.com>
2023-03-22 15:42:20 -0400

    feat(category): add injectable product reducer

--- libs/category/state/src/category-state.module.ts
+++ libs/category/state/src/category-state.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
-import { DaffProductStateModule } from '@daffodil/product/state';
+import {
+  daffProductProvideExtraReducers,
+  DaffProductStateModule,
+} from '@daffodil/product/state';
 
 import { DaffCategoryPageFilterEffects } from './effects/category-page-filter.effects';
 import { DaffCategoryPageEffects } from './effects/category-page.effects';
 import { DaffCategoryEffects } from './effects/category.effects';
 import { daffCategoryReducers } from './reducers/category-reducers';
+import { daffCategoryProductReducer } from './reducers/product.reducer';
 import { DAFF_CATEGORY_STORE_FEATURE_KEY } from './reducers/public_api';
 
 /**
@@ -18,6 +22,11 @@ import { DAFF_CATEGORY_STORE_FEATURE_KEY } from './reducers/public_api';
     StoreModule.forFeature(DAFF_CATEGORY_STORE_FEATURE_KEY, daffCategoryReducers),
     EffectsModule.forFeature([DaffCategoryEffects, DaffCategoryPageEffects, DaffCategoryPageFilterEffects]),
     DaffProductStateModule,
+  ],
+  providers: [
+    ...daffProductProvideExtraReducers(
+      daffCategoryProductReducer,
+    ),
   ],
 })
 export class DaffCategoryStateModule {}
---
+++ libs/category/state/src/reducers/product.reducer.ts
@@ -0,0 +1,48 @@
+import { DaffGenericCategory } from '@daffodil/category';
+import {
+  daffCompleteOperation,
+  daffStartMutation,
+  daffStartResolution,
+} from '@daffodil/core/state';
+import { DaffProduct } from '@daffodil/product';
+import { DaffProductReducerState } from '@daffodil/product/state';
+
+import {
+  DaffCategoryPageProductCollectionActionTypes,
+  DaffCategoryPageProductCollectionActions,
+} from '../actions/category-page-filter.actions';
+import { DaffCategoryPageActions } from '../actions/category-page.actions';
+import { DaffCategoryPageActionTypes } from '../actions/category-page.actions';
+import { DaffCategoryActions } from '../actions/category.actions';
+
+/**
+ * Modifies product state for category actions.
+ */
+export function daffCategoryProductReducer<U extends DaffGenericCategory<U>, W extends DaffProduct>(
+  state: DaffProductReducerState,
+  action: DaffCategoryActions<U, W> | DaffCategoryPageActions<U, W> | DaffCategoryPageProductCollectionActions,
+): DaffProductReducerState {
+  switch (action.type) {
+    case DaffCategoryPageActionTypes.CategoryPageLoadAction:
+    case DaffCategoryPageActionTypes.CategoryPageLoadByUrlAction:
+      return daffStartResolution(state);
+
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageChangeSizeAction:
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageChangeCurrentPageAction:
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageChangeSortingOptionAction:
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageChangeFiltersAction:
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageReplaceFiltersAction:
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageApplyFiltersAction:
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageClearFiltersAction:
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageRemoveFiltersAction:
+    case DaffCategoryPageProductCollectionActionTypes.CategoryPageToggleFilterAction:
+      return daffStartMutation(state);
+
+    case DaffCategoryPageActionTypes.CategoryPageLoadSuccessAction:
+    case DaffCategoryPageActionTypes.CategoryPageLoadFailureAction:
+      return daffCompleteOperation(state);
+
+    default:
+      return state;
+  }
+}
