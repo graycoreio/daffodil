6216e65191a2877de576f6082bb9cf0b48b7215b
Damien Retzinger <damienwebdev@gmail.com>
2023-03-29 21:56:44 -0400

    test(core): improve test coverage for arithmetic.

--- libs/core/src/utils/long-arithmetic.spec.ts
+++ libs/core/src/utils/long-arithmetic.spec.ts
@@ -3,87 +3,155 @@ import {
   daffSubtract,
   daffMultiply,
   daffDivide,
+  daffPrecision,
 } from './long-arithmetic';
 
 describe('Core | Utils | Long Arithmetic', () => {
 
+  const testEdgeCases = [];
+  const specialCases  = [[], [1]];
+  const edges = [0, -0, 1, -1, undefined, null, NaN, Infinity, -Infinity];
+  edges.forEach((num) => {
+    edges.forEach((denom) => {
+      testEdgeCases.push({ i: [num, denom], o: undefined, c: '' });
+    });
+  });
+
+  specialCases.forEach((i) => {
+    testEdgeCases.push({ i, o: undefined, c: '' });
+  });
+
   describe('daffAdd', () => {
+    const testIntegerCases = [
+      { i: [1,2], o: 3, c: 'Integer Addition' },
+      { i: [3,3], o: 6, c: 'Integer Addition' },
+      { i: [3,3,4], o: 10, c: 'Integer Addition' },
+    ];
+
+    const testLongCases = [
+      { i: [0.2, 0.1], o: 0.3, c: 'Rational Addition' },
+      { i: [.333, 3], o: 3.333, c: 'Rational Addition' },
+      { i: [.333, 3, 0.3], o: 3.633, c: 'Rational Addition' },
+    ];
 
     it('should add long decimals and return the expected value', () => {
-      expect(1.005 + 0.99 + 0.002).not.toEqual(1.997);
-      expect(daffAdd(1.005, 0.99, 0.002)).toEqual(1.997);
+      expect(0.2 + 0.1).not.toEqual(0.3);
+      expect(daffAdd(0.2, 0.1)).toEqual(0.3);
     });
 
-    it('should return NaN if given an undefined value', () => {
-      expect(daffAdd(undefined, 1)).toEqual(NaN);
+    [...testLongCases, ...testIntegerCases].forEach(test => {
+      it(`should add long decimals correctly: ${test.i} - ${test.c}`, () => {
+        expect(daffAdd(...test.i)).toEqual(test.o);
+      });
     });
 
-    it('should treat null values as zero', () => {
-      expect(daffAdd(null, 1)).toEqual(1);
+    [...testIntegerCases, ...testEdgeCases].forEach(test => {
+      it(`should add integers and edgeCases the same as native js:  ${test.i} - ${test.c}`, () => {
+        expect(daffAdd(...test.i))
+          .toEqual(test.i.slice(1).reduce((acc, i) => acc + i, test.i[0]));
+      });
     });
   });
 
   describe('daffSubtract', () => {
+    const testIntegerCases = [
+      { i: [1,2], o: -1, c: 'Integer Subtraction' },
+      { i: [3,3], o: 0, c: 'Integer Subtraction' },
+    ];
+
+    const testLongCases = [
+      { i: [0.2, 0.1], o: 0.1, c: 'Rational Subtraction' },
+      { i: [.333, 3], o: -2.667, c: 'Rational Subtraction' },
+    ];
 
     it('should subtract long decimals and return the expected value', () => {
       expect(1.005 - 0.99 - 0.002).not.toEqual(0.013);
       expect(daffSubtract(1.005, 0.99, 0.002)).toEqual(0.013);
     });
 
-    it('should return NaN if given an undefined value', () => {
-      expect(daffSubtract(undefined, 1)).toEqual(NaN);
+    [...testLongCases, ...testIntegerCases].forEach(test => {
+      it(`should subtract long decimals correctly: ${test.i} - ${test.c}`, () => {
+        expect(daffSubtract(...test.i)).toEqual(test.o);
+      });
     });
 
-    it('should treat null values as zero', () => {
-      expect(daffSubtract(null, 1)).toEqual(-1);
+    [...testIntegerCases, ...testEdgeCases].forEach(test => {
+      it(`should subtract integers and edgeCases the same as native js:  ${test.i} - ${test.c}`, () => {
+        expect(daffSubtract(...test.i))
+          .toEqual(test.i.slice(1).reduce((acc, i) => acc - i, test.i[0]));
+      });
     });
   });
 
   describe('daffMultiply', () => {
+    const testIntegerCases = [
+      { i: [1,2], o: 2, c: 'Integer Multiplication' },
+      { i: [3,3], o: 9, c: 'Integer Multiplication' },
+    ];
 
-    it('should multiply long decimals and return the expected value', () => {
-      expect(1.005 * 0.99 * 0.002).not.toEqual(0.0019899);
-      expect(daffMultiply(1.005, 0.99, 0.002)).toEqual(0.0019899);
+    const testLongCases = [
+      { i: [0.2, 0.1], o: 0.02, c: 'Rational Multiplication' },
+      { i: [.333, 3], o: 0.999, c: 'Rational Multiplication' },
+    ];
+
+    [...testLongCases, ...testIntegerCases].forEach(test => {
+      it(`should multiply long decimals correctly: ${test.i} - ${test.c}`, () => {
+        expect(daffMultiply(...test.i)).toEqual(test.o);
+      });
     });
 
-    it('should return NaN if given an undefined value', () => {
-      expect(daffMultiply(undefined, 1)).toEqual(NaN);
-    });
-
-    it('should treat null values as zero', () => {
-      expect(daffMultiply(null, 1)).toEqual(0);
+    [...testIntegerCases, ...testEdgeCases].forEach(test => {
+      it(`should multiple integers and edgeCases the same as native js:  ${test.i} - ${test.c}`, () => {
+        expect(daffMultiply(...test.i))
+          .toEqual(test.i.slice(1).reduce((acc, i) => acc * i, test.i[0]));
+      });
     });
   });
 
   describe('daffDivide', () => {
+    const testIntegerCases = [
+      { i: [1,2], o: 0.5, c: 'Finite Rational Division' },
+      { i: [2,3], o: 0.6666666666666666, c: 'Infinite Rational Division' },
+    ];
 
-    it('should divide long decimals and return the expected value', () => {
-      expect(1.005 / 5 / 0.001).not.toEqual(201);
-      expect(daffDivide(1.005, 5, 0.001)).toEqual(201);
+    const testLongCases = [
+      { i: [1.005, 5, 0.001], o: 201, c: '' },
+    ];
+
+    [...testLongCases, ...testIntegerCases].forEach(test => {
+      it(`should divide long decimals correctly: ${test.i} - ${test.c}`, () => {
+        expect(daffDivide(...test.i)).toEqual(test.o);
+      });
     });
 
-    it('should return NaN if given an undefined value', () => {
-      expect(daffDivide(undefined, 1)).toEqual(NaN);
+    [...testIntegerCases, ...testEdgeCases].forEach(test => {
+      it(`should divide integers and edgeCases the same as native js:  ${test.i} - ${test.c}`, () => {
+        expect(daffDivide(...test.i))
+          .toEqual(test.i.slice(1).reduce((acc, i) => acc / i, test.i[0]));
+      });
     });
+  });
 
-    it('should treat null values as zero', () => {
-      expect(daffDivide(null, 1)).toEqual(0);
-      expect(daffDivide(1, null)).toEqual(NaN);
-    });
+  describe('daffPrecision', () => {
+    const testCases = [
+      { i: 0, o: 1 },
+      { i: 1, o: 1 },
+      { i: 1.1, o: 1e1 },
+      { i: 1.11, o: 1e2 },
+      { i: 1.111, o: 1e3 },
+      { i: -0, o: 1 },
+      { i: undefined, o: 1 },
+      { i: null, o: 1 },
+      { i: NaN, o: 1 },
+      { i: Infinity, o: 1 },
+      { i: Math.PI, o: 1e11 },
+      { i: -Infinity, o: 1 },
+    ];
 
-    it('should return NaN when attempting to divide by 0', () => {
-      expect(daffDivide(0, 0)).toEqual(NaN);
-      expect(daffDivide(1, 0)).toEqual(NaN);
-    });
-
-    it('should treat division with Infinity as a mathematical limit', () => {
-      expect(daffDivide(1, Infinity)).toEqual(0);
-      expect(daffDivide(Infinity, 1)).toEqual(Infinity);
-    });
-
-    it('should return NaN when attempting to perform division including NaN', () => {
-      expect(daffDivide(1, NaN)).toEqual(NaN);
-      expect(daffDivide(NaN, 1)).toEqual(NaN);
+    testCases.forEach(test => {
+      it(`should compute daffPrecision of ${test.i} correctly`, () => {
+        expect(daffPrecision(test.i)).toEqual(test.o);
+      });
     });
   });
 });
