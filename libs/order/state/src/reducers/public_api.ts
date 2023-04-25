a9104218c752164631099cfea7d605ff50a80ab3
Peter Lauck <griest024@gmail.com>
2023-04-25 16:00:27 -0400

    feat(order): add injectable reducers

--- libs/order/state/src/reducers/public_api.ts
+++ libs/order/state/src/reducers/public_api.ts
@@ -8,3 +8,4 @@ export { DAFF_ORDER_STORE_FEATURE_KEY } from './order-store-feature-key';
 export * from './order-entities/public_api';
 export * from './order/public_api';
 export * from './order-collection/public_api';
+export * from './token/public_api';
