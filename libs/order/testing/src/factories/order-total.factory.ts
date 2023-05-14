141ccc88f343b4bdd640bc3add2251341b71ef9e
Peter Lauck <griest024@gmail.com>
2023-05-14 00:07:24 +0000

    fix(order): order total factory creating mock objects with static `type`

--- libs/order/testing/src/factories/order-total.factory.ts
+++ libs/order/testing/src/factories/order-total.factory.ts
@@ -2,16 +2,13 @@ import { Injectable } from '@angular/core';
 import { faker } from '@faker-js/faker/locale/en_US';
 
 import { DaffModelFactory } from '@daffodil/core/testing';
-import {
-  DaffOrderTotal,
-  DaffOrderTotalTypeEnum,
-} from '@daffodil/order';
+import { DaffOrderTotal } from '@daffodil/order';
 
 export class MockOrderTotal implements DaffOrderTotal {
   label = faker.random.word();
   value = faker.datatype.number({ min: 1, max: 100 });
   sort_order = faker.datatype.number({ min: 1, max: 100 });
-  type = DaffOrderTotalTypeEnum.GrandTotal;
+  type = faker.helpers.unique(faker.random.word);
 };
 
 @Injectable({
