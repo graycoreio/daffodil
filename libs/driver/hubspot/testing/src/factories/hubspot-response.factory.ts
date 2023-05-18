59e89d4ad5d976601206369972704c6cf0d3272f
Hanmin Kim <hanminkim123@gmail.com>
2023-05-18 12:26:55 -0700

    test(hubspot): add DaffHubspotResponseFactory

---
+++ libs/driver/hubspot/testing/src/factories/hubspot-response.factory.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { faker } from '@faker-js/faker';
+
+import { DaffModelFactory } from '@daffodil/core/testing';
+import { HubspotError, HubspotResponse } from "../../../src/models/hubspot-response";
+
+const MockHubspotError = (): HubspotError => {
+  return {
+    message: faker.random.words(5),
+    errorType: faker.random.word()
+  }
+}
+
+export class MockHubspotResponse implements HubspotResponse {
+  redirectUri? = faker.internet.url();
+  inlineMessage = faker.random.words(5);
+  errors: HubspotError[] = Array(faker.datatype.number({ min: 1, max: 5 })).fill(MockHubspotError);
+}
+
+/**
+ * Model factory for {@link HubspotResponse}.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class DaffHubspotResponseFactory extends DaffModelFactory<HubspotResponse>{
+  constructor() {
+    super(MockHubspotResponse);
+  }
+}
