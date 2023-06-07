add0879c6fede84bdc08e4d91652bfdcbfd288ea
Hanmin Kim <hanminkim123@gmail.com>
2023-06-07 16:13:40 -0700

    docs(contact): add documentation for contact actions

--- libs/contact/state/src/actions/contact.actions.ts
+++ libs/contact/state/src/actions/contact.actions.ts
@@ -12,28 +12,56 @@ export enum DaffContactActionTypes {
   ContactResetAction = '[Daff-Contact] Contact Reset Action',
 }
 
+/**
+ * An action triggered upon submitting a contact request.
+ *
+ * @param payload - a contact request payload
+ */
 export class DaffContactSubmit implements Action {
   readonly type = DaffContactActionTypes.ContactSubmitAction;
 
   constructor(public payload: DaffContactRequest) {}
 }
 
+/**
+ * An action triggered upon resubmitting a contact request.
+ *
+ * @param payload - a contact request payload
+ */
 export class DaffContactRetry implements Action {
   readonly type = DaffContactActionTypes.ContactRetryAction;
 
   constructor(public payload: DaffContactRequest) {}
 }
+
+/**
+ * An action triggered upon failure of a contact request submission.
+ *
+ * @param payload - an array of errors
+ */
 export class DaffContactFailedSubmit implements Action {
   readonly type = DaffContactActionTypes.ContactFailedSubmitAction;
 
   constructor(public payload: DaffStateError[]) {}
 }
+
+/**
+ * An action triggered upon cancelling a contact request.
+ */
 export class DaffContactCancel implements Action {
   readonly type = DaffContactActionTypes.ContactCancelAction;
 }
+
+/**
+ * An action triggered upon success of a contact request submission.
+ */
 export class DaffContactSuccessSubmit implements Action {
   readonly type = DaffContactActionTypes.ContactSuccessSubmitAction;
 }
+
+/**
+ * An action triggered upon resetting of a contact.
+ */
 export class DaffContactReset implements Action {
   readonly type = DaffContactActionTypes.ContactResetAction;
 }
