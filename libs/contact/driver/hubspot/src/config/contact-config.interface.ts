2231d364f56190a27b234363bf00b7c0a9fb95de
Hanmin Kim <hanminkim123@gmail.com>
2023-06-07 16:15:59 -0700

    docs(contact): fix documentation for contact-config.interface.ts

--- libs/contact/driver/hubspot/src/config/contact-config.interface.ts
+++ libs/contact/driver/hubspot/src/config/contact-config.interface.ts
@@ -3,7 +3,7 @@ import { InjectionToken } from '@angular/core';
 import { DaffHubspotConfig } from '@daffodil/driver/hubspot';
 
 /**
- * The injection token that holds the configuration of the DaffContact feature.
+ * The injection token that holds the configuration of the hubspot contact driver.
  */
 export const DaffContactConfigToken = new InjectionToken<DaffHubspotConfig>(
   'DaffContactConfig',
