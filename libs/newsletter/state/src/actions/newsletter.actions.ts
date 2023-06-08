dfad61a4bbb2f54b6bd5a0c5641f85bbb54df3e9
Hanmin Kim <hanminkim123@gmail.com>
2023-06-07 18:18:55 -0700

    docs(newsletter): add docs for newsletter actions

--- libs/newsletter/state/src/actions/newsletter.actions.ts
+++ libs/newsletter/state/src/actions/newsletter.actions.ts
@@ -12,29 +12,57 @@ export enum DaffNewsletterActionTypes {
   NewsletterReset = '[Daff-Newsletter] Reset Newsletter'
 }
 
+/**
+ * An action triggered upon subscribing to a newsletter.
+ *
+ * @param payload - a newsletter submission payload
+ */
 export class DaffNewsletterSubscribe implements Action {
   readonly type = DaffNewsletterActionTypes.NewsletterSubscribeAction;
 
   constructor(public payload: DaffNewsletterSubmission) { }
 }
+
+/**
+ * An action triggered upon attempting to retry subscribing to a newsletter.
+ *
+ * @param payload - a newsletter submission payload
+ */
 export class DaffNewsletterRetry implements Action {
   readonly type = DaffNewsletterActionTypes.NewsletterRetry;
 
   constructor(public payload: DaffNewsletterSubmission) { }
 }
 
+/**
+ * An action triggered upon cancelling a newsletter subscription request.
+ */
 export class DaffNewsletterCancel implements Action {
   readonly type = DaffNewsletterActionTypes.NewsletterCancelAction;
 
 }
+
+/**
+ * An action triggered upon failure of a newsletter subscription request.
+ *
+ * @param payload - an array of errors
+ */
 export class DaffNewsletterFailedSubscribe implements Action {
   readonly type = DaffNewsletterActionTypes.NewsletterFailedSubscribeAction;
 
   constructor(public payload: DaffStateError) { }
 }
+
+/**
+ * An action triggered upon success of a newsletter subscription request.
+ */
 export class DaffNewsletterSuccessSubscribe implements Action {
   readonly type = DaffNewsletterActionTypes.NewsletterSuccessSubscribeAction;
 }
+
+/**
+ * An action triggered upon resetting of a newsletter subscription.
+ */
 export class DaffNewsletterReset implements Action {
   readonly type = DaffNewsletterActionTypes.NewsletterReset;
 }
