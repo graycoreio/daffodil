dfd17612d040401fb1812e6cfbc90b19ee70347b
Hanmin Kim <hanminkim123@gmail.com>
2023-06-07 17:41:25 -0700

    docs(newsletter): update docs for DaffNewsletterStorage

--- libs/newsletter/driver/in-memory/src/models/newsletter_storage.ts
+++ libs/newsletter/driver/in-memory/src/models/newsletter_storage.ts
@@ -1,5 +1,7 @@
 /**
- * A type for the storage of a newsletter subscription.
+ * A type used for storing a newsletter subscription.
+ *
+ * Used in the in-memory driver as a mock database.
  */
 export interface DaffNewsletterStorage {
   email: string;
