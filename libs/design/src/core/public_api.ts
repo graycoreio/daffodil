c5a152249602393b853cc2833a451c79987adfa2
xelaint <xelaint@gmail.com>
2023-06-27 13:43:48 -0400

    feat(design): expose DaffStatusable to the public api

--- libs/design/src/core/public_api.ts
+++ libs/design/src/core/public_api.ts
@@ -3,7 +3,7 @@ export * from './prefix-suffix/public_api';
 export * from './colorable/colorable';
 export * from './breakpoints/breakpoints';
 export * from './constructor/constructor';
-export * from './statusable/statusable';
+export * from './statusable/public_api';
 export * from './skeletonable/skeletonable';
 export * from './mutable/mutable';
 export * from './text-alignable/text-alignable';
