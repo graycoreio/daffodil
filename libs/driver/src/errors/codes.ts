2142c0fa0562fb84a396122f09d0fb2179adf0d5
Peter Lauck <griest024@gmail.com>
2023-08-10 17:05:13 +0000

    feat(driver): add network error code to enum

--- libs/driver/src/errors/codes.ts
+++ libs/driver/src/errors/codes.ts
@@ -1,3 +1,6 @@
+import { DAFF_DRIVER_NETWORK_ERROR_CODE } from './network-error.class';
+
 export enum DaffDriverErrorCodes {
-  BAD_INPUT = 'DAFF_BAD_INPUT'
+  BAD_INPUT = 'DAFF_BAD_INPUT',
+  NETWORK_ERROR = DAFF_DRIVER_NETWORK_ERROR_CODE
 }
