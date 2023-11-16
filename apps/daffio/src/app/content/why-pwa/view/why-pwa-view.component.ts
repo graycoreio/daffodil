4a90668fe29b195c29636b65ff81aac47218e45e
Elain T <elain.tsai@graycore.io>
2023-11-16 17:05:53 -0500

    feat(daffio): clean up why pwa page styles (#2603)

--- apps/daffio/src/app/content/why-pwa/view/why-pwa-view.component.ts
+++ apps/daffio/src/app/content/why-pwa/view/why-pwa-view.component.ts
@@ -6,7 +6,6 @@ import {
 @Component({
   selector: 'daffio-why-pwa-view',
   templateUrl: './why-pwa-view.component.html',
-  styleUrls: ['./why-pwa-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DaffioWhyPwaViewComponent {}
