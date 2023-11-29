647b87ed865f508c00c8b9640fe878c2402a705e
Peter Lauck <griest024@gmail.com>
2023-11-29 18:38:51 +0000

    feat(core): add `collect`

---
+++ libs/core/src/utils/collect.ts
@@ -0,0 +1,11 @@
+type Dictionary = Record<string | number | symbol, any>;
+
+/**
+ * Recurses through a tree, collecting each element in that tree and returning it in an array.
+ * The value of getKey should be a field that contains a list of the same type of object.
+ * Accepts an optional depth argument which will limit the traversal. This should be used for trees with circular references.
+ */
+export const collect = <T>(obj: T, getChildren: (val: T) => T[], depth?: number): T[] =>
+  !depth || depth > 0
+    ? [obj].concat(...getChildren(obj).map((child) => collect(child, getChildren, depth - 1)))
+    : [obj];
