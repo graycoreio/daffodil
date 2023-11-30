40cf496e67f3e131357b025416c155d99cbec561
griest024 <griest024@gmail.com>
2023-11-30 11:18:39 -0500

    feat(navigation,demo): pull navigation tree from category in-memory tree (#2632)

--- libs/navigation/driver/in-memory/src/backend/navigation.service.spec.ts
+++ libs/navigation/driver/in-memory/src/backend/navigation.service.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
-import { isNavigation } from '@daffodil/navigation/testing';
+import { DaffNavigationTree } from '@daffodil/navigation';
+import { DAFF_NAVIGATION_IN_MEMORY_SEED_DATA_PROVIDER } from '@daffodil/navigation/driver/in-memory';
+import {
+  DaffNavigationTreeFactory,
+  isNavigation,
+} from '@daffodil/navigation/testing';
 
 import { DaffInMemoryBackendNavigationService } from './navigation.service';
 
-describe('Driver | InMemory | Navigation | DaffInMemoryBackendNavigationService', () => {
-  let navigationTestingService;
+describe('@daffodil/navigation/driver/in-memory | DaffInMemoryBackendNavigationService', () => {
+  let navigationTestingService: DaffInMemoryBackendNavigationService;
+  let navigationFactory: DaffNavigationTreeFactory;
+  let navigationTree: DaffNavigationTree;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DaffInMemoryBackendNavigationService],
+      providers: [
+        DaffInMemoryBackendNavigationService,
+        {
+          provide: DAFF_NAVIGATION_IN_MEMORY_SEED_DATA_PROVIDER,
+          useFactory: () => {
+            navigationFactory = TestBed.inject(DaffNavigationTreeFactory);
+            navigationTree = navigationFactory.create();
+            return () => navigationTree;
+          },
+        },
+      ],
     });
 
     navigationTestingService = TestBed.inject(DaffInMemoryBackendNavigationService);
@@ -17,6 +34,10 @@ describe('Driver | InMemory | Navigation | DaffInMemoryBackendNavigationService'
 
   it('should be created', () => {
     expect(navigationTestingService).toBeTruthy();
+  });
+
+  it('should seed the navigation tree from the provider', () => {
+    expect(navigationTestingService.navigationTree).toEqual(navigationTree);
   });
 
   describe('createDb', () => {
--- libs/navigation/driver/in-memory/src/backend/navigation.service.ts
+++ libs/navigation/driver/in-memory/src/backend/navigation.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@angular/core';
+import {
+  Inject,
+  Injectable,
+  Optional,
+} from '@angular/core';
 import {
   InMemoryDbService,
   RequestInfoUtilities,
@@ -9,14 +13,26 @@ import {
 import { DaffNavigationTree } from '@daffodil/navigation';
 import { DaffNavigationTreeFactory } from '@daffodil/navigation/testing';
 
+import {
+  DAFF_NAVIGATION_IN_MEMORY_SEED_DATA_PROVIDER,
+  DaffNavigationInMemorySeedDataProvider,
+} from '../seed-data-provider/public_api';
+
 @Injectable({
   providedIn: 'root',
 })
 export class DaffInMemoryBackendNavigationService implements InMemoryDbService {
-  navigationTree: DaffNavigationTree;
+  private _navigationTree: DaffNavigationTree;
 
-  constructor(private navigationTreeFactory: DaffNavigationTreeFactory) {
-    this.navigationTree = this.navigationTreeFactory.create();
+  get navigationTree(): DaffNavigationTree {
+    return this.seedDataProvider?.() || this._navigationTree;
+  }
+
+  constructor(
+    private navigationTreeFactory: DaffNavigationTreeFactory,
+    @Inject(DAFF_NAVIGATION_IN_MEMORY_SEED_DATA_PROVIDER) @Optional() private seedDataProvider: DaffNavigationInMemorySeedDataProvider | null,
+  ) {
+    this._navigationTree = seedDataProvider?.() || this.navigationTreeFactory.create();
   }
 
   parseRequestUrl(url: string, utils: RequestInfoUtilities): ParsedRequestUrl {
