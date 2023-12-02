a6ab936535edcab25250b45da3e7fc3f7fa2c0f3
Peter Lauck <griest024@gmail.com>
2023-12-02 17:18:11 +0000

    fix lint

--- libs/design/src/molecules/select/select.module.ts
+++ libs/design/src/molecules/select/select.module.ts
@@ -5,11 +5,11 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { DaffSelectOptionDirective } from './option/option.directive';
+import { DaffSelectComponent } from './select.component';
 import { DaffErrorMessageModule } from '../../atoms/form/error-message/error-message.module';
 import { DaffFormLabelModule } from '../../atoms/form/form-label/form-label.module';
 import { DaffPrefixSuffixModule } from '../../core/prefix-suffix/prefix-suffix.module';
-import { DaffSelectOptionDirective } from './option/option.directive';
-import { DaffSelectComponent } from './select.component';
 
 @NgModule({
   imports: [
