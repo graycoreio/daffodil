776faa3806030c648b56ca971799e9277dc4fbae
Elain T <elain.tsai@graycore.io>
2023-12-13 11:08:25 -0500

    feat(design): add DaffSizeable to design core's public api (#2653)

--- libs/design/src/core/sizeable/sizeable.ts
+++ libs/design/src/core/sizeable/sizeable.ts
@@ -1,6 +1,15 @@
+/**
+ * An interface for giving a component the ability to customize sizing for component-specific UI.
+ * In order to be sizable, a component class must implement this property.
+ */
+
 export interface DaffSizeable<T extends DaffSizeAllType> {
   size: T;
 }
+
+/**
+ * The possible types that can be passed to a component that implements DaffSizeable
+ */
 
 export type DaffSizeXSmallType = 'xs';
 export type DaffSizeSmallType = 'sm';
