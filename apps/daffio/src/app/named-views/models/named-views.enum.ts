e2016e3946af25a47b93db3004d44dc521285fa7
xelaint <xelaint@gmail.com>
2024-01-11 14:47:41 -0500

    revert named views enum

--- apps/daffio/src/app/named-views/models/named-views.enum.ts
+++ apps/daffio/src/app/named-views/models/named-views.enum.ts
@@ -1,6 +1,4 @@
 export enum DaffioRouterNamedViewsEnum {
-  SIDEBARHEADER = 'sidebar-header',
-  SIDEBARCONTENT = 'sidebar-content',
-  SIDEBARFOOTER = 'sidebar-footer',
+  SIDEBAR = 'sidebar',
   NAV = 'nav'
 }
