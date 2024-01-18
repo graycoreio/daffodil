38c9431de03efe171722acca16b0a93e20b272e0
Peter Lauck <griest024@gmail.com>
2024-01-18 21:52:46 +0000

    fix(daffio): wrong asset path on prod

--- apps/daffio/src/app/app.server.module.ts
+++ apps/daffio/src/app/app.server.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
+import { environment } from '../environments/environment';
 import { DaffioAppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { DaffioAssetFetchServerService } from './core/assets/fetch/server.service';
@@ -16,7 +17,7 @@ import { DAFFIO_DOCS_PATH_TOKEN } from './docs/services/docs-path.token';
   providers: [
     {
       provide: DAFFIO_DOCS_PATH_TOKEN,
-      useValue: process.env.DAFFIO_DOCS_PATH || '',
+      useValue: environment.docsPath,
     },
     {
       provide: DaffioAssetFetchService,
