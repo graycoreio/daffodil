004457504e2be80893613432a7a94d81a6ac3d83
Peter Lauck <griest024@gmail.com>
2024-01-18 22:11:44 +0000

    fix lint

--- apps/daffio/src/app/app.server.module.ts
+++ apps/daffio/src/app/app.server.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
-import { environment } from '../environments/environment';
 import { DaffioAppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { DaffioAssetFetchServerService } from './core/assets/fetch/server.service';
 import { DaffioAssetFetchService } from './core/assets/fetch/service.interface';
 import { DAFFIO_DOCS_PATH_TOKEN } from './docs/services/docs-path.token';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
