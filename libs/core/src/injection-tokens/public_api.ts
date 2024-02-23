f0678d133dc1d57e9b9b735a6f2cbb3bce055089
Peter Lauck <griest024@gmail.com>
2024-02-23 05:17:28 +0000

    add services factory

--- libs/core/src/injection-tokens/public_api.ts
+++ libs/core/src/injection-tokens/public_api.ts
@@ -4,3 +4,5 @@ export * from './multi.type';
 export * from './multi.factory';
 export * from './config.type';
 export * from './config.factory';
+export * from './services.type';
+export * from './services.factory';
