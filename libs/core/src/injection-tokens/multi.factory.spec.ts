a82681f9fbb65894bf2272bb4d8a6a49dba3b9f7
Peter Lauck <griest024@gmail.com>
2024-02-23 05:17:22 +0000

    add default factory for multi token

--- libs/core/src/injection-tokens/multi.factory.spec.ts
+++ libs/core/src/injection-tokens/multi.factory.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { faker } from '@faker-js/faker/locale/en_US';
 
 import { DaffMultiInjectionToken } from '@daffodil/core';
@@ -28,6 +29,43 @@ describe('@daffodil/core | createMultiInjectionToken', () => {
     values.forEach((value, i) => {
       expect(res[i].provide).toEqual(result.token);
       expect(res[i].useValue).toEqual(value);
+    });
+  });
+});
+
+describe('@daffodil/core | createMultiInjectionToken | Integration', () => {
+  let name: string;
+  let values: Array<number>;
+
+  let result: DaffMultiInjectionToken<number>;
+
+  beforeEach(() => {
+    name = faker.random.word();
+    values = [
+      faker.datatype.number(),
+      faker.datatype.number(),
+    ];
+
+    result = createMultiInjectionToken(name);
+  });
+
+  describe('when values are provided', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          result.provider(...values),
+        ],
+      });
+    });
+
+    it('should inject the values', () => {
+      expect(TestBed.inject(result.token)).toEqual(values);
+    });
+  });
+
+  describe('when values are not provided', () => {
+    it('should inject an empty array', () => {
+      expect(TestBed.inject(result.token)).toEqual([]);
     });
   });
 });
