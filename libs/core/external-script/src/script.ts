2f2bd063c30ac315630294652af7e0caca116c68
Damien Retzinger <damienwebdev@gmail.com>
2024-04-23 11:53:33 -0400

    document type

--- libs/core/external-script/src/script.ts
+++ libs/core/external-script/src/script.ts
@@ -1,6 +1,36 @@
+/**
+ * Represents the structure of an external script that can be loaded into a document.
+ *
+ * ```ts
+ * // Example of using DaffExternalScript interface to define an external script object.
+ *
+ * const externalScript: DaffExternalScript = {
+ *  src: 'https://example.com/script.js',
+ *  async: true,
+ *  defer: false,
+ *  'data-custom-attribute': 'value',
+ * };
+ * ```
+ */
 export interface DaffExternalScript {
+  /** The source URL of the script. */
   src: string;
+
+  /**
+   * Optional. Indicates whether the script should be loaded asynchronously.
+   * @default false
+   */
   async?: boolean;
+
+  /**
+   * Optional. Indicates whether the script should be deferred in loading.
+   * @default false
+   */
   defer?: boolean;
+
+  /**
+   * Optional. Custom attributes for the script.
+   * The keys must start with 'data-' followed by any string.
+   */
   [key: `data-${string}`]: string;
 }
