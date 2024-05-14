1b2763d82a88e19769c952ea59ce24682cba9ab2
Peter Lauck <griest024@gmail.com>
2024-05-14 17:50:04 +0000

    docs

--- libs/cart/src/helpers/get-item-total-discount.ts
+++ libs/cart/src/helpers/get-item-total-discount.ts
@@ -2,6 +2,9 @@ import { daffAdd } from '@daffodil/core';
 
 import { DaffCartItem } from '../models/public_api';
 
+/**
+ * Sums the discount amounts of the cart item.
+ */
 export function daffCartGetItemTotalDiscount(item: DaffCartItem): number {
   return item?.discounts?.reduce((acc, { amount }) => daffAdd(acc, amount), 0) || 0;
 }
