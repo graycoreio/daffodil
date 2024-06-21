44d16d7451337653154b05b68d973bcab3f2829f
griest024 <griest024@gmail.com>
2024-06-21 14:18:24 -0400

    fix(navgiation): deep fragment fields might not get data (#2889)
    this is a workaround of https://github.com/magento/magento2/issues/38863

--- libs/navigation/driver/magento/src/queries/fragments/category-node/category-node.ts
+++ libs/navigation/driver/magento/src/queries/fragments/category-node/category-node.ts
@@ -6,42 +6,43 @@ import {
   daffBuildFragmentNameSpread,
 } from '@daffodil/core/graphql';
 
-
-const CATEGORY_NODE_FRAGMENT_NAME = 'categoryNode';
+/**
+ * A category tree fragment with no nested children.
+ */
+const categoryNodeFragment = `
+	uid
+  url_path
+  url_suffix
+	level
+	name
+	include_in_menu
+	breadcrumbs {
+		category_uid
+		category_name
+		category_level
+		category_url_path
+	}
+	position
+	product_count
+`;
 
 /**
  * Generates a category tree fragment with the specified number of nested child category trees.
  *
  * @param depth The maximum depth to which category children should be added to the fragment.
  */
+//todo: use nested fragments when this bug is fixed: https://github.com/magento/magento2/issues/31086
 export function getCategoryNodeFragment(depth: number = 3, extraFragments: Array<DocumentNode> = []): DocumentNode {
   const fragmentBody = new Array(depth).fill(null).reduce(acc => `
-    ...${CATEGORY_NODE_FRAGMENT_NAME}
+    ${categoryNodeFragment}
     children_count
     children {
       ${acc}
     }
-  `, `...${CATEGORY_NODE_FRAGMENT_NAME}`);
+    ${daffBuildFragmentNameSpread(...extraFragments)}
+  `, categoryNodeFragment);
 
   return gql`
-    fragment ${CATEGORY_NODE_FRAGMENT_NAME} on CategoryTree {
-      uid
-      url_path
-      url_suffix
-      level
-      name
-      include_in_menu
-      breadcrumbs {
-        category_uid
-        category_name
-        category_level
-        category_url_path
-      }
-      position
-      product_count
-      ${daffBuildFragmentNameSpread(...extraFragments)}
-    }
-
     fragment recursiveCategoryNode on CategoryTree {
       ${fragmentBody}
     }
