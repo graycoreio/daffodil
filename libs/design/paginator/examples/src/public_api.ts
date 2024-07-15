d16b19ccfa3bf0156bac1101b37067982b62fb51
Peter Lauck <griest024@gmail.com>
2024-07-15 19:26:42 +0000

    remove duplicates?

--- libs/design/paginator/examples/src/public_api.ts
+++ libs/design/paginator/examples/src/public_api.ts
@@ -1,4 +1,1 @@
-export { BasicPaginatorComponent } from './basic-paginator/basic-paginator.component';
-export { LinkPaginatorComponent } from './link-paginator/link-paginator.component';
-
 export * from './examples';
