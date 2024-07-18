ab2520c734a79ab574656a74cbcaa7c6ea18ff8e
Peter Lauck <griest024@gmail.com>
2024-07-18 13:44:45 +0000

    fix test

--- libs/documentation/testing/src/factories/palette-color.factory.ts
+++ libs/documentation/testing/src/factories/palette-color.factory.ts
@@ -15,7 +15,7 @@ export class DaffDocsPaletteColorFactory extends DaffModelFactory<DaffDocsPalett
   create(partial: Partial<DaffDocsPaletteColor> = {}): DaffDocsPaletteColor {
     return {
       ...Array(faker.datatype.number({ min: 1, max: 10 })).fill(0).reduce<DaffDocsPaletteColor>((acc) => {
-        acc[String(faker.datatype.number({ min: 1, max: 10 }) * 10)] = `#${faker.datatype.hexadecimal({ length: 6 })}`;
+        acc[String(faker.datatype.number({ min: 1, max: 10 }) * 10)] = faker.datatype.hexadecimal({ length: 6, prefix: '#' });
         return acc;
       }, {}),
       ...partial,
