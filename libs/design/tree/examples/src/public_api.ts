40ee05dae55f5e24d7ab4646f8c5c17c13231cfa
Damien Retzinger <damienwebdev@gmail.com>
2024-07-19 11:43:28 -0400

    chore(design): make tree examples standalone (#2924)

--- libs/design/tree/examples/src/public_api.ts
+++ libs/design/tree/examples/src/public_api.ts
@@ -1,6 +1,7 @@
 import { BasicTreeComponent } from './basic-tree/basic-tree.component';
-export { BasicTreeModule } from './basic-tree/basic-tree.module';
+
 export { BasicTreeComponent  };
+
 export const TREE_EXAMPLES = [
   BasicTreeComponent,
 ];
