cf36e6ff4e25a072b558862fc7a63cfee74f7c79
Damien Retzinger <damienwebdev@gmail.com>
2024-07-19 11:18:55 -0400

    chore(design): make image examples standalone (#2924)

--- libs/design/image/examples/src/public_api.ts
+++ libs/design/image/examples/src/public_api.ts
@@ -1,14 +1,9 @@
-import { ComponentExample } from '@daffodil/design';
-
 import { BasicImageComponent } from './basic-image/basic-image.component';
-import { BasicImageModule } from './basic-image/basic-image.module';
 import { LoadImageComponent } from './load-image/load-image.component';
-import { LoadImageModule } from './load-image/load-image.module';
 import { SkeletonImageComponent } from './skeleton-image/skeleton-image.component';
-import { SkeletonImageModule } from './skeleton-image/skeleton-image.module';
 
-export const IMAGE_EXAMPLES: ComponentExample[] = [
-  { component: BasicImageComponent, module: BasicImageModule },
-  { component: LoadImageComponent, module: LoadImageModule },
-  { component: SkeletonImageComponent, module: SkeletonImageModule },
+export const IMAGE_EXAMPLES = [
+  BasicImageComponent,
+  LoadImageComponent,
+  SkeletonImageComponent,
 ];
