97cd2aae1fff2183cc378e524d4f9ec5ff5309d2
Damien Retzinger <damienwebdev@gmail.com>
2024-07-19 11:32:15 -0400

    chore(design): make navbar examples standalone (#2924)

--- libs/design/navbar/examples/src/public_api.ts
+++ libs/design/navbar/examples/src/public_api.ts
@@ -1,9 +1,5 @@
 export { BasicNavbarComponent } from './basic-navbar/basic-navbar.component';
-export { BasicNavbarModule } from './basic-navbar/basic-navbar.module';
 export { ContainedNavbarComponent } from './contained-navbar/contained-navbar.component';
-export { ContainedNavbarModule } from './contained-navbar/contained-navbar.module';
 export { NavbarThemingComponent } from './navbar-theming/navbar-theming.component';
-export { NavbarThemingModule } from './navbar-theming/navbar-theming.module';
 export { RaisedNavbarComponent } from './raised-navbar/raised-navbar.component';
-export { RaisedNavbarModule } from './raised-navbar/raised-navbar.module';
 export { NAVBAR_EXAMPLES } from './examples';
--- libs/design/navbar/examples/src/raised-navbar/raised-navbar.component.ts
+++ libs/design/navbar/examples/src/raised-navbar/raised-navbar.component.ts
@@ -3,11 +3,16 @@ import {
   Component,
 } from '@angular/core';
 
+import { DaffButtonModule } from '@daffodil/design/button';
+import { DaffNavbarModule } from '@daffodil/design/navbar';
+
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'raised-navbar',
   templateUrl: './raised-navbar.component.html',
   styleUrls: ['./raised-navbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [DaffNavbarModule, DaffButtonModule],
 })
 export class RaisedNavbarComponent {}
