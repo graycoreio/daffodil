1348c71a6585f428e17e3b171d5891cc6b3b130e
Damien Retzinger <damienwebdev@gmail.com>
2024-07-19 11:40:03 -0400

    chore(design): make sidebar examples standalone (#2924)

--- libs/design/sidebar/examples/src/over-and-under-sidebars/over-and-under-sidebars.component.ts
+++ libs/design/sidebar/examples/src/over-and-under-sidebars/over-and-under-sidebars.component.ts
@@ -2,8 +2,15 @@ import {
   ChangeDetectionStrategy,
   Component,
 } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {
+  FormControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import { DaffButtonModule } from '@daffodil/design/button';
+import { DaffSidebarModule } from '@daffodil/design/sidebar';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -11,6 +18,13 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './over-and-under-sidebars.component.html',
   styleUrls: ['over-and-under-sidebars.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    DaffSidebarModule,
+    DaffButtonModule,
+    FaIconComponent,
+    ReactiveFormsModule,
+  ],
 })
 export class OverandUnderSidebarsComponent {
   faTimes = faTimes;
--- libs/design/sidebar/examples/src/side-fixed-sidebar/side-fixed-sidebar.component.ts
+++ libs/design/sidebar/examples/src/side-fixed-sidebar/side-fixed-sidebar.component.ts
@@ -3,11 +3,16 @@ import {
   Component,
 } from '@angular/core';
 
+import { DaffNavbarModule } from '@daffodil/design/navbar';
+import { DaffSidebarModule } from '@daffodil/design/sidebar';
+
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'side-fixed-sidebar',
   templateUrl: './side-fixed-sidebar.component.html',
   styleUrls: ['side-fixed-sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [DaffSidebarModule, DaffNavbarModule],
 })
 export class SideFixedSidebarComponent {}
