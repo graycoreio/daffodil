5dc636b68cb789fd1702c67be652b625670be2a9
Damien Retzinger <damienwebdev@gmail.com>
2024-07-19 11:14:31 -0400

    chore(design): make checkbox examples standalone (#2924)

--- libs/design/checkbox/examples/src/examples.ts
+++ libs/design/checkbox/examples/src/examples.ts
@@ -1,15 +1,8 @@
 import { BasicCheckboxComponent } from './basic-checkbox/basic-checkbox.component';
-import { BasicCheckboxModule } from './basic-checkbox/basic-checkbox.module';
 import { CheckboxSetComponent } from './checkbox-set/checkbox-set.component';
-import { CheckboxSetModule } from './checkbox-set/checkbox-set.module';
 
 
 export const CHECKBOX_EXAMPLES = [
   CheckboxSetComponent,
   BasicCheckboxComponent,
-];
-
-export const CHECKBOX_EXAMPLES_MODULES = [
-  BasicCheckboxModule,
-  CheckboxSetModule,
 ];
--- libs/design/checkbox/examples/src/public_api.ts
+++ libs/design/checkbox/examples/src/public_api.ts
@@ -1,3 +1,1 @@
 export { CHECKBOX_EXAMPLES } from './examples';
-
-export { CHECKBOX_EXAMPLES_MODULES } from './examples';
