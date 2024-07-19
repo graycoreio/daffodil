698a055f137749fc1df0919bc726d4c302a12286
Damien Retzinger <damienwebdev@gmail.com>
2024-07-19 11:38:49 -0400

    chore(design): make radio examples standalone (#2924)

--- libs/design/radio/examples/src/examples.ts
+++ libs/design/radio/examples/src/examples.ts
@@ -1,10 +1,5 @@
 import { BasicRadioComponent } from './basic-radio/basic-radio.component';
-import { BasicRadioModule } from './basic-radio/basic-radio.module';
 
 export const RADIO_EXAMPLES = [
   BasicRadioComponent,
-];
-
-export const RADIO_EXAMPLES_MODULES = [
-  BasicRadioModule,
 ];
