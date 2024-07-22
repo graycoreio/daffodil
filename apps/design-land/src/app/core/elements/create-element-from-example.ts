f52244f50fc0d68d55597732b8784b32a5e565c1
Damien Retzinger <damienwebdev@gmail.com>
2024-07-22 09:28:43 -0400

    fix(design): update createCustomElementFromExample for API break (#2924)

--- apps/design-land/src/app/core/elements/create-element-from-example.ts
+++ apps/design-land/src/app/core/elements/create-element-from-example.ts
@@ -1,26 +1,15 @@
 import {
-  ɵcreateInjector,
   Injector,
+  Type,
 } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
-
-import { ComponentExample } from '@daffodil/design';
 
 /**
  * Creates a custom element from a Component Example to be used to render complex
  * component examples in the docs.
  */
-export const createCustomElementFromExample = (example: ComponentExample, injector: Injector) => {
-  if(typeof example === 'function'){
-    return {
-      element: createCustomElement(example, { injector }),
-      class: example,
-    };
-  } else {
-    return {
-      element: createCustomElement(example.component, { injector: ɵcreateInjector(example.module, injector) }),
-      class: example.component,
-    };
-  }
-};
+export const createCustomElementFromExample = (example: Type<unknown>, injector: Injector) => ({
+  element: createCustomElement(example, { injector }),
+  class: example,
+});
 
