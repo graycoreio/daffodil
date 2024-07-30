6a2c9478806d8a996837dac3f467d40ca29f8163
Damien Retzinger <damienwebdev@gmail.com>
2024-07-30 11:31:31 -0400

    docs(design): document article encapsulation

--- libs/design/src/core/article-encapsulated/article-encapsulated.directive.ts
+++ libs/design/src/core/article-encapsulated/article-encapsulated.directive.ts
@@ -3,11 +3,23 @@ import {
   HostBinding,
 } from '@angular/core';
 
+/**
+ * The `DaffArticleEncapsulatedDirective` is used to encapsulate custom components
+ * within an article, preventing article styles from bleeding into the component.
+ *
+ * ## Example
+ *
+ * ```html
+ * <my-custom-component daffArticleEncapsulated></my-custom-component>
+ * ```
+ *
+ * This directive will apply the `daff-ae` class to your component, ensuring that it is encapsulated
+ * from the article's styles.
+ */
 @Directive({
   selector: '[daffArticleEncapsulated]',
   standalone: true,
 })
-
 export class DaffArticleEncapsulatedDirective {
   @HostBinding('class.daff-ae') class = true;
 }
