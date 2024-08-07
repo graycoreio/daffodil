961512dc5b028fdffe8d61dcf8b70e2a242c857e
griest024 <griest024@gmail.com>
2024-08-07 11:21:45 -0400

    fix(external-router): extract daff paths ignoring full path (#2955)

--- libs/external-router/src/util/extract-daff-path-data.ts
+++ libs/external-router/src/util/extract-daff-path-data.ts
@@ -8,6 +8,6 @@ import {
  * RouteSnapshot.
  */
 export const daffExtractDaffPathData = <T extends Data = Data, K extends keyof T = keyof T>(snapshot: ActivatedRouteSnapshot, key: K): T[K] => {
-  const pathFromRoot = snapshot.pathFromRoot[snapshot.pathFromRoot.length - 1].url.map((seg) => seg.path).join('/');
+  const pathFromRoot = snapshot.pathFromRoot.flatMap((route) => route.url).map((seg) => seg.path).join('/');
   return snapshot.data?.daffPaths?.[pathFromRoot]?.[key] ?? null;
 };
