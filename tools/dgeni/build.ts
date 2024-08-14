975307283f3dd739032202db412fa43df4506fb0
Peter Lauck <griest024@gmail.com>
2024-08-14 18:13:46 +0000

    feat(dgeni): run design packages (#2968)

--- tools/dgeni/build.ts
+++ tools/dgeni/build.ts
@@ -1,19 +1,34 @@
 import { Dgeni } from 'dgeni';
 import { rimraf } from 'rimraf';
 
-import { apiDocs } from './src/transforms/daffodil-api-package';
+import {
+  apiDocs,
+  designApiPackage,
+} from './src/transforms/daffodil-api-package';
 import { designExamplePackage } from './src/transforms/daffodil-design-examples-package';
 import {
   packageDocsPackage,
   guideDocsPackage,
   explanationDocsPackage,
+  designDocsPackage,
 } from './src/transforms/daffodil-guides-package';
 
 rimraf('../../dist/docs/*', { glob: true }).then(() => {
   new Dgeni([apiDocs]).generate().then(() => {
-    new Dgeni([packageDocsPackage]).generate().catch(() => process.exit(1));
-    new Dgeni([guideDocsPackage]).generate().catch(() => process.exit(1));
-    new Dgeni([explanationDocsPackage]).generate().catch(() => process.exit(1));
-    new Dgeni([designExamplePackage]).generate().catch(() => process.exit(1));
+    // base docs
+    Promise.all([
+      new Dgeni([packageDocsPackage]).generate().catch(() => process.exit(1)),
+      new Dgeni([guideDocsPackage]).generate().catch(() => process.exit(1)),
+      new Dgeni([explanationDocsPackage]).generate().catch(() => process.exit(1)),
+    ]).then(() => {
+      // design docs
+      // run them after base docs so that config between shared processors does not conflict
+      // a design flaw of dgeni, it wasn't meant to be run in parallel
+      new Dgeni([designApiPackage]).generate().then(() => {
+        new Dgeni([designDocsPackage]).generate().catch(() => process.exit(1));
+        new Dgeni([designExamplePackage]).generate().catch(() => process.exit(1));
+      }).catch(() => process.exit(1));
+    }).catch(() => process.exit(1));
+
   }).catch(() => process.exit(1));
 });
