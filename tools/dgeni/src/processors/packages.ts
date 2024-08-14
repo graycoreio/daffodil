f7f792343e6f2ff9a5f6df333c3b42bc22ee4844
Peter Lauck <griest024@gmail.com>
2024-08-14 18:13:42 +0000

    feat(dgeni): add configable name computer to packages processor (#2968)

--- tools/dgeni/src/processors/packages.ts
+++ tools/dgeni/src/processors/packages.ts
@@ -7,16 +7,18 @@ export class PackagesProcessor implements Processor {
   name = 'packages';
   $runBefore = ['computing-ids'];
   docTypes = [];
+  nameComputer = (id: string): string => `@daffodil/${id}`;
 
   $process(docs: Document[]): Document[] {
     return docs.map(doc => {
       if (doc.docType === 'module') {
         doc.id = doc.id
-          .replace(/\/src$/, '')
+          .replace('src/', '')
+          .replace('/src', '')
           .replace(/^.*libs\//, '');
         doc.docType = 'package';
         // The name is actually the full id
-        doc.name = `@daffodil/${doc.id}`;
+        doc.name = this.nameComputer(doc.id);
       }
       return doc;
     });
