995241ae6d561ff752b02cfbd082895de814fbca
Peter Lauck <griest024@gmail.com>
2024-08-14 18:13:40 +0000

    feat(docs-utils): add design path (#2968)

--- libs/docs-utils/src/path.ts
+++ libs/docs-utils/src/path.ts
@@ -2,3 +2,8 @@
  * The path segment (both for filesystem and routing) under which the docs will be stored.
  */
 export const DAFF_DOCS_PATH = 'docs';
+
+/**
+ * The path segment (both for filesystem and routing) under which the design docs will be stored.
+ */
+export const DAFF_DOCS_DESIGN_PATH = 'design';
