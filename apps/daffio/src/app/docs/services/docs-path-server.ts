edb805d11b4359d1ba654c823498461dae99d450
Peter Lauck <griest024@gmail.com>
2024-08-14 18:04:42 +0000

    fix(daffio): unable to resolve assets in dev server

--- apps/daffio/src/app/docs/services/docs-path-server.ts
+++ apps/daffio/src/app/docs/services/docs-path-server.ts
@@ -1,5 +1,6 @@
 import { Provider } from '@angular/core';
-import { join } from 'path';
+import { config } from 'dotenv';
+import { resolve } from 'path';
 
 import { DAFFIO_DOCS_PATH_TOKEN } from './docs-path.token';
 import { environment } from '../../../environments/environment';
@@ -7,7 +8,12 @@ import { environment } from '../../../environments/environment';
 /**
  * The path on the server to the docs folder.
  */
-const daffioDocsPathServerFactory = () => join(process.cwd(), environment.docsPath);
+const daffioDocsPathServerFactory = () => {
+  const { error, parsed } = config();
+  // the SSR runs from the daffio directory and doesn't use dotenv so we rely on cwd
+  // devs should have the DAFF_ROOT env var set in their .env
+  return resolve(error || !parsed.DAFF_ROOT ? process.cwd() : resolve(parsed.DAFF_ROOT, 'apps/daffio'), environment.docsPath);
+};
 
 /**
  * A provider for the docs path for the server.
