c0d00896d607529bda944aecb3937e5f72c9f497
Peter Lauck <griest024@gmail.com>
2024-08-15 17:10:51 +0000

    add docs

--- apps/daffio/src/app/core/router/route.type.ts
+++ apps/daffio/src/app/core/router/route.type.ts
@@ -3,6 +3,9 @@ import { Route } from '@angular/router';
 import { DaffDocKind } from '@daffodil/docs-utils';
 import { DaffRouteWithNamedViews } from '@daffodil/router';
 
+/**
+ * A route config that possibly contains different kinds of data used in the application.
+ */
 export interface DaffioRoute extends Route {
   data?: Route['data']
   & DaffRouteWithNamedViews['data']
