83d4b2cb18dd2902858bd7e60b13e2fe1886a551
Peter Lauck <griest024@gmail.com>
2024-08-15 17:09:05 +0000

    make data optional to align with `Route` typing

--- apps/daffio/src/app/core/router/route.type.ts
+++ apps/daffio/src/app/core/router/route.type.ts
@@ -4,7 +4,7 @@ import { DaffDocKind } from '@daffodil/docs-utils';
 import { DaffRouteWithNamedViews } from '@daffodil/router';
 
 export interface DaffioRoute extends Route {
-  data: Route['data']
+  data?: Route['data']
   & DaffRouteWithNamedViews['data']
   & {
     docKind?: DaffDocKind;
