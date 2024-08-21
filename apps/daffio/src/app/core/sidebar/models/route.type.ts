525d77450d665285b93d6e18d2181b1ec67829ff
Peter Lauck <griest024@gmail.com>
2024-08-21 18:51:26 +0000

    feat(daffio): add `DaffioRouteWithSidebars`

---
+++ apps/daffio/src/app/core/sidebar/models/route.type.ts
@@ -0,0 +1,20 @@
+import { Route } from '@angular/router';
+
+import {
+  DaffSidebarModeEnum,
+  DaffSidebarRegistration,
+} from '@daffodil/design/sidebar';
+
+export interface DaffioRouteWithSidebars extends Route {
+  data: Route['data'] & {
+    /**
+     * A collection of sidebars available on the current page.
+     */
+    daffioSidebars?: Record<DaffSidebarRegistration['id'], DaffSidebarRegistration>;
+    /**
+     * The sidebar that should be shown automatically (if any) when the viewport enters big tablet.
+     */
+    daffioDockedSidebar?: DaffSidebarRegistration['id'];
+    sidebarMode?: DaffSidebarModeEnum;
+  };
+}
