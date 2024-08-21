22deda7773e0d280771031bdf3b55e106e688661
Peter Lauck <griest024@gmail.com>
2024-08-21 18:51:27 +0000

    feat(daffio): add sidebar registrations

---
+++ apps/daffio/src/app/docs/containers/docs-list/sidebar.provider.ts
@@ -0,0 +1,12 @@
+import { DaffioDocsListContainer } from './docs-list.component';
+import { DaffioSidebarFooterComponent } from '../../../core/sidebar/components/sidebar-footer/sidebar-footer.component';
+import { DaffioSidebarHeaderComponent } from '../../../core/sidebar/components/sidebar-header/sidebar-header.component';
+
+export const DAFFIO_DOCS_LIST_SIDEBAR_ID = 'daffioDocsList';
+
+export const DAFFIO_DOCS_LIST_SIDEBAR_REGISTRATION = {
+  id: DAFFIO_DOCS_LIST_SIDEBAR_ID,
+  header: DaffioSidebarHeaderComponent,
+  body: DaffioDocsListContainer,
+  footer: DaffioSidebarFooterComponent,
+};
