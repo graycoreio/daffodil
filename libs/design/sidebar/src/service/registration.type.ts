1025f730526dfffed37071ea258cbfb61d6c4780
Peter Lauck <griest024@gmail.com>
2024-08-22 22:12:54 +0000

    more docs

--- libs/design/sidebar/src/service/registration.type.ts
+++ libs/design/sidebar/src/service/registration.type.ts
@@ -1,5 +1,9 @@
 import { Type } from '@angular/core';
 
+/**
+ * Represents a registration of a sidebar.
+ * A collection of sidebar components is associated with an ID which can be passed to {@link DaffSidebarService#open}.
+ */
 export interface DaffSidebarRegistration {
   id: string;
   header?: Type<unknown>;
