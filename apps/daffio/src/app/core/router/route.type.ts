2960d51af827b6ea15dd2ecd235834852896b8ad
griest024 <griest024@gmail.com>
2024-08-22 19:14:09 -0400

    feat(daffio): add `DaffioRouteWithSidebars` (#3025)

--- apps/daffio/src/app/core/router/route.type.ts
+++ apps/daffio/src/app/core/router/route.type.ts
@@ -4,6 +4,7 @@ import { DaffDocKind } from '@daffodil/docs-utils';
 import { DaffRouteWithNamedViews } from '@daffodil/router';
 
 import { DaffioRouteWithNavLinks } from '../nav/link/route.type';
+import { DaffioRouteWithSidebars } from '../sidebar/models/route.type';
 
 /**
  * A route config that possibly contains different kinds of data used in the application.
@@ -12,6 +13,7 @@ export interface DaffioRoute extends Route {
   data?: Route['data']
   & DaffRouteWithNamedViews['data']
   & DaffioRouteWithNavLinks['data']
+  & DaffioRouteWithSidebars['data']
   & {
     docKind?: DaffDocKind;
   };
