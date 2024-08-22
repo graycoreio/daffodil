882e5af4fbaae1fffc5dbe8e207bb85de1fc7b97
Peter Lauck <griest024@gmail.com>
2024-08-22 21:55:28 +0000

    docs

--- apps/daffio/src/app/core/nav/link/route.type.ts
+++ apps/daffio/src/app/core/nav/link/route.type.ts
@@ -2,8 +2,12 @@ import { Route } from '@angular/router';
 
 import { DaffioNavLink } from './type';
 
+/**
+ * A route that possibly contains nav links.
+ * These links are typically rendered in the header and nav sidebar.
+ */
 export interface DaffioRouteWithNavLinks extends Route {
-  data: Route['data'] & {
+  data?: Route['data'] & {
     daffioNavLinks?: Array<DaffioNavLink>;
   };
 }
