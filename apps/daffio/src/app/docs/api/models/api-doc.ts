81fb2d0837d696befbaaee4b59d2283fe6eea7d4
Peter Lauck <griest024@gmail.com>
2024-08-23 23:07:02 +0000

    feat(daffio): render API package descriptions

--- apps/daffio/src/app/docs/api/models/api-doc.ts
+++ apps/daffio/src/app/docs/api/models/api-doc.ts
@@ -4,6 +4,7 @@ import { DaffioDoc } from '../../models/doc';
  * An object for an API document.
  */
 export interface DaffioApiDoc extends DaffioDoc {
+  description: string;
   docType: string;
   docTypeShorthand: string;
 }
