93801fcdec3e77f63b301c25d2e6abe434afb06d
Peter Lauck <griest024@gmail.com>
2024-08-27 13:54:58 +0000

    build(dgeni): log runtime errors

--- tools/dgeni/build.ts
+++ tools/dgeni/build.ts
@@ -17,18 +17,42 @@ rimraf('../../dist/docs/*', { glob: true }).then(() => {
   new Dgeni([apiDocs]).generate().then(() => {
     // base docs
     Promise.all([
-      new Dgeni([packageDocsPackage]).generate().catch(() => process.exit(1)),
-      new Dgeni([guideDocsPackage]).generate().catch(() => process.exit(1)),
-      new Dgeni([explanationDocsPackage]).generate().catch(() => process.exit(1)),
+      new Dgeni([packageDocsPackage]).generate().catch((err) => {
+        console.log(err);
+        process.exit(1);
+      }),
+      new Dgeni([guideDocsPackage]).generate().catch((err) => {
+        console.log(err);
+        process.exit(1);
+      }),
+      new Dgeni([explanationDocsPackage]).generate().catch((err) => {
+        console.log(err);
+        process.exit(1);
+      }),
     ]).then(() => {
       // design docs
       // run them after base docs so that config between shared processors does not conflict
       // a design flaw of dgeni, it wasn't meant to be run in parallel
       new Dgeni([designApiPackage]).generate().then(() => {
-        new Dgeni([designDocsPackage]).generate().catch(() => process.exit(1));
-        new Dgeni([designExamplePackage]).generate().catch(() => process.exit(1));
-      }).catch(() => process.exit(1));
-    }).catch(() => process.exit(1));
+        new Dgeni([designDocsPackage]).generate().catch((err) => {
+          console.log(err);
+          process.exit(1);
+        });
+        new Dgeni([designExamplePackage]).generate().catch((err) => {
+          console.log(err);
+          process.exit(1);
+        });
+      }).catch((err) => {
+        console.log(err);
+        process.exit(1);
+      });
+    }).catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
 
-  }).catch(() => process.exit(1));
+  }).catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 });
