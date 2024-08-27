69348f1916671b9c31dd7cccdb9a7732077dabbc
griest024 <griest024@gmail.com>
2024-08-27 13:19:07 -0400

    refactor(core): specify function params in single token factory (#3034)

--- libs/core/src/injection-tokens/single.factory.ts
+++ libs/core/src/injection-tokens/single.factory.ts
@@ -1,14 +1,21 @@
 import { InjectionToken } from '@angular/core';
 
 import { DaffSingleInjectionToken } from './single.type';
+import {
+  TokenDesc,
+  TokenOptions,
+} from './token-constuctor-params.type';
 
 /**
  * Creates an injection token/provider pair for a single valued DI token.
  *
  * See {@link DaffSingleInjectionToken}.
  */
-export const createSingleInjectionToken = <T = unknown>(...args: ConstructorParameters<typeof InjectionToken<T>>): DaffSingleInjectionToken<T> => {
-  const token = new InjectionToken<T>(...args);
+export const createSingleInjectionToken = <T = unknown>(
+  desc: TokenDesc<T>,
+  options?: TokenOptions<T>,
+): DaffSingleInjectionToken<T> => {
+  const token = new InjectionToken<T>(desc, options);
   const provider = <R extends T = T>(value: R) => ({
     provide: token,
     useValue: value,
