fa6484f07c5c9c877315b73d266cad74d571da98
Peter Lauck <griest024@gmail.com>
2024-09-16 17:28:05 +0000

    feat(design): add deep tree example

--- libs/design/tree/examples/src/public_api.ts
+++ libs/design/tree/examples/src/public_api.ts
@@ -1,7 +1,12 @@
 import { BasicTreeComponent } from './basic-tree/basic-tree.component';
+import { DeepTreeComponent } from './deep-tree/deep-tree.component';
 
-export { BasicTreeComponent  };
+export {
+  BasicTreeComponent,
+  DeepTreeComponent,
+};
 
 export const TREE_EXAMPLES = [
   BasicTreeComponent,
+  DeepTreeComponent,
 ];
