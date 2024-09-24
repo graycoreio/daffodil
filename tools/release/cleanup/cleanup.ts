1cff70a957d53ed255a91f6c9dd8566247d2c041
Peter Lauck <griest024@gmail.com>
2024-09-24 15:28:15 +0000

    ci: fix release please step

--- tools/release/cleanup/cleanup.ts
+++ tools/release/cleanup/cleanup.ts
@@ -1,5 +1,5 @@
 import { series } from 'gulp';
-import git from 'simple-git/promise';
+import git from 'simple-git';
 
 import { ARGS } from '../args/args';
 import { ARG as GIT_REMOTE_ARG } from '../args/git-remote';
@@ -7,7 +7,7 @@ import { RELEASE_CONFIG } from '../config';
 import { getGitRemote } from '../get-git-remote';
 
 const removeTemporaryBranch = async () => {
-  const repo = await git(RELEASE_CONFIG.PROJECT_PATH);
+  const repo = git(RELEASE_CONFIG.PROJECT_PATH);
   const remote = getGitRemote(ARGS[GIT_REMOTE_ARG.key]);
   await repo.checkout(remote + '/' + RELEASE_CONFIG.BASE_BRANCH);
   await repo.branch([
--- tools/release/publish/publish.ts
+++ tools/release/publish/publish.ts
@@ -1,5 +1,5 @@
 import { series } from 'gulp';
-import git from 'simple-git/promise';
+import git from 'simple-git';
 
 import { ARGS } from '../args/args';
 import { ARG as GIT_REMOTE_ARG } from '../args/git-remote';
@@ -9,8 +9,8 @@ import { getGitRemote } from '../get-git-remote';
 export const pushToRemote = async () => {
   const repo = await git(RELEASE_CONFIG.PROJECT_PATH);
   const remote = getGitRemote(ARGS[GIT_REMOTE_ARG.key]);
-  await repo.push([remote, 'HEAD:' + RELEASE_CONFIG.BASE_BRANCH] as any);
-  await repo.push([remote, 'HEAD:master'] as any);
+  await repo.push([remote, 'HEAD:' + RELEASE_CONFIG.BASE_BRANCH]);
+  await repo.push([remote, 'HEAD:master']);
   await repo.pushTags();
 };
 
