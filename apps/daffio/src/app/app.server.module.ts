bfbc3022af680d6fd2808edc303e9b7de2910655
Peter Lauck <griest024@gmail.com>
2024-09-26 19:48:46 +0000

    fix(daffio): legacy `ServerModule` imported

--- apps/daffio/src/app/app.server.module.ts
+++ apps/daffio/src/app/app.server.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { provideServerRendering } from '@angular/platform-server';
 
 import { DaffioAppComponent } from './app.component';
 import { AppModule } from './app.module';
@@ -10,10 +10,10 @@ import { provideServerDocsPath } from './docs/services/docs-path-server';
 @NgModule({
   imports: [
     AppModule,
-    ServerModule,
   ],
   bootstrap: [DaffioAppComponent],
   providers: [
+    provideServerRendering(),
     provideServerDocsPath(),
     {
       provide: DaffioAssetFetchService,
