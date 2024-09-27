361d07a8cf932e53e1d1f943f4241ed4d59209b9
xelaint <xelaint@gmail.com>
2024-09-27 08:52:15 -0400

    add tests

--- apps/daffio/src/app/core/header/components/header-item/header-item.directive.spec.ts
+++ apps/daffio/src/app/core/header/components/header-item/header-item.directive.spec.ts
@@ -1,25 +1,34 @@
-import { Component } from '@angular/core';
 import {
+  Component,
+  DebugElement,
+} from '@angular/core';
+import {
+  waitForAsync,
   ComponentFixture,
   TestBed,
-  waitForAsync,
 } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DaffioHeaderItemDirective } from './header-item.directive';
 
-@Component({ template: '<div daffioHeaderItem>Item</div>' })
+@Component({
+  template: `<a daffioHeaderItem [active]="active">Header Item</a>`,
+})
+class WrapperComponent {
+  active: boolean;
+}
 
-class WrapperComponent {}
-
-describe('DaffioHeaderItemComponent', () => {
-  let component: WrapperComponent;
+describe('DaffioHeaderItemDirective', () => {
   let fixture: ComponentFixture<WrapperComponent>;
+  let de: DebugElement;
+  let wrapper: WrapperComponent;
+  let component: DaffioHeaderItemDirective;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
-        WrapperComponent,
         DaffioHeaderItemDirective,
+        WrapperComponent,
       ],
     })
       .compileComponents();
@@ -27,12 +36,30 @@ describe('DaffioHeaderItemComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WrapperComponent);
-    component = fixture.componentInstance;
-
+    wrapper = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('[daffioHeaderItem]'));
+    component = de.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(wrapper).toBeTruthy();
+  });
+
+  it('should add a class of "daffio-header-item" to the host element', () => {
+    expect(de.classes).toEqual(jasmine.objectContaining({
+      'daffio-header-item': true,
+    }));
+  });
+
+  it('should be able to take `active` as an input', () => {
+    expect(component.active).toEqual(wrapper.active);
+  });
+
+  it('should add a class of "active" to the host element when active is true', () => {
+    wrapper.active = true;
+    fixture.detectChanges();
+
+    expect(de.classes['active']).toBeTrue();
   });
 });
