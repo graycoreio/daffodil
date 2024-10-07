b6714f41ec04923e81afa1875b6b1d12beefefc5
Peter Lauck <griest024@gmail.com>
2024-10-07 14:01:20 +0000

    docs

--- libs/driver/in-memory/src/driver/base.class.ts
+++ libs/driver/in-memory/src/driver/base.class.ts
@@ -1,6 +1,12 @@
 import { InMemoryBackendConfigArgs } from 'angular-in-memory-web-api';
 
+/**
+ * The base class for in-memory drivers.
+ */
 export abstract class DaffInMemoryDriverBase {
+  /**
+   * The URL of the corresponding backend.
+   */
   protected readonly url = `/${this.config.apiBase}/${this.collectionName}`;
 
   constructor(
