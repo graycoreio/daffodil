aa5246af82f25dff919ace3ffaea9cd7b6573deb
Peter Lauck <griest024@gmail.com>
2024-10-09 15:28:14 +0000

    feat(navigation): support in-memory backend delegate

--- libs/navigation/driver/in-memory/src/navigation.service.ts
+++ libs/navigation/driver/in-memory/src/navigation.service.ts
@@ -1,9 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InMemoryBackendConfig } from 'angular-in-memory-web-api';
 import { Observable } from 'rxjs';
 
+import { DaffInMemoryDriverBase } from '@daffodil/driver/in-memory';
 import { DaffNavigationTree } from '@daffodil/navigation';
 import { DaffNavigationServiceInterface } from '@daffodil/navigation/driver';
+
+import { DAFF_NAVIGATION_IN_MEMORY_COLLECTION_NAME } from './collection-name.const';
 
 /**
  * @inheritdoc
@@ -11,16 +15,19 @@ import { DaffNavigationServiceInterface } from '@daffodil/navigation/driver';
 @Injectable({
   providedIn: 'root',
 })
-export class DaffInMemoryNavigationService implements DaffNavigationServiceInterface<DaffNavigationTree> {
-  url = '/api/navigation/';
-
-  constructor(private http: HttpClient) {}
+export class DaffInMemoryNavigationService extends DaffInMemoryDriverBase implements DaffNavigationServiceInterface<DaffNavigationTree> {
+  constructor(
+    private http: HttpClient,
+    config: InMemoryBackendConfig,
+  ) {
+    super(config, DAFF_NAVIGATION_IN_MEMORY_COLLECTION_NAME);
+  }
 
   getTree(): Observable<DaffNavigationTree> {
     return this.http.get<DaffNavigationTree>(this.url);
   }
 
   get(navigationId: DaffNavigationTree['id']): Observable<DaffNavigationTree> {
-    return this.http.get<DaffNavigationTree>(this.url + navigationId);
+    return this.http.get<DaffNavigationTree>(`${this.url}/${navigationId}`);
   }
 }
