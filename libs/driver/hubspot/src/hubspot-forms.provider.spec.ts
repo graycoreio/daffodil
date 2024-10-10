d77e9a08f09a973d08dd898d0a5b5880373f24df
Peter Lauck <griest024@gmail.com>
2024-10-10 20:44:44 +0000

    fix lint

--- libs/driver/hubspot/src/hubspot-forms.provider.spec.ts
+++ libs/driver/hubspot/src/hubspot-forms.provider.spec.ts
@@ -10,12 +10,8 @@ import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import {
-  DaffHubspotFormsService,
-  daffHubspotFormsServiceFactory,
-} from 'libs/driver/hubspot/src/public_api';
-
-
+import { daffHubspotFormsServiceFactory } from './hubspot-forms.provider';
+import { DaffHubspotFormsService } from './hubspot-forms.service';
 
 describe('DaffHubspotForms Factory Provider', () => {
   let hubspotService: DaffHubspotFormsService;
