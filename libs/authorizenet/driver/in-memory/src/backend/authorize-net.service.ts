1fa2825a7608f85d4487a44f2aeb62bd21aef092
griest024 <griest024@gmail.com>
2024-10-10 12:08:50 -0400

    feat(authorizenet): support in-memory backend delegate (#3178)

--- libs/authorizenet/driver/in-memory/src/backend/authorize-net.service.ts
+++ libs/authorizenet/driver/in-memory/src/backend/authorize-net.service.ts
@@ -4,12 +4,17 @@ import {
   STATUS,
 } from 'angular-in-memory-web-api';
 
+import { DaffInMemorySingleRouteableBackend } from '@daffodil/driver/in-memory';
+
+import { DAFF_AUTHORIZE_NET_IN_MEMORY_COLLECTION_NAME } from '../collection-name.const';
 import { DAFF_IN_MEMORY_AUTHORIZE_NET_PAYMENT_ID } from '../drivers/authorize-net-payment-id';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DaffInMemoryBackendAuthorizenetService implements InMemoryDbService {
+export class DaffInMemoryBackendAuthorizenetService implements InMemoryDbService, DaffInMemorySingleRouteableBackend {
+  readonly collectionName = DAFF_AUTHORIZE_NET_IN_MEMORY_COLLECTION_NAME;
+
   createDb(): any {
     return {};
   }
--- libs/authorizenet/driver/in-memory/src/drivers/authorize-net.service.ts
+++ libs/authorizenet/driver/in-memory/src/drivers/authorize-net.service.ts
@@ -1,17 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InMemoryBackendConfig } from 'angular-in-memory-web-api';
 import { Observable } from 'rxjs';
 
 import { DaffAuthorizeNetTokenRequest } from '@daffodil/authorizenet';
 import { DaffAuthorizeNetService } from '@daffodil/authorizenet/driver';
+import { DaffInMemoryDriverBase } from '@daffodil/driver/in-memory';
+
+import { DAFF_AUTHORIZE_NET_IN_MEMORY_COLLECTION_NAME } from '../collection-name.const';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DaffInMemoryAuthorizeNetService implements DaffAuthorizeNetService<DaffAuthorizeNetTokenRequest> {
-  url = '/api/authorizenet';
-
-  constructor(private http: HttpClient) {}
+export class DaffInMemoryAuthorizeNetService extends DaffInMemoryDriverBase implements DaffAuthorizeNetService<DaffAuthorizeNetTokenRequest> {
+  constructor(
+    private http: HttpClient,
+    config: InMemoryBackendConfig,
+  ) {
+    super(config, DAFF_AUTHORIZE_NET_IN_MEMORY_COLLECTION_NAME);
+  }
 
   generateToken(paymentTokenRequest: DaffAuthorizeNetTokenRequest): Observable<any> {
     return this.http.post<DaffAuthorizeNetTokenRequest>(this.url + '/generateToken', paymentTokenRequest);
