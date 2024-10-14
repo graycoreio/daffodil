0cb7458450e62b8281add1e40296a05e459791ab
Peter Lauck <griest024@gmail.com>
2024-10-14 16:59:42 +0000

    feat(customer-payment-authorizenet): create injection tokens with factory

--- libs/customer-payment-authorizenet/driver/src/interfaces/payment-service.interface.ts
+++ libs/customer-payment-authorizenet/driver/src/interfaces/payment-service.interface.ts
@@ -1,14 +1,17 @@
-import { InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { createSingleInjectionToken } from '@daffodil/core';
 import { DaffCustomerPaymentAuthorizeNetApplyRequest } from '@daffodil/customer-payment-authorizenet';
 import { DaffPaymentResponse } from '@daffodil/payment';
 import { DaffPaymentDriverInterface } from '@daffodil/payment/driver';
 
-/**
- * An injection token for the customer authorize.net payment driver.
- */
-export const DaffCustomerPaymentAuthorizeNetPaymentDriver = new InjectionToken<DaffCustomerPaymentAuthorizeNetPaymentDriverInterface>('DaffCustomerPaymentAuthorizeNetPaymentDriver');
+export const {
+  /**
+   * An injection token for the customer authorize.net payment driver.
+   */
+  token: DaffCustomerPaymentAuthorizeNetPaymentDriver,
+  provider: daffProvideCustomerPaymentAuthorizeNetPaymentDriver,
+} = createSingleInjectionToken<DaffCustomerPaymentAuthorizeNetPaymentDriverInterface>('DaffCustomerPaymentAuthorizeNetPaymentDriver');
 
 /**
  * @inheritdoc
--- libs/customer-payment-authorizenet/state/src/injection-tokens/public_api.ts
+++ libs/customer-payment-authorizenet/state/src/injection-tokens/public_api.ts
@@ -1,1 +1,1 @@
-export { DAFF_CUSTOMER_PAYMENT_AUTHORIZENET_ERROR_MATCHER } from './error-matcher.token';
+export * from './error-matcher.token';
