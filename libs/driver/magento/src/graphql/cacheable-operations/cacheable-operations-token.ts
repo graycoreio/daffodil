bcbe341f4f74edb8f768c5858077a6d5890c4663
Peter Lauck <griest024@gmail.com>
2024-10-14 16:59:44 +0000

    feat(driver): create injection tokens with factory

--- libs/driver/magento/src/graphql/cacheable-operations/cacheable-operations-token.ts
+++ libs/driver/magento/src/graphql/cacheable-operations/cacheable-operations-token.ts
@@ -1,12 +1,18 @@
-import { InjectionToken } from '@angular/core';
+import { createMultiInjectionToken } from '@daffodil/core';
 
-/**
- * A multi-provider injection token of the operation names of magento queries that should
- * be cached. This is needed because queries need to be converted into GET queries in order for Magento
- * to cache them. This token should be used by daffodil driver platform modules to register operation names that are cacheable, and is
- * intended for internal daffodil use only.
- */
-export const DAFF_MAGENTO_CACHEABLE_OPERATIONS = new InjectionToken<string[]>('DAFF_MAGENTO_CACHEABLE_OPERATIONS', { factory: () => []});
+export const {
+  /**
+   * A multi-provider injection token of the operation names of magento queries that should
+   * be cached. This is needed because queries need to be converted into GET queries in order for Magento
+   * to cache them. This token should be used by daffodil driver platform modules to register operation names that are cacheable, and is
+   * intended for internal daffodil use only.
+   */
+  token: DAFF_MAGENTO_CACHEABLE_OPERATIONS,
+  /**
+   * Adds many operation names to the list of cacheable Magento operations. Use only with Angular 9+.
+   */
+  provider: provideManyDaffMagentoCacheableOperations,
+} = createMultiInjectionToken<string>('DAFF_MAGENTO_CACHEABLE_OPERATIONS');
 
 /**
  * Adds an operation name to the list of cacheable Magento operations. Use only with Angular 9+.
@@ -17,15 +23,4 @@ export function provideDaffMagentoCacheableOperation(operationName: string) {
     useValue: operationName,
     multi: true,
   };
-}
-
-/**
- * Adds many operation names to the list of cacheable Magento operations. Use only with Angular 9+.
- */
-export function provideManyDaffMagentoCacheableOperations(...operationNames: string[]) {
-  return operationNames.map(name => ({
-    provide: DAFF_MAGENTO_CACHEABLE_OPERATIONS,
-    useValue: name,
-    multi: true,
-  }));
 }
