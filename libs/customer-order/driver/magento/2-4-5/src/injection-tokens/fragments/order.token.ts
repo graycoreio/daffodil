a0147d545036c149facab52eeac1301b890cb524
Peter Lauck <griest024@gmail.com>
2024-10-14 16:59:41 +0000

    feat(customer-order): create injection tokens with factory

--- libs/customer-order/driver/magento/2-4-5/src/injection-tokens/fragments/order.token.ts
+++ libs/customer-order/driver/magento/2-4-5/src/injection-tokens/fragments/order.token.ts
@@ -1,42 +1,33 @@
-import {
-  InjectionToken,
-  Provider,
-} from '@angular/core';
 import { DocumentNode } from 'graphql';
 
-/**
- * A multi-provider injection token for providing extra GraphQL fragments that will be spread into order queries.
- * This can be used to retrieve additional data that is not covered by the standard Daffodil interfaces.
- *
- * Fragment structure is platform-specific and this feature should be used with care.
- *
- * This can increase query complexity above the Magento default of 300.
- * Daffodil only guarantees that stock queries don't exceed the limit of 300.
- * Apps using this token should therefore increase the query complexity limit
- * to accommodate the extra complexity contributed by the provided fragments.
- */
-export const DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_FRAGMENTS = new InjectionToken<DocumentNode[]>(
-  'DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_FRAGMENTS',
-  { factory: () => []},
-);
+import { createMultiInjectionToken } from '@daffodil/core';
 
-/**
- * Provides extra GraphQL fragments for the Magento order driver.
- *
- * See {@link DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_FRAGMENTS}.
- *
- * ```ts
- * providers: [
- *   ...daffProvideCustomerOrderMagentoExtraOrderFragments(
- *     myExtraOrderFragment
- *   )
- * ]
- * ```
- */
-export function daffProvideCustomerOrderMagentoExtraOrderFragments(...fragments: DocumentNode[]): Provider[] {
-  return fragments.map(fragment => ({
-    provide: DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_FRAGMENTS,
-    useValue: fragment,
-    multi: true,
-  }));
-}
+export const {
+  /**
+   * A multi-provider injection token for providing extra GraphQL fragments that will be spread into order queries.
+   * This can be used to retrieve additional data that is not covered by the standard Daffodil interfaces.
+   *
+   * Fragment structure is platform-specific and this feature should be used with care.
+   *
+   * This can increase query complexity above the Magento default of 300.
+   * Daffodil only guarantees that stock queries don't exceed the limit of 300.
+   * Apps using this token should therefore increase the query complexity limit
+   * to accommodate the extra complexity contributed by the provided fragments.
+   */
+  token: DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_FRAGMENTS,
+
+  /**
+   * Provides extra GraphQL fragments for the Magento order driver.
+   *
+   * See {@link DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_FRAGMENTS}.
+   *
+   * ```ts
+   * providers: [
+   *   ...daffProvideCustomerOrderMagentoExtraOrderFragments(
+   *     myExtraOrderFragment
+   *   )
+   * ]
+   * ```
+   */
+  provider: daffProvideCustomerOrderMagentoExtraOrderFragments,
+} = createMultiInjectionToken<DocumentNode>('DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_FRAGMENTS');
--- libs/customer-order/driver/magento/2-4-5/src/injection-tokens/transforms/extra.token.ts
+++ libs/customer-order/driver/magento/2-4-5/src/injection-tokens/transforms/extra.token.ts
@@ -1,42 +1,31 @@
-import {
-  InjectionToken,
-  Provider,
-} from '@angular/core';
-
 // workaround https://github.com/graycoreio/daffodil/issues/1667
+import { createMultiInjectionToken } from '@daffodil/core';
 import { DaffOrder } from '@daffodil/order';
 
 import { DaffMagentoCustomerOrderExtraTransform } from '../../interfaces/public_api';
 import { MagentoCustomerOrder } from '../../models/public_api';
 
-/**
- * A multi-provider injection token for providing extra transform logic in the Order Magento driver.
- * It is run after the standard transforms for each customer order preview and passed both the current transformed Daffodil customer order and the Magento customer order.
- *
- * See {@link MagentoCustomerOrder} for more info.
- */
-export const DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_TRANSFORMS = new InjectionToken<DaffMagentoCustomerOrderExtraTransform[]>(
-  'DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_TRANSFORMS',
-  { factory: () => []},
-);
+export const {
+  /**
+   * A multi-provider injection token for providing extra transform logic in the Order Magento driver.
+   * It is run after the standard transforms for each customer order preview and passed both the current transformed Daffodil customer order and the Magento customer order.
+   *
+   * See {@link MagentoCustomerOrder} for more info.
+   */
+  token: DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_TRANSFORMS,
 
-/**
- * Provides extra customer order preview transforms for the Magento customer order driver.
- *
- * See {@link DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_TRANSFORMS}.
- *
- * ```ts
- * providers: [
- *   ...daffProvideCustomerOrderMagentoExtraOrderTransforms(
- *     myExtraOrderTransform
- *   )
- * ]
- * ```
- */
-export function daffProvideCustomerOrderMagentoExtraOrderTransforms<T extends MagentoCustomerOrder = MagentoCustomerOrder, V extends DaffOrder = DaffOrder>(...transforms: DaffMagentoCustomerOrderExtraTransform<T, V>[]): Provider[] {
-  return transforms.map(transform => ({
-    provide: DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_TRANSFORMS,
-    useValue: transform,
-    multi: true,
-  }));
-}
+  /**
+   * Provides extra customer order preview transforms for the Magento customer order driver.
+   *
+   * See {@link DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_TRANSFORMS}.
+   *
+   * ```ts
+   * providers: [
+   *   ...daffProvideCustomerOrderMagentoExtraOrderTransforms(
+   *     myExtraOrderTransform
+   *   )
+   * ]
+   * ```
+   */
+  provider: daffProvideCustomerOrderMagentoExtraOrderTransforms,
+} = createMultiInjectionToken<DaffMagentoCustomerOrderExtraTransform>('DAFF_CUSTOMER_ORDER_MAGENTO_EXTRA_ORDER_TRANSFORMS');
