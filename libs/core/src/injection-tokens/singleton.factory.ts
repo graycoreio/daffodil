519d692104394f220156ff3cde9c35c2b7c495df
Peter Lauck <griest024@gmail.com>
2024-10-14 20:37:28 +0000

    fiux

--- libs/core/src/injection-tokens/singleton.factory.ts
+++ libs/core/src/injection-tokens/singleton.factory.ts
@@ -25,7 +25,6 @@ export const createSingletonInjectionToken = <T = unknown>(
   const provider = <R extends T = T>(klass: Type<R>) => ({
     provide: token,
     useExisting: klass,
-    multi: true,
   });
 
   return {
