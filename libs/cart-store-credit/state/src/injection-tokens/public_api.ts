0518d602b8b2fce3601954d317a8d93028ea3685
griest024 <griest024@gmail.com>
2024-10-14 14:45:58 -0400

    feat(cart-store-credit): create injection tokens with factory (#3237)

--- libs/cart-store-credit/state/src/injection-tokens/public_api.ts
+++ libs/cart-store-credit/state/src/injection-tokens/public_api.ts
@@ -1,1 +1,1 @@
-export { DAFF_CART_STORE_CREDIT_ERROR_MATCHER } from './error-matcher.token';
+export * from './error-matcher.token';
--- libs/cart-store-credit/state/src/reducers/token/reducers.token.ts
+++ libs/cart-store-credit/state/src/reducers/token/reducers.token.ts
@@ -1,13 +1,11 @@
-import {
-  inject,
-  InjectionToken,
-} from '@angular/core';
+import { inject } from '@angular/core';
 import {
   ActionReducer,
   combineReducers,
 } from '@ngrx/store';
 
 import { DaffCartWithStoreCredit } from '@daffodil/cart-store-credit';
+import { createSingleInjectionToken } from '@daffodil/core';
 import { daffComposeReducers } from '@daffodil/core/state';
 // these unused imports are a workaround
 
@@ -15,13 +13,16 @@ import { DAFF_CART_STORE_CREDIT_EXTRA_REDUCERS } from './extra.token';
 import { DaffCartStoreCreditReducersState } from '../reducers.interface';
 import { daffCartStoreCreditReducer } from '../store-credit/public_api';
 
-/**
- * An internal token to hold the Daffodil cart reducers.
- * Includes the extra and standard reducers.
- *
- * @docs-private
- */
-export const DAFF_CART_STORE_CREDIT_REDUCERS = new InjectionToken<ActionReducer<DaffCartStoreCreditReducersState>>(
+export const {
+  /**
+   * An internal token to hold the Daffodil cart reducers.
+   * Includes the extra and standard reducers.
+   *
+   * @docs-private
+   */
+  token: DAFF_CART_STORE_CREDIT_REDUCERS,
+  provider: daffProvideCartStoreCreditReducers,
+} = createSingleInjectionToken<ActionReducer<DaffCartStoreCreditReducersState>>(
   'DAFF_CART_STORE_CREDIT_REDUCERS',
   {
     providedIn: 'any',
