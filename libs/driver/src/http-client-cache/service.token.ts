aba565c1515de95cf9764fac07e02f2bd3cd7c3c
griest024 <griest024@gmail.com>
2024-10-14 14:49:44 -0400

    feat(driver): create injection tokens with factory (#3247)

--- libs/driver/src/http-client-cache/service.token.ts
+++ libs/driver/src/http-client-cache/service.token.ts
@@ -1,9 +1,14 @@
-import {
-  InjectionToken,
-  inject,
-} from '@angular/core';
+import { inject } from '@angular/core';
+
+import { createSingleInjectionToken } from '@daffodil/core';
 
 import { DaffDriverHttpClientCacheNoopService } from './noop.service';
 import { DaffDriverHttpClientCacheServiceInterface } from './service.type';
 
-export const DAFF_DRIVER_HTTP_CLIENT_CACHE_SERVICE = new InjectionToken<DaffDriverHttpClientCacheServiceInterface>('DAFF_DRIVER_HTTP_CLIENT_CACHE_SERVICE', { factory: () => inject(DaffDriverHttpClientCacheNoopService) });
+export const {
+  token: DAFF_DRIVER_HTTP_CLIENT_CACHE_SERVICE,
+  provider: daffProvideDriverHttpClientCacheService,
+} = createSingleInjectionToken<DaffDriverHttpClientCacheServiceInterface>(
+  'DAFF_DRIVER_HTTP_CLIENT_CACHE_SERVICE',
+  { factory: () => inject(DaffDriverHttpClientCacheNoopService) },
+);
