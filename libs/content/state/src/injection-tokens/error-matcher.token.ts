5ac537a50f92de79dfea1b322565d1b18a3de108
griest024 <griest024@gmail.com>
2024-10-14 19:27:36 -0400

    feat(content): create injection tokens with factory (#3265)

--- libs/content/state/src/injection-tokens/error-matcher.token.ts
+++ libs/content/state/src/injection-tokens/error-matcher.token.ts
@@ -1,12 +1,14 @@
-import { InjectionToken } from '@angular/core';
-
+import { createSingleInjectionToken } from '@daffodil/core';
 import { daffTransformErrorToStateError } from '@daffodil/core/state';
 
-/**
- * Transforms `DaffError`s into `DaffStateError`s before they are serialized into state.
- * Can be used to further refine Daffodil errors into more specific app errors.
- */
-export const DAFF_CONTENT_ERROR_MATCHER = new InjectionToken<typeof daffTransformErrorToStateError>(
-  'DAFF_CONTENT_ERROR_MATCHER',
-  { factory: () => daffTransformErrorToStateError },
-);
+export const {
+  /**
+   * Transforms `DaffError`s into `DaffStateError`s before they are serialized into state.
+   * Can be used to further refine Daffodil errors into more specific app errors.
+   */
+  token: DAFF_CONTENT_ERROR_MATCHER,
+  /**
+   * Provider for {@link DAFF_CONTENT_ERROR_MATCHER}.
+   */
+  provider: provideDaffContentErrorMatcher,
+} = createSingleInjectionToken<typeof daffTransformErrorToStateError>('DAFF_CONTENT_ERROR_MATCHER');
