4bd935675dde97cfaf0b827895340c8f1f738d04
Peter Lauck <griest024@gmail.com>
2024-11-01 22:19:56 +0000

    wire up components

--- apps/daffio/src/app/docs/design/pages/components-overview/component-overview.component.ts
+++ apps/daffio/src/app/docs/design/pages/components-overview/component-overview.component.ts
@@ -1,12 +1,21 @@
+import { AsyncPipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  OnInit,
 } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+  switchMap,
+  map,
+  Observable,
+} from 'rxjs';
 
 import { DAFF_CARD_COMPONENTS } from '@daffodil/design/card';
 import { DAFF_CONTAINER_COMPONENTS } from '@daffodil/design/container';
 import { DAFF_HERO_COMPONENTS } from '@daffodil/design/hero';
+import { DaffDocsDesignGuideNavList } from '@daffodil/docs-utils';
+import { DaffRouterActivatedRoute } from '@daffodil/router';
 
 @Component({
   selector: 'daffio-docs-design-component-overview',
@@ -16,9 +25,24 @@ import { DAFF_HERO_COMPONENTS } from '@daffodil/design/hero';
   standalone: true,
   imports: [
     RouterLink,
+    AsyncPipe,
     DAFF_CARD_COMPONENTS,
     DAFF_HERO_COMPONENTS,
     DAFF_CONTAINER_COMPONENTS,
   ],
 })
-export class DaffioDocsDesignComponentOverviewPageComponent { }
+export class DaffioDocsDesignComponentOverviewPageComponent implements OnInit {
+  components$: Observable<Array<DaffDocsDesignGuideNavList>>;
+
+  constructor(
+    private route: DaffRouterActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.components$ = this.route.route$.pipe(
+      switchMap((route) => route.data),
+      map((data) => data.index),
+      map((docsList: DaffDocsDesignGuideNavList) => docsList.children.find(({ id }) => id === 'components').children.filter(({ id }) => !!id)),
+    );
+  }
+}
