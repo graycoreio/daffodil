4338be84e970a3ba6b1aacf632b0995503e2943d
xelaint <xelaint@gmail.com>
2025-03-03 14:36:11 +0000

    add routerLinkActive

--- apps/daffio/src/app/docs/components/table-of-contents/link/link.component.ts
+++ apps/daffio/src/app/docs/components/table-of-contents/link/link.component.ts
@@ -4,9 +4,16 @@ import {
   Component,
   Input,
 } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 
 import { DaffDocTableOfContents } from '@daffodil/docs-utils';
+
+const DEFAULT_ROUTER_LINK_ACTIVE_CONFIG: RouterLinkActive['routerLinkActiveOptions'] = {
+  exact: true,
+};
 
 @Component({
   selector: 'daffio-docs-table-of-contents-link',
@@ -15,9 +22,12 @@ import { DaffDocTableOfContents } from '@daffodil/docs-utils';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     RouterLink,
+    RouterLinkActive,
   ],
 })
 export class DaffioDocsTableOfContentsLinkComponent {
+  readonly ROUTER_LINK_ACTIVE_CONFIG = DEFAULT_ROUTER_LINK_ACTIVE_CONFIG;
+
   /**
    * The doc to render
    */
