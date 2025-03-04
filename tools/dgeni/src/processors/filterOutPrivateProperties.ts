6900d2725d917a662610d64b8da5113d0ec66e23
Peter Lauck <griest024@gmail.com>
2025-03-04 22:38:40 +0000

    fix(dgeni): API docs include private members

--- tools/dgeni/src/processors/filterOutPrivateProperties.ts
+++ tools/dgeni/src/processors/filterOutPrivateProperties.ts
@@ -3,28 +3,26 @@ import {
   Document,
 } from 'dgeni';
 
+import { ADD_SOURCE_NAME } from '../transforms/daffodil-api-package/processors/add-source';
+
 /**
  * Filter out properties that are marked as @docs-private
  */
 export class FilterOutPrivatePropertiesProcessor implements Processor {
   name = 'filterOutPrivateProperties';
   $runAfter = ['docs-processed'];
-  $runBefore = ['rendering-docs'];
+  $runBefore = ['rendering-docs', ADD_SOURCE_NAME];
 
   $process(docs: Document[]): Document[] {
     return docs.map(doc => {
-      if(!doc.members) {
-        return doc;
+      if (doc.members) {
+        doc.members = filterOutPrivateProperties(doc.members);
       }
-
-      return doc.members ? {
-        ...doc,
-        members: filterOutPrivateProperties(doc.members),
-      } : doc;
+      return doc;
     });
   }
 }
 
 function filterOutPrivateProperties(members): any {
-  return members.filter(member => !member.tags.tags.filter(tag => tag.tagName ==='docs-private').length);
+  return members.filter(member => !member.tags.tags.find(tag => tag.tagName ==='docs-private'));
 }
