528836a8b35bdab16269d248b3e8b624b562fd21
xelaint <xelaint@gmail.com>
2025-04-02 12:12:07 -0400

    update tests

--- libs/design/youtube-player/src/youtube-player.component.spec.ts
+++ libs/design/youtube-player/src/youtube-player.component.spec.ts
@@ -1,26 +1,87 @@
 import {
+  Component,
+  DebugElement,
+} from '@angular/core';
+import {
+  waitForAsync,
   ComponentFixture,
   TestBed,
 } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DaffYoutubePlayerComponent } from './youtube-player.component';
 
-describe('DaffYoutubePlayerComponent', () => {
-  let component: DaffYoutubePlayerComponent;
-  let fixture: ComponentFixture<DaffYoutubePlayerComponent>;
+@Component({
+  template: `<daff-youtube-player [width]="width" [height]="height"></daff-youtube-player>`,
+  imports: [
+    DaffYoutubePlayerComponent,
+  ],
+})
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DaffYoutubePlayerComponent],
+class WrapperComponent {
+  width = 100;
+  height = 100;
+}
+
+describe('@daffodil/design/image | DaffYoutubePlayerComponent', () => {
+  let wrapper: WrapperComponent;
+  let component: DaffYoutubePlayerComponent;
+  let de: DebugElement;
+  let fixture: ComponentFixture<WrapperComponent>;
+  let playerDE: DebugElement;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        WrapperComponent,
+      ],
     })
       .compileComponents();
+  }));
 
-    fixture = TestBed.createComponent(DaffYoutubePlayerComponent);
-    component = fixture.componentInstance;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WrapperComponent);
+    wrapper = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('daff-youtube-player'));
+    playerDE = fixture.debugElement.query(By.css('.daff-youtube-player'));
+    component = de.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be able to take `width` as an input', () => {
+    wrapper.width = 100;
+    fixture.detectChanges();
+
+    expect(component.width).toEqual(100);
+  });
+
+  it('should be able to take `height` as an input', () => {
+    wrapper.height = 100;
+    fixture.detectChanges();
+
+    expect(component.height).toEqual(100);
+  });
+
+  it('should throw an error when width is invalid', () => {
+    wrapper.width = null;
+    expect(() => fixture.detectChanges()).toThrowError(/width/);
+  });
+
+  it('should throw an error when height is invalid', () => {
+    wrapper.height = undefined;
+    expect(() => fixture.detectChanges()).toThrowError(/height/);
+  });
+
+  it('sets `max-width` on the host element based on the width', () => {
+    wrapper.width = 300;
+
+    fixture.detectChanges();
+
+    expect(de.styles['max-width']).toEqual(wrapper.width + 'px');
   });
 });
