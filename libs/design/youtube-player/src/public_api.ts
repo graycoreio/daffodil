8aaff8a691b966e09855c8795371cd57ba2ae259
xelaint <xelaint@gmail.com>
2025-04-02 12:39:50 -0400

    update export

--- libs/design/youtube-player/src/public_api.ts
+++ libs/design/youtube-player/src/public_api.ts
@@ -1,2 +1,3 @@
 export { DaffYoutubePlayerComponent } from './youtube-player.component';
 export { DaffYoutubeSafePipe } from './safe-url.pipe';
+export { DAFF_YOUTUBE_PLAYER_COMPONENTS } from './youtube-player';
