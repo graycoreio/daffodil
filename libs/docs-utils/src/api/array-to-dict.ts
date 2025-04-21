5c336131fdc685269fd28e9b3b1d14e448c91cdb
Peter Lauck <griest024@gmail.com>
2025-04-21 21:16:21 +0000

    fix build

--- libs/docs-utils/src/api/array-to-dict.ts
+++ libs/docs-utils/src/api/array-to-dict.ts
@@ -1,12 +1,12 @@
-import {
-  DaffApiDoc,
-  DaffPackageGuideDoc,
-} from '../doc/public_api';
+import { DaffApiDoc } from '../doc/public_api';
+import { DaffDocsApiRole } from './role/public_api';
 
 /**
  * Groups a list of API docs by roles.
  */
-export const daffDocsApiArrayToDict = (docs: Array<DaffApiDoc>): DaffPackageGuideDoc['api'] =>
+export const daffDocsApiArrayToDict = (docs: Array<DaffApiDoc>): {
+  [key in DaffDocsApiRole]?: Array<DaffApiDoc>;
+} =>
   docs.reduce((acc, apiDoc) => {
     if (acc[apiDoc.role]) {
       acc[apiDoc.role].push(apiDoc);
