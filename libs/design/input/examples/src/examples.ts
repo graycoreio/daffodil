88a2159146e5750a20f3884708465c8712a8f601
xelaint <xelaint@gmail.com>
2025-05-05 16:06:07 -0400

    update theming

--- libs/design/input/examples/src/examples.ts
+++ libs/design/input/examples/src/examples.ts
@@ -3,13 +3,11 @@ import { InputDisabledComponent } from './input-disabled/input-disabled.componen
 import { InputErrorComponent } from './input-error/input-error.component';
 import { InputHintComponent } from './input-hint/input-hint.component';
 import { InputWithFormFieldComponent } from './input-with-form-field/input-with-form-field.component';
-import { PasswordWithFormFieldComponent } from './password-with-form-field/password-with-form-field.component';
 
 export const INPUT_EXAMPLES = [
   BasicInputComponent,
   InputWithFormFieldComponent,
   InputDisabledComponent,
   InputErrorComponent,
-  PasswordWithFormFieldComponent,
   InputHintComponent,
 ];
