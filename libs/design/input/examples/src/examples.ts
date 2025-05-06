6dcfca1700b84e3b66958305f72e8f29ae48f825
xelaint <xelaint@gmail.com>
2025-05-06 13:51:19 -0400

    remove basic input example

--- libs/design/input/examples/src/examples.ts
+++ libs/design/input/examples/src/examples.ts
@@ -1,11 +1,9 @@
-import { BasicInputComponent } from './basic-input/basic-input.component';
 import { InputDisabledComponent } from './input-disabled/input-disabled.component';
 import { InputErrorComponent } from './input-error/input-error.component';
 import { InputHintComponent } from './input-hint/input-hint.component';
 import { InputWithFormFieldComponent } from './input-with-form-field/input-with-form-field.component';
 
 export const INPUT_EXAMPLES = [
-  BasicInputComponent,
   InputWithFormFieldComponent,
   InputDisabledComponent,
   InputErrorComponent,
