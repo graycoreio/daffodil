18c8022ca6265cdef910b709e17943ece06b29dd
Peter Lauck <griest024@gmail.com>
2025-06-09 15:56:01 +0000

    remove toc from api packages

--- apps/daffio/src/app/docs/api/components/api-package/api-package.component.ts
+++ apps/daffio/src/app/docs/api/components/api-package/api-package.component.ts
@@ -3,22 +3,16 @@ import {
   ChangeDetectionStrategy,
   HostBinding,
   input,
-  computed,
-  viewChildren,
 } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { DaffArticleEncapsulatedDirective } from '@daffodil/design';
 import { DAFF_ARTICLE_COMPONENTS } from '@daffodil/design/article';
-import {
-  DaffApiPackageDoc,
-  DaffDocTableOfContents,
-} from '@daffodil/docs-utils';
+import { DaffApiPackageDoc } from '@daffodil/docs-utils';
 
 import { DaffioApiPackageFilterPipe } from './not-packages.pipe';
 import { DaffioDocViewerComponent } from '../../../components/doc-viewer/doc-viewer.component';
 import { DaffioDocsDynamicContent } from '../../../dynamic-content/dynamic-content.type';
-import { DaffioDocsTocHeaderDirective } from '../../../toc/header.directive';
 import { DaffioApiListSectionComponent } from '../api-list-section/api-list-section.component';
 
 @Component({
@@ -35,16 +29,9 @@ import { DaffioApiListSectionComponent } from '../api-list-section/api-list-sect
     DAFF_ARTICLE_COMPONENTS,
     DaffioApiPackageFilterPipe,
     DaffioDocViewerComponent,
-    DaffioDocsTocHeaderDirective,
   ],
 })
 export class DaffioApiPackageComponent implements DaffioDocsDynamicContent<DaffApiPackageDoc> {
-  private readonly viewHeaders = viewChildren(DaffioDocsTocHeaderDirective);
-  readonly toc = computed(() => this.viewHeaders().reduce((toc, directive) => {
-    toc.push(directive.entry());
-    return toc;
-  }, <DaffDocTableOfContents>[]));
-
   @HostBinding('class.daffio-api-package') class = true;
 
   /**
