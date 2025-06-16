90f0078ff94c578f6b477b71ac2f9eedf714ac9a
Peter Lauck <griest024@gmail.com>
2025-06-16 17:28:00 +0000

    set toc on registry for packages and guides too

--- apps/daffio/src/app/docs/guides/components/guides-content/guides-content.component.ts
+++ apps/daffio/src/app/docs/guides/components/guides-content/guides-content.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   ChangeDetectionStrategy,
   input,
+  effect,
 } from '@angular/core';
 
 import {
@@ -12,6 +13,7 @@ import {
 import { DaffioSafeHtmlPipe } from '../../../../core/html-sanitizer/safe.pipe';
 import { DaffioDocViewerComponent } from '../../../components/doc-viewer/doc-viewer.component';
 import { DaffioDocsDynamicContent } from '../../../dynamic-content/dynamic-content.type';
+import { DaffioDocsTocService } from '../../../toc/toc.service';
 
 @Component({
   selector: 'daffio-docs-guides-content',
@@ -26,4 +28,13 @@ export class DaffioDocsGuidesContentComponent implements DaffioDocsDynamicConten
   static readonly kind = DaffDocKind.GUIDE;
 
   doc = input<DaffDoc>();
+
+  constructor(
+    private tocRegistry: DaffioDocsTocService,
+  ) {
+    effect((onCleanup) => {
+      this.tocRegistry.set(this.doc().tableOfContents);
+      onCleanup(() => this.tocRegistry.set([]));
+    });
+  }
 }
--- apps/daffio/src/app/docs/packages/components/packages-content/packages-content.component.ts
+++ apps/daffio/src/app/docs/packages/components/packages-content/packages-content.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   ChangeDetectionStrategy,
   input,
+  effect,
 } from '@angular/core';
 
 import {
@@ -12,6 +13,7 @@ import {
 import { DaffioSafeHtmlPipe } from '../../../../core/html-sanitizer/safe.pipe';
 import { DaffioDocViewerComponent } from '../../../components/doc-viewer/doc-viewer.component';
 import { DaffioDocsDynamicContent } from '../../../dynamic-content/dynamic-content.type';
+import { DaffioDocsTocService } from '../../../toc/toc.service';
 
 @Component({
   selector: 'daffio-docs-packages-content',
@@ -26,4 +28,13 @@ export class DaffioDocsPackagesContentComponent implements DaffioDocsDynamicCont
   static readonly kind = DaffDocKind.PACKAGE;
 
   doc = input<DaffPackageGuideDoc>();
+
+  constructor(
+    private tocRegistry: DaffioDocsTocService,
+  ) {
+    effect((onCleanup) => {
+      this.tocRegistry.set(this.doc().tableOfContents);
+      onCleanup(() => this.tocRegistry.set([]));
+    });
+  }
 }
