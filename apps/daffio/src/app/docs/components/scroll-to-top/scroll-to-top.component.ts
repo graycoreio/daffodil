456820579d3fb8234e7b6ed1bc541684e9cf0e8f
Ammar Ahmad <ammarahmadwrk@gmail.com>
2025-07-16 16:57:33 -0400

    fix(daffio): lint

--- apps/daffio/src/app/docs/components/scroll-to-top/scroll-to-top.component.ts
+++ apps/daffio/src/app/docs/components/scroll-to-top/scroll-to-top.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
 import { debounce } from '@daffodil/core';
 
 @Component({
