35c9ab6991a1be49fe912abb2dacd2fd18c865fa
Ammar Ahmad <ammarahmadwrk@gmail.com>
2025-07-23 15:14:43 -0400

    fix(dgeni): add sequential execution for windows

--- tools/dgeni/build.ts
+++ tools/dgeni/build.ts
@@ -16,37 +16,68 @@ import { daffodilRoutesPackage } from './src/transforms/daffodil-routes-package'
 
 rimraf('../../dist/docs/*', { glob: true }).then(() => {
   new Dgeni([apiDocs]).generate().then(() => {
-    // base docs
-    Promise.all([
-      new Dgeni([packageDocsPackage]).generate().catch((err) => {
-        console.log(err);
-        process.exit(1);
-      }),
-      new Dgeni([guideDocsPackage]).generate().catch((err) => {
-        console.log(err);
-        process.exit(1);
-      }),
-      new Dgeni([explanationDocsPackage]).generate().catch((err) => {
-        console.log(err);
-        process.exit(1);
-      }),
-    ]).then(() => {
+    // base docs 
+    const runBaseDocsSequentially = async () => {
+      if (process.platform === 'win32') {
+        // Sequential execution on Windows
+        try {
+          await new Dgeni([packageDocsPackage]).generate();
+          await new Dgeni([guideDocsPackage]).generate();
+          await new Dgeni([explanationDocsPackage]).generate();
+        } catch (err) {
+          console.log(err);
+          process.exit(1);
+        }
+      } else {
+        // Parallel execution on other platforms
+        await Promise.all([
+          new Dgeni([packageDocsPackage]).generate().catch((err) => {
+            console.log(err);
+            process.exit(1);
+          }),
+          new Dgeni([guideDocsPackage]).generate().catch((err) => {
+            console.log(err);
+            process.exit(1);
+          }),
+          new Dgeni([explanationDocsPackage]).generate().catch((err) => {
+            console.log(err);
+            process.exit(1);
+          }),
+        ]);
+      }
+    };
+    
+    runBaseDocsSequentially().then(() => {
       // design docs
       // run them after base docs so that config between shared processors does not conflict
       // a design flaw of dgeni, it wasn't meant to be run in parallel
-      new Dgeni([designApiPackage]).generate().then(() => {
-        Promise.all([new Dgeni([designDocsPackage]).generate().catch((err) => {
+      new Dgeni([designApiPackage]).generate().then(async () => {
+        const runDesignDocsSequentially = async () => {
+          if (process.platform === 'win32') {
+            try {
+              await new Dgeni([designDocsPackage]).generate();
+              await new Dgeni([designExamplePackage]).generate();
+            } catch (err) {
+              console.log(err);
+              process.exit(1);
+            }
+          } else {
+            await Promise.all([new Dgeni([designDocsPackage]).generate().catch((err) => {
+              console.log(err);
+              process.exit(1);
+            }),
+            new Dgeni([designExamplePackage]).generate().catch((err) => {
+              console.log(err);
+              process.exit(1);
+            })]);
+          }
+        };
+        
+        await runDesignDocsSequentially();
+        
+        new Dgeni([daffodilRoutesPackage]).generate().catch((err) => {
           console.log(err);
           process.exit(1);
-        }),
-        new Dgeni([designExamplePackage]).generate().catch((err) => {
-          console.log(err);
-          process.exit(1);
-        })]).then(() => {
-          new Dgeni([daffodilRoutesPackage]).generate().catch((err) => {
-            console.log(err);
-            process.exit(1);
-          });
         });
       }).catch((err) => {
         console.log(err);
