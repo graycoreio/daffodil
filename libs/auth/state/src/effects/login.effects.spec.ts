b70a6afdda74a4e0afa7fbb9e12332abbc9aaa1d
Ammar Ahmad <ammarahmadwrk@gmail.com>
2025-08-22 18:41:07 -0400

    test(auth): improve test isolation and maintainability

--- libs/auth/state/src/effects/login.effects.spec.ts
+++ libs/auth/state/src/effects/login.effects.spec.ts
@@ -48,27 +48,7 @@ import { daffTransformErrorToStateError } from '@daffodil/core/state';
 
 import { DaffAuthLoginEffects } from './login.effects';
 
-interface BaseTestState {
-  didActionSucceed: boolean;
-  whatActionWasReturned: Action;
-};
 
-interface DriverErrorTestState {
-  whatDriverErrorWasThrown: DaffServerSideStorageError;
-}
-
-interface StorageErrorTestState {
-  whatStorageErrorWasThrown: DaffStorageServiceError;
-}
-
-interface TokenStorageTestState {
-  didTokenStorageSucceed: boolean;
-  whatWasTheValueOfTheStoredToken: string;
-}
-
-type LoginTestState = BaseTestState & Partial<DriverErrorTestState & StorageErrorTestState & TokenStorageTestState>;
-
-type LogoutTestState = BaseTestState & Partial<DriverErrorTestState>;
 
 describe('@daffodil/auth/state | DaffAuthLoginEffects', () => {
   let actions$: Observable<any>;
@@ -118,6 +98,60 @@ describe('@daffodil/auth/state | DaffAuthLoginEffects', () => {
     mockLoginInfo = { email, password };
   });
 
+  afterEach(() => {
+    daffLoginDriver.login.calls.reset();
+    daffLoginDriver.logout.calls.reset();
+    setAuthTokenSpy.calls.reset();
+    removeAuthTokenSpy.calls.reset();
+  });
+
+  const setupLoginDriver = (
+    testScheduler: TestScheduler,
+    shouldSucceed: boolean,
+    errorToThrow?: any,
+  ) => {
+    if (shouldSucceed) {
+      daffLoginDriver.login.and.returnValue(of(mockAuth));
+    } else {
+      daffLoginDriver.login.and.returnValue(
+        testScheduler.createColdObservable('#', {}, errorToThrow),
+      );
+    }
+  };
+
+  const setupLogoutDriver = (
+    testScheduler: TestScheduler,
+    shouldSucceed: boolean,
+    errorToThrow?: any,
+  ) => {
+    if (shouldSucceed) {
+      daffLoginDriver.logout.and.returnValue(of(undefined));
+    } else {
+      daffLoginDriver.logout.and.returnValue(
+        testScheduler.createColdObservable('#', {}, errorToThrow),
+      );
+    }
+  };
+
+  const setupStorageService = (shouldSucceed: boolean, errorToThrow?: any) => {
+    if (!shouldSucceed && errorToThrow) {
+      setAuthTokenSpy.and.throwError(errorToThrow);
+    }
+  };
+
+  const testEffectWithMarbles = (
+    effect$: Observable<any>,
+    action: any,
+    expectedAction: any,
+    testScheduler: TestScheduler,
+  ) => {
+    testScheduler.run(helpers => {
+      actions$ = helpers.hot('--a', { a: action });
+      helpers.expectObservable(effect$).toBe('--b', { b: expectedAction });
+      helpers.flush();
+    });
+  };
+
   it('should be created', () => {
     expect(effects).toBeTruthy();
   });
@@ -125,62 +159,74 @@ describe('@daffodil/auth/state | DaffAuthLoginEffects', () => {
   describe('login$ | when the user logs in', () => {
     const mockAuthLoginAction = new DaffAuthLogin(mockLoginInfo);
 
-    it('should compute the next action correctly', () => {
-      const testStates: LoginTestState[] = [
-        {
-          didActionSucceed: true,
-          didTokenStorageSucceed: true,
-          whatActionWasReturned: new DaffAuthLoginSuccess(mockAuth),
-          whatWasTheValueOfTheStoredToken: mockAuth.token,
-        },
-        {
-          didActionSucceed: true,
-          didTokenStorageSucceed: false,
-          whatStorageErrorWasThrown: new DaffStorageServiceError('Storage of auth token has failed.'),
-          whatActionWasReturned: new DaffAuthLoginFailure({
-            code: 'DAFF_STORAGE_FAILURE', recoverable: false, message: 'Storage of auth token has failed.',
-          }),
-        },
-        {
-          didActionSucceed: true,
-          didTokenStorageSucceed: false,
-          whatStorageErrorWasThrown: new DaffServerSideStorageError('Server side'),
-          whatActionWasReturned: new DaffAuthServerSide(
-            daffTransformErrorToStateError(new DaffServerSideStorageError('Server side')),
-          ),
-        },
-        {
-          didActionSucceed: false,
-          whatDriverErrorWasThrown: new DaffAuthenticationFailedError('Failed to log in'),
-          whatActionWasReturned: new DaffAuthLoginFailure(daffTransformErrorToStateError(
-            new DaffAuthenticationFailedError('Failed to log in'),
-          )),
-        },
-      ];
-
-      testStates.forEach((el) => {
+    describe('when login succeeds and token storage succeeds', () => {
+      it('should return DaffAuthLoginSuccess and store the token', () => {
         const testScheduler = new TestScheduler((actual, expected) => {
           expect(actual).toEqual(expected);
         });
-        testScheduler.run(helpers => {
-          if(el.didActionSucceed) {
-            daffLoginDriver.login.and.returnValue(of(mockAuth));
-          } else {
-            daffLoginDriver.login.and.returnValue(helpers.cold('#', {}, el.whatDriverErrorWasThrown));
-          }
+        
+        setupLoginDriver(testScheduler, true);
+        setupStorageService(true);
+        
+        const expectedAction = new DaffAuthLoginSuccess(mockAuth);
+        testEffectWithMarbles(effects.login$, mockAuthLoginAction, expectedAction, testScheduler);
+        
+        expect(setAuthTokenSpy).toHaveBeenCalledWith(mockAuth.token);
+      });
+    });
 
-          actions$ = helpers.hot('--a', { a: mockAuthLoginAction });
-
-          if(!el.didTokenStorageSucceed) {
-            setAuthTokenSpy.and.throwError(el.whatStorageErrorWasThrown);
-          }
-
-          helpers.expectObservable(effects.login$).toBe('--b', { b: el.whatActionWasReturned });
-          helpers.flush();
-          if(el.whatWasTheValueOfTheStoredToken) {
-            expect(setAuthTokenSpy).toHaveBeenCalledWith(mockAuth.token);
-          }
+    describe('when login succeeds but token storage fails with DaffStorageServiceError', () => {
+      it('should return DaffAuthLoginFailure with storage failure error', () => {
+        const testScheduler = new TestScheduler((actual, expected) => {
+          expect(actual).toEqual(expected);
         });
+        
+        const storageError = new DaffStorageServiceError('Storage of auth token has failed.');
+        setupLoginDriver(testScheduler, true);
+        setupStorageService(false, storageError);
+        
+        const expectedAction = new DaffAuthLoginFailure({
+          code: 'DAFF_STORAGE_FAILURE', 
+          recoverable: false, 
+          message: 'Storage of auth token has failed.',
+        });
+        
+        testEffectWithMarbles(effects.login$, mockAuthLoginAction, expectedAction, testScheduler);
+      });
+    });
+
+    describe('when login succeeds but token storage fails with DaffServerSideStorageError', () => {
+      it('should return DaffAuthServerSide action', () => {
+        const testScheduler = new TestScheduler((actual, expected) => {
+          expect(actual).toEqual(expected);
+        });
+        
+        const serverSideError = new DaffServerSideStorageError('Server side');
+        setupLoginDriver(testScheduler, true);
+        setupStorageService(false, serverSideError);
+        
+        const expectedAction = new DaffAuthServerSide(
+          daffTransformErrorToStateError(serverSideError),
+        );
+        
+        testEffectWithMarbles(effects.login$, mockAuthLoginAction, expectedAction, testScheduler);
+      });
+    });
+
+    describe('when login fails with authentication error', () => {
+      it('should return DaffAuthLoginFailure with authentication error', () => {
+        const testScheduler = new TestScheduler((actual, expected) => {
+          expect(actual).toEqual(expected);
+        });
+        
+        const authError = new DaffAuthenticationFailedError('Failed to log in');
+        setupLoginDriver(testScheduler, false, authError);
+        
+        const expectedAction = new DaffAuthLoginFailure(
+          daffTransformErrorToStateError(authError),
+        );
+        
+        testEffectWithMarbles(effects.login$, mockAuthLoginAction, expectedAction, testScheduler);
       });
     });
   });
@@ -188,37 +234,33 @@ describe('@daffodil/auth/state | DaffAuthLoginEffects', () => {
   describe('logout$ | when the user logs out', () => {
     const mockAuthLogoutAction = new DaffAuthLogout();
 
-    it('should compute the next action correctly', () => {
-      const testStates: LogoutTestState[] = [
-        {
-          didActionSucceed: true,
-          whatActionWasReturned: new DaffAuthLogoutSuccess(),
-        },
-        {
-          didActionSucceed: false,
-          whatDriverErrorWasThrown: new DaffAuthInvalidAPIResponseError('Failed to log out'),
-          whatActionWasReturned: new DaffAuthLogoutFailure(
-            daffTransformErrorToStateError(
-              new DaffAuthInvalidAPIResponseError('Failed to log out'),
-            ),
-          ),
-        },
-      ];
-
-      testStates.forEach((el) => {
+    describe('when logout succeeds', () => {
+      it('should return DaffAuthLogoutSuccess', () => {
         const testScheduler = new TestScheduler((actual, expected) => {
           expect(actual).toEqual(expected);
         });
-        testScheduler.run(helpers => {
-          if(el.didActionSucceed) {
-            daffLoginDriver.logout.and.returnValue(of(undefined));
-          } else {
-            daffLoginDriver.logout.and.returnValue(helpers.cold('#', {}, el.whatDriverErrorWasThrown));
-          }
+        
+        setupLogoutDriver(testScheduler, true);
+        
+        const expectedAction = new DaffAuthLogoutSuccess();
+        testEffectWithMarbles(effects.logout$, mockAuthLogoutAction, expectedAction, testScheduler);
+      });
+    });
 
-          actions$ = helpers.hot('--a', { a: mockAuthLogoutAction });
-          helpers.expectObservable(effects.logout$).toBe('--b', { b: el.whatActionWasReturned });
+    describe('when logout fails with API response error', () => {
+      it('should return DaffAuthLogoutFailure with the error', () => {
+        const testScheduler = new TestScheduler((actual, expected) => {
+          expect(actual).toEqual(expected);
         });
+        
+        const apiError = new DaffAuthInvalidAPIResponseError('Failed to log out');
+        setupLogoutDriver(testScheduler, false, apiError);
+        
+        const expectedAction = new DaffAuthLogoutFailure(
+          daffTransformErrorToStateError(apiError),
+        );
+        
+        testEffectWithMarbles(effects.logout$, mockAuthLogoutAction, expectedAction, testScheduler);
       });
     });
   });
