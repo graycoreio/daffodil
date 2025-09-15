18c643a025721317a20bd973128a36135569ea7e
Peter Lauck <griest024@gmail.com>
2025-09-15 16:09:04 +0000

    feat(core): align model factory interface with implementation

--- libs/core/testing/src/factories/factory.interface.ts
+++ libs/core/testing/src/factories/factory.interface.ts
@@ -1,4 +1,5 @@
 export interface IDaffModelFactory<T> {
-  create(partial: Partial<T>): T;
+  create<R extends T = T>(partial: Partial<T> & T extends R ? Partial<T> : R): T & R;
+  create(partial?: Partial<T>): T;
   createMany(qty: number, partial: Partial<T>): T[];
 }
