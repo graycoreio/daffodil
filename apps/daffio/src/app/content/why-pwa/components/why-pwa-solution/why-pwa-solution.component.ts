277519b2527d03774c1f9c42033f6b0745984861
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-09-24 20:26:58 +0330

    refactor(daffio): convert why-pwa-solution comp to standalone
    https://github.com/graycoreio/daffodil/issues/3971

--- apps/daffio/src/app/content/why-pwa/components/why-pwa-solution/why-pwa-solution.component.ts
+++ apps/daffio/src/app/content/why-pwa/components/why-pwa-solution/why-pwa-solution.component.ts
@@ -4,12 +4,23 @@ import {
   HostBinding,
 } from '@angular/core';
 
+import { DaffCalloutModule } from '@daffodil/design/callout';
+import { DaffContainerModule } from '@daffodil/design/container';
+import { DaffListModule } from '@daffodil/design/list';
+
+import { DaffioFeatureComparisonComponent } from '../feature-comparison/feature-comparison.component';
+
 @Component({
   selector: 'daffio-why-pwa-solution',
   templateUrl: './why-pwa-solution.component.html',
   styleUrls: ['./why-pwa-solution.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: false,
+  imports: [
+    DaffContainerModule,
+    DaffCalloutModule,
+    DaffListModule,
+    DaffioFeatureComparisonComponent,
+  ],
 })
 
 export class DaffioWhyPwaSolutionComponent {
