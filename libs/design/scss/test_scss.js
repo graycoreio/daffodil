98ca1b2f2f22113a751e7d42c111722bf41a4760
xelaint <xelaint@gmail.com>
2025-09-24 08:59:56 -0400

    update test js file

--- libs/design/scss/test_scss.js
+++ libs/design/scss/test_scss.js
@@ -1,24 +1,17 @@
-const path = require('path')
-const sassTrue = require('sass-true')
+const path = require('path');
+const sassTrue = require('sass-true');
 const glob = require('glob');
 
 describe('Sass', () => {
-
   // Find all of the Sass files that end in `*.spec.scss` in any directory in this project.
   // I use path.resolve because True requires absolute paths to compile test files.
   const sassTestFiles = glob.sync(path.resolve(__dirname, './**/*.spec.scss'));
 
   // Run True on every file found with the describe and it methods provided
-  sassTestFiles.forEach(file =>
+  sassTestFiles.forEach(sassFile =>
     sassTrue.runSass(
-      {
-        file: file,
-      },
-      {
-        sass: require('sass'),
-        describe,
-        it,
-      }
+      { describe, it },
+      sassFile,
     )
   )
 })
