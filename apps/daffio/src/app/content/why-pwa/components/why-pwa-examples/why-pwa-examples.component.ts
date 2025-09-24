476f47839a7dee494b7b4dfa19b6421b9ae6e59d
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-09-24 20:25:23 +0330

    refactor(daffio): convert why-pwa-examples comp to standalone
    https://github.com/graycoreio/daffodil/issues/3971

--- apps/daffio/src/app/content/why-pwa/components/why-pwa-examples/why-pwa-examples.component.ts
+++ apps/daffio/src/app/content/why-pwa/components/why-pwa-examples/why-pwa-examples.component.ts
@@ -4,12 +4,20 @@ import {
   HostBinding,
 } from '@angular/core';
 
+import { DaffCalloutModule } from '@daffodil/design/callout';
+import { DaffCardModule } from '@daffodil/design/card';
+import { DaffContainerModule } from '@daffodil/design/container';
+
 @Component({
   selector: 'daffio-why-pwa-examples',
   templateUrl: './why-pwa-examples.component.html',
   styleUrls: ['./why-pwa-examples.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: false,
+  imports: [
+    DaffContainerModule,
+    DaffCalloutModule,
+    DaffCardModule,
+  ],
 })
 
 export class DaffioWhyPwaExamplesComponent {
