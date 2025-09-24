830bd72e2949a6c81ce8ef2276f5ff66139bb8ac
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-09-24 20:25:43 +0330

    refactor(daffio): convert why-pwa-hero comp to standalone
    https://github.com/graycoreio/daffodil/issues/3971

--- apps/daffio/src/app/content/why-pwa/components/why-pwa-hero/why-pwa-hero.component.ts
+++ apps/daffio/src/app/content/why-pwa/components/why-pwa-hero/why-pwa-hero.component.ts
@@ -4,12 +4,20 @@ import {
   HostBinding,
 } from '@angular/core';
 
+import { DaffButtonModule } from '@daffodil/design/button';
+import { DaffContainerModule } from '@daffodil/design/container';
+import { DaffHeroModule } from '@daffodil/design/hero';
+
 @Component({
   selector: 'daffio-why-pwa-hero',
   templateUrl: './why-pwa-hero.component.html',
   styleUrls: ['./why-pwa-hero.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: false,
+  imports: [
+    DaffContainerModule,
+    DaffHeroModule,
+    DaffButtonModule,
+  ],
 })
 
 export class DaffioWhyPwaHeroComponent {
