bef289e8b7fb552abf5c7afdb53dd42c4450c391
Peter Ashwood <griest024@gmail.com>
2025-10-01 17:05:14 +0000

    fix

--- libs/product-configurable/driver/magento/src/product-configurable-driver.module.ts
+++ libs/product-configurable/driver/magento/src/product-configurable-driver.module.ts
@@ -6,13 +6,14 @@ import {
 } from '@angular/core';
 
 import {
-  provideDaffProductMagentoExtraProductFragments,
   provideDaffProductMagentoExtraProductPreviewTransforms,
   MagentoProductTypeEnum,
   DAFF_PRODUCT_MAGENTO_EXTRA_PRODUCT_TRANSFORMS,
+  DAFF_PRODUCT_MAGENTO_EXTRA_PRODUCT_FRAGMENTS,
 } from '@daffodil/product/driver/magento';
 
 import { magentoConfigurableProductFragment } from './fragments/configurable-product';
+import { DAFF_PRODUCT_CONFIGURABLE_MAGENTO_EXTRA_VARIANT_FRAGMENTS } from './injection-tokens/public_api';
 import { MagentoConfigurableProduct } from './models/configurable-product';
 import { MagentoConfigurableProductTransformer } from './transforms/configurable-product-transformers';
 
@@ -30,9 +31,15 @@ export class DaffConfigurableProductMagentoDriverModule {
       ngModule: DaffConfigurableProductMagentoDriverModule,
       providers: [
         MagentoConfigurableProductTransformer,
-        ...provideDaffProductMagentoExtraProductFragments(magentoConfigurableProductFragment),
+        {
+          provide: DAFF_PRODUCT_MAGENTO_EXTRA_PRODUCT_FRAGMENTS,
+          multi: true,
+          useFactory: () =>
+            magentoConfigurableProductFragment(inject(DAFF_PRODUCT_CONFIGURABLE_MAGENTO_EXTRA_VARIANT_FRAGMENTS)),
+        },
         {
           provide: DAFF_PRODUCT_MAGENTO_EXTRA_PRODUCT_TRANSFORMS,
+          multi: true,
           useFactory: () => {
             const transformer = inject(MagentoConfigurableProductTransformer);
             return (daffProduct, magentoProduct) =>
