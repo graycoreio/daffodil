0f4e76618d1173361398a4fa77e21cf13e9156ad
Leon Zhao <lzhao92@gmail.com>
2025-10-01 12:56:47 -0400

    feature(DaffMemoryStorageService): add to public api

--- libs/core/src/storage/public_api.ts
+++ libs/core/src/storage/public_api.ts
@@ -2,6 +2,7 @@ export * from './error/public_api';
 export * from './server-error/public_api';
 export { DaffLocalStorageService } from './localstorage/localstorage.service';
 export { DaffNoopStorageService } from './noopstorage/noopstorage.service';
+export { DaffMemoryStorageService } from './memory/memory.service';
 export {
   DaffPersistenceService,
   DaffPersistenceServiceToken,
