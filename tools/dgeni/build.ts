ddfcd34d3cf7211d638e4d8ebde7cb95abaa5682
Al Amin <oduntanalamin@gmail.com>
2025-10-11 11:08:23 +0100

    Migrate from highlight.js to Shiki and fix ESLint console errors

--- tools/dgeni/build.ts
+++ tools/dgeni/build.ts
@@ -19,15 +19,15 @@ rimraf('../../dist/docs-assets/*', { glob: true }).then(() => {
     // base docs
     Promise.all([
       new Dgeni([packageDocsPackage]).generate().catch((err) => {
-        console.log(err);
+        console.error(err);
         process.exit(1);
       }),
       new Dgeni([guideDocsPackage]).generate().catch((err) => {
-        console.log(err);
+        console.error(err);
         process.exit(1);
       }),
       new Dgeni([explanationDocsPackage]).generate().catch((err) => {
-        console.log(err);
+        console.error(err);
         process.exit(1);
       }),
     ]).then(() => {
@@ -35,30 +35,32 @@ rimraf('../../dist/docs-assets/*', { glob: true }).then(() => {
       // run them after base docs so that config between shared processors does not conflict
       // a design flaw of dgeni, it wasn't meant to be run in parallel
       new Dgeni([designApiPackage]).generate().then(() => {
-        Promise.all([new Dgeni([designDocsPackage]).generate().catch((err) => {
-          console.log(err);
-          process.exit(1);
-        }),
-        new Dgeni([designExamplePackage]).generate().catch((err) => {
-          console.log(err);
-          process.exit(1);
-        })]).then(() => {
+        Promise.all([
+          new Dgeni([designDocsPackage]).generate().catch((err) => {
+            console.error(err);
+            process.exit(1);
+          }),
+          new Dgeni([designExamplePackage]).generate().catch((err) => {
+            console.error(err);
+            process.exit(1);
+          }),
+        ]).then(() => {
           new Dgeni([daffodilRoutesPackage]).generate().catch((err) => {
-            console.log(err);
+            console.error(err);
             process.exit(1);
           });
         });
       }).catch((err) => {
-        console.log(err);
+        console.error(err);
         process.exit(1);
       });
     }).catch((err) => {
-      console.log(err);
+      console.error(err);
       process.exit(1);
     });
 
   }).catch((err) => {
-    console.log(err);
+    console.error(err);
     process.exit(1);
   });
 });
