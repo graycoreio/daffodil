5c990505bc8454aaf44275a3f8befa14207892a2
stevenanthonyrevo <stevenanthonyrevo@live.com>
2025-10-15 16:43:18 -0400

    4 spaces not 3 fix

--- libs/design/radio/src/specs/radio-accessibility.spec.ts
+++ libs/design/radio/src/specs/radio-accessibility.spec.ts
@@ -48,10 +48,10 @@ describe('@daffodil/design/radio | DaffRadioComponent | Accessibility', () => {
   });
 
   it('should have a role of radio', () => {
-   fixture.detectChanges();
-   const radioDebugElement = fixture.debugElement.query(By.css('daff-radio'));
-   const roleAttribute = radioDebugElement.nativeElement.getAttribute('role');
-   expect(roleAttribute).toBe('radio');
+    fixture.detectChanges();
+    const radioDebugElement = fixture.debugElement.query(By.css('daff-radio'));
+    const roleAttribute = radioDebugElement.nativeElement.getAttribute('role');
+    expect(roleAttribute).toBe('radio');
   });
 
   it('should take `label` as an input', () => {
