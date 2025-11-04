6c98ae5c4c599261fc4e589f285b1768af4369b0
Peter Ashwood <griest024@gmail.com>
2025-11-04 16:48:08 +0000

    reenable prerendering for select routes

--- apps/daffio/src/app/app.server.routes.ts
+++ apps/daffio/src/app/app.server.routes.ts
@@ -3,12 +3,17 @@ import {
   ServerRoute,
 } from '@angular/ssr';
 
+import { homeServerRoute } from './content/home/home.server.routes';
+import { notFoundServerRoutes } from './content/not-found/not-found.server.routes';
+import { supportServerRoutes } from './content/support/support.server.routes';
+import { whyPwaServerRoutes } from './content/why-pwa/why-pwa.server.routes';
+
 export const serverRoutes: Array<ServerRoute> = [
-  // homeServerRoute,
+  homeServerRoute,
   // ...docsServerRoutes(DAFF_DOCS_PATH),
-  // ...supportServerRoutes('support'),
-  // ...whyPwaServerRoutes('why-pwa'),
-  // ...notFoundServerRoutes('404'),
+  ...supportServerRoutes('support'),
+  ...whyPwaServerRoutes('why-pwa'),
+  ...notFoundServerRoutes('404'),
   {
     path: '**',
     renderMode: RenderMode.Server,
