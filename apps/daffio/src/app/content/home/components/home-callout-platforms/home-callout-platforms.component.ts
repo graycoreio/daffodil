8577635f3afb764e7005c8cb9808ef6b437d12e1
xelaint <xelaint@gmail.com>
2025-11-24 10:10:26 -0500

    remove example in daffio

--- apps/daffio/src/app/content/home/components/home-callout-platforms/home-callout-platforms.component.ts
+++ apps/daffio/src/app/content/home/components/home-callout-platforms/home-callout-platforms.component.ts
@@ -7,10 +7,6 @@ import {
 import { DAFF_CALLOUT_COMPONENTS } from '@daffodil/design/callout';
 import { DAFF_CONTAINER_COMPONENTS } from '@daffodil/design/container';
 import { DAFF_IMAGE_COMPONENTS } from '@daffodil/design/image';
-import {
-  DAFF_SF_CAROUSEL_COMPONENTS,
-  DaffSfCarouselOptions,
-} from '@daffodil/storefront/carousel';
 
 @Component({
   selector: 'daffio-home-callout-platforms',
@@ -21,18 +17,9 @@ import {
     DAFF_CALLOUT_COMPONENTS,
     DAFF_IMAGE_COMPONENTS,
     DAFF_CONTAINER_COMPONENTS,
-    DAFF_SF_CAROUSEL_COMPONENTS,
   ],
 })
 
 export class DaffioHomeCalloutPlatformsComponent {
   @HostBinding('class.daffio-home-callout-platforms') class = true;
-
-  options: DaffSfCarouselOptions = {
-    spaceBetween: 8,
-  };
-
-  carouselItemId(index: number, value: number): number {
-    return value;
-  }
 }
