6781768d41661bfdc08813a922e2a01e91fbdd22
Peter Ashwood <griest024@gmail.com>
2025-11-25 18:06:49 -0500

    feat(driver): add magento cache feature providers (#4222)
    * feat(driver): add `provideDaffMagentoCacheHeader`
    
    * feat(driver): add `provideDaffMagentoApolloCacheableOperations`

--- libs/driver/magento/src/graphql/cacheable-operations/apollo-cacheable-operations-link-generator.ts
+++ libs/driver/magento/src/graphql/cacheable-operations/apollo-cacheable-operations-link-generator.ts
@@ -1,17 +1,38 @@
 import {
+  inject,
   Inject,
   Injectable,
+  makeEnvironmentProviders,
 } from '@angular/core';
 import { ApolloLink } from '@apollo/client/core';
 
-import { DaffApolloLinkGenerator } from '@daffodil/core/graphql';
+import {
+  DaffApolloLinkGenerator,
+  provideDaffApolloRequestHandlerFactories,
+} from '@daffodil/core/graphql';
 
 import { DAFF_MAGENTO_CACHEABLE_OPERATIONS } from './cacheable-operations-token';
+
+/**
+ * A request handler provider that will convert cacheable apollo operations into a format that Magento will understand as cacheable.
+ */
+export const provideDaffMagentoApolloCacheableOperations = () => makeEnvironmentProviders([
+  provideDaffApolloRequestHandlerFactories(() => {
+    const cacheableOperations = inject(DAFF_MAGENTO_CACHEABLE_OPERATIONS);
+    return (operation, forward) => {
+      if (cacheableOperations.indexOf(operation.operationName) > -1) {
+        operation.setContext({ method: 'GET' });
+      }
+      return forward(operation);
+    };
+  }),
+]);
 
 /**
  * A service that will convert cacheable apollo operations into a format that Magento will understand as cacheable.
  *
  * @inheritdoc
+ * @deprecated Prefer using {@link provideDaffMagentoApolloCacheableOperations} instead.
  */
 @Injectable({
   providedIn: 'root',
