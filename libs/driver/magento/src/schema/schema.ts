12867439a9a4d0987a1ac9fe25deab5e19018620
Peter Ashwood <griest024@gmail.com>
2025-11-25 22:11:46 +0000

    fix?

--- libs/driver/magento/src/schema/schema.ts
+++ libs/driver/magento/src/schema/schema.ts
@@ -1,3 +1,8 @@
 import introspectionQueryResultData from './introspection.json';
 
 export const MAGENTO_POSSIBLE_TYPES = introspectionQueryResultData;
+
+/**
+ * @deprecated Prefer {@link MAGENTO_POSSIBLE_TYPES} instead.
+ */
+export { MAGENTO_POSSIBLE_TYPES as schema };
