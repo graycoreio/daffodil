f2d354c0daeb9207f2b33e8ead90d5c3e727d5ff
Ammar Ahmad <ammarahmadwrk@gmail.com>
2025-11-26 16:07:20 +0000

    refactor(tools): simplify SassDoc build process

--- tools/sassdoc/build.ts
+++ tools/sassdoc/build.ts
@@ -1,92 +1,27 @@
 // @ts-ignore
 import sassdoc = require('sassdoc');
-import config from './sassdoc.config';
-import { SassValueParser } from './value-parser';
-import * as fs from 'fs';
-import * as path from 'path';
+import { SassValueParser, SassDocItem } from './value-parser';
+import { SassDocConfig } from './sassdoc.config';
 
-interface SassDocConfig {
-  src: string[];
-  dest: string;
-  theme: string;
-  autofill: string[];
-  groups: Record<string, string>;
-  display: {
-    access: string[];
-    alias: boolean;
-  };
-  package: string;
-  verbose: boolean;
-  parse: boolean;
+export interface ParsedSassDocResult {
+  data: SassDocItem[];
+  config: SassDocConfig;
 }
 
-interface SassDocItem {
-  [key: string]: any;
-}
-
-const isTestMode = process.argv.includes('--test');
-
-async function buildSassDoc(): Promise<void> {
+export async function processSassDoc(config: SassDocConfig): Promise<ParsedSassDocResult> {
   try {
-    let finalConfig: SassDocConfig = { ...config };
+    const data: SassDocItem[] = await sassdoc.parse(config.src, { verbose: config.verbose });
     
-    if (isTestMode) {
-      finalConfig.src = ['../../libs/design/scss/theming/_color-palettes.scss'];
-      finalConfig.dest = '../../dist/docs/sass-docs-test';
-      console.log('Running SassDoc in test mode');
-    } else {
-      console.log('Building SassDoc for all SCSS files...');
-    }
-
-    const destDir: string = path.resolve(__dirname, finalConfig.dest);
-    if (!fs.existsSync(destDir)) {
-      fs.mkdirSync(destDir, { recursive: true });
-    }
-
-    if (isTestMode) {
-      const data: SassDocItem[] = await sassdoc.parse(finalConfig.src, { verbose: true });
-      console.log('Found', data.length, 'documented items');
-      
-      const jsonFile: string = path.resolve(__dirname, '../../dist/docs/sassdoc-test-output.json');
-      const jsonDir: string = path.dirname(jsonFile);
-      
-      if (!fs.existsSync(jsonDir)) {
-        fs.mkdirSync(jsonDir, { recursive: true });
-      }
-      
-      fs.writeFileSync(jsonFile, JSON.stringify(data, null, 2), 'utf8');
-      console.log('Raw JSON output saved to:', jsonFile);
-      
-      const parser = new SassValueParser();
-      parser.processSassDocOutput(jsonFile, jsonFile);
-      
-      await sassdoc(finalConfig.src, finalConfig);
-    } else {
-      const data: SassDocItem[] = await sassdoc.parse(finalConfig.src, { verbose: true });
-      console.log('Found', data.length, 'documented items');
-      
-      const jsonFile: string = path.resolve(__dirname, '../../dist/docs/sassdoc-output.json');
-      const jsonDir: string = path.dirname(jsonFile);
-      
-      if (!fs.existsSync(jsonDir)) {
-        fs.mkdirSync(jsonDir, { recursive: true });
-      }
-      
-      fs.writeFileSync(jsonFile, JSON.stringify(data, null, 2), 'utf8');
-      console.log('Raw JSON output saved to:', jsonFile);
-      
-      const parser = new SassValueParser();
-      parser.processSassDocOutput(jsonFile, jsonFile);
-      
-      await sassdoc(finalConfig.src, finalConfig);
-    }
+    const parser = new SassValueParser();
+    const enhancedData = parser.processSassDocData(data);
     
-    console.log('SassDoc generation completed');
+    return {
+      data: enhancedData,
+      config
+    };
     
   } catch (error) {
-    console.error('SassDoc generation failed:', error);
-    process.exit(1);
+    console.error('SassDoc processing failed:', error);
+    throw error;
   }
-}
-
-buildSassDoc(); 
+} 
