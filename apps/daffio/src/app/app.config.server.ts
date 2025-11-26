c705884405f6be14e38ba7a908e4077cd4a60353
Damien Retzinger <damienwebdev@gmail.com>
2025-11-26 14:59:34 -0500

    use server routes

--- apps/daffio/src/app/app.config.server.ts
+++ apps/daffio/src/app/app.config.server.ts
@@ -2,16 +2,20 @@ import {
   mergeApplicationConfig,
   ApplicationConfig,
 } from '@angular/core';
-import { provideServerRendering } from '@angular/ssr';
+import {
+  provideServerRendering,
+  withRoutes,
+} from '@angular/ssr';
 
 import { appConfig } from './app.config';
+import { serverRoutes } from './app.server.routes';
 import { DaffioAssetFetchServerService } from './core/assets/fetch/server.service';
 import { provideDaffioAssetFetchService } from './core/assets/fetch/service.interface';
 import { provideServerDocsPath } from './docs/services/docs-path-server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(),
+    provideServerRendering(withRoutes(serverRoutes)),
     provideServerDocsPath(),
     provideDaffioAssetFetchService(DaffioAssetFetchServerService),
   ],
