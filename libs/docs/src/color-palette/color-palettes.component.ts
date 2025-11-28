c71e7e3f99a03360aee1bf4ea5aa39717e885950
Peter Ashwood <griest024@gmail.com>
2025-11-28 16:06:05 +0000

    feat(docs): add color palette component

---
+++ libs/docs/src/color-palette/color-palettes.component.ts
@@ -0,0 +1,36 @@
+import { KeyValuePipe } from '@angular/common';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  input,
+  computed,
+} from '@angular/core';
+
+import {
+  DaffDocsSassItem,
+  DaffDocsSassType,
+} from '@daffodil/docs-utils';
+
+import { DaffDocsPaletteShadeSortPipe } from './shade-sort.pipe';
+
+@Component({
+  selector: 'daff-docs-color-palettes',
+  templateUrl: './color-palettes.component.html',
+  styleUrls: ['./color-palettes.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    KeyValuePipe,
+    DaffDocsPaletteShadeSortPipe,
+  ],
+})
+export class DaffDocsColorPalettesComponent {
+  readonly COLOR = DaffDocsSassType.COLOR;
+  readonly MAP = DaffDocsSassType.MAP;
+
+  readonly items = input.required<Array<DaffDocsSassItem>>();
+  readonly palettes = computed(() =>
+  // TODO: filter by group when bug is fixed
+    this.items().filter((item) => item.file.name.includes('color-palettes')),
+  );
+}
---
+++ libs/docs/src/color-palette/shade-sort.pipe.ts
@@ -0,0 +1,17 @@
+import { KeyValue } from '@angular/common';
+import {
+  Pipe,
+  PipeTransform,
+} from '@angular/core';
+
+import { DaffDocsSassParsedMap } from '@daffodil/docs-utils';
+
+@Pipe({
+  name: 'daffDocsPaletteShadeSort',
+  standalone: true,
+})
+export class DaffDocsPaletteShadeSortPipe implements PipeTransform {
+  transform(value: Array<KeyValue<keyof DaffDocsSassParsedMap, DaffDocsSassParsedMap[keyof DaffDocsSassParsedMap]>>) {
+    return value.sort((a, b) => Number(a.key) - Number(b.key));
+  }
+}
