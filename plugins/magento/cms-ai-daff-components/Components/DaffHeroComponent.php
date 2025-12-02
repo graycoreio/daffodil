b488193ba8d756faf6719e84073e95bac4bce87a
Damien Retzinger <damienwebdev@gmail.com>
2025-12-02 14:25:09 -0500

    feat(plugins/magento/cms-ai-daff-components): init new package

---
+++ plugins/magento/cms-ai-daff-components/Components/DaffHeroComponent.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Copyright © Graycore, LLC. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace Graycore\CmsAiDaffComponents\Service\Schema\Component;
+
+use Graycore\CmsAiBuilder\Api\ComponentInterface;
+
+class DaffHeroComponent implements ComponentInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getSchema(): array
+    {
+        return [
+            'type' => 'object',
+            'properties' => [
+                'type' => ['type' => 'string', 'const' => 'componentSchema'],
+                'name' => ['type' => 'string', 'const' => 'DaffHeroComponent'],
+                'inputs' => [
+                    'type' => 'object',
+                    'properties' => [
+                        'textAlignment' => [
+                            'type' => 'string',
+                            'enum' => ['left', 'center', 'right'],
+                            'description' => 'Text alignment for hero content'
+                        ],
+                        'compact' => [
+                            'type' => 'boolean',
+                            'description' => 'Reduces padding for tighter spacing'
+                        ],
+                        'color' => [
+                            'type' => 'string',
+                            'enum' => ['primary', 'secondary', 'tertiary', 'light', 'dark', 'theme', 'theme-contrast'],
+                            'description' => 'Background color palette'
+                        ]
+                    ],
+                    'required' => ['textAlignment', 'compact', 'color'],
+                    'additionalProperties' => false
+                ],
+                'children' => [
+                    'type' => 'array',
+                    'items' => ['$ref' => '#/$defs/DaffContentSchema']
+                ]
+            ],
+            'required' => ['type', 'name', 'inputs', 'children'],
+            'additionalProperties' => false
+        ];
+    }
+}
---
+++ plugins/magento/cms-ai-daff-components/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © Graycore, LLC. All rights reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Graycore_CmsAiDaffComponents',
+    __DIR__
+);
