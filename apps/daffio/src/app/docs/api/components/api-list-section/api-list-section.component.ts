39b82d422b132708bc20ec2667a09520f53c3053
Peter Ashwood <griest024@gmail.com>
2025-12-10 15:46:36 -0500

    feat(docs,daffio): use role labels in API tags (#4244)

--- apps/daffio/src/app/docs/api/components/api-list-section/api-list-section.component.ts
+++ apps/daffio/src/app/docs/api/components/api-list-section/api-list-section.component.ts
@@ -9,6 +9,7 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faExclamation } from '@fortawesome/free-solid-svg-icons';
 
 import { DaffArticleEncapsulatedDirective } from '@daffodil/design';
+import { DaffDocsApiRoleTagLabelPipe } from '@daffodil/docs';
 import { DaffApiNavDoc } from '@daffodil/docs-utils';
 
 import { DaffioDocsApiItemLabelComponent } from '../api-item-label/api-item-label.component';
@@ -25,10 +26,12 @@ import { DaffioDocsApiItemLabelComponent } from '../api-item-label/api-item-labe
     RouterLink,
     DaffioDocsApiItemLabelComponent,
     FaIconComponent,
+    DaffDocsApiRoleTagLabelPipe,
   ],
 })
 export class DaffioApiListSectionComponent {
   faExclamation = faExclamation;
+
   @HostBinding('class.daffio-api-list-section') class = true;
 
   /**
