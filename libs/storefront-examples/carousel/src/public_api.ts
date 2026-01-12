3b7bb573d5485be8474c2ba857da7a6d3daaad14
xelaint <xelaint@gmail.com>
2026-01-12 16:46:36 -0500

    docs(storefront): add carousel example

---
+++ libs/storefront-examples/carousel/src/public_api.ts
@@ -0,0 +1,5 @@
+import { BasicCarouselStorefrontExampleComponent } from './basic-carousel/basic-carousel.component';
+
+export const BASIC_CAROUSEL_EXAMPLES = [
+  BasicCarouselStorefrontExampleComponent,
+];
