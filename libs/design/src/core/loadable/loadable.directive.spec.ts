a9bd7caa82cc6f30f9bcc78e9407a9e730cec215
xelaint <xelaint@gmail.com>
2026-02-12 13:29:53 -0600

    feat(design): create DaffLoadableDirective

---
+++ libs/design/src/core/loadable/loadable.directive.spec.ts
@@ -0,0 +1,74 @@
+import {
+  Component,
+  DebugElement,
+} from '@angular/core';
+import {
+  waitForAsync,
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { DaffLoadableDirective } from '@daffodil/design';
+
+@Component({
+  template: `
+		<div daffLoadable [loading]="loading"></div>`,
+  imports: [
+    DaffLoadableDirective,
+  ],
+})
+
+class WrapperComponent {
+  loading: boolean;
+}
+
+describe('@daffodil/design | DaffLoadableDirective', () => {
+  let wrapper: WrapperComponent;
+  let de: DebugElement;
+  let fixture: ComponentFixture<WrapperComponent>;
+  let directive: DaffLoadableDirective;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        WrapperComponent,
+      ],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WrapperComponent);
+    wrapper = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('[daffLoadable]'));
+
+    directive = de.injector.get(DaffLoadableDirective);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(wrapper).toBeTruthy();
+    expect(directive).toBeTruthy();
+  });
+
+  it('should take loading as an input', () => {
+    expect(directive.loading).toEqual(wrapper.loading);
+  });
+
+  it('should add a class of .daff-loading to the host element if loading is set to true', () => {
+    wrapper.loading = true;
+    fixture.detectChanges();
+
+    expect(de.classes).toEqual(jasmine.objectContaining({
+      'daff-loading': true,
+    }));
+  });
+
+  it('should not add a class of .daff-loading to the host element if loading is set to false', () => {
+    wrapper.loading = false;
+    fixture.detectChanges();
+
+    expect(de.classes['daff-loading']).toBeUndefined();
+  });
+});
---
+++ libs/design/src/core/loadable/public_api.ts
@@ -0,0 +1,1 @@
+export { DaffLoadableDirective } from './loadable.directive';
