e9b5462044c9df61c152fa43aea95d60beecbcf4
xelaint <xelaint@gmail.com>
2026-02-19 21:32:34 +0000

    set up docs theme

--- libs/docs/src/public_api.ts
+++ libs/docs/src/public_api.ts
@@ -1,3 +1,3 @@
-export { DaffDocsColorPalettesComponent } from './color-palette/color-palettes.component';
+export { DaffDocsColorPalettesComponent } from './color-palettes/color-palettes.component';
 
 export * from './api/role/public_api';
