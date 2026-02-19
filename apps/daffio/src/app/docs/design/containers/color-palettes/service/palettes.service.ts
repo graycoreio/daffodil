b1872e03467f3a8eb10bbd22c890548e7b4fc160
Peter Ashwood <griest024@gmail.com>
2026-02-19 21:32:36 +0000

    load separate files

--- apps/daffio/src/app/docs/design/containers/color-palettes/service/palettes.service.ts
+++ apps/daffio/src/app/docs/design/containers/color-palettes/service/palettes.service.ts
@@ -18,7 +18,7 @@ import { DAFFIO_DOCS_PATH_TOKEN } from '../../../../services/docs-path.token';
 /**
  * The relative path to the sassdoc output file containing color palette definitions.
  */
-export const DOCS_LOCATION = 'sassdoc/output';
+export const DOCS_LOCATION = 'sassdoc/color-palettes';
 
 /**
  * A service that fetches color palette documentation from the sassdoc output.
@@ -39,6 +39,6 @@ export class DaffioColorPaletteService {
    * @returns Sass documentation items describing available color palettes.
    */
   get(): Observable<Array<DaffDocsSassItem>> {
-    return this.fetchAsset.fetch<Array<DaffDocsSassItem>>(`${this.docsPath}/${crossOsFilename(DOCS_LOCATION)}.json`, 'DOCS_LOCATION');
+    return this.fetchAsset.fetch<Array<DaffDocsSassItem>>(`${this.docsPath}/${crossOsFilename(DOCS_LOCATION)}.json`, DOCS_LOCATION);
   }
 }
