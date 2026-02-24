9147b2cb7bc48bf910d3599c0bbff883ec441a7a
xelaint <xelaint@gmail.com>
2026-02-24 17:32:58 -0500

    feat(design): deprecate DaffQuantityFieldComponent

--- libs/design-examples/quantity-field/src/public_api.ts
+++
@@ -1,5 +0,0 @@
-export { BasicQuantityFieldExampleComponent } from './basic-quantity-field/basic-quantity-field.component';
-export { CustomRangeQuantityFieldExampleComponent } from './custom-range-quantity-field/custom-range-quantity-field.component';
-export { DisabledQuantityFieldExampleComponent } from './disabled-quantity-field/disabled-quantity-field.component';
-export { SelectMaxQuantityFieldExampleComponent } from './select-max-quantity-field/select-max-quantity-field.component';
-export { provideDaffDesignQuantityFieldExamplesContent } from './provider';
