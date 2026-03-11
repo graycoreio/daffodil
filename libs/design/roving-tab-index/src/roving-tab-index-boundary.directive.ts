fadb4d29957a14ef246e1e68485d37de6a8cadb9
Peter Ashwood <griest024@gmail.com>
2026-03-11 14:14:26 +0000

    focus trap inside group

--- libs/design/roving-tab-index/src/roving-tab-index-boundary.directive.ts
+++ libs/design/roving-tab-index/src/roving-tab-index-boundary.directive.ts
@@ -1,5 +1,7 @@
+import { CdkTrapFocus } from '@angular/cdk/a11y';
 import {
   Directive,
+  effect,
   input,
 } from '@angular/core';
 
@@ -12,13 +14,21 @@ import { DaffRovingTabIndexService } from './roving-tab-index-group.service';
     '[attr.data-rti-boundary]': 'rtiBoundary()',
     '(keydown.space)': 'enterGroup($event)',
   },
+  hostDirectives: [
+    CdkTrapFocus,
+  ],
 })
 export class DaffRovingTabIndexBoundaryDirective {
   readonly rtiBoundary = input<string | null>(null);
 
   constructor(
     private groupService: DaffRovingTabIndexService,
-  ) {}
+    private focusTrap: CdkTrapFocus,
+  ) {
+    effect(() => {
+      this.focusTrap.enabled = this.rtiBoundary() === this.groupService.group();
+    });
+  }
 
   enterGroup(evt: Event) {
     evt.preventDefault();
