bc5486889d4c7d13a0c2f75cc6b3a61a2417bab7
Peter Ashwood <griest024@gmail.com>
2026-03-11 14:14:29 +0000

    remove design land

--- apps/design-land/src/app/roving-tab-index/roving-tab-index.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { DesignLandRtiComponent } from './roving-tab-index.component';
-
-const routes: Routes = [
-  { path: '', component: DesignLandRtiComponent },
-];
-
-export default routes;
