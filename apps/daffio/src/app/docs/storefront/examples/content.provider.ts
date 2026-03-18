fa8cf7f90861bb96b720cfc323aace23ad1ae84d
xelaint <xelaint@gmail.com>
2026-03-18 11:21:40 -0400

    feat(daffio, storefront-examples): register quantity field examples in docs

--- apps/daffio/src/app/docs/storefront/examples/content.provider.ts
+++ apps/daffio/src/app/docs/storefront/examples/content.provider.ts
@@ -1,9 +1,11 @@
 import { makeEnvironmentProviders } from '@angular/core';
 
 import { provideDaffStorefrontCarouselExamplesContent } from '@daffodil/storefront-examples/carousel';
+import { provideDaffStorefrontQuantityFieldExamplesContent } from '@daffodil/storefront-examples/quantity-field';
 import { provideDaffStorefrontThemeToggleExamplesContent } from '@daffodil/storefront-examples/theme-toggle';
 
 export const provideDaffioStorefrontExamplesContent = () => makeEnvironmentProviders([
   provideDaffStorefrontThemeToggleExamplesContent(),
   provideDaffStorefrontCarouselExamplesContent(),
+  provideDaffStorefrontQuantityFieldExamplesContent(),
 ]);
