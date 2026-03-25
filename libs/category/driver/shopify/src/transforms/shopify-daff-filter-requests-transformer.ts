56d9a3531b438b8849c6bd33535e58f4091b7654
Joanna Lau <118241363+joannalauu@users.noreply.github.com>
2026-03-25 13:44:51 -0400

    Fix `DaffFilterRequest` to `ShopifyProductFilter` mapping

--- libs/category/driver/shopify/src/transforms/shopify-daff-filter-requests-transformer.ts
+++ libs/category/driver/shopify/src/transforms/shopify-daff-filter-requests-transformer.ts
@@ -11,34 +11,22 @@ import { ShopifyProductFilter } from '@daffodil/driver/shopify';
  * @returns A corresponding array of shopify-readable filters
  */
 export const shopifyProductFilterRequestsTransformer = (daffFilterRequests: DaffFilterRequest[]): ShopifyProductFilter[] => {
+  if (!daffFilterRequests) {
+    return [];
+  }
   const result: ShopifyProductFilter[] = [];
   for (const filter of daffFilterRequests) {
-    const shopifyFilter: ShopifyProductFilter = {};
     if (filter.type === DaffFilterType.Equal) {
-      if (filter.value[0] === 'available') {
-        shopifyFilter.available = filter.value[1].toLowerCase() === 'true';
-      } else if (filter.value[0] === 'category') {
-        shopifyFilter.category = { id: filter.value[1] };
-      } else if (filter.value[0] === 'productMetafield' || filter.value[0] === 'taxonomyMetafield' || filter.value[0] === 'variantMetafield') {
-        shopifyFilter[filter.value[0]] = {
-          key: filter.name,
-          namespace: filter.value[1],
-          value: filter.value[2],
-        };
-      } else if (filter.value[0] === 'tag' || filter.value[0] === 'productVendor' || filter.value[0] === 'productType') {
-        shopifyFilter[filter.value[0]] = filter.value[1];
-      } else if (filter.value[0] === 'variantOption') {
-        shopifyFilter[filter.value[0]] = {
-          name: filter.name,
-          value: filter.value[1],
-        };
+      // Each `jsonInputStr` is a JSON input string from Shopify's FilterValue.input
+      // e.g., '{"tag":"summer"}' or '{"variantOption":{"name":"Color","value":"Red"}}'
+      for (const jsonInputStr of filter.value) {
+        result.push(JSON.parse(jsonInputStr));
       }
     } else if (filter.type === DaffFilterType.RangeNumeric) {
-      if (filter.name.toLowerCase() === 'price') {
-        shopifyFilter.price = { min: filter.value.min, max: filter.value.max };
-      }
+      // Hardcoded ProductFilter type to 'price' because PRICE_RANGE is currently the only
+      // range filter type in Shopify's ProductFilter schema
+      result.push({ price: { min: filter.value.min, max: filter.value.max }});
     }
-    result.push(shopifyFilter);
   }
   return result;
 };
