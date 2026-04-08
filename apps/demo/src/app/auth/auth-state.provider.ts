25c2b0744cb38626e3c3a5382817b1ba5b4ca843
Joanna Lau <118241363+joannalauu@users.noreply.github.com>
2026-04-08 16:47:06 -0400

    Update apps/demo/src/app/auth/auth-state.provider.ts
    Co-authored-by: Peter Ashwood <griest024@gmail.com>

--- apps/demo/src/app/auth/auth-state.provider.ts
+++ apps/demo/src/app/auth/auth-state.provider.ts
@@ -3,6 +3,5 @@ import { EffectsModule } from '@ngrx/effects';
 
 import { DemoAuthEffects } from './effects/auth.effects';
 
-export const provideDemoAuthState = () => [
-  importProvidersFrom(EffectsModule.forFeature([DemoAuthEffects])),
-];
+export const provideDemoAuthState = () =>
+  importProvidersFrom(EffectsModule.forFeature([DemoAuthEffects]));
