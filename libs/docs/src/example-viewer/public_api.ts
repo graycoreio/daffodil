fb2eaf99c22779ce823bfdd04940fbdae897be04
Joanna Lau <118241363+joannalauu@users.noreply.github.com>
2026-04-08 11:47:39 -0400

    `example-viewer` has its own public api

---
+++ libs/docs/src/example-viewer/public_api.ts
@@ -0,0 +1,6 @@
+export { DaffDocsExampleViewerComponent } from './example-viewer.component';
+export {
+  DAFF_DOCS_EXAMPLE_SERVICE,
+  provideDaffDocsExampleService,
+} from './service/example-docs.service';
+export { DaffDocsExampleServiceInterface } from './service/example-docs.service.interface';
--- libs/docs/src/public_api.ts
+++ libs/docs/src/public_api.ts
@@ -1,16 +1,9 @@
 export { DaffDocsColorPaletteGeneratorComponent } from './color-palette-generator/color-palette-generator/color-palette-generator.component';
 export { provideDaffDocsColorPaletteGeneratorExampleContent } from './color-palette-generator/provider';
 export { DaffDocsColorPalettesComponent } from './color-palettes/color-palettes.component';
-export * from './color-strip/public_api';
+export { DaffDocsDocRendererComponent } from './doc-renderer/doc-renderer.component';
 
+export * from './color-strip/public_api';
 export * from './api/role/public_api';
 export * from './example/public_api';
-
-export { DaffDocsExampleViewerComponent } from './example-viewer/example-viewer.component';
-export {
-  DAFF_DOCS_EXAMPLE_SERVICE,
-  provideDaffDocsExampleService,
-} from './example-viewer/service/example-docs.service';
-export { DaffDocsExampleServiceInterface } from './example-viewer/service/example-docs.service.interface';
-
-export { DaffDocsDocRendererComponent } from './doc-renderer/doc-renderer.component';
+export * from './example-viewer/public_api';
