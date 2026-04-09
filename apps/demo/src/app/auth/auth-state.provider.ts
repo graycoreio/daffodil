40bf56aabcbb36b48b213a74144b3e882c439ab6
Joanna Lau <118241363+joannalauu@users.noreply.github.com>
2026-04-09 08:59:30 -0400

    refactor(demo): Covert auth to standalone (#4305)
    Co-authored-by: Peter Ashwood <griest024@gmail.com>

---
+++ apps/demo/src/app/auth/auth-state.provider.ts
@@ -0,0 +1,7 @@
+import { importProvidersFrom } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+
+import { DemoAuthEffects } from './effects/auth.effects';
+
+export const provideDemoAuthState = () =>
+  importProvidersFrom(EffectsModule.forFeature([DemoAuthEffects]));
