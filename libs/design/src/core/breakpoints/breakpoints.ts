1d0df7a1b5dbade6efeb7c336a967e9b45d93d12
xelaint <xelaint@gmail.com>
2026-04-09 15:33:32 -0400

    docs(design): add breakpoints docs

--- libs/design/src/core/breakpoints/breakpoints.ts
+++ libs/design/src/core/breakpoints/breakpoints.ts
@@ -1,8 +1,17 @@
+/**
+ * Sensible breakpoints for layouts and interfaces used across `@daffodil/design` components.
+ */
 export enum DaffBreakpoints {
+  /** `min-width: 1920px` */
   DESKTOP = '(min-width: 1920px)',
+  /** `min-width: 1440px` */
   LAPTOP = '(min-width: 1440px)',
+  /** `min-width: 1200px` */
   SMALL_LAPTOP = '(min-width: 1200px)',
+  /** `min-width: 1024px` */
   BIG_TABLET = '(min-width: 1024px)',
+  /** `min-width: 768px` */
   TABLET = '(min-width: 768px)',
+  /** `min-width: 480px` */
   MOBILE = '(min-width: 480px)',
 }
