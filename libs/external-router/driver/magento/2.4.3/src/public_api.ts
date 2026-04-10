cdb7da69faa69b50614ec65a3901ebce9c208c07
Joanna Lau <118241363+joannalauu@users.noreply.github.com>
2026-04-10 17:57:30 -0400

    Magento versions export base magento driver api

--- libs/external-router/driver/magento/2.4.3/src/public_api.ts
+++ libs/external-router/driver/magento/2.4.3/src/public_api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of @daffodil/external-router/driver/magento/2.4.3
  */
 
+export * from '@daffodil/external-router/driver/magento/base';
 export { DaffExternalRouterMagentoDriver } from './magento.service';
 export { DaffExternalRouterDriverMagentoModule } from './magento.module';
 export { provideDaffExternalRouterMagentoDriver } from './provider';
