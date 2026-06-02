92f3397709a4393a89b79d174ff910da6734d3c9
xelaint <xelaint@gmail.com>
2026-06-02 09:07:15 +0800

    docs(design): clean up toast documentation

--- libs/design-examples/toast/src/provider.ts
+++ libs/design-examples/toast/src/provider.ts
@@ -1,23 +1,27 @@
 import { makeEnvironmentProviders } from '@angular/core';
 
+import { provideDaffToast } from '@daffodil/design/toast';
 import { provideDaffDocsExampleContent } from '@daffodil/docs';
 
-export const provideDaffDesignToastExamplesContent = () => makeEnvironmentProviders(provideDaffDocsExampleContent(
-  {
-    id: 'custom-duration-toast',
-    component: () => import('./custom-duration-toast/custom-duration-toast.component').then(c => c.CustomDurationToastExampleComponent),
-  },
-  {
-    id: 'default-toast',
-    component: () => import('./default-toast/default-toast.component').then(c => c.DefaultToastExampleComponent),
-  },
-  {
-    id: 'dismissible-toast',
-    component: () => import('./dismissible-toast/dismissible-toast.component').then(c => c.DismissibleToastExampleComponent),
-  },
-  {
-    id: 'toast-status',
-    component: () => import('./toast-status/toast-status.component').then(c => c.ToastStatusExampleComponent),
-  },
-));
+export const provideDaffDesignToastExamplesContent = () => makeEnvironmentProviders([
+  ...provideDaffToast(),
+  ...provideDaffDocsExampleContent(
+    {
+      id: 'custom-duration-toast',
+      component: () => import('./custom-duration-toast/custom-duration-toast.component').then(c => c.CustomDurationToastExampleComponent),
+    },
+    {
+      id: 'default-toast',
+      component: () => import('./default-toast/default-toast.component').then(c => c.DefaultToastExampleComponent),
+    },
+    {
+      id: 'dismissible-toast',
+      component: () => import('./dismissible-toast/dismissible-toast.component').then(c => c.DismissibleToastExampleComponent),
+    },
+    {
+      id: 'toast-status',
+      component: () => import('./toast-status/toast-status.component').then(c => c.ToastStatusExampleComponent),
+    },
+  ),
+]);
 
