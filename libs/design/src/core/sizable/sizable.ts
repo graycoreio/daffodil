651eea8a4c38207146c0e564cd4a8862ce0003a7
GrayBot <automation@graycore.io>
2026-06-04 02:09:59 +0000

    docs: annotate deprecation messages

--- libs/design/src/core/sizable/sizable.ts
+++ libs/design/src/core/sizable/sizable.ts
@@ -18,7 +18,7 @@ export type DaffSizeXLargeType = 'xl';
 export type DaffSizeAllType = DaffSizeXSmallType | DaffSizeSmallType | DaffSizeMediumType | DaffSizeLargeType | DaffSizeXLargeType;
 
 /**
- * @deprecated
+ * @deprecated Deprecated in version 0.93.0. Will be removed in version 0.96.0.
  *
  * This enum will be removed from the public api in v1.0.0.
  */
