44252bee5290032649739cc648b6f524b1b0d6ff
xelaint <xelaint@gmail.com>
2026-06-16 12:19:48 -0400

    docs(design): clean up spinner documentation

--- libs/design-examples/spinner/src/basic-spinner/basic-spinner.component.ts
+++ libs/design-examples/spinner/src/basic-spinner/basic-spinner.component.ts
@@ -7,12 +7,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { DAFF_SPINNER_COMPONENTS } from '@daffodil/design/spinner';
 
 @Component({
-  selector: 'spinner-with-label-example',
-  templateUrl: './spinner-with-label.component.html',
+  selector: 'basic-spinner-example',
+  templateUrl: './basic-spinner.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     DAFF_SPINNER_COMPONENTS,
     ReactiveFormsModule,
   ],
 })
-export class SpinnerWithLabelExampleComponent {}
+export class BasicSpinnerExampleComponent {}
--- libs/design-examples/spinner/src/spinner-sizes/spinner-sizes.component.ts
+++ libs/design-examples/spinner/src/spinner-sizes/spinner-sizes.component.ts
@@ -2,29 +2,16 @@ import {
   ChangeDetectionStrategy,
   Component,
 } from '@angular/core';
-import {
-  FormControl,
-  ReactiveFormsModule,
-} from '@angular/forms';
 
 import { DAFF_SPINNER_COMPONENTS } from '@daffodil/design/spinner';
 
 @Component({
   selector: 'spinner-sizes-example',
   templateUrl: './spinner-sizes.component.html',
-  styles: [`
-    :host {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      gap: 1rem;
-    }`],
+  styleUrl: './spinner-sizes.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     DAFF_SPINNER_COMPONENTS,
-    ReactiveFormsModule,
   ],
 })
-export class SpinnerSizesExampleComponent {
-  sizeControl: FormControl = new FormControl('');
-}
+export class SpinnerSizesExampleComponent {}
