f991ba854de03d5bfcc9add8b6fe699441591620
Peter Ashwood <griest024@gmail.com>
2026-06-24 17:41:48 +0000

    fix(daffio): `DaffioActiveHeaderService` zombies event listener

--- apps/daffio/src/app/core/dynamic-fragment/service.ts
+++ apps/daffio/src/app/core/dynamic-fragment/service.ts
@@ -1,8 +1,8 @@
-
 import {
   Inject,
   Injectable,
   DOCUMENT,
+  OnDestroy,
 } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -13,39 +13,46 @@ const HEADER_WITH_ID_SELECTOR = 'h1[id],h2[id],h3[id],h4[id],h5[id],[daffioDocsT
 const NAV_HEADER_OFFSET = 64;
 
 @Injectable()
-export class DaffioActiveHeaderService {
+export class DaffioActiveHeaderService implements OnDestroy {
   /**
    * True when the service is processing a scroll event.
    */
   private _ticking = false;
   private _fragment = new BehaviorSubject<string | null>(null);
+  private _scrollListener = () => {
+    if (!this._ticking) {
+      this.document.defaultView.requestAnimationFrame(() => {
+        const headers = [...this.document.querySelectorAll(HEADER_WITH_ID_SELECTOR)];
+        const onScreenHeaderIndex = headers.findIndex(el => {
+          const rect = el.getBoundingClientRect();
+          return rect.bottom >= 0;
+        });
+        let fragment: string | null = null;
+        if (onScreenHeaderIndex !== -1) {
+          // if the topmost visible header is within the "top of the screen" (or just the first header in the DOM)
+          // use that header as the active fragment
+          // if not, use the header just above the topmost visible one
+          fragment = onScreenHeaderIndex === 0 || headers[onScreenHeaderIndex].getBoundingClientRect().top <= (NAV_HEADER_OFFSET * 1.5)
+            ? headers[onScreenHeaderIndex]?.getAttribute('id')
+            : headers[onScreenHeaderIndex - 1]?.getAttribute('id');
+        }
+        this._fragment.next(fragment);
+        this._ticking = false;
+      });
+
+      this._ticking = true;
+    }
+  };
 
   readonly fragment$ = this._fragment.asObservable();
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
   ) {
-    this.document.addEventListener('scroll', () => {
-      if (!this._ticking) {
-        this.document.defaultView.requestAnimationFrame(() => {
-          const headers = [...document.querySelectorAll(HEADER_WITH_ID_SELECTOR)];
-          const onScreenHeaderIndex = headers.findIndex(el => {
-            const rect = el.getBoundingClientRect();
-            return rect.bottom >= 0;
-          });
-          // if the topmost visible header is within the "top of the screen" (or just the first header in the DOM)
-          // use that header as the active fragment
-          // if not, use the header just above the topmost visible one
-          const fragment = onScreenHeaderIndex === 0 || headers[onScreenHeaderIndex].getBoundingClientRect().top <= (NAV_HEADER_OFFSET * 1.5)
-            ? headers[onScreenHeaderIndex]?.getAttribute('id')
-            : headers[onScreenHeaderIndex - 1]?.getAttribute('id');
+    this.document.addEventListener('scroll', this._scrollListener);
+  }
 
-          this._fragment.next(fragment);
-          this._ticking = false;
-        });
-
-        this._ticking = true;
-      }
-    });
+  ngOnDestroy(): void {
+    this.document.removeEventListener('scroll', this._scrollListener);
   }
 }
