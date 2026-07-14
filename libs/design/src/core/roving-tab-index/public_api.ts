ae86170a2285b702a9ab1bac531401e6de822da8
Damien Retzinger <damienwebdev@gmail.com>
2026-07-14 15:55:31 -0400

    fix(design): break dependency cycle in DaffRovingTabIndexDirective

--- libs/design/src/core/roving-tab-index/public_api.ts
+++ libs/design/src/core/roving-tab-index/public_api.ts
@@ -1,2 +1,6 @@
 export { DaffRovingTabIndexDirective } from './roving-tab-index.directive';
 export { DaffRovingTabIndexBoundaryDirective } from './roving-tab-index-boundary.directive';
+export {
+  DAFF_ROVING_TAB_INDEX_BOUNDARY,
+  DaffRovingTabIndexBoundary,
+} from './roving-tab-index-boundary.token';
