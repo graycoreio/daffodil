b200a06b11d3ff468ef950d38c216dd615453172
Peter Ashwood <griest024@gmail.com>
2026-08-11 16:35:54 -0400

    feat(eslint-config): remove daff-packages config (#4654)
    this introduced annoying peer deps and is totally unnecessary given that this is just used internally

--- tools/eslint/config/index.ts
+++ tools/eslint/config/index.ts
@@ -2,4 +2,3 @@ export { baseEslintConfig } from './src/base.config.js';
 export { angularEslintConfig } from './src/angular.config.js';
 export { jestEslintConfig } from './src/jest.config.js';
 export { typescriptEslintConfig } from './src/typescript.config.js';
-export { packageJsonEslintConfig } from './src/package-json.config.js';
--- tools/eslint/daff-packages/index.ts
+++ tools/eslint/daff-packages/index.ts
@@ -1,5 +1,21 @@
+import { defineConfig } from 'eslint/config';
+import * as jsoncParser from 'jsonc-eslint-parser';
+
 import magentoDriverVersions from './magento-driver-versions';
 
 export const rules = {
   'magento-driver-versions': magentoDriverVersions,
 };
+
+export const config = defineConfig([
+  {
+    files: ['libs/*/package.json'],
+    languageOptions: {
+      parser: jsoncParser,
+    },
+    plugins: {
+      'daff-packages': { rules },
+    },
+    rules: { 'daff-packages/magento-driver-versions': 'error' },
+  },
+]);
